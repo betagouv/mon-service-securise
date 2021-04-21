4796c510142366042c89e52fab2b443b82128ea2
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-04-21 17:11:07 +0200

    Sers des pages statiques

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Mon Service Sécurisé est démarré et écoute le port ${port} !…`);
