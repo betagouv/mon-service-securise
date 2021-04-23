9946cbbb79a7637fdae12c42540f3a90e7c6161b
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-04-23 17:49:53 +0200

    Persiste données utilisateur sur le serveur

--- server.js
+++ server.js
@@ -2,7 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+let utilisateurs = {};
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/compte", (requete, reponse) => {
+  const utilisateurCourant = requete.headers["x-auth-login"];
+  const nomProjet = utilisateurs[utilisateurCourant];
+  const payload = { utilisateurCourant, nomProjet };
+  reponse.json(payload);
+});
+
+app.post("/compte", (requete, reponse) => {
+  const pseudo = requete.body.pseudo;
+  const payload = { pseudo };
+  reponse.json(payload);
+});
+
+app.post("/service", (requete, reponse) => {
+  const utilisateur = requete.headers["x-auth-login"];
+  const nomService = requete.body.nomService;
+  utilisateurs[utilisateur] = nomService;
+  reponse.json({ nomService });
+});
 
 app.listen(port, () => {
   console.log(`Mon Service Sécurisé est démarré et écoute le port ${port} !…`);
