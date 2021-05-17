f0413e6b90b6a41fe2d8dd555d826e59ddd96223
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-05-17 14:29:26 +0200

    Permets d'exécuter une commande ad hoc

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,4 @@ DOCKER_IMAGE=mon_service_securise
 PORT=${PORT:-1917}
 
 docker build -t ${DOCKER_IMAGE} .
-docker run --rm -ti -p ${PORT}:3000 -v $PWD:/usr/src/app ${DOCKER_IMAGE}
+docker run --rm -ti -p ${PORT}:3000 -v $PWD:/usr/src/app ${DOCKER_IMAGE} $*
