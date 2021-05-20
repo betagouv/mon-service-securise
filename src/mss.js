fde7aab18515ec9de08080d50f8329358d8bf5cb
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-05-20 21:55:23 +0200

    Utilise templates Pug pour supprimer duplication sur pages HTML

--- src/mss.js
+++ src/mss.js
@@ -4,6 +4,18 @@ const creeServeur = () => {
   let serveur;
 
   const app = express();
+
+  app.set('view engine', 'pug');
+  app.set('views', './src/vues');
+
+  app.get('/', (requete, reponse) => {
+    reponse.render('index');
+  });
+
+  app.get('/homologations', (requete, reponse) => {
+    reponse.render('homologations');
+  });
+
   app.use(express.static('public'));
 
   const ecoute = (port, succes) => {
