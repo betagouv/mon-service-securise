9fc49acd69677f0cfdcda73edf03d1023b597d50
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-05-28 17:17:18 +0200

    Authentifie les utilisateurs

--- src/depotDonnees.js
+++ src/depotDonnees.js
@@ -1,13 +1,31 @@
-const Homologation = require('./modeles/homologation');
+const bcrypt = require('bcrypt');
 
-const creeDepot = (donnees) => {
+const Homologation = require('./modeles/homologation');
+const Utilisateur = require('./modeles/utilisateur');
+
+const creeDepot = (donnees, adaptateurJWT) => {
   const homologations = (idUtilisateur) => donnees.homologations
     .filter((h) => h.idUtilisateur === idUtilisateur)
     .map((h) => new Homologation(h));
 
-  return { homologations };
+  const utilisateurAuthentifie = (login, motDePasse) => {
+    const donneesUtilisateur = donnees.utilisateurs.find((u) => u.email === login);
+    const motDePasseStocke = donneesUtilisateur && donneesUtilisateur.motDePasse;
+    const echecAuthentification = undefined;
+
+    if (!motDePasseStocke) return new Promise((resolve) => resolve(echecAuthentification));
+
+    return bcrypt.compare(motDePasse, motDePasseStocke)
+      .then((authentificationReussie) => (authentificationReussie
+        ? new Utilisateur(donneesUtilisateur, adaptateurJWT)
+        : echecAuthentification
+      ))
+      .catch((error) => error);
+  };
+
+  return { homologations, utilisateurAuthentifie };
 };
 
-const creeDepotVide = () => creeDepot({ homologations: [] });
+const creeDepotVide = () => creeDepot({ utilisateurs: [], homologations: [] });
 
 module.exports = { creeDepot, creeDepotVide };
--- test/depotDonnees.spec.js
+++ test/depotDonnees.spec.js
@@ -1,12 +1,25 @@
 const expect = require('expect.js');
+const bcrypt = require('bcrypt');
 
 const DepotDonnees = require('../src/depotDonnees');
+const Homologation = require('../src/modeles/homologation');
+const Utilisateur = require('../src/modeles/utilisateur');
 
 describe('Le dépôt de données', () => {
   describe('quand il est vide', () => {
     it('ne retourne aucune homologation', () => {
       const depot = DepotDonnees.creeDepotVide();
       expect(depot.homologations('456')).to.eql([]);
+    });
+
+    it("n'authentifie pas l'utilisateur", (done) => {
+      const depot = DepotDonnees.creeDepotVide();
+      depot.utilisateurAuthentifie('jean.dupont@mail.fr', 'mdp_12345')
+        .then((utilisateur) => {
+          expect(utilisateur).to.be(undefined);
+          done();
+        })
+        .catch((error) => done(error));
     });
   });
 
@@ -20,6 +33,30 @@ describe('Le dépôt de données', () => {
 
     const homologations = depot.homologations('456');
     expect(homologations.length).to.equal(1);
+    expect(homologations[0]).to.be.a(Homologation);
     expect(homologations[0].id).to.equal('123');
+  });
+
+  it("retourne l'utilisateur authentifié", (done) => {
+    const adaptateurJWT = {};
+
+    bcrypt.hash('mdp_12345', 10)
+      .then(async (hash) => {
+        const depot = DepotDonnees.creeDepot({
+          utilisateurs: [{
+            id: '123', prenom: 'Jean', nom: 'Dupont', email: 'jean.dupont@mail.fr', motDePasse: hash,
+          }],
+        }, adaptateurJWT);
+
+        return depot.utilisateurAuthentifie('jean.dupont@mail.fr', 'mdp_12345');
+      })
+      .then((utilisateur) => {
+        expect(utilisateur).to.be.an(Utilisateur);
+        expect(utilisateur.id).to.equal('123');
+        expect(utilisateur.adaptateurJWT).to.equal(adaptateurJWT);
+
+        done();
+      })
+      .catch((error) => done(error));
   });
 });
