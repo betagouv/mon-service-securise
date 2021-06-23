37c217172076dddd30eada837734f75ad2fdff55
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-06-23 12:51:07 +0200

    PREMANIEMENT : convertis params formulaire en objet avant requête POST

--- public/homologation/creation.js
+++ public/homologation/creation.js
@@ -1,8 +1,32 @@
+const avecPremierElementCommeValeurs = (params, nomsParams) => {
+  const resultat = params;
+  Object.keys(params).forEach((clef) => {
+    if (nomsParams.includes(clef)) [resultat[clef]] = params[clef];
+  });
+
+  return resultat;
+};
+
+const parametres = (selecteurFormulaire) => {
+  const params = $(selecteurFormulaire)
+    .serializeArray()
+    .reduce((acc, nomValeur) => {
+      const nomParam = nomValeur.name;
+      const valeurParam = nomValeur.value;
+      acc[nomParam] ||= [];
+      acc[nomParam].push(valeurParam);
+
+      return acc;
+    }, {});
+  return avecPremierElementCommeValeurs(params, ['nomService']);
+};
+
 $(() => {
   const $bouton = $('.bouton');
   $bouton.click(() => {
-    const nomService = $('#nom-service').val();
-    axios.post('/api/homologation', { nomService })
+    const params = parametres('form#homologation');
+
+    axios.post('/api/homologation', params)
       .then((reponse) => (window.location = `/homologation/${reponse.data.idHomologation}`));
   });
 });
