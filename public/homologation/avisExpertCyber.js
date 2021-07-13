6c6c8d73338b53efb551cb86287d3744dbbb9668
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-07-13 16:47:40 +0200

    Conditionne affichage renouvellement

--- public/homologation/avisExpertCyber.js
+++ public/homologation/avisExpertCyber.js
@@ -1,6 +1,25 @@
 import parametres from '../modules/parametres.js';
 
+const conditionneAffichageRenouvellement = (selecteurAvis, selecteurRenouvellement, condition) => {
+  const $avis = $(selecteurAvis);
+  const $fieldsetRenouvellement = $(selecteurRenouvellement).parent();
+  $fieldsetRenouvellement.toggle(condition());
+
+  $avis.change(() => {
+    const fieldsetRenouvellementDoitEtreAffiche = condition();
+
+    if (!fieldsetRenouvellementDoitEtreAffiche) $(selecteurRenouvellement).prop('checked', false);
+    $fieldsetRenouvellement.toggle(fieldsetRenouvellementDoitEtreAffiche);
+  });
+};
+
 $(() => {
+  conditionneAffichageRenouvellement(
+    'input[name=avis]',
+    'input[name=dateRenouvellement]',
+    () => $('input[id=avis-favorable]').prop('checked')
+  );
+
   const $bouton = $('.bouton');
   const identifiantHomologation = $bouton.attr('identifiant');
 
