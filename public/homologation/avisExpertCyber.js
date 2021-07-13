dee1ceae1d4fbf10719619de5f52ecadaa673223
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-07-13 14:57:59 +0200

    Corrige formulaire saisie avis expert cyber
    - Suppression zone saisie nom / fonction expert cyber
    - Ajout zone saisie commentaire

--- public/homologation/avisExpertCyber.js
+++ public/homologation/avisExpertCyber.js
@@ -1,24 +1,12 @@
 import parametres from '../modules/parametres.js';
-import brancheInputsIdentite from '../modules/brancheInputsIdentite.js';
 
 $(() => {
-  const idsInputsIdentite = [
-    { idJeSuis: '#je-suis-expert-cybersecurite', idZoneSaisie: '#expert-cybersecurite' },
-  ];
-
-  brancheInputsIdentite(idsInputsIdentite);
-
   const $bouton = $('.bouton');
   const identifiantHomologation = $bouton.attr('identifiant');
 
   $bouton.click(() => {
-    idsInputsIdentite
-      .map((ids) => ids.idZoneSaisie)
-      .forEach((selecteur) => $(selecteur).removeAttr('disabled'));
-
     const params = parametres('form#avis-expert-cyber');
-    axios.post(`/api/homologation/${identifiantHomologation}/partiesPrenantes`, params)
-      .then(() => axios.post(`/api/homologation/${identifiantHomologation}/avisExpertCyber`, params))
+    axios.post(`/api/homologation/${identifiantHomologation}/avisExpertCyber`, params)
       .then((reponse) => (window.location = `/homologation/${reponse.data.idHomologation}`));
   });
 });
