1cbcff9f09671213ef5520b70d752a8bbfff6a0c
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-08-09 12:04:09 +0200

    [NETTOYAGE] Homogénéise déclaration constantes classe

--- src/modeles/avisExpertCyber.js
+++ src/modeles/avisExpertCyber.js
@@ -2,13 +2,15 @@ const Base = require('./base');
 const { ErreurAvisInvalide, ErreurDateRenouvellementInvalide } = require('../erreurs');
 const Referentiel = require('../referentiel');
 
-const AVIS_FAVORABLE = 'favorable';
-const AVIS_DEFAVORABLE = 'defavorable';
+const AVIS = {
+  FAVORABLE: 'favorable',
+  DEFAVORABLE: 'defavorable',
+};
 
 const valide = (donnees, referentiel) => {
   const { avis, dateRenouvellement } = donnees;
 
-  if (avis && ![AVIS_FAVORABLE, AVIS_DEFAVORABLE].includes(avis)) {
+  if (avis && !Object.values(AVIS).includes(avis)) {
     throw new ErreurAvisInvalide(`L'avis "${avis}" est invalide`);
   }
 
@@ -33,14 +35,13 @@ class AvisExpertCyber extends Base {
     return this.referentiel.descriptionExpiration(this.dateRenouvellement);
   }
 
-  favorable() { return this.avis === AVIS_FAVORABLE; }
+  favorable() { return this.avis === AVIS.FAVORABLE; }
 
-  defavorable() { return this.avis === AVIS_DEFAVORABLE; }
+  defavorable() { return this.avis === AVIS.DEFAVORABLE; }
 
   inconnu() { return typeof this.avis === 'undefined'; }
 }
 
-AvisExpertCyber.FAVORABLE = AVIS_FAVORABLE;
-AvisExpertCyber.DEFAVORABLE = AVIS_DEFAVORABLE;
+Object.assign(AvisExpertCyber, AVIS);
 
 module.exports = AvisExpertCyber;
