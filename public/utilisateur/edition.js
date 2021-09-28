4429bfd0909ab7019d16293246a79619f4567ffa
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-09-28 09:46:06 +0200

    [CORRECTION] N'affiche pas le mot de passe dans l'URL
    Lorsqu'on appuie sur la touche entrée après la saisie d'un nouveau mot de passe,
    le formulaire adopte le comportement par défaut de l'événement « submit » et
    lance une requête GET en passant les paramètres en clair dans l'URL de la
    requête. Non seulement le formulaire ne devrait pas faire un GET, il est par
    ailleurs malvenu d'afficher en clair le mot de passe qui vient d'être saisi.
    
    Le présent commit corrige ce problème.
    
    Note : il sera judicieux par la suite de rétablir le comportement par défaut
    en indiquant dans le code HTMl que le formulaire doit envoyer un PUT vers
    `/api/utilisateur`.

--- public/utilisateur/edition.js
+++ public/utilisateur/edition.js
@@ -1,12 +1,17 @@
 import parametres from '../modules/parametres.js';
 
 $(() => {
-  const $bouton = $('.bouton');
-  $bouton.click(() => {
+  const $formulaire = $('form.utilisateur#edition');
+  $formulaire.submit((e) => {
+    e.preventDefault();
+
     const params = parametres('form#edition');
     params.cguAcceptees = params.cguAcceptees && params.cguAcceptees[0] === 'on';
 
     axios.put('/api/utilisateur', params)
       .then(() => (window.location = '/homologations'));
   });
+
+  const $bouton = $('.bouton');
+  $bouton.click(() => $formulaire.submit());
 });
