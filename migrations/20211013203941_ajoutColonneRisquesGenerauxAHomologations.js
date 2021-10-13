04931ceecf3e98240c42c76e286c1200d85d9e46
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-10-13 23:27:37 +0200

    Duplique colonne `risques` en colonne `risquesGeneraux`

---
+++ migrations/20211013203941_ajoutColonneRisquesGenerauxAHomologations.js
@@ -0,0 +1,21 @@
+exports.up = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => donnees.risques)
+      .map(({ id, donnees: { risques, ...autresDonnees } }) => knex('homologations')
+        .where({ id })
+        .update({ donnees: { risques, risquesGeneraux: risques, ...autresDonnees } }));
+
+    return Promise.all(misesAJour);
+  });
+
+exports.down = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => donnees.risquesGeneraux)
+      .map(({ id, donnees: { risques, risquesGeneraux, ...autresDonnees } }) => knex('homologations')
+        .where({ id })
+        .update({ donnees: { risques: risquesGeneraux, ...autresDonnees } }));
+
+    return Promise.all(misesAJour);
+  });
