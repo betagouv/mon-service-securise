e4b2a4d73371270e9e1283b839ad68f19830a411
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-10-22 15:29:58 +0200

    Duplique colonne `mesures` en `mesuresGenerales`

---
+++ migrations/20211022131727_ajoutColonneMesuresGeneralesAHomologations.js.js
@@ -0,0 +1,21 @@
+exports.up = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => donnees.mesures)
+      .map(({ id, donnees: { mesures, ...autresDonnees } }) => knex('homologations')
+        .where({ id })
+        .update({ donnees: { mesures, mesuresGenerales: mesures, ...autresDonnees } }));
+
+    return Promise.all(misesAJour);
+  });
+
+exports.down = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => donnees.mesuresGenerales)
+      .map(({ id, donnees: { mesures, mesuresGenerales, ...autresDonnees } }) => knex('homologations')
+        .where({ id })
+        .update({ donnees: { mesures: mesuresGenerales, ...autresDonnees } }));
+
+    return Promise.all(misesAJour);
+  });
