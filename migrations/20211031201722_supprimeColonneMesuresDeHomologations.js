fe3a1e683694c92137c2d5784e15fece67bcbcac
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-10-31 21:40:13 +0100

    Supprime colonne `mesures` devenue superflue

---
+++ migrations/20211031201722_supprimeColonneMesuresDeHomologations.js
@@ -0,0 +1,20 @@
+exports.up = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .map(({ id, donnees: { mesures, ...autresDonnees } }) => knex('homologations')
+        .where({ id })
+        .update({ donnees: { ...autresDonnees } }));
+
+    return Promise.all(misesAJour);
+  });
+
+exports.down = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => donnees.mesuresGenerales)
+      .map(({ id, donnees: { mesuresGenerales, ...autresDonnees } }) => knex('homologations')
+        .where({ id })
+        .update({ donnees: { mesures: mesuresGenerales, mesuresGenerales, ...autresDonnees } }));
+
+    return Promise.all(misesAJour);
+  });
