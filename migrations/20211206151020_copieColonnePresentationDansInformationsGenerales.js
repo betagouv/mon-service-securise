d6597eae1090ac6d18728766c8d3c252244335bb
Jacques Raguin <jacques.raguin@codeworks.fr>
2021-12-07 15:30:11 +0100

    Crée une migration pour copier la présentation de caractéristique complémentaire vers informations générales

---
+++ migrations/20211206151020_copieColonnePresentationDansInformationsGenerales.js
@@ -0,0 +1,34 @@
+const miseAJour = (contientDonneesCiblees, actionMiseAJour) => (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(contientDonneesCiblees)
+      .map(({ id, donnees }) => {
+        const donneesModifiees = actionMiseAJour(donnees);
+        return knex('homologations')
+          .where({ id })
+          .update({ donnees: donneesModifiees });
+      });
+    return Promise.all(misesAJour);
+  });
+
+const contientCaracteristiquesComplementaires = ({ donnees }) => (
+  donnees.caracteristiquesComplementaires
+);
+
+const copieDansInformationsGenerales = (donnees) => {
+  donnees.informationsGenerales.presentation = (
+    donnees.caracteristiquesComplementaires.presentation
+  );
+  return donnees;
+};
+
+const contientInformationsGenerales = ({ donnees }) => donnees.informationsGenerales;
+
+const supprimeDansInformationsGenerales = (donnees) => {
+  delete donnees.informationsGenerales.presentation;
+  return donnees;
+};
+
+exports.up = miseAJour(contientCaracteristiquesComplementaires, copieDansInformationsGenerales);
+
+exports.down = miseAJour(contientInformationsGenerales, supprimeDansInformationsGenerales);
