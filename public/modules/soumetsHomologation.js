bfc532f06b565ef5cc11568d1683b530329dcda0
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2021-12-27 18:35:27 +0100

    Revois l'algorithme de criticité
    L'algorithme est maintenant simplifié. Certaines sélections permettent
    de déclarer le service comme « critique ». Dans ce cas, on n'autorise plus
    la sauvegarde du projet d'homologation, et on redirige l'utilisateur soit
    vers le guide en 9 étapes de l'ANSSI, soit vers son espace personnel.

--- public/modules/soumetsHomologation.js
+++ public/modules/soumetsHomologation.js
@@ -5,67 +5,10 @@ const fermeModale = () => {
   $('body').css('overflow', '');
 };
 
-const afficheModale = (fragmentHtml) => {
-  $('.informations-complementaires').html(fragmentHtml);
+const afficheModaleSeuilCritique = () => {
   $('body').css('overflow', 'hidden');
   $('.rideau').css('display', 'flex');
 };
-
-const afficheModaleSeuilMoyen = () => afficheModale(`
-<div>
-  Compte tenu des risques auxquels votre service est susceptible de faire face,
-  aux vues des fonctionnalités offertes et de la sensibilité des données
-  traitées, il vous sera demandé, en plus des mesures de sécurité recommandées,
-  de :
-</div>
-<ul>
-  <li>
-    réaliser un test technique de la sécurité du service (ex. test automatique
-    en ligne, test d'intrusion, bug bounty, etc.).
-  </li>
-</ul>
-`);
-
-const afficheModaleSeuilEleve = () => afficheModale(`
-<div>
-  Compte tenu des risques auxquels votre service est susceptible de faire face,
-  aux vues des fonctionnalités offertes et de la sensibilité des données
-  traitées, il vous sera demandé, en plus des mesures de sécurité recommandées,
-  de :
-</div>
-<ul>
-  <li>
-    réaliser un test technique de la sécurité du service (ex. test automatique
-    en ligne, test d'intrusion, bug bounty, etc.) ;
-  </li>
-  <li>
-    réaliser un atelier d'analyse de risques en vue de compléter les risques et
-    de préciser les mesures de sécurité (vous pouvez vous aider du guide «
-    Sécurité et Agilité numériques »).
-  </li>
-</ul>
-`);
-
-const afficheModaleSeuilCritique = () => afficheModale(`
-<div>
-  Compte tenu des risques auxquels votre service est susceptible de faire face,
-  aux vues des fonctionnalités offertes et de la sensibilité des données
-  traitées, l'ANSSI vous recommande de réaliser une démarche d'homologation
-  approfondie en vous appuyant sur le « guide d’homologation en 9 étapes ».
-</div>
-<br>
-<div>
-  Celle-ci peut être effectuée en substitution ou en complément d'une démarche
-  via Mon Service Sécurisé. Le cas échéant, toutes les pièces complémentaires
-  au dossier d'homologation recommandées dans le guide en 9 étapes devront être
-  jointes au dossier généré sur Mon Service Sécurisé.
-</div>
-<br>
-<div>
-  Cette recommandation sera rappelée dans le dossier d’homologation généré sur
-  Mon Service Sécurisé.
-</div>
-`);
 
 const organiseParametresPointsAcces = (params) => (
   modifieParametresAvecItemsExtraits(params, 'pointsAcces', '^(description)-point-acces-')
@@ -82,44 +25,43 @@ const tousLesParametres = (selecteurFormulaire) => {
   return params;
 };
 
-const initialiseComportementModale = (url, parametresFormulaire) => {
+const initialiseComportementModale = () => {
   $('.fermeture-modale').click((eFermeture) => {
     eFermeture.stopPropagation();
     fermeModale();
   });
-
-  $('a#annulation').click(fermeModale);
-
-  $('.bouton#envoi').click(() => {
-    const params = tousLesParametres(parametresFormulaire);
-    Object.assign(url, { data: params });
-
-    axios(url)
-      .then((reponse) => (window.location = `/homologation/${reponse.data.idHomologation}`));
-  });
 };
 
-const soumetsHomologation = (selecteurFormulaire) => {
+const soumetsHomologation = (url, selecteurFormulaire) => {
   const params = tousLesParametres(selecteurFormulaire);
+  Object.assign(url, { data: params });
 
   axios.get('/api/seuilCriticite', { params })
-    .then((reponse) => {
-      switch (reponse.data.seuilCriticite) {
-        case 'faible':
-          $('.bouton#envoi').click();
-          break;
-        case 'moyen':
-          afficheModaleSeuilMoyen();
-          break;
-        case 'eleve':
-          afficheModaleSeuilEleve();
-          break;
-        case 'critique':
-          afficheModaleSeuilCritique();
-          break;
-        default:
+    .then(({ data: { seuilCriticite } }) => {
+      if (seuilCriticite === 'critique') afficheModaleSeuilCritique();
+      else {
+        axios(url)
+          .then(({ data: { idHomologation } }) => (window.location = `/homologation/${idHomologation}`));
       }
     });
 };
 
-export { initialiseComportementModale, soumetsHomologation };
+const initialiseComportementFormulaire = (selecteurFormulaire, selecteurBouton) => {
+  initialiseComportementModale();
+
+  const $bouton = $(selecteurBouton);
+  const identifiantHomologation = $bouton.attr('identifiant');
+  const url = identifiantHomologation
+    ? { method: 'put', url: `/api/homologation/${identifiantHomologation}` }
+    : { method: 'post', url: '/api/homologation' };
+  const $form = $(selecteurFormulaire);
+
+  $form.submit((e) => {
+    e.preventDefault();
+    soumetsHomologation(url, selecteurFormulaire);
+  });
+
+  $bouton.click(() => $form.submit());
+};
+
+export { initialiseComportementFormulaire as default };
