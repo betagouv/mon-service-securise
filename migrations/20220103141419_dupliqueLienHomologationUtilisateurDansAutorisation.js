ab426ba1a76591c04b31a72cf7900d1553d32165
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2022-01-03 16:02:43 +0100

    [MIGRATION] Duplique lien entre homologation et utilisateur dans table autorisations

---
+++ migrations/20220103141419_dupliqueLienHomologationUtilisateurDansAutorisation.js
@@ -0,0 +1,23 @@
+const uuid = require('uuid');
+
+const ajouteAutorisationCreateurSiInexistante = (knex, idUtilisateur, idHomologation) => knex('autorisations')
+  .then((lignes) => {
+    const autorisationsExistantes = lignes.filter(({ donnees }) => (
+      donnees.idHomologation === idHomologation
+        && donnees.idUtilisateur === idUtilisateur
+        && donnees.type === 'createur'
+    ));
+    if (autorisationsExistantes.length > 0) return Promise.resolve();
+
+    return knex('autorisations')
+      .insert({ id: uuid.v4(), donnees: { idHomologation, idUtilisateur, type: 'createur' } });
+  });
+
+exports.up = (knex) => knex('homologations')
+  .then((lignes) => Promise.all(
+    lignes.map(({ id, donnees: { idUtilisateur } }) => (
+      ajouteAutorisationCreateurSiInexistante(knex, idUtilisateur, id)
+    ))
+  ));
+
+exports.down = (knex) => knex('autorisations').del();
