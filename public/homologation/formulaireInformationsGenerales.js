0fb7f83e6bf95b735c9cb19412789b93f095809b
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-01-03 17:27:16 +0100

    Permets de renseigner manuellement des données sensibles

--- public/homologation/formulaireInformationsGenerales.js
+++ public/homologation/formulaireInformationsGenerales.js
@@ -5,11 +5,13 @@ const sourceRegExpParamsItem = (nomIndicatif) => `^(description)-${nomIndicatif}
 const listesAvecItemsExtraits = [
   { cle: 'pointsAcces', nomIndicatif: 'point-acces' },
   { cle: 'fonctionnalitesSpecifiques', nomIndicatif: 'fonctionnalite' },
+  { cle: 'donneesSensiblesSpecifiques', nomIndicatif: 'donnees-sensibles' },
 ].map((valeur) => (
   { ...valeur, sourceRegExpParamsItem: sourceRegExpParamsItem(valeur.nomIndicatif) }
 ));
 
 $(() => {
+  brancheElementsAjoutables('donnees-sensibles-specifiques', 'donnees-sensibles');
   brancheElementsAjoutables('fonctionnalites-specifiques', 'fonctionnalite');
   brancheElementsAjoutables('points-acces', 'point-acces', 'exemple : https://www.adresse.fr, App Store, Play Store…');
 });
