ddf470937cf92852d8cb79c2a7256a4bf1b270e7
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2022-01-11 11:55:00 +0100

    [MIGRATION] convertis emails persistés en minuscules

---
+++ migrations/20220111092352_conversionEmailsEnMinuscules.js
@@ -0,0 +1,9 @@
+exports.up = (knex) => knex('utilisateurs')
+  .then((lignes) => Promise.all(
+    lignes.map(({ id, donnees }) => {
+      donnees.email = donnees.email.toLowerCase();
+      return knex('utilisateurs').where({ id }).update({ donnees });
+    })
+  ));
+
+exports.down = () => {};
