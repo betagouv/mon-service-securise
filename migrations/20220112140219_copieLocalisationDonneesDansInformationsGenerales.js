b54944707333531324b5eaaef8d7c0f776c47495
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-01-12 15:10:52 +0100

    [MIGRATION] Copie les localisations des données dans les informations générales

---
+++ migrations/20220112140219_copieLocalisationDonneesDansInformationsGenerales.js
@@ -0,0 +1,34 @@
+const miseAJour = (contientDonneesCiblees, actionMiseAJour) => (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(contientDonneesCiblees)
+      .map(({ id, donnees }) => {
+        const donneesModifiees = actionMiseAJour(donnees);
+        return knex('homologations')
+          .where({ id })
+          .update({ donnees: donneesModifiees });
+      });
+    return Promise.all(misesAJour);
+  });
+
+const contientCaracteristiquesComplementaires = ({ donnees }) => (
+  donnees.caracteristiquesComplementaires
+);
+
+const copieDansInformationsGenerales = (donnees) => {
+  donnees.informationsGenerales.localisationDonnees = (
+    donnees.caracteristiquesComplementaires.localisationDonnees
+  );
+  return donnees;
+};
+
+const contientInformationsGenerales = ({ donnees }) => donnees.informationsGenerales;
+
+const supprimeDansInformationsGenerales = (donnees) => {
+  delete donnees.informationsGenerales.localisationDonnees;
+  return donnees;
+};
+
+exports.up = miseAJour(contientCaracteristiquesComplementaires, copieDansInformationsGenerales);
+
+exports.down = miseAJour(contientInformationsGenerales, supprimeDansInformationsGenerales);
