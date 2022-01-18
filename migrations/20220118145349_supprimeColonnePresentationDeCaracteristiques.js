50a246666d067bebadc7c1e203a8f5f99afcd3ca
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-01-18 16:09:39 +0100

    [MIGRATION] Supprime la présentation des caractéristiques complémentaires

---
+++ migrations/20220118145349_supprimeColonnePresentationDeCaracteristiques.js
@@ -0,0 +1,30 @@
+exports.up = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => (donnees.caracteristiquesComplementaires))
+      .map(({ id, donnees: { caracteristiquesComplementaires, ...autresDonnees } }) => {
+        delete caracteristiquesComplementaires.presentation;
+        return knex('homologations')
+          .where({ id })
+          .update({ donnees: { caracteristiquesComplementaires, ...autresDonnees } });
+      });
+    return Promise.all(misesAJour);
+  });
+
+exports.down = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => (
+        donnees.descriptionService
+      ))
+      .map(({ id, donnees }) => {
+        donnees.caracteristiquesComplementaires ||= {};
+        donnees.caracteristiquesComplementaires.presentation = (
+          donnees.descriptionService.presentation
+        );
+        return knex('homologations')
+          .where({ id })
+          .update({ donnees });
+      });
+    return Promise.all(misesAJour);
+  });
