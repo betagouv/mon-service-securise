b3e2c0efdfb325787609bf9e10e3b4f0286d76a4
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2022-01-18 18:05:02 +0100

    [MIGRATION] Supprime colonne `idUtilisateur` de la table `homologations`

---
+++ migrations/20220118162341_suppressionColonneHomologationsIdUtilisateur.js
@@ -0,0 +1,23 @@
+exports.up = (knex) => knex('homologations')
+  .then((lignes) => {
+    const suppressions = lignes
+      .map(({ id, donnees: { idUtilisateur, ...autresDonnees } }) => knex('homologations')
+        .where({ id })
+        .update({ donnees: { ...autresDonnees } }));
+
+    return Promise.all(suppressions);
+  });
+
+exports.down = (knex) => knex('autorisations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees: { type } }) => type === 'createur')
+      .map(({ donnees: { idHomologation, idUtilisateur } }) => knex('homologations')
+        .where({ id: idHomologation })
+        .first()
+        .then(({ donnees }) => knex('homologations')
+          .where({ id: idHomologation })
+          .update({ donnees: Object.assign(donnees, { idUtilisateur }) })));
+
+    return Promise.all(misesAJour);
+  });
