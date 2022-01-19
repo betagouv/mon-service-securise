26e8529a0e3ac06fba571026000be1f8c50230f9
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-01-19 09:44:02 +0100

    [MIGRATION] Supprime la localisation des données des caractéristiques complémentaires

---
+++ migrations/20220119083758_supprimeColonneLocalisationDonneesDeCaracteristiques.js
@@ -0,0 +1,28 @@
+exports.up = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => (donnees.caracteristiquesComplementaires))
+      .map(({ id, donnees: { caracteristiquesComplementaires, ...autresDonnees } }) => {
+        delete caracteristiquesComplementaires.localisationDonnees;
+        return knex('homologations')
+          .where({ id })
+          .update({ donnees: { caracteristiquesComplementaires, ...autresDonnees } });
+      });
+    return Promise.all(misesAJour);
+  });
+
+exports.down = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => donnees.descriptionService)
+      .map(({ id, donnees }) => {
+        donnees.caracteristiquesComplementaires ||= {};
+        donnees.caracteristiquesComplementaires.localisationDonnees = (
+          donnees.descriptionService.localisationDonnees
+        );
+        return knex('homologations')
+          .where({ id })
+          .update({ donnees });
+      });
+    return Promise.all(misesAJour);
+  });
