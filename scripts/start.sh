6cd89bb05973a97a813504bd6ba5bf208915fb75
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2022-01-31 21:45:38 +0100

    [NETTOYAGE] Simplifie scripts de lancement du serveur et des tests depuis Docker
    Et profites-en pour donner un nom répétable aux conteneurs en passant par
    `docker-compose up` plutôt que par `docker-compose run`

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -e
 
-PORT=${PORT:-1917}
-
-docker-compose build
-docker-compose run --rm -p ${PORT}:3000 -v $PWD:/usr/src/app web $*
+docker-compose up web
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-source scripts/variables.sh
-
-docker run --rm -ti -e NODE_ENV=test -v $PWD:/usr/src/app ${DOCKER_IMAGE} npx mocha --recursive --watch
+docker-compose up test
