a45e3749f43ff30dd0293bacfabbd04c0870203e
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-02-16 15:04:15 +0100

    Duplique les données des parties prenantes dans les rôles et responsabilités

---
+++ migrations/20220216134942_dupliquePartiesPrenantesDansRolesResponsabilites.js
@@ -0,0 +1,21 @@
+exports.up = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => donnees.partiesPrenantes)
+      .map(({ id, donnees: { partiesPrenantes, ...autresDonnees } }) => knex('homologations')
+        .where({ id })
+        .update({ donnees: {
+          partiesPrenantes, rolesResponsabilites: partiesPrenantes, ...autresDonnees,
+        } }));
+    return Promise.all(misesAJour);
+  });
+
+exports.down = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => donnees.rolesResponsabilites)
+      .map(({ id, donnees: { rolesResponsabilites, ...autresDonnees } }) => knex('homologations')
+        .where({ id })
+        .update({ donnees: { ...autresDonnees } }));
+    return Promise.all(misesAJour);
+  });
