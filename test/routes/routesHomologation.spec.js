7ed24c9538c557fd95b952167f9be01553d1dab3
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2022-02-22 17:46:47 +0100

    Utilise testeurMSS

--- test/routes/routesHomologation.spec.js
+++ test/routes/routesHomologation.spec.js
@@ -1,73 +1,88 @@
-const MSS = require('../../src/mss');
-const Referentiel = require('../../src/referentiel');
-
-const middleware = require('../mocks/middleware');
+const testeurMSS = require('./testeurMSS');
 
 describe('Le serveur MSS des routes /homologation/*', () => {
-  let serveur;
+  const testeur = testeurMSS();
 
-  before((done) => {
-    const referentiel = Referentiel.creeReferentielVide();
+  beforeEach(testeur.initialise);
 
-    serveur = MSS.creeServeur({}, middleware, referentiel, {}, false);
-    serveur.ecoute(1234, done);
-  });
-
-  beforeEach(() => {
-    middleware.reinitialise();
-  });
-
-  after(() => { serveur.arreteEcoute(); });
+  afterEach(testeur.arrete);
 
   describe('quand requête GET sur `/homologation/:id`', () => {
     it("recherche l'homologation correspondante", (done) => {
-      middleware.verifieRechercheHomologation('http://localhost:1234/homologation/456', done);
+      testeur.middleware().verifieRechercheHomologation(
+        'http://localhost:1234/homologation/456',
+        done,
+      );
     });
   });
 
   describe('quand requête GET sur `/homologation/:id/descriptionService`', () => {
     it("recherche l'homologation correspondante", (done) => {
-      middleware.verifieRechercheHomologation('http://localhost:1234/homologation/456/descriptionService', done);
+      testeur.middleware().verifieRechercheHomologation(
+        'http://localhost:1234/homologation/456/descriptionService',
+        done,
+      );
     });
   });
 
   describe('quand requête GET sur `/homologation/:id/caracteristiquesComplementaires`', () => {
     it("recherche l'homologation correspondante", (done) => {
-      middleware.verifieRechercheHomologation('http://localhost:1234/homologation/456/caracteristiquesComplementaires', done);
+      testeur.middleware().verifieRechercheHomologation(
+        'http://localhost:1234/homologation/456/caracteristiquesComplementaires',
+        done,
+      );
     });
   });
 
   describe('quand requête GET sur `/homologation/:id/decision`', () => {
     it("recherche l'homologation correspondante", (done) => {
-      middleware.verifieRechercheHomologation('http://localhost:1234/homologation/456/decision', done);
+      testeur.middleware().verifieRechercheHomologation(
+        'http://localhost:1234/homologation/456/decision',
+        done,
+      );
     });
 
     it('sert la page avec un nonce', (done) => {
-      middleware.verifieRequetePositionneHeadersAvecNonce('http://localhost:1234/homologation/456/decision', done);
+      testeur.middleware().verifieRequetePositionneHeadersAvecNonce(
+        'http://localhost:1234/homologation/456/decision',
+        done,
+      );
     });
   });
 
   describe('quand requête GET sur `/homologation/:id/mesures`', () => {
     it("recherche l'homologation correspondante", (done) => {
-      middleware.verifieRechercheHomologation('http://localhost:1234/homologation/456/mesures', done);
+      testeur.middleware().verifieRechercheHomologation(
+        'http://localhost:1234/homologation/456/mesures',
+        done
+      );
     });
   });
 
   describe('quand requete GET sur `/homologation/:id/partiesPrenantes`', () => {
     it("recherche l'homologation correspondante", (done) => {
-      middleware.verifieRechercheHomologation('http://localhost:1234/homologation/456/partiesPrenantes', done);
+      testeur.middleware().verifieRechercheHomologation(
+        'http://localhost:1234/homologation/456/partiesPrenantes',
+        done,
+      );
     });
   });
 
   describe('quand requête GET sur `/homologation/:id/risques`', () => {
     it("recherche l'homologation correspondante", (done) => {
-      middleware.verifieRechercheHomologation('http://localhost:1234/homologation/456/risques', done);
+      testeur.middleware().verifieRechercheHomologation(
+        'http://localhost:1234/homologation/456/risques',
+        done,
+      );
     });
   });
 
   describe('quand requête GET sur `/homologation/:id/avisExpertCyber`', () => {
     it("recherche l'homologation correspondante", (done) => {
-      middleware.verifieRechercheHomologation('http://localhost:1234/homologation/456/avisExpertCyber', done);
+      testeur.middleware().verifieRechercheHomologation(
+        'http://localhost:1234/homologation/456/avisExpertCyber',
+        done,
+      );
     });
   });
 });
