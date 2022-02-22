9b5c15454a03283e7f1edb0bae9b4dab81b75398
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2022-02-22 18:22:49 +0100

    Utilise testeurMSS dans fichier tests routes initial

--- test/mss.spec.js
+++ test/mss.spec.js
@@ -1,53 +1,14 @@
 const axios = require('axios');
 const expect = require('expect.js');
 
-const MSS = require('../src/mss');
-const Referentiel = require('../src/referentiel');
-const DepotDonnees = require('../src/depotDonnees');
-
-const middleware = require('./mocks/middleware');
-
-const verifieRequeteGenereErreurHTTP = (status, messageErreur, requete, suite) => {
-  axios(requete)
-    .then(() => suite('Réponse OK inattendue'))
-    .catch((erreur) => {
-      expect(erreur.response.status).to.equal(status);
-      expect(erreur.response.data).to.equal(messageErreur);
-      suite();
-    })
-    .catch(suite);
-};
-
-const verifieJetonDepose = (reponse, suite) => {
-  const valeurHeader = reponse.headers['set-cookie'][0];
-  expect(valeurHeader).to.match(/^token=.+; path=\/; expires=.+; samesite=strict; httponly$/);
-  suite();
-};
+const testeurMSS = require('./routes/testeurMSS');
 
 describe('Le serveur MSS', () => {
-  let adaptateurMail;
-  let depotDonnees;
-  let referentiel;
-  let serveur;
+  const testeur = testeurMSS();
 
-  beforeEach((done) => {
-    middleware.reinitialise();
-    referentiel = Referentiel.creeReferentielVide();
-    adaptateurMail = {
-      envoieMessageFinalisationInscription: () => Promise.resolve(),
-      envoieMessageReinitialisationMotDePasse: () => Promise.resolve(),
-    };
+  beforeEach(testeur.initialise);
 
-    DepotDonnees.creeDepotVide()
-      .then((depot) => {
-        depotDonnees = depot;
-        depotDonnees.nouvelleHomologation = () => Promise.resolve();
-        serveur = MSS.creeServeur(depotDonnees, middleware, referentiel, adaptateurMail, false);
-        serveur.ecoute(1234, done);
-      });
-  });
-
-  afterEach(() => { serveur.arreteEcoute(); });
+  afterEach(testeur.arrete);
 
   it('sert des pages HTML', (done) => {
     axios.get('http://localhost:1234/')
@@ -60,7 +21,7 @@ describe('Le serveur MSS', () => {
 
   describe('quand une page est servie', () => {
     it('positionne les headers', (done) => {
-      middleware.verifieRequetePositionneHeaders('http://localhost:1234/', done);
+      testeur.middleware().verifieRequetePositionneHeaders('http://localhost:1234/', done);
     });
 
     it("n'affiche pas d'information sur la nature du serveur", (done) => {
@@ -73,19 +34,19 @@ describe('Le serveur MSS', () => {
     });
 
     it("repousse l'expiration du cookie", (done) => {
-      middleware.verifieRequeteRepousseExpirationCookie('http://localhost:1234/', done);
+      testeur.middleware().verifieRequeteRepousseExpirationCookie('http://localhost:1234/', done);
     });
   });
 
   describe('quand requête GET sur `/connexion`', () => {
     it("déconnecte l'utilisateur courant", (done) => {
-      middleware.verifieRequeteExigeSuppressionCookie('http://localhost:1234/connexion', done);
+      testeur.middleware().verifieRequeteExigeSuppressionCookie('http://localhost:1234/connexion', done);
     });
   });
 
   describe('quand requête GET sur `/reinitialisationMotDePasse`', () => {
     it("déconnecte l'utilisateur courant", (done) => {
-      middleware.verifieRequeteExigeSuppressionCookie(
+      testeur.middleware().verifieRequeteExigeSuppressionCookie(
         'http://localhost:1234/reinitialisationMotDePasse', done
       );
     });
@@ -96,32 +57,32 @@ describe('Le serveur MSS', () => {
       const utilisateur = { id: '123', genereToken: () => 'un token', accepteCGU: () => false };
 
       beforeEach(() => {
-        depotDonnees.utilisateurAFinaliser = () => Promise.resolve(utilisateur);
-        depotDonnees.utilisateur = () => Promise.resolve(utilisateur);
+        testeur.depotDonnees().utilisateurAFinaliser = () => Promise.resolve(utilisateur);
+        testeur.depotDonnees().utilisateur = () => Promise.resolve(utilisateur);
       });
 
       it('dépose le jeton dans un cookie', (done) => {
-        depotDonnees.utilisateurAFinaliser = (idReset) => new Promise((resolve) => {
+        testeur.depotDonnees().utilisateurAFinaliser = (idReset) => {
           expect(idReset).to.equal('999');
-          resolve(utilisateur);
-        });
+          return Promise.resolve(utilisateur);
+        };
 
         axios.get('http://localhost:1234/initialisationMotDePasse/999')
-          .then((reponse) => verifieJetonDepose(reponse, done))
+          .then((reponse) => testeur.verifieJetonDepose(reponse, done))
           .catch(done);
       });
     });
 
     it("aseptise l'identifiant reçu", (done) => {
-      middleware.verifieAseptisationParametres(
+      testeur.middleware().verifieAseptisationParametres(
         ['idReset'], 'http://localhost:1234/initialisationMotDePasse/999', done
       );
     });
 
     it('retourne une erreur HTTP 404 si idReset inconnu', (done) => {
-      depotDonnees.utilisateurAFinaliser = () => Promise.resolve(undefined);
+      testeur.depotDonnees().utilisateurAFinaliser = () => Promise.resolve(undefined);
 
-      verifieRequeteGenereErreurHTTP(
+      testeur.verifieRequeteGenereErreurHTTP(
         404, "Identifiant d'initialisation de mot de passe \"999\" inconnu",
         'http://localhost:1234/initialisationMotDePasse/999', done
       );
@@ -130,21 +91,27 @@ describe('Le serveur MSS', () => {
 
   describe('quand requête GET sur `/admin/inscription`', () => {
     it("verrouille l'accès par une authentification basique", (done) => {
-      middleware.verifieRequeteExigeAuthentificationBasique('http://localhost:1234/admin/inscription', done);
+      testeur.middleware().verifieRequeteExigeAuthentificationBasique(
+        'http://localhost:1234/admin/inscription',
+        done,
+      );
     });
   });
 
   describe('quand requête GET sur `/espacePersonnel`', () => {
     it("vérifie que l'utilisateur est authentifié", (done) => {
-      middleware.verifieRequeteExigeAcceptationCGU('http://localhost:1234/espacePersonnel', done);
+      testeur.middleware().verifieRequeteExigeAcceptationCGU(
+        'http://localhost:1234/espacePersonnel',
+        done,
+      );
     });
   });
 
   describe('quand requête GET sur `/utilisateur/edition`', () => {
     it("vérifie que l'utilisateur est authentifié", (done) => {
       const utilisateur = { accepteCGU: () => true };
-      depotDonnees.utilisateur = () => new Promise((resolve) => resolve(utilisateur));
-      middleware.verifieRequeteExigeJWT('http://localhost:1234/utilisateur/edition', done);
+      testeur.depotDonnees().utilisateur = () => Promise.resolve(utilisateur);
+      testeur.middleware().verifieRequeteExigeJWT('http://localhost:1234/utilisateur/edition', done);
     });
   });
 });
