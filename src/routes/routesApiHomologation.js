d1f9d7d8a1b02e09530cd06e9dc8eaa428c8da14
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-02-28 10:17:36 +0100

    Lie les parties prenantes spécifiques aux données

--- src/routes/routesApiHomologation.js
+++ src/routes/routesApiHomologation.js
@@ -90,6 +90,11 @@ const routesApiHomologation = (middleware, depotDonnees, referentiel) => {
       try {
         const caracteristiques = new CaracteristiquesComplementaires(requete.body, referentiel);
         depotDonnees.ajouteCaracteristiquesAHomologation(requete.params.id, caracteristiques)
+          .then(() => (
+            depotDonnees.ajoutePartiesPrenantesSpecifiquesAHomologation(
+              requete.params.id, caracteristiques.entitesExternes.toutes()
+            )
+          ))
           .then(() => reponse.send({ idHomologation: requete.homologation.id }));
       } catch {
         reponse.status(422).send('Données invalides');
