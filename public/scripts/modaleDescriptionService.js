06b73aa82cd041badc5f41f13b64b63dce7f1b14
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2022-03-01 17:00:24 +0100

    [REMANIEMENT] Extrais comportement ouverture modale

--- public/scripts/modaleDescriptionService.js
+++ public/scripts/modaleDescriptionService.js
@@ -1,13 +1,22 @@
-const fermeModale = () => {
-  $('.rideau').css('display', '');
+const afficheModale = (selecteurModale) => {
+  $('body').css('overflow', 'hidden');
+  $(selecteurModale).css('display', 'flex');
+};
+
+const fermeModale = (selecteurModale) => {
+  $(selecteurModale).css('display', '');
   $('body').css('overflow', '');
 };
 
-const initialiseComportementModale = () => {
+const initialiseComportementModale = (selecteurModale) => {
+  $(selecteurModale)
+    .on('afficheModaleDescriptionService', () => afficheModale(selecteurModale))
+    .on('fermeModaleDescriptionService', () => fermeModale(selecteurModale));
+
   $('.fermeture-modale').on('click', (eFermeture) => {
     eFermeture.stopPropagation();
-    fermeModale();
+    $(selecteurModale).trigger('fermeModaleDescriptionService');
   });
 };
 
-$(initialiseComportementModale);
+$(() => initialiseComportementModale('.rideau'));
