dfb5fbfd4271309d2f964b53a3adc16cfdc8e285
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-03-03 10:19:29 +0100

    Mets des onglets dans l'affichage des rôles et responsabilités

--- public/homologation/partiesPrenantes.js
+++ public/homologation/partiesPrenantes.js
@@ -1,28 +1,31 @@
 import parametres, { modifieParametresAvecItemsExtraits, modifieParametresGroupementElements } from '../modules/parametres.mjs';
 import brancheElementsAjoutables from '../modules/brancheElementsAjoutables.js';
+import brancheOnglets from '../modules/interactions/brancheOnglets.mjs';
+
+const tousLesParametres = (selecteurFormulaire) => {
+  const params = parametres(selecteurFormulaire);
+  modifieParametresAvecItemsExtraits(
+    params, 'acteursHomologation', '^(role|nom|fonction)-acteur-homologation-'
+  );
+  ['developpementFourniture', 'hebergement', 'maintenanceService'].forEach(
+    (identifiant) => modifieParametresGroupementElements(params, 'partiesPrenantes', identifiant)
+  );
+  return params;
+};
 
 $(() => {
+  brancheOnglets('#onglets-liens > a');
+
   brancheElementsAjoutables('acteurs-homologation', 'acteur-homologation', {
     role: { label: 'Rôle au regard du projet' },
     nom: { label: 'Nom / Prénom' },
     fonction: { label: 'Fonction' },
   });
 
-  const tousLesParametres = (selecteurFormulaire) => {
-    const params = parametres(selecteurFormulaire);
-    modifieParametresAvecItemsExtraits(
-      params, 'acteursHomologation', '^(role|nom|fonction)-acteur-homologation-'
-    );
-    ['developpementFourniture', 'hebergement', 'maintenanceService', 'securiteService'].forEach(
-      (identifiant) => modifieParametresGroupementElements(params, 'partiesPrenantes', identifiant)
-    );
-    return params;
-  };
-
   const $bouton = $('.bouton');
   const identifiantHomologation = $bouton.attr('identifiant');
 
-  $bouton.click(() => {
+  $bouton.on('click', () => {
     const params = tousLesParametres('form#parties-prenantes');
 
     axios.post(`/api/homologation/${identifiantHomologation}/partiesPrenantes`, params)
---
+++ public/modules/interactions/brancheOnglets.mjs
@@ -0,0 +1,23 @@
+const SELECTEUR_ONGLETS = '.onglet';
+const PREFIX_IDENTIFIANT_ONGLETS = 'onglet-';
+const CLASSE_LIEN_ACTIF = 'actif';
+const CLASSE_ONGLET_INVISIBLE = 'invisible';
+
+const identifiantOngletActif = (selecteurLienActif) => $(selecteurLienActif).prop('id').slice(PREFIX_IDENTIFIANT_ONGLETS.length);
+
+const brancheOnglets = (selecteurLiens) => {
+  $(SELECTEUR_ONGLETS).addClass(CLASSE_ONGLET_INVISIBLE);
+  
+  const selecteurLienActif = `${selecteurLiens}.${CLASSE_LIEN_ACTIF}`;
+  $(`#${identifiantOngletActif(selecteurLienActif)}`).removeClass(CLASSE_ONGLET_INVISIBLE);
+
+  $(selecteurLiens).on('click', (event) => {
+    $(selecteurLiens).removeClass(CLASSE_LIEN_ACTIF);
+    $(event.target).addClass(CLASSE_LIEN_ACTIF);
+
+    $(SELECTEUR_ONGLETS).addClass(CLASSE_ONGLET_INVISIBLE);
+    $(`#${identifiantOngletActif(event.target)}`).removeClass(CLASSE_ONGLET_INVISIBLE);
+  });
+};
+
+export default brancheOnglets;
