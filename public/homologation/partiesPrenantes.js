d3a6e8eb746d67e24f029f84a0835c5f0d35009d
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-03-04 15:39:17 +0100

    Prépare l'intégration des paramètres parties prenantes spécifiques

--- public/homologation/partiesPrenantes.js
+++ public/homologation/partiesPrenantes.js
@@ -1,15 +1,13 @@
-import parametres, { modifieParametresAvecItemsExtraits, modifieParametresGroupementElements } from '../modules/parametres.mjs';
+import arrangeParametresPartiesPrenantes from '../modules/arrangeParametresPartiesPrenantes.mjs';
+import parametres from '../modules/parametres.mjs';
 import brancheElementsAjoutables from '../modules/brancheElementsAjoutables.js';
 import brancheOnglets from '../modules/interactions/brancheOnglets.mjs';
 
 const tousLesParametres = (selecteurFormulaire) => {
   const params = parametres(selecteurFormulaire);
-  modifieParametresAvecItemsExtraits(
-    params, 'acteursHomologation', '^(role|nom|fonction)-acteur-homologation-'
-  );
-  ['developpementFourniture', 'hebergement', 'maintenanceService', 'securiteService'].forEach(
-    (identifiant) => modifieParametresGroupementElements(params, 'partiesPrenantes', identifiant)
-  );
+
+  arrangeParametresPartiesPrenantes(params);
+
   return params;
 };
 
