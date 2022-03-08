983991856aa8dac6a07585768611953f674ae82c
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-03-08 16:27:32 +0100

    [MIGRATION] Supprime l'hébergeur des caractéristiques complémentaires

---
+++ migrations/20220308150804_suppressionColonneHebergeur.js
@@ -0,0 +1,36 @@
+const miseAJour = (contientDonneesCiblees, actionMiseAJour) => (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(contientDonneesCiblees)
+      .map(({ id, donnees }) => {
+        const donneesModifiees = actionMiseAJour(donnees);
+        return knex('homologations')
+          .where({ id })
+          .update({ donnees: donneesModifiees });
+      });
+    return Promise.all(misesAJour);
+  });
+
+const contientCaracteristiquesComplementaires = ({ donnees }) => (
+  donnees?.caracteristiquesComplementaires
+);
+
+const supprimeHebergeur = (donnees) => {
+  delete donnees.caracteristiquesComplementaires.hebergeur;
+
+  return donnees;
+};
+
+const contientPartiesPrenantes = ({ donnees }) => donnees?.partiesPrenantes;
+
+const copieNomHebergement = (donnees) => {
+  donnees.caracteristiquesComplementaires ||= {};
+  donnees.caracteristiquesComplementaires.hebergeur = donnees.partiesPrenantes?.partiesPrenantes
+    ?.find((partiePrenante) => partiePrenante.type === 'Hebergement')?.nom;
+
+  return donnees;
+};
+
+exports.up = miseAJour(contientCaracteristiquesComplementaires, supprimeHebergeur);
+
+exports.down = miseAJour(contientPartiesPrenantes, copieNomHebergement);
