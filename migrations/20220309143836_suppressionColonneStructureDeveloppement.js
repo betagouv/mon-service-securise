a045a89bc934237390b1b2f270c7609f2249c323
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-03-09 15:59:57 +0100

    Suppression des données de la structure du développement dans les caractéristiques complémentaires

---
+++ migrations/20220309143836_suppressionColonneStructureDeveloppement.js
@@ -0,0 +1,38 @@
+const miseAJour = (contientDonneesCiblees, actionMiseAJour) => (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(contientDonneesCiblees)
+      .map(({ id, donnees }) => {
+        const donneesModifiees = actionMiseAJour(donnees);
+        return knex('homologations')
+          .where({ id })
+          .update({ donnees: donneesModifiees });
+      });
+    return Promise.all(misesAJour);
+  });
+
+const contientCaracteristiquesComplementaires = ({ donnees }) => (
+  donnees?.caracteristiquesComplementaires
+);
+
+const supprimeStructureDeveloppement = (donnees) => {
+  delete donnees.caracteristiquesComplementaires.structureDeveloppement;
+
+  return donnees;
+};
+
+const contientPartiesPrenantes = ({ donnees }) => donnees?.partiesPrenantes;
+
+const copieNomDeveloppementFourniture = (donnees) => {
+  donnees.caracteristiquesComplementaires ||= {};
+  donnees.caracteristiquesComplementaires.structureDeveloppement = donnees
+    .partiesPrenantes
+    ?.partiesPrenantes
+    ?.find((partiePrenante) => partiePrenante.type === 'DeveloppementFourniture')?.nom;
+
+  return donnees;
+};
+
+exports.up = miseAJour(contientCaracteristiquesComplementaires, supprimeStructureDeveloppement);
+
+exports.down = miseAJour(contientPartiesPrenantes, copieNomDeveloppementFourniture);
