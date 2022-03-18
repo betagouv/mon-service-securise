4f385357c9880a8c99eb77485a85a81bfcba3830
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-03-18 15:29:49 +0100

    Fusionne les scripts de description du service

--- public/homologation/descriptionService.js
+++ public/homologation/descriptionService.js
@@ -1,6 +1,9 @@
 import { brancheElementsAjoutablesDescription } from '../modules/brancheElementsAjoutables.js';
+import initialiseComportementFormulaire from '../modules/soumetsHomologation.mjs';
 
 $(() => {
+  initialiseComportementFormulaire('form#homologation', '.bouton#diagnostic');
+
   brancheElementsAjoutablesDescription('donnees-sensibles-specifiques', 'donnees-sensibles');
   brancheElementsAjoutablesDescription('fonctionnalites-specifiques', 'fonctionnalite');
   brancheElementsAjoutablesDescription('points-acces', 'point-acces', 'exemple : https://www.adresse.fr, App Store, Play Store…');
