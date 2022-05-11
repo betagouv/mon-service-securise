3dc01ccaa938d6149fd75f866427684c23974217
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-05-11 11:39:58 +0200

    [MIGRATION] Nettoie les mesures des statuts et modalités vides

---
+++ migrations/20220509153038_nettoyageMesuresGeneralesVides.js
@@ -0,0 +1,16 @@
+exports.up = (knex) => knex('homologations')
+  .then((lines) => {
+    const misesAJour = lines
+      .filter(({ donnees }) => donnees?.mesuresGenerales)
+      .map(({ id, donnees: { mesuresGenerales, ...autresDonnees } }) => {
+        const mesuresNettoyees = mesuresGenerales.filter(
+          (mesure) => mesure.statut || mesure.modalites
+        );
+        return knex('homologations')
+          .where({ id })
+          .update({ donnees: { mesuresGenerales: mesuresNettoyees, ...autresDonnees } });
+      });
+    return Promise.all(misesAJour);
+  });
+
+exports.down = () => {};
