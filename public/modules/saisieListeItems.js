fdb8c0c7bef4ff73db4e2525c8158598c97e979e
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-05-31 11:33:36 +0200

    Affiche les mesures spécifiques en haut et en ordre inverse

--- public/modules/saisieListeItems.js
+++ public/modules/saisieListeItems.js
@@ -5,34 +5,47 @@ const brancheSuppressionElement = () => {
   });
 };
 
-const afficheZoneSaisieItem = (selecteur, zoneSaisie) => {
+const afficheZoneSaisieItem = (selecteur, zoneSaisie, ordreInverse) => {
   const $conteneurSaisieItem = $(`
 <label class="item-ajoute">
   <div class="icone-suppression"></div>
 </label>
   `);
 
-  $(selecteur).append($conteneurSaisieItem);
+  const methodeAjout = ordreInverse ? 'prepend' : 'append';
+  $(selecteur)[methodeAjout]($conteneurSaisieItem);
   brancheSuppressionElement();
 
   $conteneurSaisieItem.append(zoneSaisie);
 };
 
-const brancheAjoutItem = (selecteurAction, selecteurConteneur, cbZoneSaisie, cbIncrementeIndex) => {
+const brancheAjoutItem = (
+  selecteurAction, selecteurConteneur, cbZoneSaisie, cbIncrementeIndex,
+  options = { ordreInverse: false },
+) => {
   $(selecteurAction).click((e) => {
     e.preventDefault();
     const index = cbIncrementeIndex();
-    afficheZoneSaisieItem(selecteurConteneur, cbZoneSaisie(index, {}));
+    afficheZoneSaisieItem(selecteurConteneur, cbZoneSaisie(index, {}), options.ordreInverse);
   });
 };
 
-const peupleListeItems = (selecteurConteneur, selecteurDonnees, cbZoneSaisie) => {
+const peupleListeItems = (
+  selecteurConteneur,
+  selecteurDonnees,
+  cbZoneSaisie,
+  options = { ordreInverse: false },
+) => {
   const donneesItems = JSON.parse($(selecteurDonnees).text());
   donneesItems.forEach((donnees, index) => {
-    afficheZoneSaisieItem(selecteurConteneur, cbZoneSaisie(index, donnees));
+    afficheZoneSaisieItem(selecteurConteneur, cbZoneSaisie(index, donnees), options.ordreInverse);
   });
 
   return donneesItems.length;
 };
 
-export { brancheAjoutItem, peupleListeItems, brancheSuppressionElement };
+export {
+  brancheAjoutItem,
+  peupleListeItems,
+  brancheSuppressionElement,
+};
