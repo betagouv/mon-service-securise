d96ad4310b1d5ab7050c7c25af5456e5f309141c
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-06-09 17:45:14 +0200

    Rends compatible le décompte de déconnexion multi fenêtres

--- public/modules/deconnexion.js
+++ public/modules/deconnexion.js
@@ -1,7 +1,18 @@
 import afficheModaleDeconnexion from './interactions/afficheModaleDeconnexion.mjs';
 
+const HEURE_DECONNEXION = 'heureDeconnexion';
+
+const lanceDecompteAffichage = () => {
+  if (Date.now() >= localStorage.getItem(HEURE_DECONNEXION)) {
+    afficheModaleDeconnexion('.rideau#deconnexion', '.rideau');
+  } else {
+    setTimeout(lanceDecompteAffichage, localStorage.getItem(HEURE_DECONNEXION) - Date.now());
+  }
+};
+
 const lanceDecompteDeconnexion = (dureeSession) => {
-  setTimeout(afficheModaleDeconnexion, dureeSession, '.rideau#deconnexion', '.rideau');
+  localStorage.setItem(HEURE_DECONNEXION, new Date(Date.now() + dureeSession).valueOf());
+  setTimeout(lanceDecompteAffichage, dureeSession);
 };
 
 export default lanceDecompteDeconnexion;
