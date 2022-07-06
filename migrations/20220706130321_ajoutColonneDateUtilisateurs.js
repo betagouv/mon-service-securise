ca604a4e92ec7474c90314c37cb2c3ee8007a869
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-07-06 16:15:43 +0200

    [MIGRATION] Ajoute une colonne date de création à utilisateurs

---
+++ migrations/20220706130321_ajoutColonneDateUtilisateurs.js
@@ -0,0 +1,7 @@
+exports.up = (knex) => knex.schema.alterTable('utilisateurs', (table) => {
+  table.datetime('date_creation').defaultTo(knex.fn.now());
+});
+
+exports.down = (knex) => knex.schema.alterTable('utilisateurs', (table) => {
+  table.dropColumn('date_creation');
+});
