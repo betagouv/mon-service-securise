1e8db7a2aeb6291734d849c1a3fbe0eac7924d47
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2022-07-15 10:35:01 +0200

    Retourne une erreur HTTP 404 (not found) si bibliothèque inconnue

--- src/routes/routesBibliotheques.js
+++ src/routes/routesBibliotheques.js
@@ -1,17 +1,24 @@
 const axios = require('axios');
 const express = require('express');
 
+const CHEMINS_BIBLIOTHEQUES = {
+  'axios.min.js': 'https://unpkg.com/axios/dist/axios.min.js',
+  'jquery-3.6.0.min.js': 'https://code.jquery.com/jquery-3.6.0.min.js',
+};
+
 const routesBibliotheques = () => {
   const routes = express.Router();
 
-  routes.get('/jquery-3.6.0.min.js', (_requete, reponse) => {
-    axios.get('https://code.jquery.com/jquery-3.6.0.min.js')
-      .then((reponseJquery) => reponse.type('text/javascript').send(reponseJquery.data));
-  });
+  routes.get('/:nomBibliotheque', (requete, reponse) => {
+    const { nomBibliotheque } = requete.params;
+    const chemin = CHEMINS_BIBLIOTHEQUES[nomBibliotheque];
 
-  routes.get('/axios.min.js', (_requete, reponse) => {
-    axios.get('https://unpkg.com/axios/dist/axios.min.js')
-      .then((reponseAxios) => reponse.type('text/javascript').send(reponseAxios.data));
+    if (chemin) {
+      axios.get(CHEMINS_BIBLIOTHEQUES[requete.params.nomBibliotheque])
+        .then((reponseDepot) => reponse.type('text/javascript').send(reponseDepot.data));
+    } else {
+      reponse.status(404).send(`Bibliothèque inconnue : ${nomBibliotheque}`);
+    }
   });
 
   return routes;
---
+++ test/routes/routesBibliotheques.spec.js
@@ -0,0 +1,16 @@
+const testeurMSS = require('./testeurMSS');
+
+describe('Le serveur MSS des routes /bibliotheques/*', () => {
+  const testeur = testeurMSS();
+
+  beforeEach(testeur.initialise);
+
+  afterEach(testeur.arrete);
+
+  it('retourne une erreur HTTP 404 si la bibliothèque est inconnue', (done) => {
+    testeur.verifieRequeteGenereErreurHTTP(404, 'Bibliothèque inconnue : bibliothequeInconnue.js', {
+      method: 'get',
+      url: 'http://localhost:1234/bibliotheques/bibliothequeInconnue.js',
+    }, done);
+  });
+});
