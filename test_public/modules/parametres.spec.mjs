7dc4a9f38d3218292253acbe714aa9618bb6fa58
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2022-08-01 17:03:22 +0200

    [CORRECTION] Orthographie correctement « champ » au singulier

--- test_public/modules/parametres.spec.mjs
+++ test_public/modules/parametres.spec.mjs
@@ -5,7 +5,7 @@ import parametres from '../../public/modules/parametres.mjs';
 
 describe('Les paramètres', () => {
   before(() => {
-    const sourcePage = '<form class="formulaire"><input name="champs-1" value="valeur 1"></form>';
+    const sourcePage = '<form class="formulaire"><input name="champ-1" value="valeur 1"></form>';
     const dom = new JSDOM(sourcePage);
     global.$ = jquery(dom.window);
   });
@@ -13,7 +13,7 @@ describe('Les paramètres', () => {
   it('renvoient les données du formulaire', () => {
     const parametresFormulaire = parametres('.formulaire');
 
-    expect(parametresFormulaire).to.have.key('champs-1');
-    expect(parametresFormulaire['champs-1']).to.be('valeur 1');
+    expect(parametresFormulaire).to.have.key('champ-1');
+    expect(parametresFormulaire['champ-1']).to.be('valeur 1');
   });
 });
