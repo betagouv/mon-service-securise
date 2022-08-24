1a578ba194ccf8c9b9f607cd976f9d202b5d3604
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-08-24 14:32:57 +0200

    Affiche un message d'erreur quand l'envoi de l'e-mail a échoué

--- public/inscription.js
+++ public/inscription.js
@@ -1,9 +1,21 @@
 import brancheSoumissionFormulaireUtilisateur from './modules/interactions/brancheSoumissionFormulaireUtilisateur.js';
+import { brancheValidationPersonnalisee, declencheErreurPersonnalisee } from './modules/interactions/validation.js';
+
+const TYPE_ERREUR_ENVOI_EMAIL = 'ERREUR_ENVOI_EMAIL';
+const MESSAGE_ERREUR_ENVOI_EMAIL = "Cet e-mail n'existe pas ou est introuvable. Veuillez en saisir un autre ou nous contacter.";
 
 $(() => {
   const selecteurFormulaire = 'form#inscription';
-  const action = (donnees) => axios.post('/api/utilisateur', donnees)
-    .then(() => (window.location = '/activation'));
+  const selecteurChampEmail = '#email';
 
+  const action = (donnees) => axios.post('/api/utilisateur', donnees)
+    .then(() => (window.location = '/activation'))
+    .catch(({ response }) => {
+      if (response.status === 424 && response.data.type === TYPE_ERREUR_ENVOI_EMAIL) {
+        declencheErreurPersonnalisee(selecteurChampEmail, MESSAGE_ERREUR_ENVOI_EMAIL);
+      }
+    });
+
+  brancheValidationPersonnalisee(selecteurChampEmail);
   brancheSoumissionFormulaireUtilisateur(selecteurFormulaire, action);
 });
