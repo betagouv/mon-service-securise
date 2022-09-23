6259b2f5aba01087268f31e3b30c98f7047efbea
Jacques Raguin <jacques.raguin@codeworks.fr>
2022-09-23 17:08:14 +0200

    Ajoute le graphique des mesures recommandées à la synthèse

--- public/homologation/syntheseSecurite.js
+++ public/homologation/syntheseSecurite.js
@@ -3,6 +3,7 @@ import graphiqueCamembert from '../modules/graphiqueCamembert.js';
 
 $(() => {
   graphiqueCamembert('indispensables', '.indispensables');
+  graphiqueCamembert('recommandees', '.recommandees');
 
   $('#telecharger').on('click', telechargementPdf('syntheseSecurite.pdf'));
 });
