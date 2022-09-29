a3f43350b04b100c76c23ed05c656e4d7e79db18
Colin Garriga-Salaün <colin.garriga@gmail.com>
2022-09-29 16:13:17 +0200

    L'interface web ignore la criticité élevée éventuellement indiquée par le serveur

--- test_public/modules/soumetsHomologation.spec.mjs
+++ test_public/modules/soumetsHomologation.spec.mjs
@@ -2,7 +2,6 @@ import expect from 'expect.js';
 import { JSDOM } from 'jsdom';
 import jquery from 'jquery';
 import initialiseComportementFormulaire from '../../public/modules/soumetsHomologation.mjs';
-import ErreurSeuilCriticiteTropEleve from '../../public/modules/erreurs.mjs';
 
 describe("L'initialisation du comportement du formulaire", () => {
   describe('après la soumission du formulaire', () => {
@@ -12,7 +11,6 @@ describe("L'initialisation du comportement du formulaire", () => {
 
     beforeEach(() => {
       const dom = new JSDOM(`
-        <div class="rideau"></<div>
         <form class="formulaire">
           <input name="champ-1" value="valeur 1">
           <div class="bouton"></div>
@@ -23,75 +21,54 @@ describe("L'initialisation du comportement du formulaire", () => {
       global.window = { location: '/' };
     });
 
-    it('affiche la modale quand le seuil de criticité est critique', (done) => {
-      const adaptateurAjax = {
-        verifieSeuilCriticite: () => Promise.reject(new ErreurSeuilCriticiteTropEleve('Seuil de criticité critique')),
+    let ajaxRequete;
+    const adaptateurAjax = {};
+
+    beforeEach(() => {
+      adaptateurAjax.execute = (requete) => {
+        ajaxRequete = requete;
+        return Promise.resolve({ data: { idHomologation: '123' } });
       };
+    });
 
+    it("envoie au serveur les données de l'homologation à créer", (done) => {
       const evenementsDifferes = $.Deferred();
-      $('.rideau').on('afficheModale', () => {
-        evenementsDifferes.resolve();
-      });
+      initialiseComportementFormulaire('.formulaire', '.bouton', fonctionExtractionParametres, adaptateurAjax);
 
-      initialiseComportementFormulaire('.formulaire', '.bouton', '.rideau', fonctionExtractionParametres, adaptateurAjax);
-      $('.bouton').trigger('click');
-
-      evenementsDifferes.promise()
+      evenementsDifferes.resolveWith($('.bouton').trigger('click'))
+        .then(() => {
+          expect(ajaxRequete.method).to.equal('post');
+          expect(ajaxRequete.url).to.equal('/api/homologation');
+          expect(ajaxRequete.data['champ-1']).to.equal('valeur 1');
+        })
         .then(() => done())
         .catch(done);
     });
 
-    describe("quand le seuil de criticité n'est pas critique", () => {
-      let ajaxRequete;
-      const adaptateurAjax = {};
+    it("envoie au serveur les données de l'homologation à modifier", (done) => {
+      const evenementsDifferes = $.Deferred();
+      $('.bouton').attr('idHomologation', '12345');
 
-      beforeEach(() => {
-        adaptateurAjax.verifieSeuilCriticite = () => Promise.resolve();
-        adaptateurAjax.execute = (requete) => {
-          ajaxRequete = requete;
-          return Promise.resolve({ data: { idHomologation: '123' } });
-        };
-      });
+      initialiseComportementFormulaire('.formulaire', '.bouton', fonctionExtractionParametres, adaptateurAjax);
 
-      it("envoie au serveur les données de l'homologation à créer", (done) => {
-        const evenementsDifferes = $.Deferred();
-        initialiseComportementFormulaire('.formulaire', '.bouton', '.rideau', fonctionExtractionParametres, adaptateurAjax);
+      evenementsDifferes.resolveWith($('.bouton').trigger('click'))
+        .then(() => {
+          expect(ajaxRequete.method).to.equal('put');
+          expect(ajaxRequete.url).to.equal('/api/homologation/12345');
+          expect(ajaxRequete.data['champ-1']).to.equal('valeur 1');
+        })
+        .then(() => done())
+        .catch(done);
+    });
 
-        evenementsDifferes.resolveWith($('.bouton').trigger('click'))
-          .then(() => {
-            expect(ajaxRequete.method).to.equal('post');
-            expect(ajaxRequete.url).to.equal('/api/homologation');
-            expect(ajaxRequete.data['champ-1']).to.equal('valeur 1');
-          })
-          .then(() => done())
-          .catch(done);
-      });
+    it("renvoie vers la synthèse de l'homologation", (done) => {
+      const evenementsDifferes = $.Deferred();
+      initialiseComportementFormulaire('.formulaire', '.bouton', fonctionExtractionParametres, adaptateurAjax);
 
-      it("envoie au serveur les données de l'homologation à modifier", (done) => {
-        const evenementsDifferes = $.Deferred();
-        $('.bouton').attr('idHomologation', '12345');
-
-        initialiseComportementFormulaire('.formulaire', '.bouton', '.rideau', fonctionExtractionParametres, adaptateurAjax);
-
-        evenementsDifferes.resolveWith($('.bouton').trigger('click'))
-          .then(() => {
-            expect(ajaxRequete.method).to.equal('put');
-            expect(ajaxRequete.url).to.equal('/api/homologation/12345');
-            expect(ajaxRequete.data['champ-1']).to.equal('valeur 1');
-          })
-          .then(() => done())
-          .catch(done);
-      });
-
-      it("renvoie vers la synthèse de l'homologation", (done) => {
-        const evenementsDifferes = $.Deferred();
-        initialiseComportementFormulaire('.formulaire', '.bouton', '.rideau', fonctionExtractionParametres, adaptateurAjax);
-
-        evenementsDifferes.resolveWith($('.bouton').trigger('click'))
-          .then(() => expect(window.location).to.equal('/homologation/123'))
-          .then(() => done())
-          .catch(done);
-      });
+      evenementsDifferes.resolveWith($('.bouton').trigger('click'))
+        .then(() => expect(window.location).to.equal('/homologation/123'))
+        .then(() => done())
+        .catch(done);
     });
   });
 });
