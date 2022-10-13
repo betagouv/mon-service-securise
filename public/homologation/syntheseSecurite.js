a527263b63f1fa295c674783620a194ed00267c1
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2022-10-13 16:20:30 +0200

    Dimensionne chaque statut en fonction de son poids

--- public/homologation/syntheseSecurite.js
+++ public/homologation/syntheseSecurite.js
@@ -1,5 +1,12 @@
 import telechargementPdf from '../modules/interactions/telechargementPdf.js';
 
+const brancheDimensionnementStatuts = () => {
+  $('.statistiques-par-categorie .statut')
+    .each((_, statut) => $(statut).css('flex', $(statut).text()));
+};
+
 $(() => {
   $('#telecharger').on('click', telechargementPdf('syntheseSecurite.pdf'));
+
+  brancheDimensionnementStatuts();
 });
