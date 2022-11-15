6006ba234c97dc8c11b8d52f2c2bbc9a703acc7f
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2022-11-15 15:26:36 +0100

    Supprime « Diffusion restreinte » des données à caractère personnel

---
+++ migrations/20221115150500_suppressionDiffusionRestreinte.js
@@ -0,0 +1,21 @@
+// Il s'agit de supprimer la valeur 'diffusionRestreinte' dans les
+// 'donneesCaracterePersonnel' de la description des services.
+// Car 'Diffusion restreinte' est supprimée du référentiel.
+
+exports.up = (knex) => knex('homologations')
+  .then((lignes) => {
+    const misesAJour = lignes
+      .filter(({ donnees }) => donnees.descriptionService?.donneesCaracterePersonnel)
+      .map(({ id, donnees }) => {
+        donnees.descriptionService.donneesCaracterePersonnel = donnees
+          .descriptionService
+          .donneesCaracterePersonnel
+          .filter((d) => d !== 'diffusionRestreinte');
+
+        return knex('homologations').where({ id }).update({ donnees });
+      });
+
+    return Promise.all(misesAJour);
+  });
+
+exports.down = () => Promise.resolve();
