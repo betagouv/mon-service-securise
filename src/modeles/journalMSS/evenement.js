50a9cc1a563cb23b1efbe5f73bed4e9c8a6e51cc
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2022-11-17 17:09:19 +0100

    Horodate les événements

--- src/modeles/journalMSS/evenement.js
+++ src/modeles/journalMSS/evenement.js
@@ -1,10 +1,14 @@
 class Evenement {
-  constructor(type) {
+  constructor(type, date = Date.now()) {
     this.type = type;
+    this.date = date;
   }
 
   toJSON() {
-    return { type: this.type };
+    return {
+      type: this.type,
+      date: this.date,
+    };
   }
 }
 
--- test/modeles/journalMSS/evenement.spec.js
+++ test/modeles/journalMSS/evenement.spec.js
@@ -4,8 +4,8 @@ const Evenement = require('../../../src/modeles/journalMSS/evenement');
 
 describe('Un événement du journal MSS', () => {
   it('sait se convertir en JSON', () => {
-    const evenement = new Evenement('TEST_EXECUTE');
+    const evenement = new Evenement('TEST_EXECUTE', '17/11/2022');
 
-    expect(evenement.toJSON()).to.eql({ type: 'TEST_EXECUTE' });
+    expect(evenement.toJSON()).to.eql({ type: 'TEST_EXECUTE', date: '17/11/2022' });
   });
 });
