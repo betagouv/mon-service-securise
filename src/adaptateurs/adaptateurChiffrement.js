67737acae848c5007b45f6aade8809c71c8f212a
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2022-11-29 17:23:50 +0100

    Chiffre identifiant utilisateur dans événement

--- src/adaptateurs/adaptateurChiffrement.js
+++ src/adaptateurs/adaptateurChiffrement.js
@@ -1,10 +1,15 @@
+const { createHash } = require('crypto');
 const bcrypt = require('bcrypt');
 
 const NOMBRE_DE_PASSES = 10;
 
 const chiffre = (chaineEnClair) => bcrypt.hash(chaineEnClair, NOMBRE_DE_PASSES);
+
 const { compare } = bcrypt;
+
+const hacheSha256 = (chaine) => createHash('sha256').update(chaine).digest('hex');
+
 const nonce = () => chiffre(`${Math.random()}`)
   .then((s) => s.replace(/[/$.]/g, ''));
 
-module.exports = { chiffre, compare, nonce };
+module.exports = { chiffre, compare, hacheSha256, nonce };
