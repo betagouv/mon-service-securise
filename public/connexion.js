4f6dc28a9c2c0f7ce4aa0ce07fed3ff3b0974ee8
Colin Garriga-Salaün <colin@yacto.fr>
2022-12-07 15:13:09 +0100

    Affiche un message d'erreur lorsque la connexion échoue

--- public/connexion.js
+++ public/connexion.js
@@ -1,9 +1,27 @@
 $(() => {
+  const $inputs = $('input');
+  const $erreur = $('.message-erreur');
   const $bouton = $('.bouton');
-  $bouton.click(() => {
+
+  const rendsAffichageNeutre = () => {
+    $erreur.hide();
+  };
+  const afficheAlerte = () => {
+    $erreur.show();
+  };
+  rendsAffichageNeutre();
+  $inputs.addClass('intouche');
+  $inputs.on('input', rendsAffichageNeutre);
+
+  $bouton.on('click', () => {
     const login = $('#login').val();
     const motDePasse = $('#mot-de-passe').val();
     axios.post('/api/token', { login, motDePasse })
-      .then(() => (window.location = '/espacePersonnel'));
+      .then(() => (window.location = '/espacePersonnel'))
+      .catch((error) => {
+        if (error.response.status === 401) {
+          afficheAlerte();
+        }
+      });
   });
 });
