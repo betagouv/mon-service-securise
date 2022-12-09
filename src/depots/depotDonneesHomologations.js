29937d3922fd988a035465bd8fd939b2f132d4d6
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2022-12-09 13:03:51 +0100

    Persiste dossier courant

--- src/depots/depotDonneesHomologations.js
+++ src/depots/depotDonneesHomologations.js
@@ -1,7 +1,9 @@
 const {
+  ErreurHomologationInexistante,
   ErreurNomServiceDejaExistant,
   ErreurNomServiceManquant,
 } = require('../erreurs');
+const Dossier = require('../modeles/dossier');
 const Homologation = require('../modeles/homologation');
 const { EvenementNouveauServiceCree } = require('../modeles/journalMSS/evenements');
 
@@ -73,6 +75,24 @@ const creeDepot = (config = {}) => {
       })
   );
 
+  const ajouteDossierCourantSiNecessaire = (idHomologation) => homologation(idHomologation)
+    .then((h) => {
+      if (typeof h === 'undefined') {
+        return Promise.reject(new ErreurHomologationInexistante(
+          `Homologation "${idHomologation}" non trouvée`
+        ));
+      }
+
+      if (h.nombreDossiers() === 0) {
+        const idDossier = adaptateurUUID.genereUUID();
+        const dossier = new Dossier({ id: idDossier });
+        return ajouteAItemsDansHomologation('dossiers', idHomologation, dossier)
+          .then(() => dossier);
+      }
+
+      return Promise.resolve(h.dossierCourant());
+    });
+
   const ajouteMesureGeneraleAHomologation = (...params) => (
     ajouteAItemsDansHomologation('mesuresGenerales', ...params)
   );
@@ -132,6 +152,15 @@ const creeDepot = (config = {}) => {
       .then((toutesHomologations) => toutesHomologations.flatMap((h) => h));
   };
 
+  const metsAJourDossierCourant = (idHomologation, dossier) => (
+    ajouteDossierCourantSiNecessaire(idHomologation)
+      .then((d) => {
+        const donneesDossier = { ...d.toJSON(), ...dossier.toJSON() };
+        const dossierMisAJour = new Dossier(donneesDossier, referentiel);
+        return ajouteAItemsDansHomologation('dossiers', idHomologation, dossierMisAJour);
+      })
+  );
+
   const nouvelleHomologation = (idUtilisateur, donneesDescriptionService) => {
     const idHomologation = adaptateurUUID.genereUUID();
     const idAutorisation = adaptateurUUID.genereUUID();
@@ -172,6 +201,7 @@ const creeDepot = (config = {}) => {
   return {
     ajouteAvisExpertCyberAHomologation,
     ajouteDescriptionServiceAHomologation,
+    ajouteDossierCourantSiNecessaire,
     ajouteMesureGeneraleAHomologation,
     ajouteRisqueGeneralAHomologation,
     ajouteRolesResponsabilitesAHomologation,
@@ -179,6 +209,7 @@ const creeDepot = (config = {}) => {
     homologationExiste,
     homologations,
     homologationsCreeesAvantLe,
+    metsAJourDossierCourant,
     nouvelleHomologation,
     remplaceMesuresSpecifiquesPourHomologation,
     remplaceRisquesSpecifiquesPourHomologation,
