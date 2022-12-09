275e67a99f68f22f96eb39e00d75f0b5cde384a5
Jacques Raguin <jacques.raguin@beta.gouv.fr>
2022-12-09 16:32:56 +0100

    Utilise pdf-lib pour générer les annexes mesures et risques

--- test/routes/routesPdf.spec.js
+++ test/routes/routesPdf.spec.js
@@ -101,4 +101,36 @@ describe('Le serveur MSS des routes /pdf/*', () => {
         .catch(done);
     });
   });
+
+  describe('quand requête GET sur `/pdf/:id/annexes.pdf`', () => {
+    beforeEach(() => {
+      testeur.adaptateurPdf().genereAnnexes = () => Promise.resolve('Pdf annexes');
+    });
+
+    it("recherche l'homologation correspondante", (done) => {
+      testeur.middleware().verifieRechercheHomologation(
+        'http://localhost:1234/pdf/456/annexes.pdf',
+        done,
+      );
+    });
+
+    it('sert un fichier de type pdf', (done) => {
+      verifieTypeFichierServiEstPDF('http://localhost:1234/pdf/456/annexes.pdf', done);
+    });
+
+    it('utilise un adaptateur de pdf pour la génération', (done) => {
+      let adaptateurPdfAppele = false;
+      testeur.adaptateurPdf().genereAnnexes = () => {
+        adaptateurPdfAppele = true;
+        return Promise.resolve('Pdf annexes');
+      };
+
+      axios.get('http://localhost:1234/pdf/456/annexes.pdf')
+        .then(() => {
+          expect(adaptateurPdfAppele).to.be(true);
+          done();
+        })
+        .catch(done);
+    });
+  });
 });
