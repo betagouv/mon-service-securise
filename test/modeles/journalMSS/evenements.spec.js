b610d8047cc1c7af7d38503ee8b1f4d84f941298
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2022-12-16 12:02:02 +0100

    Crée événement « Complétude de service modifiée »

--- test/modeles/journalMSS/evenements.spec.js
+++ test/modeles/journalMSS/evenements.spec.js
@@ -1,10 +1,15 @@
 const expect = require('expect.js');
-const { EvenementNouveauServiceCree } = require('../../../src/modeles/journalMSS/evenements');
-const { ErreurIdentifiantServiceManquant, ErreurIdentifiantUtilisateurManquant } = require('../../../src/modeles/journalMSS/erreurs');
+const { EvenementNouveauServiceCree, EvenementCompletudeServiceModifiee } = require('../../../src/modeles/journalMSS/evenements');
+const {
+  ErreurIdentifiantServiceManquant,
+  ErreurIdentifiantUtilisateurManquant,
+  ErreurNombreTotalMesuresManquant,
+  ErreurNombreMesuresCompletesManquant,
+} = require('../../../src/modeles/journalMSS/erreurs');
+
+const hacheEnMajuscules = { hacheSha256: (valeur) => valeur?.toUpperCase() };
 
 describe('Un événement de nouveau service créé', () => {
-  const hacheEnMajuscules = { hacheSha256: (valeur) => valeur?.toUpperCase() };
-
   it("chiffre l'identifiant du service qui lui est donné", () => {
     const evenement = new EvenementNouveauServiceCree(
       { idService: 'abc', idUtilisateur: 'def' },
@@ -59,6 +64,76 @@ describe('Un événement de nouveau service créé', () => {
       done(Error("L'instanciation de l'événement aurait dû lever une exception"));
     } catch (e) {
       expect(e).to.be.an(ErreurIdentifiantServiceManquant);
+      done();
+    }
+  });
+});
+
+describe('Un événement de complétude modifiée', () => {
+  it("chiffre l'identifiant du service qui lui est donné", () => {
+    const evenement = new EvenementCompletudeServiceModifiee(
+      { idService: 'abc', nombreTotalMesures: 54, nombreMesuresCompletes: 38 },
+      { adaptateurChiffrement: hacheEnMajuscules }
+    );
+
+    expect(evenement.toJSON().donnees.idService).to.be('ABC');
+  });
+
+  it('sait se convertir en JSON', () => {
+    const evenement = new EvenementCompletudeServiceModifiee(
+      { idService: 'abc', nombreTotalMesures: 54, nombreMesuresCompletes: 38 },
+      { date: '17/11/2022', adaptateurChiffrement: hacheEnMajuscules }
+    );
+
+    expect(evenement.toJSON()).to.eql({
+      type: 'COMPLETUDE_SERVICE_MODIFIEE',
+      donnees: {
+        idService: 'ABC',
+        nombreTotalMesures: 54,
+        nombreMesuresCompletes: 38,
+      },
+      date: '17/11/2022',
+    });
+  });
+
+  it("exige que l'identifiant du service soit renseigné", (done) => {
+    try {
+      new EvenementCompletudeServiceModifiee(
+        { nombreTotalMesures: 54, nombreMesuresCompletes: 38 },
+        { adaptateurChiffrement: hacheEnMajuscules }
+      );
+
+      done(Error("L'instanciation de l'événement aurait dû lever une exception"));
+    } catch (e) {
+      expect(e).to.be.an(ErreurIdentifiantServiceManquant);
+      done();
+    }
+  });
+
+  it('exige que le nombre total de mesures soit renseigné', (done) => {
+    try {
+      new EvenementCompletudeServiceModifiee(
+        { idService: 'abc', nombreMesuresCompletes: 38 },
+        { adaptateurChiffrement: hacheEnMajuscules }
+      );
+
+      done(Error("L'instanciation de l'événement aurait dû lever une exception"));
+    } catch (e) {
+      expect(e).to.be.an(ErreurNombreTotalMesuresManquant);
+      done();
+    }
+  });
+
+  it('exige que le nombre de mesures complètes soit renseigné', (done) => {
+    try {
+      new EvenementCompletudeServiceModifiee(
+        { idService: 'abc', nombreTotalMesures: 54 },
+        { adaptateurChiffrement: hacheEnMajuscules }
+      );
+
+      done(Error("L'instanciation de l'événement aurait dû lever une exception"));
+    } catch (e) {
+      expect(e).to.be.an(ErreurNombreMesuresCompletesManquant);
       done();
     }
   });
