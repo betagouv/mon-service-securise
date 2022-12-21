c911ae611b5306589f707299c49b3160f04ce684
Jacques Raguin <jacques.raguin@beta.gouv.fr>
2022-12-21 10:31:28 +0100

    Crée un mécanisme de validation pour les cases à cocher

---
+++ public/modules/interactions/brancheValidationCasesACocher.mjs
@@ -0,0 +1,22 @@
+const brancheValidationGroupeCasesACocher = ($groupeCasesACocher) => {
+  const $toutesCasesACocher = $('input:checkbox', $groupeCasesACocher);
+
+  const verifieEtatCasesACocher = () => {
+    const messageErreur = ($toutesCasesACocher.filter(':checked').length === 0) ? 'Erreur de saisie' : '';
+    $toutesCasesACocher.each((_, caseACocher) => {
+      caseACocher.setCustomValidity(messageErreur);
+      caseACocher.reportValidity();
+    });
+  };
+
+  verifieEtatCasesACocher();
+  $toutesCasesACocher.on('change', () => verifieEtatCasesACocher());
+};
+
+const brancheValidationCasesACocher = () => {
+  $('fieldset.casesACocher[required]').each((_, $groupeCasesACocher) => {
+    brancheValidationGroupeCasesACocher($groupeCasesACocher);
+  });
+};
+
+export default brancheValidationCasesACocher;
