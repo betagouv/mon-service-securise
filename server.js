2cc8ad7f76d2af8a5bbaacb02e92b2331bc1868c
Colin Garriga-Salaün <colin@yacto.fr>
2023-01-06 21:59:35 +0100

    [NETTOYAGE] Supprime l'adaptateur SMTP inutilisé

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const adaptateurChiffrement = require('./src/adaptateurs/adaptateurChiffrement')
 const adaptateurEnvironnement = require('./src/adaptateurs/adaptateurEnvironnement');
 const adaptateurEquations = require('./src/adaptateurs/adaptateurEquations');
 const adaptateurJWT = require('./src/adaptateurs/adaptateurJWT');
-const adaptateurMailSmtp = require('./src/adaptateurs/adaptateurMailSmtp');
-const adaptateurMailSendinblue = require('./src/adaptateurs/adaptateurMailSendinblue');
+const adaptateurMail = require('./src/adaptateurs/adaptateurMailSendinblue');
 const adaptateurPdfLatex = require('./src/adaptateurs/adaptateurPdfLatex');
 
 const port = process.env.PORT || 3000;
@@ -22,8 +21,6 @@ const middleware = Middleware({
   login: process.env.LOGIN_ADMIN,
   motDePasse: process.env.MOT_DE_PASSE_ADMIN,
 });
-const adaptateurMail = process.env.AVEC_MAIL_VIA_SENDINBLUE
-  ? adaptateurMailSendinblue : adaptateurMailSmtp;
 
 const serveur = MSS.creeServeur(
   depotDonnees,
