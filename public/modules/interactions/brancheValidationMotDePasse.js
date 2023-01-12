f4fcccecce26015d28f847500a749356ed40dd3e
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2023-01-12 13:33:36 +0100

    Vérifie la double saisie du mot de passe

---
+++ public/modules/interactions/brancheValidationMotDePasse.js
@@ -0,0 +1,14 @@
+$(() => {
+  const $champMdp = () => $('#mot-de-passe');
+  const $confirmationMdp = () => $('#mot-de-passe-confirmation');
+
+  const verifieMotDePasse = () => {
+    const erreur = $champMdp().val() !== $confirmationMdp().val()
+      ? 'erreurDoubleSaisieAvecDifference'
+      : '';
+    $confirmationMdp().get(0).setCustomValidity(erreur);
+  };
+
+  $champMdp().on('input', verifieMotDePasse);
+  $confirmationMdp().on('input', verifieMotDePasse);
+});
