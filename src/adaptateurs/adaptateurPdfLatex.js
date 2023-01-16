f84a068841e423eb02c9f40df2a06c0e20881364
Jacques Raguin <jacques.raguin@beta.gouv.fr>
2023-01-16 17:50:35 +0100

    Améliore la lisibilité du code de génération des annexes

--- src/adaptateurs/adaptateurPdfLatex.js
+++ src/adaptateurs/adaptateurPdfLatex.js
@@ -16,31 +16,29 @@ const generationPdfLatex = (cheminFichierTex, donnees = {}) => fsPromises
     return pdflatex(texConfectionne);
   });
 
+const ajoutePdf = (accumulateurPdf, pdfAAjouter) => accumulateurPdf
+  .then((pdfFusion) => pdfAAjouter
+    .then((pdfAnnexe) => PDFDocument.load(pdfAnnexe))
+    .then((pdf) => {
+      pdfFusion.copyPages(pdf, pdf.getPageIndices()).then((pagesCopiees) => {
+        pagesCopiees.forEach((page) => pdfFusion.addPage(page));
+      });
+      return pdfFusion;
+    }));
+
 const genereAnnexeDescription = (donnees) => generationPdfLatex('src/vuesTex/annexeDescription.template.tex', donnees);
 
 const genereAnnexeMesures = (donnees) => generationPdfLatex('src/vuesTex/annexeMesures.template.tex', donnees);
 
 const genereAnnexeRisques = (donnees) => generationPdfLatex('src/vuesTex/annexeRisques.template.tex', donnees);
 
-const genereAnnexes = (donneesMesures, donneesRisques) => {
-  const ajoutePdf = (accumulateurPdf, pdfAAjouter) => accumulateurPdf
-    .then((pdfFusion) => pdfAAjouter.then((pdfAnnexe) => ({ pdfAnnexe, pdfFusion })))
-    .then(({ pdfAnnexe, pdfFusion }) => (
-      PDFDocument.load(pdfAnnexe).then((pdf) => ({ pdf, pdfFusion }))
-    ))
-    .then(({ pdf, pdfFusion }) => {
-      pdfFusion.copyPages(pdf, pdf.getPageIndices()).then((pagesCopiees) => {
-        pagesCopiees.forEach((page) => pdfFusion.addPage(page));
-      });
-      return pdfFusion;
-    });
-
-  return ajoutePdf(PDFDocument.create(), genereAnnexeMesures(donneesMesures))
+const genereAnnexes = ({ donneesMesures, donneesRisques }) => (
+  ajoutePdf(PDFDocument.create(), genereAnnexeMesures(donneesMesures))
     .then((pdfFusion) => ajoutePdf(
       Promise.resolve(pdfFusion), genereAnnexeRisques(donneesRisques)
     ))
     .then((pdfFusion) => pdfFusion.save())
-    .then((pdf) => Buffer.from(pdf.buffer, 'binary'));
-};
+    .then((pdf) => Buffer.from(pdf.buffer, 'binary'))
+);
 
 module.exports = { genereAnnexes, genereAnnexeDescription };
