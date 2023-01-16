e8d16360949c46bec765287161955fc549fcf4ae
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2023-01-16 19:34:45 +0100

    Valide robustesse du mot de passe
    … avec du code dupliqué.
    Nous aurions aimé mutualiser la logique de validation.
    Mais nous n'y sommes pas parvenus en l'état.
    Le serveur n'est pas un « module » nodejs, donc la syntaxe « import … from … » n'est pas supportée. C'est le support de cette syntaxe qui permettrait une mutualisation.
    Pour le moment, nous préférons dupliquer plutôt que de remanier le serveur en module.

--- public/modules/interactions/brancheValidationMotDePasse.js
+++ public/modules/interactions/brancheValidationMotDePasse.js
@@ -1,3 +1,5 @@
+import { valideMotDePasse } from './validationMotDePasse.mjs';
+
 $(() => {
   const $champMdp = $('#mot-de-passe');
   const $confirmationMdp = $('#mot-de-passe-confirmation');
@@ -9,6 +11,14 @@ $(() => {
     $confirmationMdp.get(0).setCustomValidity(erreur);
   };
 
+  const verifieRobustesse = () => {
+    const motDePasse = $champMdp.val();
+    const resultatValidation = valideMotDePasse(motDePasse);
+
+    $champMdp.get(0).setCustomValidity(resultatValidation);
+  };
+
   $champMdp.on('input', verifieMotDePasse);
+  $champMdp.on('input', verifieRobustesse);
   $confirmationMdp.on('input', verifieMotDePasse);
 });
---
+++ src/validationMotDePasse.js
@@ -0,0 +1,26 @@
+// ⚠️ Code dupliqué
+// La logique de validation du MDP est codée côté fron et côté back.
+// La mutualisation est prévue lorsque le back supportera la syntaxe « import … from »
+
+const resultatValidation = {
+  ERREUR_MOT_DE_PASSE_TROP_COURT: 'erreurMotDePasseTropCourt',
+  ERREUR_PAS_DE_CARACTERE_SPECIAL: 'erreurPasDeCaractereSpecial',
+  ERREUR_PAS_DE_CHIFFRE: 'erreurPasDeChiffre',
+  ERREUR_PAS_DE_MAJUSCULE: 'erreurPasDeMajuscule',
+  ERREUR_PAS_DE_MINUSCULE: 'erreurPasDeMinuscule',
+  MOT_DE_PASSE_VALIDE: '',
+};
+
+const valideMotDePasse = (motDePasse) => {
+  if (motDePasse === '') return resultatValidation.MOT_DE_PASSE_VALIDE;
+
+  if (motDePasse.length < 12) return resultatValidation.ERREUR_MOT_DE_PASSE_TROP_COURT;
+  if (!motDePasse.match(/[A-Z]/)) return resultatValidation.ERREUR_PAS_DE_MAJUSCULE;
+  if (!motDePasse.match(/[a-z]/)) return resultatValidation.ERREUR_PAS_DE_MINUSCULE;
+  if (!motDePasse.match(/[0-9]/)) return resultatValidation.ERREUR_PAS_DE_CHIFFRE;
+  if (!motDePasse.match(/[#?!@$%^&*'+_()[\]-]/)) return resultatValidation.ERREUR_PAS_DE_CARACTERE_SPECIAL;
+
+  return resultatValidation.MOT_DE_PASSE_VALIDE;
+};
+
+module.exports = { valideMotDePasse, resultatValidation };
