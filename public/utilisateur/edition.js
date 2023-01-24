d3828aec882f58c2197bb3c3f37e93bf86222fdb
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2023-01-24 22:07:13 +0100

    Retire formulaire mot de passe du profil
    … car il est désormais affiché sur `/motDePasse/edition`.

--- public/utilisateur/edition.js
+++ public/utilisateur/edition.js
@@ -2,12 +2,8 @@ import brancheSoumissionFormulaireUtilisateur from '../modules/interactions/bran
 
 $(() => {
   const selecteurFormulaire = 'form.utilisateur#edition';
-  const action = (donnees) => {
-    const { motDePasse, cguAcceptees, ...resteDuProfil } = donnees;
-    return axios.put('/api/motDePasse', { motDePasse, cguAcceptees })
-      .then(() => axios.put('/api/utilisateur', resteDuProfil))
-      .then(() => (window.location = '/espacePersonnel'));
-  };
+  const action = (donnees) => axios.put('/api/utilisateur', donnees)
+    .then(() => (window.location = '/espacePersonnel'));
 
   brancheSoumissionFormulaireUtilisateur(selecteurFormulaire, action);
 });
