7dfa9d75791399d033bce46511888bf167a269b7
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2023-02-07 15:35:56 +0100

    [MIGRATION] Supprime `idUtilisateur` des données de la table `homologations`

---
+++ migrations/utilitaires/pourChaqueLigne.js
@@ -0,0 +1,8 @@
+const pourChaqueLigne = (requete, miseAJour) => import('p-map')
+  .then((module) => {
+    const pMap = module.default;
+
+    return requete.then((lignes) => pMap(lignes, miseAJour, { concurrency: 2 }));
+  });
+
+module.exports = pourChaqueLigne;
