994d1fe10cc61b4ebf1c184c0b032afbcdfeae11
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-02-10 12:10:56 +0100

    [REMANIEMENT] Déplace JS de `modaleSuppressionService.pug`
    … afin de raisonner plus facilement sur le code. La modale devient autonome.

---
+++ public/scripts/modaleSuppressionService.js
@@ -0,0 +1,18 @@
+import { initialiseComportementModale } from '../modules/interactions/modale.mjs';
+
+$(() => {
+  const $modaleSuppression = $('.modale-suppression-service');
+  initialiseComportementModale($modaleSuppression);
+
+  let idServiceCourant;
+
+  const $bouton = $('.bouton-suppression-service', $modaleSuppression);
+  $bouton.on('click', () => axios.delete(`/api/service/${idServiceCourant}`)
+    .then(() => window.location.reload()));
+
+  $('.services').on('modaleSuppression', (_evenement, { idService, nomService }) => {
+    idServiceCourant = idService;
+    $('.nom-service', $modaleSuppression).text(nomService);
+    $modaleSuppression.trigger('afficheModale');
+  });
+});
