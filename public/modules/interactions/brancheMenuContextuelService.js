748b0427f4beb59a844887488ed1512b47ec80a2
Jacques Raguin <jacques.raguin@beta.gouv.fr>
2023-02-10 17:15:38 +0100

    Conserve le bouton du menu contextuel gris quand le menu est ouvert

--- public/modules/interactions/brancheMenuContextuelService.js
+++ public/modules/interactions/brancheMenuContextuelService.js
@@ -2,12 +2,13 @@ const brancheMenuContextuelService = ($service) => {
   $('.menu-contextuel-titre', $service).on('click', (evenement) => {
     evenement.preventDefault();
 
-    const $optionsLocales = $('.menu-contextuel-options', $service);
     const $masqueLocal = $('.masque', $service);
-    $('.menu-contextuel-options').not($optionsLocales).addClass('invisible');
+    const $menuContextuelLocal = $('.menu-contextuel', $service);
     $('.masque').not($masqueLocal).addClass('invisible');
-    $optionsLocales.toggleClass('invisible');
+    $('.menu-contextuel').not($menuContextuelLocal).removeClass('menu-contextuel-ouvert');
+
     $masqueLocal.toggleClass('invisible');
+    $menuContextuelLocal.toggleClass('menu-contextuel-ouvert');
 
     $('.masque', $service).on('click', (e) => {
       e.preventDefault();
