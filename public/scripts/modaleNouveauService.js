9b3161e8f5c904333620a7c268ae6f14906cacdb
Jacques Raguin <jacques.raguin@beta.gouv.fr>
2023-02-27 17:02:47 +0100

    Retire le bouton désactivé de la modale de création

---
+++ public/scripts/modaleNouveauService.js
@@ -0,0 +1,18 @@
+import { brancheValidation, declencheValidation } from '../modules/interactions/validation.mjs';
+
+$(() => {
+  const $modale = $('#modale-nouveau-service');
+  const selecteurFormulaire = '#modale-nouveau-service form';
+  const $caseACocher = $('input#attestation', selecteurFormulaire);
+  const $bouton = $('#modale-nouveau-service .bouton.confirmation');
+
+  $modale.on('afficheModale', () => {
+    $caseACocher.prop('checked', false);
+  });
+
+  brancheValidation(selecteurFormulaire);
+
+  $bouton.on('click', () => {
+    declencheValidation(selecteurFormulaire);
+  });
+});
