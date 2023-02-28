20e3251a5d1a19c98a5570de1034b5d42f963a10
ThibaudMZN <thibaud.marazano@gmail.com>
2023-02-28 15:50:59 +0100

    Introduis un constructeur de dossier

---
+++ test/constructeurs/constructeurDossier.js
@@ -0,0 +1,57 @@
+const adaptateurHorlogeParDefaut = require('../../src/adaptateurs/adaptateurHorloge');
+const Dossier = require('../../src/modeles/dossier');
+const Referentiel = require('../../src/referentiel');
+
+class ConstructeurDossierFantaisie {
+  constructor(
+    id = '1',
+    referentiel = Referentiel.creeReferentielVide(),
+    adaptateurHorloge = adaptateurHorlogeParDefaut
+  ) {
+    this.donnees = { id };
+    this.referentiel = referentiel;
+    this.adaptateurHorloge = adaptateurHorloge;
+  }
+
+  sansDecision() {
+    this.donnees.decision = {};
+    return this;
+  }
+
+  avecDateHomologation(date) {
+    this.donnees.decision.dateHomologation = date.toISOString();
+    return this;
+  }
+
+  quiEstComplet() {
+    this.donnees.finalise = true;
+    this.donnees.decision = { dateHomologation: '2023-01-01', dureeValidite: 'unAn' };
+    this.donnees.datesTelechargements = this.referentiel
+      .tousDocumentsHomologation()
+      .reduce((acc, { id }) => ({ ...acc, [id]: new Date() }), {});
+    this.donnees.autorite = { nom: 'Jean Dupond', fonction: 'RSSI' };
+    return this;
+  }
+
+  quiEstActif(depuis = 1) {
+    const debutActif = new Date();
+    debutActif.setDate(debutActif.getDate() - depuis);
+    this.donnees.decision = { dateHomologation: debutActif.toISOString(), dureeValidite: 'unAn' };
+    return this;
+  }
+
+  quiEstExpire() {
+    const tresVieux = new Date();
+    tresVieux.setDate(tresVieux.getDate() - 400);
+    this.donnees.decision = { dateHomologation: tresVieux.toISOString(), dureeValidite: 'unAn' };
+    return this;
+  }
+
+  construit() {
+    return new Dossier(this.donnees, this.referentiel, this.adaptateurHorloge);
+  }
+}
+
+const unDossier = (referentiel, adaptateurHorloge) => new ConstructeurDossierFantaisie('1', referentiel, adaptateurHorloge);
+
+module.exports = { ConstructeurDossierFantaisie, unDossier };
