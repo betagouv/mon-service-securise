cd0d29b70fb698294506f06ac17cb58f4083d842
ThibaudMZN <thibaud.marazano@gmail.com>
2023-03-07 17:20:30 +0100

    Ajoute style annexe description

--- src/adaptateurs/adaptateurPdfHtml.js
+++ src/adaptateurs/adaptateurPdfHtml.js
@@ -1,6 +1,6 @@
 const pug = require('pug');
 
-const genereAnnexes = ({ donneesDescription }) => Promise.resolve(pug.compileFile('src/pdf/modeles/annexes.pug')({ donnees: donneesDescription }));
+const genereAnnexes = ({ donneesDescription }) => Promise.resolve(pug.compileFile('src/pdf/modeles/annexeDescription.pug')({ donnees: donneesDescription }));
 const genereDossierDecision = () => Promise.resolve(pug.compileFile('src/pdf/modeles/dossierDecision.pug')());
 
 module.exports = { genereAnnexes, genereDossierDecision };
