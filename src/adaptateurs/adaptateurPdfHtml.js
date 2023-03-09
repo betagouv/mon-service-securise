d2023ac57f493d6396490ea261553612c323e020
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-03-09 15:43:27 +0100

    Ajoute en-tête et pied-de-page à l'annexe « Description »

--- src/adaptateurs/adaptateurPdfHtml.js
+++ src/adaptateurs/adaptateurPdfHtml.js
@@ -1,20 +1,22 @@
 const pug = require('pug');
 const puppeteer = require('puppeteer');
 
-const formatPdfA4 = () => ({
+const formatPdfA4 = (enteteHtml, piedPageHtml) => ({
   format: 'A4',
   printBackground: true,
-  displayHeaderFooter: false,
+  displayHeaderFooter: true,
+  headerTemplate: enteteHtml,
+  footerTemplate: piedPageHtml,
   margin: { bottom: '2cm', left: '1cm', right: '1cm', top: '23mm' },
 });
 
-const generePdf = async (contenuHtml) => {
+const generePdf = async (contenuHtml, enteteHtml, piedPageHtml) => {
   let browser = null;
   try {
     browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.setContent(contenuHtml);
-    return await page.pdf(formatPdfA4());
+    return await page.pdf(formatPdfA4(enteteHtml, piedPageHtml));
   } finally {
     if (browser !== null) await browser.close();
   }
@@ -26,18 +28,23 @@ const genereAnnexes = async ({
   donneesRisques,
   referentiel,
 }) => {
-  const genereAnnexesHtml = pug.compileFile('src/pdf/modeles/annexes.pug');
-  const annexesHtml = genereAnnexesHtml(
-    { donneesDescription, donneesMesures, donneesRisques, referentiel }
+  const genereAnnexesHtml = pug.compileFile('src/pdf/modeles/annexeDescription.pug');
+  const genereEntete = pug.compileFile('src/pdf/modeles/annexeDescription.entete.pug');
+  const generePiedPage = pug.compileFile('src/pdf/modeles/annexeDescription.piedpage.pug');
+
+  const pdf = await generePdf(
+    genereAnnexesHtml({ donneesDescription, donneesMesures, donneesRisques, referentiel }),
+    genereEntete(),
+    generePiedPage({ nomService: donneesDescription.nomService })
   );
-  const pdf = await generePdf(annexesHtml);
+
   return pdf;
 };
 
 const genereDossierDecision = async () => {
   const genereDossierHtml = pug.compileFile('src/pdf/modeles/dossierDecision.pug');
   const dossierHtml = genereDossierHtml();
-  const pdf = await generePdf(dossierHtml);
+  const pdf = await generePdf(dossierHtml, '<div></div>', '<div></div>');
   return pdf;
 };
 
