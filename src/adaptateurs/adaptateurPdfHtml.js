b3773b03bce621f736c7075cfb54460b7ca0806c
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-03-09 14:28:11 +0100

    Extrais la fonction de génération de PDF

--- src/adaptateurs/adaptateurPdfHtml.js
+++ src/adaptateurs/adaptateurPdfHtml.js
@@ -1,12 +1,24 @@
 const pug = require('pug');
 const puppeteer = require('puppeteer');
 
-const optionsFormatPdf = () => ({
+const formatPdfA4 = () => ({
   format: 'A4',
   printBackground: true,
   displayHeaderFooter: false,
   margin: { bottom: '2cm', left: '1cm', right: '1cm', top: '23mm' },
 });
+
+const generePdf = async (contenuHtml) => {
+  let browser = null;
+  try {
+    browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    const page = await browser.newPage();
+    await page.setContent(contenuHtml);
+    return await page.pdf(formatPdfA4());
+  } finally {
+    if (browser !== null) await browser.close();
+  }
+};
 
 const genereAnnexes = async ({
   donneesDescription,
@@ -19,27 +31,14 @@ const genereAnnexes = async ({
   const annexesHtml = genereAnnexesHtml(
     { donneesDescription, donneesMesures, donneesRisques, referentiel, nonce }
   );
-
-  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-  const page = await browser.newPage();
-  await page.setContent(annexesHtml);
-  const pdf = await page.pdf(optionsFormatPdf());
-  await browser.close();
-
+  const pdf = await generePdf(annexesHtml);
   return pdf;
 };
 
 const genereDossierDecision = async () => {
   const genereDossierHtml = pug.compileFile('src/pdf/modeles/dossierDecision.pug');
   const dossierHtml = genereDossierHtml();
-
-  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-  const page = await browser.newPage();
-
-  await page.setContent(dossierHtml);
-  const pdf = await page.pdf(optionsFormatPdf());
-  await browser.close();
-
+  const pdf = await generePdf(dossierHtml);
   return pdf;
 };
 
