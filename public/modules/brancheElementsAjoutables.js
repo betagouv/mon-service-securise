af4d8028c568c691f0d48ca7e793b43558348bcf
Jacques Raguin <jacques.raguin@beta.gouv.fr>
2023-03-21 10:23:23 +0100

    Permets l'utilisation d'une zone de saisie personnalisée dans les éléments ajoutables

--- public/modules/brancheElementsAjoutables.js
+++ public/modules/brancheElementsAjoutables.js
@@ -1,7 +1,34 @@
 import { brancheAjoutItem } from './saisieListeItems.js';
 
+const templateZoneSaisieChampsTextes = (index, nomElement, proprietes) => {
+  const label = (valeurLabel, idInputAssocie) => (
+    valeurLabel ? `<label for="${idInputAssocie}">${valeurLabel}</label>` : ''
+  );
+
+  const $inputs = Object
+    .keys(proprietes)
+    .map((cle) => {
+      const id = `${cle}-${nomElement}-${index}`;
+      return $(
+        `${label(proprietes[cle].label, id)}
+          <input
+            id="${id}"
+            name="${id}"
+            type="text"
+            value="${proprietes[cle].valeur || ''}"
+            placeholder="${proprietes[cle].valeurExemple || ''}"
+          >`
+      );
+    });
+  return $(`<div id="element-${nomElement}-${index}"></div>`).append($inputs);
+};
+
 const brancheElementsAjoutables = (
-  identifiantConteneurElements, identifiantElement, proprietesElement
+  identifiantConteneurElements,
+  identifiantElement,
+  proprietesElement,
+  templateZoneSaisie = templateZoneSaisieChampsTextes,
+  actionSurZoneSaisieApresAjout = () => {}
 ) => {
   const indexMax = () => {
     const prefixeIdentifiant = `element-${identifiantElement}`;
@@ -19,34 +46,12 @@ const brancheElementsAjoutables = (
   const selecteurConteneur = `#${identifiantConteneurElements}`;
   const selecteurLienAjout = `#ajout-element-${identifiantElement}`;
 
-  const templateZoneSaisie = (nomElement, index, proprietes) => {
-    const label = (valeurLabel, idInputAssocie) => (
-      valeurLabel ? `<label for="${idInputAssocie}">${valeurLabel}</label>` : ''
-    );
-
-    const $inputs = Object
-      .keys(proprietes)
-      .map((cle) => {
-        const id = `${cle}-${nomElement}-${index}`;
-        return $(
-          `${label(proprietes[cle].label, id)}
-          <input
-            id="${id}"
-            name="${id}"
-            type="text"
-            value="${proprietes[cle].valeur || ''}"
-            placeholder="${proprietes[cle].valeurExemple || ''}"
-          >`
-        );
-      });
-    return $(`<div id="element-${nomElement}-${index}"></div>`).append($inputs);
-  };
-
   brancheAjoutItem(
     selecteurLienAjout,
     selecteurConteneur,
-    (index) => templateZoneSaisie(identifiantElement, index, proprietesElement),
-    () => indexMax()
+    (index) => templateZoneSaisie(index, identifiantElement, proprietesElement),
+    () => indexMax(),
+    actionSurZoneSaisieApresAjout
   );
 };
 
