130cdc8173f801e48044de1257d1d79324e0f710
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-03-30 12:01:39 +0200

    Ajoute méthode dédiée à la finalisation d'un dossier
    … pour encapsuler la consignation d'événement dans le journal MSS.
    
    Co-authored-by: ThibaudMZN <thibaud.marazano@beta.gouv.fr>

--- test/depots/depotDonneesHomologations.spec.js
+++ test/depots/depotDonneesHomologations.spec.js
@@ -1004,6 +1004,66 @@ describe('Le dépôt de données des homologations', () => {
     });
   });
 
+  describe("sur demande de finalisation d'un dossier", () => {
+    let adaptateurUUID;
+    let adaptateurJournalMSS;
+    const referentiel = Referentiel.creeReferentiel({
+      echeancesRenouvellement: { sixMois: { nbMoisDecalage: 6 }, unAn: {} },
+    });
+
+    beforeEach(() => {
+      adaptateurUUID = { genereUUID: () => 'un UUID' };
+      adaptateurJournalMSS = AdaptateurJournalMSSMemoire.nouvelAdaptateur();
+    });
+
+    it('enregistre le dossier passé en paramètre', (done) => {
+      const donneesHomologations = { id: '123', descriptionService: { nomService: 'Un service' } };
+      const adaptateurPersistance = AdaptateurPersistanceMemoire.nouvelAdaptateur({
+        homologations: [copie(donneesHomologations)],
+        services: [copie(donneesHomologations)],
+      });
+      const depot = DepotDonneesHomologations.creeDepot(
+        { adaptateurJournalMSS, adaptateurPersistance, adaptateurUUID, referentiel }
+      );
+
+      const dossier = new Dossier(
+        { id: '999', decision: { dateHomologation: '2022-11-30', dureeValidite: 'sixMois' } },
+        referentiel
+      );
+      depot.finaliseDossier('123', dossier)
+        .then(() => depot.homologation('123'))
+        .then((h) => expect(h.nombreDossiers()).to.equal(1))
+        .then(() => done())
+        .catch(done);
+    });
+
+    it('consigne un événement « Nouvelle homologation créée » dans le journal MSS', (done) => {
+      adaptateurJournalMSS.consigneEvenement = (evenement) => {
+        expect(evenement.type).to.equal('NOUVELLE_HOMOLOGATION_CREEE');
+        expect(evenement.donnees.dateHomologation).to.equal('2022-11-30');
+        expect(evenement.donnees.dureeHomologationMois).to.equal(6);
+        done();
+        return Promise.resolve();
+      };
+
+      const donneesHomologations = { id: '123', descriptionService: { nomService: 'Un service' } };
+      const adaptateurPersistance = AdaptateurPersistanceMemoire.nouvelAdaptateur({
+        homologations: [copie(donneesHomologations)],
+        services: [copie(donneesHomologations)],
+      });
+      const depot = DepotDonneesHomologations.creeDepot(
+        { adaptateurJournalMSS, adaptateurPersistance, adaptateurUUID, referentiel }
+      );
+
+      const dossier = new Dossier(
+        { id: '999', decision: { dateHomologation: '2022-11-30', dureeValidite: 'sixMois' } },
+        referentiel
+      );
+
+      depot.finaliseDossier('123', dossier).catch(done);
+    });
+  });
+
   describe('sur demande de suppression des homologations créées par un utilisateur', () => {
     it("supprime les homologations dont l'utilisateur est le créateur", (done) => {
       const adaptateurPersistance = AdaptateurPersistanceMemoire.nouvelAdaptateur({
