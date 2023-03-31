98a33865b83d942765c38130a09ca522b3e42b06
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-03-31 13:07:32 +0200

    Affiche le filtre par « Organisation responsable »

--- public/espacePersonnel.js
+++ public/espacePersonnel.js
@@ -4,6 +4,26 @@ import brancheComportementPastilles from './modules/interactions/pastilles.js';
 import brancheComportementSaisieContributeur from './modules/interactions/saisieContributeur.js';
 import brancheMenuContextuelService from './modules/interactions/brancheMenuContextuelService.js';
 
+const filtreEspacePersonnel = {
+  brancheFiltres: (toutesOrganisations) => {
+    const organisationsUniques = [...new Set(toutesOrganisations).values()];
+
+    $('#filtre').removeClass('invisible');
+    $('#filtre').selectize({
+      plugins: ['remove_button'],
+      options: organisationsUniques.map((organisation) => ({ valeur: organisation })),
+      valueField: 'valeur',
+      labelField: 'valeur',
+      searchField: 'valeur',
+      render: {
+        item: (item) => `<div class="item">${item.valeur}</div>`,
+        option: (option) => `<div class="option">${option.valeur}</div>`,
+      },
+      sortField: [{ field: 'valeur', direction: 'asc' }],
+    });
+  },
+};
+
 $(() => {
   const peupleServicesDans = (placeholder, donneesServices, idUtilisateur) => {
     const $conteneurServices = $(placeholder);
@@ -34,7 +54,12 @@ $(() => {
     .then(({ data }) => data.utilisateur)
     .then((utilisateur) => {
       axios.get('/api/services')
-        .then(({ data }) => peupleServicesDans('.services', data.services, utilisateur.id));
+        .then(({ data }) => {
+          peupleServicesDans('.services', data.services, utilisateur.id);
+
+          const toutesOrganisations = data.services.map((s) => s.organisationsResponsables).flat();
+          filtreEspacePersonnel.brancheFiltres(toutesOrganisations);
+        });
 
       if (!utilisateur.profilEstComplet) afficheBandeauMajProfil();
     });
