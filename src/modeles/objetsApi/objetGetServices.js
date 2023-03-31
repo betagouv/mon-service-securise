23c26dae51f0086d127b888f555a20d991324ce5
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-03-31 11:59:59 +0200

    Réduis l'objet de données de `GET /services` au strict minimum

--- src/modeles/objetsApi/objetGetServices.js
+++ src/modeles/objetsApi/objetGetServices.js
@@ -1,3 +1,21 @@
-const donnees = (services) => ({ services: services.map((s) => s.toJSON()) });
+const donnees = (services) => ({
+  services: services
+    .map((s) => s.toJSON())
+    .map((json) => ({
+      id: json.id,
+      nomService: json.nomService,
+      createur: {
+        id: json.createur.id,
+        prenomNom: json.createur.prenomNom,
+        initiales: json.createur.initiales,
+      },
+      contributeurs: json.contributeurs.map((c) => ({
+        id: c.id,
+        prenomNom: c.prenomNom,
+        initiales: c.initiales,
+        cguAcceptees: c.cguAcceptees,
+      })),
+    })),
+});
 
 module.exports = { donnees };
--- test/modeles/objetsApi/objetGetServices.spec.js
+++ test/modeles/objetsApi/objetGetServices.spec.js
@@ -7,10 +7,9 @@ describe("L'objet d'API de `GET /services`", () => {
   const unService = new Service({
     id: '123',
     descriptionService: { nomService: 'Un service', organisationsResponsables: ['Une organisation'] },
-    createur: { email: 'email.createur@mail.fr', prenom: 'Jacques' },
+    createur: { id: 'A', email: 'email.createur@mail.fr', prenom: 'Jacques' },
     contributeurs: [
-      { email: 'email.contributeur1@mail.fr', prenom: 'Jean' },
-      { email: 'email.contributeur2@mail.fr', prenom: 'Jean II' },
+      { id: 'B', email: 'email.contributeur1@mail.fr', prenom: 'Jean' },
     ],
   });
 
@@ -22,47 +21,10 @@ describe("L'objet d'API de `GET /services`", () => {
           {
             id: '123',
             nomService: 'Un service',
+            createur: { id: 'A', prenomNom: 'Jacques', initiales: 'J' },
             contributeurs: [
-              {
-                cguAcceptees: false,
-                delegueProtectionDonnees: false,
-                departementEntitePublique: '',
-                id: undefined,
-                initiales: 'J',
-                nomEntitePublique: '',
-                poste: '',
-                prenomNom: 'Jean',
-                profilEstComplet: false,
-                rssi: false,
-                telephone: '',
-              },
-              {
-                cguAcceptees: false,
-                delegueProtectionDonnees: false,
-                departementEntitePublique: '',
-                id: undefined,
-                initiales: 'J',
-                nomEntitePublique: '',
-                poste: '',
-                prenomNom: 'Jean II',
-                profilEstComplet: false,
-                rssi: false,
-                telephone: '',
-              },
+              { id: 'B', prenomNom: 'Jean', initiales: 'J', cguAcceptees: false },
             ],
-            createur: {
-              cguAcceptees: false,
-              delegueProtectionDonnees: false,
-              departementEntitePublique: '',
-              id: undefined,
-              initiales: 'J',
-              nomEntitePublique: '',
-              poste: '',
-              prenomNom: 'Jacques',
-              profilEstComplet: false,
-              rssi: false,
-              telephone: '',
-            },
           }],
       }
     );
