559533e40b19ab53b35f1e1c64e73b47b035b7d5
ThibaudMZN <thibaud.marazano@gmail.com>
2023-04-04 11:21:28 +0200

    Ajoute l'affichage du message en cas d'erreur

--- public/homologation/descriptionService.js
+++ public/homologation/descriptionService.js
@@ -2,12 +2,42 @@ import { brancheElementsAjoutablesDescription } from '../modules/brancheElements
 import extraisParametresDescriptionService from '../modules/parametresDescriptionService.mjs';
 import initialiseComportementFormulaire from '../modules/soumetsHomologation.mjs';
 
+const messageErreurNomDejaUtilise = {
+  affiche: () => {
+    $('#nom-deja-utilise').show();
+    const $champNomService = $('#nom-service');
+    $champNomService.get(0).scrollIntoView({ behavior: 'smooth', block: 'center' });
+    $champNomService.addClass('invalide');
+  },
+  masque: () => {
+    $('#nom-deja-utilise').hide();
+    $('#nom-service').removeClass('invalide');
+  },
+};
+
+const brancheComportementMessageErreur = () => {
+  $('#nom-service').on('keydown', () => {
+    messageErreurNomDejaUtilise.masque();
+  });
+};
+
+const estNomServiceDejaUtilise = (reponseErreur) => (
+  reponseErreur.status === 422
+    && reponseErreur.data?.erreur?.code === 'NOM_SERVICE_DEJA_EXISTANT'
+);
+
 $(() => {
   initialiseComportementFormulaire(
     'form#homologation',
     '.bouton#diagnostic',
     extraisParametresDescriptionService,
+    (e) => {
+      if (estNomServiceDejaUtilise(e.response)) {
+        messageErreurNomDejaUtilise.affiche();
+      }
+    }
   );
+  brancheComportementMessageErreur();
 
   brancheElementsAjoutablesDescription('donnees-sensibles-specifiques', 'donnees-sensibles');
   brancheElementsAjoutablesDescription('fonctionnalites-specifiques', 'fonctionnalite');
