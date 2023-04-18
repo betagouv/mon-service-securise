5ee20bd8c1c516730ae44da5aa585bfccab0b245
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-04-18 16:49:40 +0200

    [CORRECTION] Respecte un maximum de 360 degrés pour les camemberts
    Avant ce commit, la fin du dernier angle pouvait arriver au-delà de 360 degrés. Ce qui entraînait un dessin partiel de la section du camembert et un affichage illisible des valeurs.
    
    Avec ce commit on veille à répartir l'ensemble des angles entre 0 et 360 degrés pour des camemberts harmonieux.

--- src/pdf/graphiques/camembert.js
+++ src/pdf/graphiques/camembert.js
@@ -1,27 +1,70 @@
-const genereGradientConique = (statistiques) => {
-  const ANGLE_MINIMUM = 20;
+const ANGLE_MINIMUM = 20;
 
-  const fixeDebutFin = (debut, fin) => {
-    const avecValeur = debut !== fin;
-    const finVisible = avecValeur ? Math.max(fin, debut + ANGLE_MINIMUM) : fin;
-    return { debut, milieu: (finVisible - debut) / 2 + debut, fin: finVisible };
+const detailAngle = (debut, fin) => ({ debut, milieu: (fin - debut) / 2 + debut, fin });
+
+const recalculPourFin360 = (angles) => {
+  const { fait, enCours, nonFait, restant } = angles;
+
+  const depassement = fait.fin - 360;
+  const nbAnglesAModifier = Object.values(angles)
+    .map(({ debut, fin }) => fin - debut === ANGLE_MINIMUM)
+    .filter((estAngleMinimum) => estAngleMinimum === false)
+    .length;
+  const angleASoustraire = depassement / nbAnglesAModifier;
+
+  const valeurRevisee = (angle) => (angle.fin - angle.debut === ANGLE_MINIMUM
+    ? ANGLE_MINIMUM
+    : angle.fin - angle.debut - angleASoustraire
+  );
+
+  const valeurRevisees = {
+    enCours: valeurRevisee(enCours),
+    nonFait: valeurRevisee(nonFait),
+    restant: valeurRevisee(restant),
+    fait: valeurRevisee(fait),
   };
 
-  const total = statistiques.enCours + statistiques.nonFait
-    + statistiques.restant + statistiques.fait;
-  const enCours = (statistiques.enCours / total) * 360;
-  const nonFait = (statistiques.nonFait / total) * 360;
-  const restant = (statistiques.restant / total) * 360;
-  const fait = (statistiques.fait / total) * 360;
+  const enCoursRevise = detailAngle(0, valeurRevisees.enCours);
+  const nonFaitRevise = detailAngle(enCoursRevise.fin, enCoursRevise.fin + valeurRevisees.nonFait);
+  const restantRevise = detailAngle(nonFaitRevise.fin, nonFaitRevise.fin + valeurRevisees.restant);
+  const faitRevise = detailAngle(restantRevise.fin, restantRevise.fin + valeurRevisees.fait);
 
   return {
-    angles: {
-      enCours: fixeDebutFin(0, enCours),
-      nonFait: fixeDebutFin(enCours, enCours + nonFait),
-      restant: fixeDebutFin(enCours + nonFait, enCours + nonFait + restant),
-      fait: fixeDebutFin(enCours + nonFait + restant, enCours + nonFait + restant + fait),
-    },
+    enCours: enCoursRevise,
+    nonFait: nonFaitRevise,
+    restant: restantRevise,
+    fait: faitRevise,
   };
+};
+
+const genereGradientConique = (statistiques) => {
+  const avecAngleMinimum = (uneStatistique) => {
+    const total = statistiques.enCours
+    + statistiques.nonFait
+    + statistiques.restant
+    + statistiques.fait;
+
+    const valeurBrute = (uneStatistique / total) * 360;
+    return (valeurBrute === 0) ? 0 : Math.max(valeurBrute, ANGLE_MINIMUM);
+  };
+
+  const enCours = avecAngleMinimum(statistiques.enCours);
+  const nonFait = avecAngleMinimum(statistiques.nonFait);
+  const restant = avecAngleMinimum(statistiques.restant);
+  const fait = avecAngleMinimum(statistiques.fait);
+
+  const anglesInitiaux = {
+    enCours: detailAngle(0, enCours),
+    nonFait: detailAngle(enCours, enCours + nonFait),
+    restant: detailAngle(enCours + nonFait, enCours + nonFait + restant),
+    fait: detailAngle(enCours + nonFait + restant, enCours + nonFait + restant + fait),
+  };
+
+  const anglesFinaux = anglesInitiaux.fait.fin <= 360
+    ? anglesInitiaux
+    : recalculPourFin360(anglesInitiaux);
+
+  return { angles: anglesFinaux };
 };
 
 module.exports = { genereGradientConique };
