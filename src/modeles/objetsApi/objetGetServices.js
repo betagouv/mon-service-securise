2d1f7a12b6a6de5968db098896cf1597b01a0c3f
ThibaudMZN <thibaud.marazano@gmail.com>
2023-04-19 18:15:15 +0200

    Passe les informations résumées des services dans la route API

--- src/modeles/objetsApi/objetGetServices.js
+++ src/modeles/objetsApi/objetGetServices.js
@@ -1,29 +1,41 @@
-const donnees = (services) => ({
-  services: services
-    .map((s) => ({
-      ...s.toJSON(),
-      organisationsResponsables: s.descriptionService.organisationsResponsables,
-      indiceCyber: s.indiceCyber().total.toFixed(1),
-      statutHomologation: s.dossiers.statutSaisie(),
-    }))
-    .map((json) => ({
-      id: json.id,
-      nomService: json.nomService,
-      organisationsResponsables: json.organisationsResponsables ?? [],
-      createur: {
-        id: json.createur.id,
-        prenomNom: json.createur.prenomNom,
-        initiales: json.createur.initiales,
-      },
-      contributeurs: json.contributeurs.map((c) => ({
-        id: c.id,
-        prenomNom: c.prenomNom,
-        initiales: c.initiales,
-        cguAcceptees: c.cguAcceptees,
+const donnees = (services) => {
+  const servicesAvecIndiceCyber = services
+    .filter((s) => s.indiceCyber().total > 0);
+
+  return {
+    services: services
+      .map((s) => ({
+        ...s.toJSON(),
+        organisationsResponsables: s.descriptionService.organisationsResponsables,
+        indiceCyber: s.indiceCyber().total.toFixed(1),
+        statutHomologation: s.dossiers.statutSaisie(),
+      }))
+      .map((json) => ({
+        id: json.id,
+        nomService: json.nomService,
+        organisationsResponsables: json.organisationsResponsables ?? [],
+        createur: {
+          id: json.createur.id,
+          prenomNom: json.createur.prenomNom,
+          initiales: json.createur.initiales,
+        },
+        contributeurs: json.contributeurs.map((c) => ({
+          id: c.id,
+          prenomNom: c.prenomNom,
+          initiales: c.initiales,
+          cguAcceptees: c.cguAcceptees,
+        })),
+        indiceCyber: json.indiceCyber,
+        statutHomologation: json.statutHomologation,
       })),
-      indiceCyber: json.indiceCyber,
-      statutHomologation: json.statutHomologation,
-    })),
-});
+    resume: {
+      nombreServices: services.length,
+      nombreServicesHomologues: services.filter((s) => s.dossiers.statutSaisie() === 'completes').length,
+      indiceCyberMoyen: servicesAvecIndiceCyber
+        .map((s) => s.indiceCyber().total)
+        .reduce((a, b) => a + b, 0) / servicesAvecIndiceCyber.length,
+    },
+  };
+};
 
 module.exports = { donnees };
--- test/modeles/objetsApi/objetGetServices.spec.js
+++ test/modeles/objetsApi/objetGetServices.spec.js
@@ -14,22 +14,57 @@ describe("L'objet d'API de `GET /services`", () => {
   });
   unService.indiceCyber = () => ({ total: 3.51 });
 
+  const unAutreService = new Service({
+    id: '456',
+    descriptionService: { nomService: 'Un autre service', organisationsResponsables: ['Une organisation'] },
+    createur: { id: 'A', email: 'email.createur@mail.fr', prenom: 'Jacques' },
+    contributeurs: [
+      { id: 'B', email: 'email.contributeur1@mail.fr', prenom: 'Jean' },
+    ],
+  });
+
   it('fournit les données nécessaires', () => {
     const services = [unService];
-    expect(vueEspacePersonnel.donnees(services)).to.eql(
+    expect(vueEspacePersonnel.donnees(services).services).to.eql(
+      [{
+        id: '123',
+        nomService: 'Un service',
+        organisationsResponsables: ['Une organisation'],
+        createur: { id: 'A', prenomNom: 'Jacques', initiales: 'J' },
+        contributeurs: [
+          { id: 'B', prenomNom: 'Jean', initiales: 'J', cguAcceptees: false },
+        ],
+        indiceCyber: 3.5,
+        statutHomologation: 'aSaisir',
+      }],
+    );
+  });
+
+  it('fournit les données de résumé des services', () => {
+    unService.indiceCyber = () => ({ total: 4 });
+    unAutreService.dossiers.statutSaisie = () => 'completes';
+    unAutreService.indiceCyber = () => ({ total: 5 });
+
+    const services = [unService, unAutreService];
+    expect(vueEspacePersonnel.donnees(services).resume).to.eql(
       {
-        services: [
-          {
-            id: '123',
-            nomService: 'Un service',
-            organisationsResponsables: ['Une organisation'],
-            createur: { id: 'A', prenomNom: 'Jacques', initiales: 'J' },
-            contributeurs: [
-              { id: 'B', prenomNom: 'Jean', initiales: 'J', cguAcceptees: false },
-            ],
-            indiceCyber: 3.5,
-            statutHomologation: 'aSaisir',
-          }],
+        nombreServices: 2,
+        nombreServicesHomologues: 1,
+        indiceCyberMoyen: 4.5,
+      }
+    );
+  });
+
+  it('ne compte pas les indice cyber nuls pour calculer la moyenne', () => {
+    unService.indiceCyber = () => ({ total: 4 });
+    unAutreService.indiceCyber = () => ({ total: 0 });
+
+    const services = [unService, unAutreService];
+    expect(vueEspacePersonnel.donnees(services).resume).to.eql(
+      {
+        nombreServices: 2,
+        nombreServicesHomologues: 1,
+        indiceCyberMoyen: 4,
       }
     );
   });
