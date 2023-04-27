fd001d5893a67e2759d0d4f142f84543b899f228
ThibaudMZN <thibaud.marazano@gmail.com>
2023-04-27 15:40:06 +0200

    Ajoute une route de désinscription de l'infolettre

--- src/routes/routesApi.js
+++ src/routes/routesApi.js
@@ -354,6 +354,32 @@ const routesApi = (
       .then((suggestions) => reponse.status(200).json({ suggestions }));
   });
 
+  routes.post('/desinscriptionInfolettre', (requete, reponse, suite) => {
+    const { event: typeEvenement, email } = requete.body;
+
+    if (typeEvenement !== 'unsubscribe') {
+      reponse.status(400).json({ erreur: "L'événement doit être de type 'unsubscribe'" });
+      return;
+    }
+
+    if (!email) {
+      reponse.status(400).json({ erreur: "Le champ 'email' doit être présent" });
+      return;
+    }
+
+    depotDonnees.utilisateurAvecEmail(email)
+      .then((utilisateur) => {
+        if (!utilisateur) {
+          reponse.status(424).json({ erreur: `L'email '${email}' est introuvable` });
+          return;
+        }
+        depotDonnees
+          .metsAJourUtilisateur(utilisateur.id, { ...utilisateur, infolettreAcceptee: false })
+          .then(() => reponse.sendStatus(200))
+          .catch(suite);
+      });
+  });
+
   return routes;
 };
 
