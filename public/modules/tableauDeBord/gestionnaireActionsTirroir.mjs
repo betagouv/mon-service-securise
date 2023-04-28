c5058e78fb48fb4d5c0f046bd7486b3f2ab4759a
ThibaudMZN <thibaud.marazano@gmail.com>
2023-04-28 16:42:35 +0200

    Extrais le gestionnaire d'actions tirroir

---
+++ public/modules/tableauDeBord/gestionnaireActionsTirroir.mjs
@@ -0,0 +1,34 @@
+import tableauDesServices from './tableauDesServices.mjs';
+import gestionnaireTirroir from './gestionnaireTirroir.mjs';
+
+const tableauDeLongueur = (longueur) => [...Array(longueur).keys()];
+
+const gestionnaireActionsTirroir = {
+  duplique: () => {
+    const $nombreCopie = $('#nombre-copie');
+    if ($nombreCopie.is(':valid')) {
+      $nombreCopie.prop('disabled', true);
+      $('#action-duplication').prop('disabled', true);
+
+      const nombreCopies = parseInt($nombreCopie.val(), 10) || 1;
+      const promesses = [...tableauDesServices.servicesSelectionnes].map((idService) => (
+        tableauDeLongueur(nombreCopies).reduce((acc) => acc.then(
+          () => axios({ method: 'copy', url: `/api/service/${idService}` })
+        ), Promise.resolve())
+      ));
+      Promise.all(promesses).then(() => {
+        $nombreCopie.prop('disabled', false);
+        $('#action-duplication').prop('disabled', false);
+        gestionnaireTirroir.basculeOuvert(false);
+        tableauDesServices.recupereServices();
+      });
+    }
+  },
+  brancheComportements: () => {
+    $('#action-duplication').on('click', () => {
+      gestionnaireActionsTirroir.duplique();
+    });
+  },
+};
+
+export default gestionnaireActionsTirroir;
