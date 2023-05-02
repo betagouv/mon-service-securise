8d5809db2ceabd83eaab7d2c736d492a06b4ecda
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-05-02 15:15:06 +0200

    Conditionne les logs console à la valeur de `AVEC_EMAIL_MEMOIRE_QUI_LOG_CONSOLE`

--- src/adaptateurs/adaptateurMailMemoire.js
+++ src/adaptateurs/adaptateurMailMemoire.js
@@ -1,46 +1,51 @@
-/* eslint-disable no-console */
+const adaptateurEnvironnement = require('./adaptateurEnvironnement');
+
+const envoyer = (texte, args) => {
+  const doitLoguer = adaptateurEnvironnement.emailMemoire().logEmailDansConsole();
+
+  // eslint-disable-next-line no-console
+  if (doitLoguer) console.log(texte, args);
+};
 
 const creeContact = (...args) => {
-  console.log("Création d'un contact email", args);
+  envoyer("Création d'un contact email", args);
   return Promise.resolve();
 };
 
 const desinscrisInfolettre = (...args) => {
-  console.log("Désinscription de l'infolettre MSS", args);
+  envoyer("Désinscription de l'infolettre MSS", args);
   return Promise.resolve();
 };
 
 const inscrisInfolettre = (...args) => {
-  console.log("Inscription à l'infolettre MSS", args);
+  envoyer("Inscription à l'infolettre MSS", args);
   return Promise.resolve();
 };
 
 const envoieMessageFinalisationInscription = (...args) => {
-  console.log("Envoie de l'email de finalisation de l'inscription", args);
+  envoyer("Envoie de l'email de finalisation de l'inscription", args);
   return Promise.resolve();
 };
 
 const envoieMessageInvitationContribution = (...args) => {
-  console.log("Envoie de l'email d'invitation à contribuer", args);
+  envoyer("Envoie de l'email d'invitation à contribuer", args);
   return Promise.resolve();
 };
 
 const envoieMessageInvitationInscription = (...args) => {
-  console.log("Envoie de l'email d'invitation à s'inscrire", args);
+  envoyer("Envoie de l'email d'invitation à s'inscrire", args);
   return Promise.resolve();
 };
 
 const envoieMessageReinitialisationMotDePasse = (...args) => {
-  console.log("Envoie de l'email de réinitialisation du mot de passe", args);
+  envoyer("Envoie de l'email de réinitialisation du mot de passe", args);
   return Promise.resolve();
 };
 
 const envoieNotificationTentativeReinscription = (...args) => {
-  console.log("Envoie de l'email de notification de tentative de réinscription", args);
+  envoyer("Envoie de l'email de notification de tentative de réinscription", args);
   return Promise.resolve();
 };
-
-/* eslint-enable no-console */
 
 module.exports = {
   creeContact,
