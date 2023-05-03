9c5a4c8cb33e8b4e8ea582d5e9df3f0e43058ec1
ThibaudMZN <thibaud.marazano@gmail.com>
2023-05-03 14:20:28 +0200

    Modifie l'étape DateTéléchargement

---
+++ test/modeles/etapes/dateTelechargement.spec.js
@@ -0,0 +1,30 @@
+const expect = require('expect.js');
+
+const DateTelechargement = require('../../../src/modeles/etapes/dateTelechargement');
+
+describe("La date de téléchargement des documents d'homologation", () => {
+  it('sait se convertir en JSON', () => {
+    const dateTelechargement = new DateTelechargement({ date: '2023-02-02T00:00:00.000Z' });
+
+    expect(dateTelechargement.toJSON()).to.eql({ date: '2023-02-02T00:00:00.000Z' });
+  });
+
+  describe("sur vérification qu'elle est complète", () => {
+    it("retourne `false` s'il n'y a pas de date de téléchargement", () => {
+      const sansTelechargementDecision = new DateTelechargement();
+      expect(sansTelechargementDecision.estComplete()).to.be(false);
+    });
+
+    it("retourne `true` s'il y a une date de téléchargement", () => {
+      const dateTelechargement = new DateTelechargement({ date: '2023-02-02T00:00:00.000Z' });
+      expect(dateTelechargement.estComplete()).to.be(true);
+    });
+  });
+
+  it('sait enregistrer une date de téléchargement', () => {
+    const sansTelechargementDecision = new DateTelechargement();
+    const date = '2023-02-02T00:00:00.000Z';
+    sansTelechargementDecision.enregistreDateTelechargement('2023-02-02T00:00:00.000Z');
+    expect(sansTelechargementDecision.date).to.be(date);
+  });
+});
