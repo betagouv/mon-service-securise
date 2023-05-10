73bd604a553c9818752d44e78d4e63273e3ce18d
ThibaudMZN <thibaud.marazano@gmail.com>
2023-05-10 10:50:23 +0200

    Ajoute l'adaptateur de génération CSV

---
+++ src/adaptateurs/adaptateurCsv.js
@@ -0,0 +1,41 @@
+const { EOL } = require('os');
+const { decode } = require('html-entities');
+
+const STATUS_HOMOLOGATION = {
+  aSaisir: 'À réaliser',
+  aCompleter: 'À finaliser',
+  completes: 'Réalisée',
+};
+const SEPARATEUR = ';';
+
+const remplaceSeparateurParEspace = (chaine) => chaine.replaceAll(SEPARATEUR, ' ');
+const remplaceBooleen = (booleen) => (booleen ? 'Oui' : 'Non');
+
+const ligneDuService = (service) => (
+  [
+    remplaceSeparateurParEspace(decode(service.nomService)),
+    service.organisationsResponsables.map((o) => decode(o)).map(remplaceSeparateurParEspace).join(' - '),
+    service.nombreContributeurs,
+    remplaceBooleen(service.estCreateur),
+    service.indiceCyber,
+    STATUS_HOMOLOGATION[service.statutHomologation],
+  ].join(SEPARATEUR)
+);
+
+const genereCsvServices = (donneesObjetGetServices) => {
+  const entete = [
+    'Nom du service',
+    'Organisations responsables',
+    'Nombre de collaborateurs',
+    'Est propriétaire ?',
+    'Indice cyber',
+    'Statut homologation',
+  ].join(SEPARATEUR);
+
+  const contenu = [entete, ...donneesObjetGetServices.services.map(ligneDuService)].join(EOL);
+
+  const avecBOM = '\uFEFF';
+  return Buffer.from(`${avecBOM}${contenu}`, 'utf-8');
+};
+
+module.exports = { genereCsvServices };
