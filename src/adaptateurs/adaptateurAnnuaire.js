0ede67efc91b7fd00e9fde15e9c5eb337305091e
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-05-12 10:51:07 +0200

    Logue les erreurs survenant dans l'adaptateur Annuaire

--- src/adaptateurs/adaptateurAnnuaire.js
+++ src/adaptateurs/adaptateurAnnuaire.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { fabriqueAdaptateurGestionErreur } = require('./fabriqueAdaptateurGestionErreur');
 
 const rechercheOrganisation = (terme, departement) => axios.get('https://recherche-entreprises.api.gouv.fr/search', {
   params: {
@@ -9,8 +10,13 @@ const rechercheOrganisation = (terme, departement) => axios.get('https://recherc
     limite_matching_etablissements: 1,
     est_entrepreneur_individuel: false,
   },
-}).then((reponse) => reponse.data.results.filter((r) => r.siege.departement !== null).map((r) => ({
-  nom: r.nom_complet, departement: r.siege.departement,
-})));
+}).then((reponse) => (
+  reponse.data.results
+    .filter((r) => r.siege.departement !== null)
+    .map((r) => ({ nom: r.nom_complet, departement: r.siege.departement }))))
+  .catch((e) => {
+    fabriqueAdaptateurGestionErreur().logueErreur(e);
+    return Promise.reject(e);
+  });
 
 module.exports = { rechercheOrganisation };
