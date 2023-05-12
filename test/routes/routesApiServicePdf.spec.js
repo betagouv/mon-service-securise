c1ae0e5a1b1239a6f726be8bd0fa02d4a615b0da
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-05-12 10:33:48 +0200

    Utilise la page d'erreur sur les routes de génération de PDF
    … pour une meilleure UX.

--- test/routes/routesApiServicePdf.spec.js
+++ test/routes/routesApiServicePdf.spec.js
@@ -108,18 +108,6 @@ describe('Le serveur MSS des routes /api/service/:id/pdf/*', () => {
         })
         .catch((e) => done(e.response?.data || e));
     });
-
-    it("reste robuste en cas d'échec de génération de pdf", (done) => {
-      testeur.adaptateurPdf().genereDossierDecision = () => Promise.reject();
-
-      axios.get('http://localhost:1234/api/service/456/pdf/dossierDecision.pdf')
-        .then(() => done('La génération aurait dû lever une erreur'))
-        .catch((e) => {
-          expect(e.response.status).to.equal(424);
-          done();
-        })
-        .catch(done);
-    });
   });
 
   describe('quand requête GET sur `/api/service/:id/pdf/syntheseSecurite.pdf`', () => {
@@ -165,18 +153,6 @@ describe('Le serveur MSS des routes /api/service/:id/pdf/*', () => {
           done();
         })
         .catch((e) => done(e.response?.data || e));
-    });
-
-    it("reste robuste en cas d'échec de génération de pdf", (done) => {
-      testeur.adaptateurPdf().genereSyntheseSecurite = () => Promise.reject();
-
-      axios.get('http://localhost:1234/api/service/456/pdf/syntheseSecurite.pdf')
-        .then(() => done('La génération aurait dû lever une erreur'))
-        .catch((e) => {
-          expect(e.response.status).to.equal(424);
-          done();
-        })
-        .catch(done);
     });
   });
 
@@ -246,18 +222,6 @@ describe('Le serveur MSS des routes /api/service/:id/pdf/*', () => {
           done();
         })
         .catch((e) => done(e.response?.data || e));
-    });
-
-    it("reste robuste en cas d'échec de génération de pdf", (done) => {
-      testeur.adaptateurPdf().genereArchiveTousDocuments = () => Promise.reject();
-
-      axios.get('http://localhost:1234/api/service/456/pdf/documentsHomologation.zip')
-        .then(() => done('La génération aurait dû lever une erreur'))
-        .catch((e) => {
-          expect(e.response.status).to.equal(424);
-          done();
-        })
-        .catch(done);
     });
   });
 });
