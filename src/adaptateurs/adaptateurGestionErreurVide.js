f93ac46e7b1bfcd2aed3ff923b734230b1675425
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-05-12 10:28:21 +0200

    Enrichis les adaptateurs de gestion d'erreur d'une fonction de log d'exception

--- src/adaptateurs/adaptateurGestionErreurVide.js
+++ src/adaptateurs/adaptateurGestionErreurVide.js
@@ -1,5 +1,6 @@
 const initialise = () => {};
 const controleurRequetes = () => (_requete, _reponse, suite) => suite();
 const controleurErreurs = () => (_requete, _reponse, suite) => suite();
+const logueErreur = (_erreur) => {};
 
-module.exports = { initialise, controleurRequetes, controleurErreurs };
+module.exports = { initialise, controleurRequetes, controleurErreurs, logueErreur };
