ed1cceff24307011428f45fcf23919a5b14b6279
ThibaudMZN <thibaud.marazano@gmail.com>
2023-06-07 12:23:29 +0200

    Ajoute les status d'homologation aux Dossiers

--- src/modeles/dossiers.js
+++ src/modeles/dossiers.js
@@ -3,6 +3,13 @@ const ElementsConstructibles = require('./elementsConstructibles');
 const { ErreurDossiersInvalides } = require('../erreurs');
 const Referentiel = require('../referentiel');
 
+const STATUTS_HOMOLOGATION = {
+  A_REALISER: 'aRealiser',
+  A_FINALISER: 'aFinaliser',
+  REALISEE: 'realisee',
+  BIENTOT_EXPIREE: 'bientotExpiree',
+  EXPIREE: 'expiree',
+};
 class Dossiers extends ElementsConstructibles {
   constructor(
     donnees = { dossiers: [] },
@@ -31,6 +38,17 @@ class Dossiers extends ElementsConstructibles {
     return this.items.filter((i) => i.finalise);
   }
 
+  statutHomologation() {
+    if (this.nombre() === 0) return Dossiers.A_REALISER;
+    if (this.dossierCourant()) return Dossiers.A_FINALISER;
+    const dossierActif = this.dossierActif();
+    if (dossierActif) {
+      if (dossierActif.estBientotExpire()) return Dossiers.BIENTOT_EXPIREE;
+      return Dossiers.REALISEE;
+    }
+    return Dossiers.EXPIREE;
+  }
+
   statutSaisie() {
     if (this.nombre() === 0) {
       return Dossiers.A_SAISIR;
@@ -51,4 +69,5 @@ class Dossiers extends ElementsConstructibles {
   }
 }
 
+Object.assign(Dossiers, STATUTS_HOMOLOGATION);
 module.exports = Dossiers;
