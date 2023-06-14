56b28b984da03482d513320b63d2cb7def1e6035
ThibaudMZN <thibaud.marazano@gmail.com>
2023-06-14 17:21:21 +0200

    Ajoute une méthode pour savoir si une nouvelle fonctionnalité est disponible

--- src/modeles/parcoursUtilisateur.js
+++ src/modeles/parcoursUtilisateur.js
@@ -1,8 +1,13 @@
 const adaptateurHorlogeParDefaut = require('../adaptateurs/adaptateurHorloge');
 const Base = require('./base');
+const Referentiel = require('../referentiel');
 
 class ParcoursUtilisateur extends Base {
-  constructor(donnees = {}, adaptateurHorloge = adaptateurHorlogeParDefaut) {
+  constructor(
+    donnees = {},
+    referentiel = Referentiel.creeReferentielVide(),
+    adaptateurHorloge = adaptateurHorlogeParDefaut
+  ) {
     super({
       proprietesAtomiquesRequises: [
         'id',
@@ -12,12 +17,30 @@ class ParcoursUtilisateur extends Base {
     });
     this.renseigneProprietes(donnees);
     this.adaptateurHorloge = adaptateurHorloge;
+    this.referentiel = referentiel;
   }
 
   enregistreDerniereConnexionMaintenant() {
     this.dateDerniereConnexion = this.adaptateurHorloge
       .maintenant()
       .toISOString();
+  }
+
+  recupereNouvelleFonctionnalite() {
+    const derniereFonctionnalite =
+      this.referentiel.derniereNouvelleFonctionnalite();
+    if (!derniereFonctionnalite) return undefined;
+    const dateDerniereConnexion = new Date(this.dateDerniereConnexion);
+    const dateDeploiementDerniereFonctionnalite = new Date(
+      derniereFonctionnalite.dateDeDeploiement
+    );
+    const dateAujourdhui = this.adaptateurHorloge.maintenant();
+    if (
+      dateDerniereConnexion < dateDeploiementDerniereFonctionnalite &&
+      dateAujourdhui > dateDeploiementDerniereFonctionnalite
+    )
+      return derniereFonctionnalite.id;
+    return undefined;
   }
 }
 
--- test/modeles/parcoursUtilisateur.spec.js
+++ test/modeles/parcoursUtilisateur.spec.js
@@ -1,6 +1,7 @@
 const expect = require('expect.js');
 
 const ParcoursUtilisateur = require('../../src/modeles/parcoursUtilisateur');
+const Referentiel = require('../../src/referentiel');
 
 describe('Un parcours utilisateur', () => {
   it('sait se convertir en JSON', () => {
@@ -35,5 +36,73 @@ describe('Un parcours utilisateur', () => {
     expect(unParcours.toJSON().dateDerniereConnexion).to.equal(
       dateDeConnexion.toISOString()
     );
+  });
+
+  describe('sur demande de recupération de nouvelle fonctionnalité', () => {
+    it("ne retourne pas de nouvelle fonctionnalité si aucune n'est présente dans le référentiel", () => {
+      const referentiel = Referentiel.creeReferentielVide({
+        nouvellesFonctionnalites: [],
+      });
+      const unParcours = new ParcoursUtilisateur({ id: '123' }, referentiel);
+      expect(unParcours.recupereNouvelleFonctionnalite()).to.be(undefined);
+    });
+
+    it("ne retourne pas de nouvelle fonctionnalité si aucune n'est apparue depuis la dernière connexion", () => {
+      const referentiel = Referentiel.creeReferentiel({
+        nouvellesFonctionnalites: [
+          { id: 'nouveautéDansLePassé', dateDeDeploiement: '2022-01-01' },
+        ],
+      });
+      const unParcours = new ParcoursUtilisateur(
+        {
+          id: '123',
+          dateDerniereConnexion: '2023-01-01',
+        },
+        referentiel
+      );
+      expect(unParcours.recupereNouvelleFonctionnalite()).to.be(undefined);
+    });
+
+    it('ne retourne pas de nouvelle fonctionnalité si elle est dans le futur', () => {
+      const referentiel = Referentiel.creeReferentiel({
+        nouvellesFonctionnalites: [
+          { id: 'nouveauté1Février', dateDeDeploiement: '2023-02-01' },
+        ],
+      });
+      const adaptateurHorloge = {
+        maintenant: () => new Date('2023-01-15'),
+      };
+      const unParcours = new ParcoursUtilisateur(
+        {
+          id: '123',
+          dateDerniereConnexion: '2023-01-01',
+        },
+        referentiel,
+        adaptateurHorloge
+      );
+      expect(unParcours.recupereNouvelleFonctionnalite()).to.be(undefined);
+    });
+
+    it("retourne l'identifiant de la nouvelle fonctionnalité si elle est apparue entre la dernière connexion et aujourd'hui", () => {
+      const referentiel = Referentiel.creeReferentiel({
+        nouvellesFonctionnalites: [
+          { id: 'nouveauté15Janvier', dateDeDeploiement: '2023-01-15' },
+        ],
+      });
+      const adaptateurHorloge = {
+        maintenant: () => new Date('2023-02-01'),
+      };
+      const unParcours = new ParcoursUtilisateur(
+        {
+          id: '123',
+          dateDerniereConnexion: '2023-01-01',
+        },
+        referentiel,
+        adaptateurHorloge
+      );
+      expect(unParcours.recupereNouvelleFonctionnalite()).to.be(
+        'nouveauté15Janvier'
+      );
+    });
   });
 });
