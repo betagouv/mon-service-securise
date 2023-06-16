7a16f0b26d79086c337d13284146ef7c931fab0d
ThibaudMZN <thibaud.marazano@gmail.com>
2023-06-16 11:13:03 +0200

    Ajoute la route pour servir les nouvelles fonctionnalités

--- test/routes/routesApi.spec.js
+++ test/routes/routesApi.spec.js
@@ -2013,4 +2013,44 @@ describe('Le serveur MSS des routes /api/*', () => {
         .catch((e) => done(e.response?.data || e));
     });
   });
+
+  describe('quand requête GET sur `/api/nouvelleFonctionnalite`', () => {
+    it("aseptise l'identifiant de nouvelle fonctionnalité", (done) => {
+      testeur.middleware().verifieAseptisationParametres(
+        ['id'],
+        {
+          method: 'get',
+          url: 'http://localhost:1234/api/nouvelleFonctionnalite/nouveaute',
+        },
+        done
+      );
+    });
+
+    it("retourne une erreur HTTP 404 si la nouvelle fonctionnalité n'existe pas", (done) => {
+      testeur.verifieRequeteGenereErreurHTTP(
+        404,
+        'Nouvelle fonctionnalité inconnue',
+        {
+          method: 'get',
+          url: 'http://localhost:1234/api/nouvelleFonctionnalite/inconnue',
+        },
+        done
+      );
+    });
+
+    it('retourne le contenu de la nouvelle fonctionnalité', (done) => {
+      testeur.referentiel().recharge({
+        nouvellesFonctionnalites: [
+          { id: 'tdb', fichierPug: 'tableauDeBord.pug' },
+        ],
+      });
+      axios
+        .get('http://localhost:1234/api/nouvelleFonctionnalite/tdb')
+        .then((reponse) => {
+          expect(reponse.status).to.equal(200);
+          done();
+        })
+        .catch((e) => done(e.response?.data || e));
+    });
+  });
 });
