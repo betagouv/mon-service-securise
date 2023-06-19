ca1e3c1d55c7a12ad1c8977462422dbc48a41f08
Bertrand Bougon <bertrandbougon@gmail.com>
2023-06-19 14:33:35 +0200

    Ajoute un service de tracking pour le taux de complétude des services d'un utilisateur

---
+++ src/depots/serviceTracking.js
@@ -0,0 +1,31 @@
+const creeService = () => {
+  const completudeDesServicesPourUtilisateur = (
+    depotHomologations,
+    idUtilisateur
+  ) =>
+    depotHomologations
+      .nombreMoyenContributeursPourUtilisateur(idUtilisateur)
+      .then((nombreMoyenContributeurs) =>
+        depotHomologations.homologations(idUtilisateur).then((hs) => ({
+          nbServices: hs.length,
+          nbMoyenContributeurs: nombreMoyenContributeurs,
+          tauxCompletudeMoyenTousServices:
+            (hs.reduce((accumulateur, h) => {
+              const completudeMesures = h.completudeMesures();
+              return (
+                accumulateur +
+                completudeMesures.nombreMesuresCompletes /
+                  completudeMesures.nombreTotalMesures
+              );
+            }, 0) /
+              hs.length) *
+            100,
+        }))
+      );
+
+  return {
+    completudeDesServicesPourUtilisateur,
+  };
+};
+
+module.exports = { creeService };
