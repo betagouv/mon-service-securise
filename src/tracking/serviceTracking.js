d9003dbd8ad53cb57df55d9fda741d7bb1cd623c
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-06-19 17:00:52 +0200

    Passe le service en `async/await`

--- src/tracking/serviceTracking.js
+++ src/tracking/serviceTracking.js
@@ -1,45 +1,49 @@
 const creeService = () => {
-  const completudeDesServicesPourUtilisateur = (
+  const nombreMoyenContributeursPourUtilisateur = async (
     depotHomologations,
     idUtilisateur
-  ) =>
-    depotHomologations
-      .nombreMoyenContributeursPourUtilisateur(idUtilisateur)
-      .then((nombreMoyenContributeurs) =>
-        depotHomologations.homologations(idUtilisateur).then((hs) => ({
-          nbServices: hs.length,
-          nbMoyenContributeurs: nombreMoyenContributeurs,
-          tauxCompletudeMoyenTousServices: Math.floor(
-            (hs.reduce((accumulateur, h) => {
-              const completudeMesures = h.completudeMesures();
-              return (
-                accumulateur +
-                completudeMesures.nombreMesuresCompletes /
-                  completudeMesures.nombreTotalMesures
-              );
-            }, 0) /
-              hs.length) *
-              100
-          ),
-        }))
-      );
+  ) => {
+    const hs = await depotHomologations.homologations(idUtilisateur);
 
-  const nombreMoyenContributeursPourUtilisateur = (
+    return hs.length === 0
+      ? 0
+      : Math.floor(
+          hs.reduce(
+            (accumulateur, h) => accumulateur + h.contributeurs.length,
+            0
+          ) / hs.length
+        );
+  };
+
+  const completudeDesServicesPourUtilisateur = async (
     depotHomologations,
     idUtilisateur
-  ) =>
-    depotHomologations
-      .homologations(idUtilisateur)
-      .then((hs) =>
-        hs.length === 0
-          ? 0
-          : Math.floor(
-              hs.reduce(
-                (accumulateur, h) => accumulateur + h.contributeurs.length,
-                0
-              ) / hs.length
-            )
-      );
+  ) => {
+    const [nbMoyenContributeurs, hs] = await Promise.all([
+      nombreMoyenContributeursPourUtilisateur(
+        depotHomologations,
+        idUtilisateur
+      ),
+      depotHomologations.homologations(idUtilisateur),
+    ]);
+
+    return {
+      nbServices: hs.length,
+      nbMoyenContributeurs,
+      tauxCompletudeMoyenTousServices: Math.floor(
+        (hs.reduce((accumulateur, h) => {
+          const completudeMesures = h.completudeMesures();
+          return (
+            accumulateur +
+            completudeMesures.nombreMesuresCompletes /
+              completudeMesures.nombreTotalMesures
+          );
+        }, 0) /
+          hs.length) *
+          100
+      ),
+    };
+  };
 
   return {
     completudeDesServicesPourUtilisateur,
