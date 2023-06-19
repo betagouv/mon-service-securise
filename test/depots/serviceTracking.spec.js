6049a37ddee1d5a5f93c2b4f2befd107b4dfdabf
Bertrand Bougon <bertrandbougon@gmail.com>
2023-06-19 14:48:28 +0200

    Retourne le taux de complétude arrondi à l'entier inférieur

--- test/depots/serviceTracking.spec.js
+++ test/depots/serviceTracking.spec.js
@@ -36,5 +36,29 @@ describe('Le service de tracking des services', () => {
         tauxCompletudeMoyenTousServices: ((18 / 20 + 18 / 100) * 100) / 2,
       });
     });
+
+    it("arrondi le taux à l'entier inférieur", async () => {
+      const premierService = unService(referentiel)
+        .avecMesures(bouchonneMesures().avecUneCompletude(33, 7).construis())
+        .construis();
+      const secondService = unService(referentiel)
+        .avecMesures(bouchonneMesures().avecUneCompletude(100, 27).construis())
+        .construis();
+      const depotHomologations = {
+        homologations: async () => [premierService, secondService],
+        nombreMoyenContributeursPourUtilisateur: async () => 3,
+      };
+      const serviceTracking = ServiceTracking.creeService();
+
+      const completude =
+        await serviceTracking.completudeDesServicesPourUtilisateur(
+          depotHomologations,
+          'AAA'
+        );
+
+      expect(completude.tauxCompletudeMoyenTousServices).to.be(
+        Math.floor(((7 / 33 + 27 / 100) * 100) / 2)
+      );
+    });
   });
 });
