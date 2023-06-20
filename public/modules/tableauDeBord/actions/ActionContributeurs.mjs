3357845541222b724d81ab60a15678175ee250b8
ThibaudMZN <thibaud.marazano@gmail.com>
2023-06-20 16:59:08 +0200

    Ajoute le menu de suppression du contributeur

--- public/modules/tableauDeBord/actions/ActionContributeurs.mjs
+++ public/modules/tableauDeBord/actions/ActionContributeurs.mjs
@@ -1,18 +1,49 @@
-const metEnFormeContributeur = (contributeur, estProprietaire) =>
-  `<li>
+const metEnFormeMenuFlotant = ({ peutSupprimer, service, utilisateur }) => {
+  let actions = '';
+  if (peutSupprimer)
+    actions += `
+      <li class="action-suppression-contributeur" 
+        data-id-service="${service.id}" 
+        data-id-contributeur="${utilisateur.id}"
+        data-nom-service="${service.nomService}"
+        data-nom-contributeur="${utilisateur.nomContributeur}">
+          Retirer du service
+      </li>`;
+  return `<div class="menu-flotant"><ul>${actions}</ul></div>`;
+};
+
+const metEnFormeLigne = (
+  estProprietaire,
+  estSupprimable,
+  utilisateur,
+  service
+) =>
+  `<li class='ligne-contributeur'>
     <div class='contenu-nom-prenom'>
       <div class='initiale ${
         estProprietaire ? 'proprietaire' : 'contributeur'
-      }'>${contributeur.initiales}</div>
+      }'>${utilisateur.initiales}</div>
       <div class='nom-prenom-poste'>
-        <div class='nom-contributeur'>${contributeur.prenomNom}</div>
-        <div class='poste-contributeur'>${contributeur.poste}</div>
+        <div class='nom-contributeur'>${utilisateur.prenomNom}</div>
+        <div class='poste-contributeur'>${utilisateur.poste}</div>
       </div>
     </div>
     <div class='role ${estProprietaire ? 'proprietaire' : 'contributeur'}'>${
     estProprietaire ? 'Propriétaire' : 'Contributeur'
   }</div>
+    ${`<div class="actions-contributeurs ${
+      estSupprimable ? '' : 'invisible'
+    }">${metEnFormeMenuFlotant({
+      peutSupprimer: estSupprimable,
+      service,
+      utilisateur,
+    })}`}
   </li>`;
+
+const metEnFormeProprietaire = (proprietaire) =>
+  metEnFormeLigne(true, false, proprietaire);
+const metEnFormeContributeur = (estSupprimable, contributeur, service) =>
+  metEnFormeLigne(false, estSupprimable, contributeur, service);
 
 class ActionContributeurs {
   constructor(tableauDesServices) {
@@ -27,16 +58,95 @@ class ActionContributeurs {
     const service = this.tableauDesServices.donneesDuService(idService);
     const $listeContributeurs = $('#liste-contributeurs');
 
+    $listeContributeurs.show();
+    $('#contenu-contributeurs .confirmation-suppression').hide();
+
     $listeContributeurs.empty();
-    $listeContributeurs.append(metEnFormeContributeur(service.createur, true));
+    $listeContributeurs.append(metEnFormeProprietaire(service.createur));
     service.contributeurs.forEach((contributeur) => {
-      $listeContributeurs.append(metEnFormeContributeur(contributeur, false));
+      $listeContributeurs.append(
+        metEnFormeContributeur(
+          service.permissions.suppressionContributeur,
+          contributeur,
+          service
+        )
+      );
     });
+
+    this.brancheComportementInteractions();
   }
 
   // eslint-disable-next-line class-methods-use-this
   estDisponible() {
     return true;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  brancheComportementInteractions() {
+    $('.actions-contributeurs', '#contenu-contributeurs').on(
+      'click',
+      (evenement) => {
+        const $boutonMenu = $(evenement.target);
+        const doitOuvrir = !$boutonMenu.hasClass('actif');
+        $('.actions-contributeurs', '#contenu-contributeurs ').removeClass(
+          'actif'
+        );
+        $('.menu-flotant', '#contenu-contributeurs').addClass('invisible');
+
+        if (doitOuvrir) {
+          const $menu = $('.menu-flotant', $boutonMenu);
+          $boutonMenu.toggleClass('actif');
+          $menu.toggleClass('invisible');
+        }
+      }
+    );
+
+    $('.action-suppression-contributeur', '#contenu-contributeurs').on(
+      'click',
+      (evenement) => {
+        const $action = $(evenement.target);
+        const $ligneContributeur = $action.parents('.ligne-contributeur');
+        const idService = $action.data('id-service');
+        const idContributeur = $action.data('id-contributeur');
+        const nomService = $action.data('nom-service');
+        const nomContributeur = $action.data('nom-contributeur');
+
+        $('#nom-contributeur-suppression').text(`${nomContributeur} `);
+        $('#nom-service-contributeur-suppression').html(
+          `<strong>${nomService}</strong> `
+        );
+
+        $('#liste-contributeurs').hide();
+        $('.confirmation-suppression', '#contenu-contributeurs').show();
+
+        $('#confirmation-suppression-contributeur')
+          .attr('data-id-service', idService)
+          .attr('data-id-contributeur', idContributeur)
+          .data('ref-dom-ligne-a-supprimer', $ligneContributeur);
+      }
+    );
+  }
+
+  execute() {
+    const $action = $(
+      '#confirmation-suppression-contributeur',
+      '#contenu-contributeurs'
+    );
+    const $ligneContributeur = $action.data('ref-dom-ligne-a-supprimer');
+    const idService = $action.attr('data-id-service');
+    const idContributeur = $action.attr('data-id-contributeur');
+
+    axios
+      .delete('/api/autorisation', {
+        params: { idHomologation: idService, idContributeur },
+      })
+      .then(() => {
+        $('#liste-contributeurs').show();
+        $('.confirmation-suppression', '#contenu-contributeurs').hide();
+
+        $ligneContributeur.fadeOut(200, (element) => $(element).remove());
+        this.tableauDesServices.recupereServices();
+      });
   }
 }
 
