f1b1dbb7aabb3bde63d59fa423ba2c8e7a03cf89
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-07-07 09:36:37 +0200

    Déplace `/api/desinscriptionInfolettre` dans les routes explicitement publiques

--- src/routes/routesApi.js
+++ src/routes/routesApi.js
@@ -604,44 +604,6 @@ const routesApi = (
       .then((suggestions) => reponse.status(200).json({ suggestions }));
   });
 
-  routes.post(
-    '/desinscriptionInfolettre',
-    middleware.verificationAddresseIP(['185.107.232.1/24', '1.179.112.1/20']),
-    (requete, reponse, suite) => {
-      const { event: typeEvenement, email } = requete.body;
-
-      if (typeEvenement !== 'unsubscribe') {
-        reponse
-          .status(400)
-          .json({ erreur: "L'événement doit être de type 'unsubscribe'" });
-        return;
-      }
-
-      if (!email) {
-        reponse
-          .status(400)
-          .json({ erreur: "Le champ 'email' doit être présent" });
-        return;
-      }
-
-      depotDonnees.utilisateurAvecEmail(email).then((utilisateur) => {
-        if (!utilisateur) {
-          reponse
-            .status(424)
-            .json({ erreur: `L'email '${email}' est introuvable` });
-          return;
-        }
-        depotDonnees
-          .metsAJourUtilisateur(utilisateur.id, {
-            ...utilisateur,
-            infolettreAcceptee: false,
-          })
-          .then(() => reponse.sendStatus(200))
-          .catch(suite);
-      });
-    }
-  );
-
   return routes;
 };
 
