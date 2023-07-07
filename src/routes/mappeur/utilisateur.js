4e1c116f5e6a264878635b69b9b2cc4ff2839a10
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-07-07 11:42:59 +0200

    Supprime la duplication de code de mapping
    Le code avait été dupliqué lorsque nous avons séparé les routes publiques et privées dans des fichiers distincts.

---
+++ src/routes/mappeur/utilisateur.js
@@ -0,0 +1,30 @@
+const Utilisateur = require('../../modeles/utilisateur');
+const { valeurBooleenne } = require('../../utilitaires/aseptisation');
+
+const obtentionDonneesDeBaseUtilisateur = (corps) => ({
+  prenom: corps.prenom,
+  nom: corps.nom,
+  telephone: corps.telephone,
+  nomEntitePublique: corps.nomEntitePublique,
+  departementEntitePublique: corps.departementEntitePublique,
+  infolettreAcceptee: valeurBooleenne(corps.infolettreAcceptee),
+  postes: corps.postes,
+});
+
+const messageErreurDonneesUtilisateur = (
+  donneesRequete,
+  utilisateurExiste,
+  referentiel
+) => {
+  try {
+    Utilisateur.valideDonnees(donneesRequete, referentiel, utilisateurExiste);
+    return { donneesInvalides: false };
+  } catch (erreur) {
+    return { donneesInvalides: true, messageErreur: erreur.message };
+  }
+};
+
+module.exports = {
+  obtentionDonneesDeBaseUtilisateur,
+  messageErreurDonneesUtilisateur,
+};
