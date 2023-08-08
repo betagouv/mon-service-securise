07e8a7e0c7a60dd30c5ec34c51e7c06dbf4f32a7
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-08-08 11:44:54 +0200

    Pose un cookie pour sauvegarder l'état ouvert/fermé du menu
    Il sera lu par le serveur pour rendre le menu ouvert ou fermé en mode « server-side rendering ».

--- public/scripts/menuNavigation.js
+++ public/scripts/menuNavigation.js
@@ -2,9 +2,29 @@ $(() => {
   const $menu = $('.menu-navigation');
   const $repliMenu = $('.repli-menu', $menu);
 
+  const cookie = () => {
+    const cookieAvecAge = (ageEnSecondes) =>
+      `etat-menu-navigation=ferme; path=/; max-age=${ageEnSecondes}`;
+    const unAn = 3600 * 24 * 365;
+    return {
+      poserPourUnAn: () => (document.cookie = cookieAvecAge(unAn)),
+      supprimer: () => (document.cookie = cookieAvecAge(-1)),
+    };
+  };
+
+  const persistance = {
+    fermer: () => cookie().poserPourUnAn(),
+    ouvrir: () => cookie().supprimer(),
+  };
+
   $repliMenu.on('click', () => {
     const menuOuvert = !$menu.hasClass('ferme');
-    if (menuOuvert) $menu.addClass('ferme');
-    else $menu.removeClass('ferme');
+    if (menuOuvert) {
+      $menu.addClass('ferme');
+      persistance.fermer();
+    } else {
+      $menu.removeClass('ferme');
+      persistance.ouvrir();
+    }
   });
 });
