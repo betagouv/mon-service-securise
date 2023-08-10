8e132dcd90ffb88c5939eca75858b92a7c7196c7
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-08-10 11:41:42 +0200

    Passe le service en `async/await`

--- src/modeles/autorisations/ajoutContributeurSurService.js
+++ src/modeles/autorisations/ajoutContributeurSurService.js
@@ -10,124 +10,100 @@ const ajoutContributeurSurService = ({
   adaptateurMail,
   adaptateurTracking,
 }) => {
-  const verifieSuccesEnvoiMessage = (promesseEnvoiMessage, utilisateur) =>
-    promesseEnvoiMessage
-      .then(() => utilisateur)
-      .catch(() =>
-        depotDonnees
-          .supprimeUtilisateur(utilisateur.id)
-          .then(() => Promise.reject(new EchecEnvoiMessage()))
-      );
-
-  const envoieMessageInvitationContribution = (
-    emetteur,
-    contributeur,
-    service
-  ) =>
-    adaptateurMail
-      .envoieMessageInvitationContribution(
-        contributeur.email,
-        emetteur.prenomNom(),
-        service.nomService(),
-        service.id
-      )
-      .then(() => contributeur);
-
-  const envoieMessageInvitationInscription = (
-    emetteur,
-    contributeur,
-    service
-  ) =>
-    verifieSuccesEnvoiMessage(
-      adaptateurMail.envoieMessageInvitationInscription(
-        contributeur.email,
-        emetteur.prenomNom(),
-        service.nomService(),
-        contributeur.idResetMotDePasse
-      ),
-      contributeur
-    );
+  const supprimeUtilisateurSiEchec = async (
+    promesseEnvoiMessage,
+    utilisateur
+  ) => {
+    try {
+      await promesseEnvoiMessage;
+      return utilisateur;
+    } catch (e) {
+      await depotDonnees.supprimeUtilisateur(utilisateur.id);
+      throw new EchecEnvoiMessage();
+    }
+  };
 
   return {
-    executer: (idEmetteur, emailContributeur, idService) => {
-      const verifiePermission = (...params) =>
-        depotDonnees
-          .autorisationPour(...params)
-          .then((a) =>
-            a.permissionAjoutContributeur
-              ? Promise.resolve()
-              : Promise.reject(new EchecAutorisation())
-          );
+    executer: async (idEmetteur, emailContributeur, idService) => {
+      const verifiePermission = async (...params) => {
+        const a = await depotDonnees.autorisationPour(...params);
+        if (!a.permissionAjoutContributeur) throw new EchecAutorisation();
+      };
 
-      const verifieAutorisationInexistante = (...params) =>
-        depotDonnees
-          .autorisationExiste(...params)
-          .then((existe) =>
-            existe
-              ? Promise.reject(
-                  new ErreurAutorisationExisteDeja("L'autorisation existe déjà")
-                )
-              : Promise.resolve()
-          );
+      const verifieAutorisationInexistante = async (...params) => {
+        const existe = await depotDonnees.autorisationExiste(...params);
+        if (existe)
+          throw new ErreurAutorisationExisteDeja("L'autorisation existe déjà");
+      };
 
-      const creeContributeurSiNecessaire = (contributeurExistant) =>
-        contributeurExistant
-          ? Promise.resolve(contributeurExistant)
-          : depotDonnees
-              .nouvelUtilisateur({
-                email: emailContributeur,
-                infolettreAcceptee: false,
-              })
-              .then((utilisateur) =>
-                adaptateurMail
-                  .creeContact(emailContributeur, '', '', true)
-                  .then(() => utilisateur)
-              );
+      const creeContributeurSiNecessaire = async (contributeurExistant) => {
+        if (contributeurExistant) return contributeurExistant;
 
-      const informeContributeur = (
+        const utilisateur = await depotDonnees.nouvelUtilisateur({
+          email: emailContributeur,
+          infolettreAcceptee: false,
+        });
+        await adaptateurMail.creeContact(emailContributeur, '', '', true);
+
+        return utilisateur;
+      };
+
+      const informeContributeur = async (
         contributeurAInformer,
-        contributeurExistant
-      ) =>
-        Promise.all([
+        contributeurEstExistant
+      ) => {
+        const [emetteur, service] = await Promise.all([
           depotDonnees.utilisateur(idEmetteur),
           depotDonnees.homologation(idService),
-        ]).then(([emetteur, service]) =>
-          contributeurExistant
-            ? envoieMessageInvitationContribution(
-                emetteur,
-                contributeurAInformer,
-                service
-              )
-            : envoieMessageInvitationInscription(
-                emetteur,
-                contributeurAInformer,
-                service
-              )
-        );
+        ]);
 
-      const inviteContributeur = (contributeurExistant) =>
-        verifieAutorisationInexistante(contributeurExistant?.id, idService)
-          .then(() =>
-            creeContributeurSiNecessaire(contributeurExistant, idService)
-          )
-          .then((c) => informeContributeur(c, contributeurExistant));
-
-      return verifiePermission(idEmetteur, idService)
-        .then(() => depotDonnees.utilisateurAvecEmail(emailContributeur))
-        .then(inviteContributeur)
-        .then((c) =>
-          depotDonnees.ajouteContributeurAHomologation(c.id, idService)
-        )
-        .then(() =>
-          ServiceTracking.creeService()
-            .nombreMoyenContributeursPourUtilisateur(depotDonnees, idEmetteur)
-            .then((nombreMoyenContributeurs) =>
-              adaptateurTracking.envoieTrackingInvitationContributeur(
-                emailContributeur,
-                { nombreMoyenContributeurs }
-              )
+        if (contributeurEstExistant)
+          await adaptateurMail.envoieMessageInvitationContribution(
+            contributeurAInformer.email,
+            emetteur.prenomNom(),
+            service.nomService(),
+            service.id
+          );
+        else
+          await supprimeUtilisateurSiEchec(
+            adaptateurMail.envoieMessageInvitationInscription(
+              contributeurAInformer.email,
+              emetteur.prenomNom(),
+              service.nomService(),
+              contributeurAInformer.idResetMotDePasse
             )
+          );
+      };
+
+      const inviteContributeur = async (contributeurExistant) => {
+        await verifieAutorisationInexistante(
+          contributeurExistant?.id,
+          idService
         );
+        const c = await creeContributeurSiNecessaire(
+          contributeurExistant,
+          idService
+        );
+        await informeContributeur(c, contributeurExistant);
+        return c;
+      };
+
+      await verifiePermission(idEmetteur, idService);
+      const contributeur = await depotDonnees.utilisateurAvecEmail(
+        emailContributeur
+      );
+      const c = await inviteContributeur(contributeur);
+      await depotDonnees.ajouteContributeurAHomologation(c.id, idService);
+
+      const nombreMoyenContributeurs =
+        await ServiceTracking.creeService().nombreMoyenContributeursPourUtilisateur(
+          depotDonnees,
+          idEmetteur
+        );
+      await adaptateurTracking.envoieTrackingInvitationContributeur(
+        emailContributeur,
+        { nombreMoyenContributeurs }
+      );
     },
   };
 };
