e0257ce74f66e63a72df50cc2929ef423c7ad20e
ThibaudMZN <thibaud.marazano@gmail.com>
2023-09-06 10:51:11 +0200

    Remplace le fonctionnement du tiroir contributeurs avec `Svelte`

---
+++ svelte/lib/gestionContributeurs.d.ts
@@ -0,0 +1,26 @@
+// Axios est importé globalement depuis le pug
+// On veut utiliser Axios ici car il est configuré pour utiliser le token CSRF
+declare global {
+  const axios: any;
+}
+
+export type GestionContributeursProps = {
+  services: Service[];
+};
+
+export type Service = {
+  id: string;
+  createur: Utilisateur;
+  contributeurs: Utilisateur[];
+  permissions: {
+    suppressionContributeur: boolean;
+  };
+  nomService: string;
+};
+
+export type Utilisateur = {
+  id: string;
+  prenomNom: string;
+  initiales: string;
+  poste: string;
+};
