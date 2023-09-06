d26d3283763889dd4eaaf2445e703fcdfb69bbd5
ThibaudMZN <thibaud.marazano@gmail.com>
2023-09-06 11:25:22 +0200

    Remplace la gestion de l'état du tiroir par un `store`

--- svelte/lib/gestionContributeurs.store.ts
+++ svelte/lib/gestionContributeurs.store.ts
@@ -1,11 +1,18 @@
 import { writable } from 'svelte/store';
+import type { Utilisateur } from './gestionContributeurs.d';
+
+type Etape = 'ListeContributeurs' | 'SuppressionContributeur';
 
 type EtatGestionContributeursStore = {
   idMenuOuvert: string;
+  etapeCourante: Etape;
+  utilisateurEnCoursDeSuppression: Utilisateur;
 };
 
 let { subscribe, update } = writable<EtatGestionContributeursStore>({
   idMenuOuvert: null,
+  etapeCourante: 'ListeContributeurs',
+  utilisateurEnCoursDeSuppression: null,
 });
 
 export const gestionContributeursStore = {
@@ -14,6 +21,21 @@ export const gestionContributeursStore = {
     update((valeurActuelle) => ({
       ...valeurActuelle,
       idMenuOuvert: idUtilisateur,
+    }));
+  },
+  afficheEtapeSuppression: (utilisateur: Utilisateur) => {
+    update((valeurActuelle) => ({
+      ...valeurActuelle,
+      etapeCourante: 'SuppressionContributeur',
+      utilisateurEnCoursDeSuppression: utilisateur,
+      idMenuOuvert: null,
+    }));
+  },
+  afficheEtapeListe: () => {
+    update((valeurActuelle) => ({
+      ...valeurActuelle,
+      etapeCourante: 'ListeContributeurs',
+      utilisateurEnCoursDeSuppression: null,
     }));
   },
 };
