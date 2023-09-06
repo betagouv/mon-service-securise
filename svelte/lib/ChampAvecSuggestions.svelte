a30231fb667742c44e91338650d5dd7279ac3214
ThibaudMZN <thibaud.marazano@gmail.com>
2023-09-06 16:25:52 +0200

    Remplace la gestion de l'invitation de contributeurs par un composant `Svelte`

---
+++ svelte/lib/ChampAvecSuggestions.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import type { Utilisateur } from './gestionContributeurs.d';
+
+  import { createEventDispatcher } from 'svelte';
+
+  export let callbackDeRecherche: (recherche: string) => Promise<Utilisateur[]>;
+  export let dureeDebounceEnMs = 300;
+
+  let saisie = '';
+  let minuteur: NodeJS.Timeout;
+  let suggestions: Utilisateur[] = [];
+
+  const envoiEvenement = createEventDispatcher();
+
+  const REGEX_EMAIL = /[\w]+@[\w]{2,}\.[\w]{2,}/i;
+  $: proposeAjout = REGEX_EMAIL.test(saisie);
+  $: suggestionsVisibles = saisie && (suggestions.length > 0 || proposeAjout);
+
+  const rechercheSuggestions = async () => {
+    if (saisie.length > 2) {
+      suggestions = await callbackDeRecherche(saisie);
+    }
+  };
+
+  const avecTemporisation = (fonction: () => Promise<any>) => {
+    clearTimeout(minuteur);
+    minuteur = setTimeout(async () => {
+      await fonction();
+    }, dureeDebounceEnMs);
+  };
+
+  const choisisContributeur = (donnees: Record<string, any>) => {
+    saisie = '';
+    envoiEvenement('contributeurChoisi', donnees);
+  };
+</script>
+
+<div class="conteneur-suggestions">
+  <input
+    type="text"
+    on:input={() => avecTemporisation(rechercheSuggestions)}
+    bind:value={saisie}
+    autocomplete="off"
+  />
+  <div class="liste-suggestions" class:visible={suggestionsVisibles}>
+    {#if proposeAjout}
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div
+        class="create option-ajout"
+        on:click={() =>
+          choisisContributeur({ email: saisie, initiales: '', prenomNom: saisie })}
+      >
+        Ajouter ce contributeur
+      </div>
+    {/if}
+    {#each suggestions as suggestion}
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div
+        class="option suggestion-contributeur"
+        on:click={() => choisisContributeur(suggestion)}
+      >
+        <div class="initiales contributeur" class:persona={!suggestion.initiales}>
+          {suggestion.initiales}
+        </div>
+        <div>{@html suggestion.prenomNom}</div>
+      </div>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .conteneur-suggestions {
+    position: relative;
+  }
+
+  .liste-suggestions {
+    display: none;
+    position: absolute;
+    background: white;
+    width: calc(100% - 0.4em);
+    z-index: 1;
+    border-bottom-left-radius: 6px;
+    border-bottom-right-radius: 6px;
+    transform: translateY(-5px);
+  }
+
+  .visible {
+    display: block;
+    border: 1px solid var(--bleu-survol);
+  }
+
+  .option:last-of-type {
+    border-bottom-left-radius: 6px;
+    border-bottom-right-radius: 6px;
+  }
+</style>
---
+++ svelte/lib/InvitationContributeur.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+  import type { Service, Utilisateur } from './gestionContributeurs.d';
+
+  import { gestionContributeursStore } from './gestionContributeurs.store';
+  import ChampAvecSuggestions from './ChampAvecSuggestions.svelte';
+
+  type Etape = 'Ajout' | 'EnvoiEnCours' | 'Rapport';
+
+  export let service: Service;
+
+  let contributeursAInviter: Utilisateur[] = [];
+  let etapeCourante: Etape = 'Ajout';
+
+  $: afficheLesBoutonsAction = $gestionContributeursStore.etapeCourante === 'InvitationContributeurs';
+
+  const rechercheContributeurs = async (recherche: string) => {
+    const reponse = await axios.get('/api/annuaire/contributeurs', {
+      params: { recherche },
+    });
+
+    return reponse.data.suggestions;
+  };
+
+  const ajouteInvitation = (evenement: CustomEvent<Utilisateur>) => {
+    gestionContributeursStore.afficheEtapeInvitation();
+    if (!contributeursAInviter.find((c) => c.email === evenement.detail.email))
+      contributeursAInviter = [...contributeursAInviter, evenement.detail];
+  };
+
+  const supprimeInvitation = (contributeur: Utilisateur) => {
+    contributeursAInviter = contributeursAInviter.filter(
+      (c) => c.email !== contributeur.email
+    );
+  };
+
+  const envoiInvitation = async () => {
+    etapeCourante = 'EnvoiEnCours';
+    const emails = contributeursAInviter.map((c) => c.email);
+    await Promise.all(
+      emails.map((emailContributeur) =>
+        axios.post('/api/autorisation', {
+          emailContributeur,
+          idServices: [service.id],
+        })
+      )
+    );
+    contributeursAInviter = [];
+    etapeCourante = 'Rapport';
+    document.body.dispatchEvent(new CustomEvent('jquery-recharge-services'));
+  };
+</script>
+
+{#if etapeCourante === 'Ajout'}
+  <form class="conteneur-formulaire" on:submit|preventDefault>
+    <label for="email-invitation-collaboration">
+      Ajouter un ou plusieurs contributeurs
+      <ChampAvecSuggestions
+        callbackDeRecherche={rechercheContributeurs}
+        on:contributeurChoisi={ajouteInvitation}
+      />
+      <ul id="liste-ajout-contributeur">
+        {#each contributeursAInviter as contributeur (contributeur.email)}
+          <li class="contributeur-a-inviter">
+            <div
+              class="initiales contributeur"
+              class:persona={!contributeur.initiales}
+            >
+              {contributeur.initiales}
+            </div>
+            <span>{@html contributeur.prenomNom}</span>
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <img
+              class="bouton-suppression-contributeur"
+              src="/statique/assets/images/icone_supprimer_gris.svg"
+              alt="bouton de suppression d'un contributeur"
+              on:click={() => supprimeInvitation(contributeur)}
+            />
+          </li>
+        {/each}
+      </ul>
+    </label>
+    {#if afficheLesBoutonsAction}
+      <div class="conteneur-actions">
+        <button class="bouton bouton-secondaire fermeture-tiroir" type="button" on:click={() => {
+          contributeursAInviter = [];
+          gestionContributeursStore.afficheEtapeListe()
+        }}>
+          Annuler
+        </button>
+        {#if contributeursAInviter.length}
+          <button
+            class="bouton"
+            id="action-invitation"
+            type="button"
+            on:click={() => envoiInvitation()}
+          >
+            Envoyer une invitation
+          </button>
+        {/if}
+      </div>
+    {/if}
+  </form>
+{:else if etapeCourante === 'EnvoiEnCours'}
+  <div class="conteneur-loader">
+    <div class="icone-chargement"></div>
+    <div class="titre-loader">Envoi en cours...</div>
+    <div class="info-loader">Merci de ne pas rafraîchir la page</div>
+  </div>
+{:else if etapeCourante === 'Rapport'}
+  <div class="conteneur-rapport">
+    <img
+      src="/statique/assets/images/image_succes_invitation.svg"
+      alt="Icône envoi email"
+    />
+    <p>Un e-mail d'invitation a bien été envoyé.</p>
+    <button
+      class="bouton"
+      id="retour-liste-contributeurs"
+      type="button"
+      on:click={() => {
+        etapeCourante = 'Ajout';
+        gestionContributeursStore.afficheEtapeListe();
+      }}
+    >
+      Revenir à la liste des contributeurs
+    </button>
+  </div>
+{/if}
