ea692cf2dbae1b58c85daeca6c0ff0bb56214613
ThibaudMZN <thibaud.marazano@gmail.com>
2023-09-08 08:59:53 +0200

    Reinitialise le store au montage du composant

--- svelte/lib/gestionContributeurs.ts
+++ svelte/lib/gestionContributeurs.ts
@@ -1,5 +1,6 @@
 import GestionContributeurs from './GestionContributeurs.svelte';
 import type { GestionContributeursProps } from './gestionContributeurs.d';
+import { gestionContributeursStore } from './gestionContributeurs.store';
 
 document.body.addEventListener(
   'svelte-recharge-contributeurs',
@@ -8,8 +9,13 @@ document.body.addEventListener(
 
 let app: GestionContributeurs;
 
+const reinitialiseStore = () => {
+  gestionContributeursStore.reinitialise();
+};
+
 const rechargeApp = (props: GestionContributeursProps) => {
   app?.$destroy();
+  reinitialiseStore();
   app = new GestionContributeurs({
     target: document.getElementById('conteneur-gestion-contributeurs'),
     props,
