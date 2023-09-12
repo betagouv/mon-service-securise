e7b3ebea52c2ca61c54598749baf7a3eaf6d8d24
ThibaudMZN <thibaud.marazano@gmail.com>
2023-09-12 16:20:42 +0200

    Intègre une notion de droit au modèle d'autorisation

--- test/modeles/autorisations/autorisationBase.spec.js
+++ test/modeles/autorisations/autorisationBase.spec.js
@@ -1,6 +1,13 @@
 const expect = require('expect.js');
 
 const AutorisationBase = require('../../../src/modeles/autorisations/autorisationBase');
+const {
+  Permissions,
+  Rubriques,
+} = require('../../../src/modeles/autorisations/gestionDroits');
+
+const { ECRITURE, LECTURE, INVISIBLE } = Permissions;
+const { DECRIRE, SECURISER } = Rubriques;
 
 describe('Une autorisation de base', () => {
   it("ne permet pas d'ajouter un contributeur", () => {
@@ -16,5 +23,56 @@ describe('Une autorisation de base', () => {
   it('ne permet pas de supprimer un service', () => {
     const autorisation = new AutorisationBase();
     expect(autorisation.permissionSuppressionService).to.be(false);
+  });
+
+  it("permet de savoir s'il y a une permission en lecture sur une rubrique", () => {
+    const autorisation = new AutorisationBase({
+      droits: { [DECRIRE]: LECTURE },
+    });
+
+    expect(autorisation.aLaPermission(LECTURE, DECRIRE)).to.be(true);
+  });
+
+  it("permet de savoir s'il y a une permission en écriture sur une rubrique", () => {
+    const autorisation = new AutorisationBase({
+      droits: { [DECRIRE]: ECRITURE },
+    });
+
+    expect(autorisation.aLaPermission(ECRITURE, DECRIRE)).to.be(true);
+  });
+
+  it("autorise la lecture sur une rubrique avec droits d'écriture", () => {
+    const autorisationEcriture = new AutorisationBase({
+      droits: { [DECRIRE]: ECRITURE },
+    });
+
+    const peutLire = autorisationEcriture.aLaPermission(LECTURE, DECRIRE);
+
+    expect(peutLire).to.be(true);
+  });
+
+  it("n'autorise pas l'accès à une rubrique non référencée", () => {
+    const autorisationSecuriser = new AutorisationBase({
+      droits: { [SECURISER]: ECRITURE },
+    });
+
+    const peutLireDecrire = autorisationSecuriser.aLaPermission(
+      LECTURE,
+      DECRIRE
+    );
+
+    expect(peutLireDecrire).to.be(false);
+  });
+
+  it("n'autorise aucun accès pour un niveau invisible", () => {
+    const niveauInvisible = new AutorisationBase({
+      droits: { [DECRIRE]: INVISIBLE },
+    });
+
+    const peutLire = niveauInvisible.aLaPermission(LECTURE, DECRIRE);
+    const peutEcrire = niveauInvisible.aLaPermission(ECRITURE, DECRIRE);
+
+    expect(peutLire).to.be(false);
+    expect(peutEcrire).to.be(false);
   });
 });
