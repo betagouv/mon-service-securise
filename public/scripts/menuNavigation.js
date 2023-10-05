9214e6fb20dcf7f393c758ff45886207f2bf24d0
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-10-05 15:05:44 +0200

    Extrais `tiroirContributeur`
    Ça augmente la cohésion, pour préparer l'arrivée du branchement d'un tiroir pour le téléchargement des PDF.

--- public/scripts/menuNavigation.js
+++ public/scripts/menuNavigation.js
@@ -1,12 +1,41 @@
 import { gestionnaireTiroir } from '../modules/tableauDeBord/gestionnaireTiroir.mjs';
 import ActionContributeurs from '../modules/tableauDeBord/actions/ActionContributeurs.mjs';
 
+const tiroirContributeur = (idService) => {
+  const contributeurs = new ActionContributeurs();
+  let donneesService;
+  const rechargeNbContributeurs = async () => {
+    const reponse = await axios.get(`/api/service/${idService}`);
+    donneesService = reponse.data;
+    $('.nombre-contributeurs', '#gerer-contributeurs').text(
+      donneesService.contributeurs.length + 1
+    );
+  };
+
+  return {
+    brancheComportement: () => {
+      gestionnaireTiroir.brancheComportement();
+
+      if (idService) rechargeNbContributeurs();
+
+      $(document.body).on('jquery-recharge-services', async () => {
+        await rechargeNbContributeurs();
+      });
+
+      $('#gerer-contributeurs').on('click', () => {
+        gestionnaireTiroir.afficheContenuAction(
+          { action: contributeurs, estSelectionMulitple: false },
+          { donneesServices: [donneesService] }
+        );
+      });
+    },
+  };
+};
+
 $(() => {
   const $menu = $('.menu-navigation');
   const $repliMenu = $('.repli-menu', $menu);
   const idService = $('.page-service').data('id-service');
-  let donneesService;
-  const contributeurs = new ActionContributeurs();
 
   const cookie = () => {
     const cookieAvecAge = (ageEnSecondes) =>
@@ -23,31 +52,6 @@ $(() => {
     ouvrir: () => cookie().supprimer(),
   };
 
-  const rechargeNbContributeurs = async () => {
-    const reponse = await axios.get(`/api/service/${idService}`);
-    donneesService = reponse.data;
-    $('.nombre-contributeurs', '#gerer-contributeurs').text(
-      donneesService.contributeurs.length + 1
-    );
-  };
-
-  const brancheComportementTiroirContributeurs = () => {
-    gestionnaireTiroir.brancheComportement();
-
-    if (idService) rechargeNbContributeurs();
-
-    $(document.body).on('jquery-recharge-services', async () => {
-      await rechargeNbContributeurs();
-    });
-
-    $('#gerer-contributeurs').on('click', () => {
-      gestionnaireTiroir.afficheContenuAction(
-        { action: contributeurs, estSelectionMulitple: false },
-        { donneesServices: [donneesService] }
-      );
-    });
-  };
-
   $repliMenu.on('click', () => {
     const menuOuvert = !$menu.hasClass('ferme');
     if (menuOuvert) {
@@ -59,5 +63,5 @@ $(() => {
     }
   });
 
-  brancheComportementTiroirContributeurs();
+  tiroirContributeur(idService).brancheComportement();
 });
