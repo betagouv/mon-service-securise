cb89fe663acf987944bc130e0977afb55c82b4cf
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2023-10-09 13:59:08 +0200

    Permets de personnaliser les droits à l'invitation
    On index `invitations` par `Email` pour faciliter les lookups de l'invité qu'on veut personnaliser.
    
    Des fonctions sont extraites car Svelte ne supporte pas le `!` dans le corps du composant.
    
    Par exemple :
    ```
      <PersonnalisationInvitation
          invite={enPersonnalisation.!utilisateur} />
    ```
    … est interdit.

---
+++ svelte/lib/gestionContributeurs/PersonnalisationInvitation.svelte
@@ -0,0 +1,126 @@
+<script lang="ts">
+  import { store } from './gestionContributeurs.store';
+  import type {
+    Rubrique,
+    Permission,
+    Utilisateur,
+  } from './gestionContributeurs.d';
+  import LigneContributeur from './LigneContributeur.svelte';
+  import TagLectureEcriture from './TagLectureEcriture.svelte';
+  import { createEventDispatcher } from 'svelte';
+
+  export let invite: Utilisateur;
+  export let droitsOriginaux: Record<Rubrique, Permission>;
+
+  $: redefinis = { ...droitsOriginaux };
+
+  let rubriques: { id: Rubrique; nom: string; droit: Permission }[];
+  $: rubriques = [
+    { id: 'DECRIRE', nom: 'Décrire', droit: redefinis.DECRIRE },
+    { id: 'SECURISER', nom: 'Sécuriser', droit: redefinis.SECURISER },
+    { id: 'HOMOLOGUER', nom: 'Homologuer', droit: redefinis.HOMOLOGUER },
+    { id: 'RISQUES', nom: 'Risques de sécurité', droit: redefinis.RISQUES },
+    { id: 'CONTACTS', nom: 'Contacts utiles', droit: redefinis.CONTACTS },
+  ];
+
+  const dispatch = createEventDispatcher<{
+    annuler: null;
+    valider: Record<Rubrique, Permission>;
+  }>();
+</script>
+
+<div class="identite-contributeur">
+  <div class="titre">Contributeur sélectionné</div>
+  <ul class="liste-contributeurs">
+    <LigneContributeur
+      utilisateur={invite}
+      droitsModifiables={false}
+      afficheDroits={false}
+    />
+  </ul>
+</div>
+<div class="permissions">
+  <div class="titre">Permissions</div>
+  <div>Sélectionner les droits d'accès pour chaque rubrique.</div>
+  <div class="personnalisation">
+    {#each rubriques as { id, nom, droit }}
+      <div class="rubrique">
+        <div class="nom-rubrique">{nom}</div>
+        <div>
+          {#if droit !== 0}
+            <TagLectureEcriture
+              {droit}
+              on:droitChange={(e) => (redefinis[id] = e.detail)}
+            />
+          {/if}
+        </div>
+      </div>
+    {/each}
+  </div>
+</div>
+<div class="conteneur-actions">
+  <button
+    class="bouton bouton-secondaire"
+    type="button"
+    on:click={() => dispatch('annuler')}
+  >
+    Annuler
+  </button>
+  <button
+    class="bouton"
+    id="action-invitation"
+    type="button"
+    on:click={() => dispatch('valider', redefinis)}
+  >
+    Enregistrer
+  </button>
+</div>
+
+<style>
+  .liste-contributeurs {
+    list-style: none;
+    padding-left: 0;
+  }
+
+  .liste-contributeurs :global(li) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid #cbd5e1;
+    border-radius: 8px;
+    padding: 1em;
+    margin-bottom: 0.5em;
+  }
+
+  .titre {
+    font-weight: 700;
+    font-size: 1rem;
+    margin-bottom: 8px;
+  }
+
+  .permissions {
+    margin-top: 32px;
+  }
+
+  .personnalisation {
+    margin: 16px 0 40px 0;
+    display: flex;
+    flex-direction: column;
+    row-gap: 20px;
+  }
+
+  .rubrique {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .rubrique > div {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+  }
+  .rubrique .nom-rubrique {
+    font-weight: 500;
+    color: #08416a;
+  }
+</style>
