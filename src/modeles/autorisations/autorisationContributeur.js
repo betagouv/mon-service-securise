0dd4362eb1614a2f9f0bdbb50df226d378e3d3db
ThibaudMZN <thibaud.marazano@gmail.com>
2023-10-19 14:08:49 +0200

    Supprimes les classes d'autorisations filles

--- src/modeles/autorisations/autorisationContributeur.js
+++
@@ -1,9 +0,0 @@
-const AutorisationBase = require('./autorisationBase');
-
-class AutorisationContributeur extends AutorisationBase {
-  donneesAPersister() {
-    return { ...super.donneesAPersister(), type: 'contributeur' };
-  }
-}
-
-module.exports = AutorisationContributeur;
