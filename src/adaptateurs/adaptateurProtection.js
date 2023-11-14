6a4c9577b0ae3ed3c9af82442ab38cabd3d06521
ThibaudMZN <thibaud.marazano@gmail.com>
2023-11-14 10:12:44 +0100

    Extrais le nombre de requêtes maximales par minute dans une variable d'environnement

--- src/adaptateurs/adaptateurProtection.js
+++ src/adaptateurs/adaptateurProtection.js
@@ -4,31 +4,49 @@ const {
   fabriqueAdaptateurGestionErreur,
 } = require('./fabriqueAdaptateurGestionErreur');
 
+const uneMinute = 60 * 1000;
+const parametresCommuns = (typeRequete, doitFermerConnexion = false) => ({
+  windowMs: uneMinute,
+  handler: (requete, reponse) => {
+    const attaque = requete.headers['x-real-ip']?.replaceAll('.', '*');
+    fabriqueAdaptateurGestionErreur().logueErreur(
+      new Error('Limite de trafic atteinte par une IP.'),
+      {
+        typeRequete,
+        'IP de la requete': attaque,
+      }
+    );
+    if (doitFermerConnexion) reponse.end();
+    else reponse.render('erreurTropDeTrafic');
+  },
+  keyGenerator: (requete) =>
+    // Utilise l'IP de l'utilisateur : https://doc.scalingo.com/platform/internals/routing
+    requete.headers['x-real-ip'],
+});
+
 const adaptateurProtection = {
   protectionCsrf: (pointsEntreesSansProtection) =>
     csrf({ blocklist: pointsEntreesSansProtection }),
 
   protectionLimiteTrafic: () => {
-    const uneMinute = 60 * 1000;
-    const maxParFenetreParIp = 600;
+    const maxParFenetreParIp = process.env.NB_REQUETES_MAX_PAR_MINUTE ?? 0;
     const routesNonLimitees = ['/statique/', '/bibliotheques/', '/favicon.ico'];
 
     return rateLimit({
-      windowMs: uneMinute,
+      ...parametresCommuns('Navigation'),
       max: maxParFenetreParIp,
-      handler: (requete, reponse) => {
-        const attaque = requete.headers['x-real-ip']?.replaceAll('.', '*');
-        fabriqueAdaptateurGestionErreur().logueErreur(
-          new Error('Limite de trafic atteinte par une IP.'),
-          { 'IP de la requete': attaque }
-        );
-        reponse.render('erreurTropDeTrafic');
-      },
       skip: (requete) =>
         routesNonLimitees.some((r) => requete.path.startsWith(r)),
-      keyGenerator: (requete) =>
-        // Utilise l'IP de l'utilisateur : https://doc.scalingo.com/platform/internals/routing
-        requete.headers['x-real-ip'],
+    });
+  },
+
+  protectionLimiteTraficEndpointSensible: () => {
+    const maxParFenetreParIp =
+      process.env.NB_REQUETES_MAX_PAR_MINUTE_ENDPOINT_SENSIBLE ?? 0;
+
+    return rateLimit({
+      ...parametresCommuns('API', true),
+      max: maxParFenetreParIp,
     });
   },
 };
