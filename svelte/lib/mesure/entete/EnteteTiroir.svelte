617efad56ca1c6d7c14271aab57377e013d5af4e
ThibaudMZN <thibaud.marazano@gmail.com>
2024-01-02 15:10:38 +0100

    Utilise le `CartoucheReferentiel` dans l'entête du tiroir

--- svelte/lib/mesure/entete/EnteteTiroir.svelte
+++ svelte/lib/mesure/entete/EnteteTiroir.svelte
@@ -1,49 +1,17 @@
 <script lang="ts">
   import { configurationAffichage } from '../mesure.store';
+  import CartoucheReferentiel from '../../ui/CartoucheReferentiel.svelte';
 
-  const contenuTexte =
-    $configurationAffichage.contenuTexteCartoucheDuReferentiel;
+  const { referentiel, indispensable } = $configurationAffichage;
 </script>
 
-<p
-  class="referentiel"
-  class:indispensable={contenuTexte === 'Indispensable'}
-  class:recommandee={contenuTexte === 'Recommandée'}
->
-  {contenuTexte}
-</p>
+<div class="conteneur">
+  <CartoucheReferentiel {referentiel} {indispensable} />
+</div>
 
 <style>
-  p {
-    padding: 4px 10px;
+  .conteneur {
     font-size: 0.6em;
-    font-weight: 500;
-    color: #08416a;
-    border-radius: 40px;
-    background: #ffffff;
-    width: fit-content;
-    margin-top: 0;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-
-  p:is(.indispensable, .recommandee):before {
-    content: '';
-    display: flex;
-    background-image: url('/statique/assets/images/logo_mss_petit.svg');
-    background-repeat: no-repeat;
-    background-size: contain;
-    width: 19px;
-    height: 16px;
-    margin-right: 6px;
-  }
-
-  .indispensable {
-    color: #7025da;
-  }
-
-  .recommandee {
-    color: #0079d0;
+    margin-bottom: 1em;
   }
 </style>
