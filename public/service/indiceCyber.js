5f28cff139a781955fba7075efa236178cd767de
ThibaudMZN <thibaud.marazano@gmail.com>
2024-01-04 15:08:18 +0100

    Passe l'`idService` au composant Svelte

--- public/service/indiceCyber.js
+++ public/service/indiceCyber.js
@@ -1,9 +1,10 @@
 $(() => {
+  const idService = $('.page-service').data('id-service');
   const { indiceCyber, noteMax } = JSON.parse($('#indice-cyber').text());
 
   document.body.dispatchEvent(
     new CustomEvent('svelte-recharge-indice-cyber', {
-      detail: { indiceCyber, noteMax },
+      detail: { indiceCyber, noteMax, idService },
     })
   );
 });
