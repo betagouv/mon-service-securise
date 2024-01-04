bf6f1790d20942cc9a5c3b41ba738d7a80d61e17
ThibaudMZN <thibaud.marazano@gmail.com>
2024-01-04 11:56:38 +0100

    Ajoute une visualisation (avec confettis) du passage au dessus des 49% de complétude
    ... Cette animation est en accord avec la maquette animée sur Figma
    ... On ajoute un composant `Confetti` qui "s'auto-détruit" après sa durée de vie

---
+++ svelte/lib/completudeMesures/confetti/Confetti.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  const couleurs = ['#006CFA', '#05E4C5', '#ED1F7D', '#711CFF', '#FF9E2A'];
+
+  const uneCouleur = couleurs[Math.floor(Math.random() * couleurs.length)];
+  const dureeAnimation = 800;
+
+  let confettiEl: HTMLElement;
+
+  setTimeout(() => {
+    confettiEl!.parentNode!.removeChild(confettiEl);
+  }, dureeAnimation);
+
+  const random = (max: number) => Math.random() * max;
+</script>
+
+<i
+  bind:this={confettiEl}
+  style="background: {uneCouleur}; animation: bang {dureeAnimation}ms ease-out forwards; transform: translate3d({random(
+    500
+  ) - 250}px, {random(200) - 150}px, 0) rotate({random(360)}deg);"
+/>
+
+<style>
+  @keyframes -global-bang {
+    from {
+      transform: translate3d(0, 0, 0);
+      opacity: 1;
+    }
+  }
+
+  i {
+    position: absolute;
+    display: block;
+    width: 3px;
+    height: 8px;
+    opacity: 0;
+    top: 50%;
+    left: 50%;
+  }
+</style>
