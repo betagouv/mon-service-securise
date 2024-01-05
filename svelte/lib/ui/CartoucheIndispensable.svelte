956258c36a518f1e9ca91de08da718ac62d07781
ThibaudMZN <thibaud.marazano@gmail.com>
2024-01-05 11:42:02 +0100

    Ajoute un cartouche dédié à l'affichage de "Indispensable" et "Recommandé" pour le référentiel ANSSI

---
+++ svelte/lib/ui/CartoucheIndispensable.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let indispensable: boolean;
+</script>
+
+<span class:indispensable>{indispensable ? 'Indispensable' : 'Recommandé'}</span
+>
+
+<style>
+  span {
+    padding: 1px 8px 3px 8px;
+    font-size: 0.9em;
+    font-weight: 500;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: fit-content;
+    background: #f1f5f9;
+  }
+
+  span.indispensable {
+    color: #7025da;
+  }
+
+  span:not(.indispensable) {
+    color: #0079d0;
+  }
+</style>
