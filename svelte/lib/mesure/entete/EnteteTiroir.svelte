2218c8e16bcbe4fe59c75a525525ff7edcc65d3a
ThibaudMZN <thibaud.marazano@gmail.com>
2024-01-05 11:42:52 +0100

    Utilise le nouveau cartouche dans l'entête du tiroir

--- svelte/lib/mesure/entete/EnteteTiroir.svelte
+++ svelte/lib/mesure/entete/EnteteTiroir.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
+  import { Referentiel } from '../../ui/types.d';
   import { configurationAffichage } from '../mesure.store';
   import CartoucheReferentiel from '../../ui/CartoucheReferentiel.svelte';
+  import CartoucheIndispensable from '../../ui/CartoucheIndispensable.svelte';
 
   const { referentiel, indispensable } = $configurationAffichage;
 </script>
 
 <div class="conteneur">
-  <CartoucheReferentiel {referentiel} {indispensable} />
+  <CartoucheReferentiel {referentiel} />
+  {#if referentiel === Referentiel.ANSSI}
+    <CartoucheIndispensable indispensable={indispensable ?? false} />
+  {/if}
 </div>
 
 <style>
   .conteneur {
     font-size: 0.6em;
     margin-bottom: 1em;
+    display: flex;
+    flex-direction: row;
+    gap: 8px;
   }
 </style>
