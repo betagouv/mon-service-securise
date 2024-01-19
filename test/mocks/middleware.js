bee5fb10d3c4d5bdc1c7785fd745dd48b9d00dee
ThibaudMZN <thibaud.marazano@gmail.com>
2024-01-19 10:26:46 +0100

    Charge les features flags sur la route de vue des mesures

--- test/mocks/middleware.js
+++ test/mocks/middleware.js
@@ -54,6 +54,7 @@ let listesAseptisees = [];
 let listeAdressesIPsAutorisee = [];
 let parametresAseptises = [];
 let preferencesChargees = false;
+let featureFlagsCharges = false;
 let rechercheDossierCourantEffectuee = false;
 let suppressionCookieEffectuee = false;
 let traficProtege = false;
@@ -83,6 +84,7 @@ const middlewareFantaisie = {
     listeAdressesIPsAutorisee = [];
     parametresAseptises = [];
     preferencesChargees = false;
+    featureFlagsCharges = false;
     rechercheDossierCourantEffectuee = false;
     suppressionCookieEffectuee = false;
     traficProtege = false;
@@ -127,6 +129,12 @@ const middlewareFantaisie = {
   chargePreferencesUtilisateur: (_requete, reponse, suite) => {
     reponse.locals.preferencesUtilisateur = {};
     preferencesChargees = true;
+    suite();
+  },
+
+  chargeFeatureFlags: (_requete, reponse, suite) => {
+    reponse.locals.featureFlags = {};
+    featureFlagsCharges = true;
     suite();
   },
 
@@ -234,6 +242,13 @@ const middlewareFantaisie = {
   verifieChargementDesPreferences: (...params) => {
     verifieRequeteChangeEtat(
       { lectureEtat: () => preferencesChargees },
+      ...params
+    );
+  },
+
+  verifieChargementDesFeatureFlags: (...params) => {
+    verifieRequeteChangeEtat(
+      { lectureEtat: () => featureFlagsCharges },
       ...params
     );
   },
