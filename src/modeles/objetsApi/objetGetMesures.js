a2b9931a2c9abdf3cff54b368ebd2982c3061c79
ThibaudMZN <thibaud.marazano@gmail.com>
2024-01-19 13:54:52 +0100

    Modifie l'`objetGetMesures` pour utiliser la nouvelle méthode

--- src/modeles/objetsApi/objetGetMesures.js
+++ src/modeles/objetsApi/objetGetMesures.js
@@ -1,34 +1,4 @@
-const fusionneGeneraleEtPersonnalisee = (
-  mesureGenerale,
-  mesurePersonnalisee
-) => {
-  const [id, donnees] = mesurePersonnalisee;
-  delete mesureGenerale?.id;
-  return {
-    [id]: { ...donnees, ...(mesureGenerale && { ...mesureGenerale }) },
-  };
-};
-
-const donnees = (service, moteurRegles) => {
-  const { mesuresGenerales, mesuresSpecifiques } = service.mesures.toJSON();
-
-  const mesuresPersonnalisees = Object.entries(
-    moteurRegles.mesures(service.descriptionService)
-  ).reduce(
-    (acc, mesurePersonnalisee) => ({
-      ...acc,
-      ...fusionneGeneraleEtPersonnalisee(
-        mesuresGenerales.find((m) => m.id === mesurePersonnalisee[0]),
-        mesurePersonnalisee
-      ),
-    }),
-    {}
-  );
-
-  return {
-    mesuresGenerales: mesuresPersonnalisees,
-    mesuresSpecifiques,
-  };
-};
+const donnees = (service) =>
+  service.mesures.enrichiesAvecDonneesPersonnalisees();
 
 module.exports = { donnees };
--- test/modeles/objetsApi/objetGetMesures.spec.js
+++ test/modeles/objetsApi/objetGetMesures.spec.js
@@ -1,87 +1,18 @@
 const expect = require('expect.js');
 const objetGetMesures = require('../../../src/modeles/objetsApi/objetGetMesures');
 const { unService } = require('../../constructeurs/constructeurService');
-const Mesures = require('../../../src/modeles/mesures');
-const referentiel = require('../../../src/referentiel');
 
 describe("L'objet d'API de `GET /mesures`", () => {
-  it('interroge le moteur de règles pour obtenir les mesures personnalisées', async () => {
-    let descriptionRecue;
+  it('fait passe-plat avec les mesures enrichies du service', async () => {
+    let passePlatEffectue = false;
 
-    const moteurRegles = {
-      mesures: (descriptionService) => {
-        descriptionRecue = descriptionService;
-        return {};
-      },
+    const service = unService().construis();
+    service.mesures.enrichiesAvecDonneesPersonnalisees = () => {
+      passePlatEffectue = true;
     };
 
-    objetGetMesures.donnees(
-      unService().avecNomService('un service').construis(),
-      moteurRegles
-    );
+    objetGetMesures.donnees(service);
 
-    expect(descriptionRecue.nomService).to.equal('un service');
-  });
-
-  it('associe les statuts et commentaires des mesures générales du service aux mesures personnalisées', async () => {
-    const service = unService()
-      .avecMesures(
-        new Mesures(
-          {
-            mesuresGenerales: [
-              { id: 'mesureA', statut: 'fait', modalites: 'un commentaire' },
-            ],
-          },
-          referentiel.creeReferentiel({ mesures: { mesureA: {} } })
-        )
-      )
-      .construis();
-
-    const moteurRegles = {
-      mesures: () => ({ mesureA: { description: 'Mesure A' } }),
-    };
-
-    const resultat = objetGetMesures.donnees(service, moteurRegles);
-
-    expect(resultat.mesuresGenerales.mesureA).to.eql({
-      description: 'Mesure A',
-      statut: 'fait',
-      modalites: 'un commentaire',
-    });
-  });
-
-  it('ne pollue pas les mesures personnalisée avec des mesures inexistantes sur le service (undefined)', async () => {
-    const service = unService()
-      .avecMesures(new Mesures({ mesuresGenerales: [] }))
-      .construis();
-
-    const moteurRegles = {
-      mesures: () => ({ mesureA: { description: 'Mesure A' } }),
-    };
-
-    const resultat = objetGetMesures.donnees(service, moteurRegles);
-
-    expect(resultat.mesuresGenerales.mesureA).to.eql({
-      description: 'Mesure A',
-    });
-  });
-
-  it('fait passe plat sur les mesures spécifiques du service', async () => {
-    const service = unService()
-      .avecMesures(
-        new Mesures({
-          mesuresGenerales: [],
-          mesuresSpecifiques: [{ description: 'une mesure' }],
-        })
-      )
-      .construis();
-
-    const moteurRegles = {
-      mesures: () => ({}),
-    };
-
-    const resultat = objetGetMesures.donnees(service, moteurRegles);
-
-    expect(resultat.mesuresSpecifiques).to.eql([{ description: 'une mesure' }]);
+    expect(passePlatEffectue).to.be(true);
   });
 });
