674e7be440f73b06174a1e4c539ca2fe985d5883
ThibaudMZN <thibaud.marazano@gmail.com>
2024-01-24 15:03:44 +0100

    Modifie la requête pour supprimer les informations sensibles
    ... avant de l'envoyer vers Sentry

--- src/mss.js
+++ src/mss.js
@@ -259,7 +259,7 @@ const creeServeur = (
     })
   );
 
-  app.use(adaptateurGestionErreur.controleurErreurs());
+  app.use(adaptateurGestionErreur.controleurErreurs);
 
   if (avecPageErreur) {
     app.use((_erreur, _requete, reponse, _suite) => {
