21b78eb72d0de185db1a4e68aea148aeff6b91bf
ThibaudMZN <thibaud.marazano@gmail.com>
2024-02-07 11:35:06 +0100

    Supprime l'ancien objet de statistiques
    ... et corrige les appelants

--- src/modeles/mesuresGenerales.js
+++ src/modeles/mesuresGenerales.js
@@ -1,6 +1,5 @@
 const ElementsConstructibles = require('./elementsConstructibles');
 const MesureGenerale = require('./mesureGenerale');
-const StatistiquesMesures = require('./statistiquesMesures');
 
 class MesuresGenerales extends ElementsConstructibles {
   constructor(donnees, referentiel) {
@@ -32,54 +31,6 @@ class MesuresGenerales extends ElementsConstructibles {
       .filter((mesure) => mesure.statutRenseigne())
       .sort((m, _) => (m.estIndispensable() ? -1 : 1))
       .reduce(rangeMesureParStatut, accumulateur);
-  }
-
-  statistiques(mesuresPersonnalisees, mesuresSpecifiques) {
-    const stats = StatistiquesMesures.donneesAZero(
-      MesureGenerale.statutsPossibles(),
-      this.referentiel.identifiantsCategoriesMesures()
-    );
-
-    this.items.forEach((mesure) => {
-      const { id, statut } = mesure;
-      const { categorie } = this.referentiel.mesure(id);
-
-      if (statut === MesureGenerale.STATUT_FAIT) {
-        stats[categorie].misesEnOeuvre += 1;
-      }
-      if (
-        statut === MesureGenerale.STATUT_FAIT ||
-        statut === MesureGenerale.STATUT_EN_COURS
-      ) {
-        stats[categorie].retenues += 1;
-      }
-
-      if (mesure.estIndispensable()) {
-        stats[categorie].indispensables[statut] += 1;
-      } else {
-        stats[categorie].recommandees[statut] += 1;
-      }
-    });
-
-    Object.keys(mesuresPersonnalisees)
-      .map(
-        (id) =>
-          new MesureGenerale(
-            {
-              id,
-              rendueIndispensable: mesuresPersonnalisees[id].indispensable,
-            },
-            this.referentiel
-          )
-      )
-      .reduce((acc, mesure) => {
-        const { categorie } = this.referentiel.mesure(mesure.id);
-        if (mesure.estIndispensable()) acc[categorie].indispensables.total += 1;
-        if (mesure.estRecommandee()) acc[categorie].recommandees.total += 1;
-        return acc;
-      }, stats);
-
-    return new StatistiquesMesures(stats, this.referentiel, mesuresSpecifiques);
   }
 
   statutSaisie() {
