ec9442dd8805cbff98b3c40c6485f651ba1033af
ThibaudMZN <thibaud.marazano@gmail.com>
2024-02-07 11:01:18 +0100

    Ajoute une classe pour les calculs de la complétude
    ... cela nous permet de détacher l'objet complétude de l'objet "statistiques mesures". La complétude est maintenant calculée à l'instanciation et non à la volée.
    ... on modifiera les appelants dans le commit suivant

---
+++ test/modeles/completudeMesures.spec.js
@@ -0,0 +1,75 @@
+const expect = require('expect.js');
+const { CompletudeMesures } = require('../../src/modeles/completudeMesures');
+const {
+  desStatistiques,
+} = require('../constructeurs/constructeurStatistiquesMesuresGenerales');
+const Referentiel = require('../../src/referentiel');
+const MesuresSpecifiques = require('../../src/modeles/mesuresSpecifiques');
+
+describe('La complétude des mesures', () => {
+  it('calcule le nombre total de mesures en additionnant générales et spécifiques', () => {
+    const referentiel = Referentiel.creeReferentiel({
+      categoriesMesures: { gouvernance: 'Gouvernance' },
+      mesures: { A: {}, B: {}, C: {} },
+      statutsMesures: { fait: '' },
+    });
+
+    const indispensable = { id: 'A', statut: 'fait' };
+    const recommandee = { id: 'B', statut: 'fait' };
+    const autreRecommandee = { id: 'C', statut: 'fait' };
+
+    const statistiquesMesuresGenerales = desStatistiques(referentiel)
+      .surLesMesuresGenerales([indispensable, recommandee, autreRecommandee])
+      .avecMesuresPersonnalisees({
+        A: { categorie: 'gouvernance', indispensable: true },
+        B: { categorie: 'gouvernance' },
+        C: { categorie: 'gouvernance' },
+      })
+      .construis();
+
+    const mesuresSpecifiques = new MesuresSpecifiques(
+      { mesuresSpecifiques: [{ categorie: 'gouvernance', statut: 'fait' }] },
+      referentiel
+    );
+
+    const completude = new CompletudeMesures({
+      statistiquesMesuresGenerales,
+      mesuresSpecifiques,
+    });
+
+    expect(completude.completude().nombreTotalMesures).to.be(4);
+  });
+
+  it('calcule le nombre de mesures complétées : celles qui ont un statut', () => {
+    const referentiel = Referentiel.creeReferentiel({
+      categoriesMesures: { gouvernance: 'Gouvernance' },
+      mesures: { A: {}, B: {}, C: {} },
+      statutsMesures: { fait: '', enCours: '' },
+    });
+
+    const indispensable = { id: 'A', statut: 'fait' };
+    const recommandee = { id: 'B', statut: 'enCours' };
+    const recommandeeSansStatut = { C: { categorie: 'gouvernance' } };
+
+    const statistiquesMesuresGenerales = desStatistiques(referentiel)
+      .surLesMesuresGenerales([indispensable, recommandee])
+      .avecMesuresPersonnalisees({
+        A: { categorie: 'gouvernance', indispensable: true },
+        B: { categorie: 'gouvernance' },
+        ...recommandeeSansStatut,
+      })
+      .construis();
+
+    const mesuresSpecifiques = new MesuresSpecifiques(
+      { mesuresSpecifiques: [{ categorie: 'gouvernance', statut: 'fait' }] },
+      referentiel
+    );
+
+    const completude = new CompletudeMesures({
+      statistiquesMesuresGenerales,
+      mesuresSpecifiques,
+    });
+
+    expect(completude.completude().nombreMesuresCompletes).to.be(3);
+  });
+});
