e794c414a54a847bdc704a056d19a4b8cf8924ab
ThibaudMZN <thibaud.marazano@gmail.com>
2024-02-14 10:42:20 +0100

    Passe la méthode `rechercheOrganisations` en async/await

--- src/adaptateurs/adaptateurRechercheEntrepriseAPI.js
+++ src/adaptateurs/adaptateurRechercheEntrepriseAPI.js
@@ -3,28 +3,30 @@ const {
   fabriqueAdaptateurGestionErreur,
 } = require('./fabriqueAdaptateurGestionErreur');
 
-const rechercheOrganisations = (terme, departement) =>
-  axios
-    .get('https://recherche-entreprises.api.gouv.fr/search', {
-      params: {
-        q: terme,
-        ...(departement && { departement }),
-        per_page: 25,
-        page: 1,
-        limite_matching_etablissements: 1,
-        est_entrepreneur_individuel: false,
-      },
-    })
-    .then((reponse) =>
-      reponse.data.results
-        .filter((r) => r.siege.departement !== null)
-        .map((r) => ({ nom: r.nom_complet, departement: r.siege.departement }))
-    )
-    .catch((e) => {
-      fabriqueAdaptateurGestionErreur().logueErreur(e, {
-        'Erreur renvoyée par API recherche-entreprise': e.response.data,
-      });
-      return Promise.resolve([]);
+const rechercheOrganisations = async (terme, departement) => {
+  try {
+    const reponse = await axios.get(
+      'https://recherche-entreprises.api.gouv.fr/search',
+      {
+        params: {
+          q: terme,
+          ...(departement && { departement }),
+          per_page: 25,
+          page: 1,
+          limite_matching_etablissements: 1,
+          est_entrepreneur_individuel: false,
+        },
+      }
+    );
+    return reponse.data.results
+      .filter((r) => r.siege.departement !== null)
+      .map((r) => ({ nom: r.nom_complet, departement: r.siege.departement }));
+  } catch (e) {
+    fabriqueAdaptateurGestionErreur().logueErreur(e, {
+      'Erreur renvoyée par API recherche-entreprise': e.response?.data,
     });
+    return [];
+  }
+};
 
 module.exports = { rechercheOrganisations };
