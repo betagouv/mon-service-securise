43c445f1dcd8b7b89f30ea0cbc8427baead087ec
ThibaudMZN <thibaud.marazano@gmail.com>
2024-02-15 11:10:32 +0100

    Termine le carrousel pour la version mobile

---
+++ public/home.js
@@ -0,0 +1,59 @@
+$(() => {
+  const $carrousel = $('#carrousel');
+  const $conteneurLiens = $('.conteneur-liens-carrousel');
+  const $boutonsLienDirect = $('.lien-carte', $conteneurLiens);
+
+  const margeColonne = parseFloat($carrousel.css('gap').replace('px', ''));
+  const margeDebut = parseFloat(
+    $carrousel
+      .children('.carte-carrousel')
+      .first()
+      .css('margin-left')
+      .replace('px', '')
+  );
+  const margesDebutFin =
+    margeDebut +
+    parseFloat(
+      $carrousel
+        .children('.carte-carrousel')
+        .last()
+        .css('margin-right')
+        .replace('px', '')
+    );
+  const largeurCarte = $carrousel.find('.carte-carrousel').width();
+  const largeurTotale = largeurCarte + margeColonne + margesDebutFin;
+
+  const afficheLienActif = (idx) => {
+    $('.lien-carte', $conteneurLiens).removeClass('actif');
+    const cible = $conteneurLiens.children('.lien-carte')[idx];
+    if (cible) $(cible).addClass('actif');
+  };
+
+  const glisseVers = (idx) => {
+    const decallage = margeDebut + idx * (largeurCarte + margeColonne);
+    $carrousel.scrollLeft(decallage);
+  };
+
+  $carrousel.on('scroll', () => {
+    const decalageCourant = $carrousel.scrollLeft();
+    const idxSlideActif = Math.round(decalageCourant / largeurTotale);
+    afficheLienActif(idxSlideActif);
+  });
+
+  $boutonsLienDirect.on('click', (e) => {
+    const bouton = $(e.target);
+    glisseVers(bouton.data('cible'));
+  });
+
+  $('.precedent', $conteneurLiens).on('click', () => {
+    const decalageCourant = $carrousel.scrollLeft();
+    $carrousel.scrollLeft(decalageCourant - largeurCarte);
+  });
+
+  $('.suivant', $conteneurLiens).on('click', () => {
+    const decalageCourant = $carrousel.scrollLeft();
+    $carrousel.scrollLeft(decalageCourant + largeurCarte);
+  });
+
+  $carrousel.scrollLeft(0);
+});
