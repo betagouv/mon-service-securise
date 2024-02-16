d5cb26d7f0e49e5438a2975c92bd7e6392e034be
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-02-16 16:15:52 +0100

    Déplace le chiffrement Vault dans un adaptateur séparé
    On a donc deux adaptateurs de chiffrement : un sachant faire du Vault, un ne faisant que retourner ce qu'il reçoit.
    Avec la fabrique d'adaptateur, on peut ainsi déployer notre code sur un environnement où Vault n'est pas disponible.

--- src/adaptateurs/fabriqueAdaptateurChiffrement.js
+++ src/adaptateurs/fabriqueAdaptateurChiffrement.js
@@ -1,8 +1,11 @@
 const { chiffrement } = require('./adaptateurEnvironnement');
 const adaptateurChiffrement = require('./adaptateurChiffrement');
+const adaptateurChiffrementVault = require('./adaptateurChiffrementVault');
 
 const fabriqueAdaptateurChiffrement = () => {
-  if (chiffrement().utiliseChiffrementVault()) return adaptateurChiffrement;
+  if (chiffrement().utiliseChiffrementVault())
+    return adaptateurChiffrementVault;
+
   return adaptateurChiffrement;
 };
 
