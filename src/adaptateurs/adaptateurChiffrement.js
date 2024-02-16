381ed3c2ae94c08eb822b00fc8b60d3d145cd628
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-02-16 16:15:52 +0100

    Déplace le chiffrement Vault dans un adaptateur séparé
    On a donc deux adaptateurs de chiffrement : un sachant faire du Vault, un ne faisant que retourner ce qu'il reçoit.
    Avec la fabrique d'adaptateur, on peut ainsi déployer notre code sur un environnement où Vault n'est pas disponible.

--- src/adaptateurs/adaptateurChiffrement.js
+++ src/adaptateurs/adaptateurChiffrement.js
@@ -1,50 +1,9 @@
-const axios = require('axios');
 const { createHash } = require('crypto');
 const bcrypt = require('bcrypt');
-const { chiffrement } = require('./adaptateurEnvironnement');
 
-const chiffre = async (chaineOuObjet) => {
-  const env = chiffrement();
-  const base = env.urlBaseDuService();
-  const cleTransit = env.cleDuMoteurTransit();
+const chiffre = async (chaineOuObjet) => chaineOuObjet;
 
-  const brut = JSON.stringify(chaineOuObjet);
-  const base64 = Buffer.from(brut).toString('base64');
-
-  const reponse = await axios.put(
-    `${base}/v1/transit/encrypt/${cleTransit}`,
-    { plaintext: base64 },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Vault-Token': env.tokenVault(),
-      },
-    }
-  );
-
-  return reponse.data.data.ciphertext;
-};
-
-const dechiffre = async (chaineChiffree) => {
-  const env = chiffrement();
-  const base = env.urlBaseDuService();
-  const cleTransit = env.cleDuMoteurTransit();
-
-  const reponse = await axios.put(
-    `${base}/v1/transit/decrypt/${cleTransit}`,
-    { ciphertext: chaineChiffree },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Vault-Token': env.tokenVault(),
-      },
-    }
-  );
-
-  const base64 = Buffer.from(reponse.data.data.plaintext, 'base64');
-  const brut = base64.toString('utf-8');
-  return JSON.parse(brut);
-};
+const dechiffre = async (chaineChiffree) => chaineChiffree;
 
 const NOMBRE_DE_PASSES = 10;
 const hacheBCrypt = (chaineEnClair) =>
---
+++ src/adaptateurs/adaptateurChiffrementVault.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const { chiffrement } = require('./adaptateurEnvironnement');
+const {
+  compareBCrypt,
+  hacheBCrypt,
+  hacheSha256,
+  nonce,
+} = require('./adaptateurChiffrement');
+
+const chiffre = async (chaineOuObjet) => {
+  const env = chiffrement();
+  const base = env.urlBaseDuService();
+  const cleTransit = env.cleDuMoteurTransit();
+
+  const brut = JSON.stringify(chaineOuObjet);
+  const base64 = Buffer.from(brut).toString('base64');
+
+  const reponse = await axios.put(
+    `${base}/v1/transit/encrypt/${cleTransit}`,
+    { plaintext: base64 },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Vault-Token': env.tokenVault(),
+      },
+    }
+  );
+
+  return reponse.data.data.ciphertext;
+};
+
+const dechiffre = async (chaineChiffree) => {
+  const env = chiffrement();
+  const base = env.urlBaseDuService();
+  const cleTransit = env.cleDuMoteurTransit();
+
+  const reponse = await axios.put(
+    `${base}/v1/transit/decrypt/${cleTransit}`,
+    { ciphertext: chaineChiffree },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Vault-Token': env.tokenVault(),
+      },
+    }
+  );
+
+  const base64 = Buffer.from(reponse.data.data.plaintext, 'base64');
+  const brut = base64.toString('utf-8');
+  return JSON.parse(brut);
+};
+
+module.exports = {
+  chiffre,
+  dechiffre,
+  compareBCrypt,
+  hacheBCrypt,
+  hacheSha256,
+  nonce,
+};
