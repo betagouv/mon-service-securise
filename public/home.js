0f63cadea882aa74c33ff9f1bcecbc0ca2c555bf
ThibaudMZN <thibaud.marazano@gmail.com>
2024-02-19 14:32:00 +0100

    Ajoute le carrousel des témoignages

--- public/home.js
+++ public/home.js
@@ -2,49 +2,8 @@ const brancheComportementCarrousel = (selecteurCarrousel) => {
   const $conteneur = $(selecteurCarrousel);
   const $carrousel = $('.conteneur-carrousel', $conteneur);
   const $conteneurLiens = $('.conteneur-liens-carrousel', $conteneur);
-  const $boutonsLienDirect = $('.lien-carte', $conteneurLiens);
 
-  const margeColonne = parseFloat($carrousel.css('gap').replace('px', ''));
-  const margeDebut = parseFloat(
-    $carrousel
-      .children('.carte-carrousel')
-      .first()
-      .css('margin-left')
-      .replace('px', '')
-  );
-  const margesDebutFin =
-    margeDebut +
-    parseFloat(
-      $carrousel
-        .children('.carte-carrousel')
-        .last()
-        .css('margin-right')
-        .replace('px', '')
-    );
-  const largeurCarte = $carrousel.find('.carte-carrousel').width();
-  const largeurTotale = largeurCarte + margeColonne + margesDebutFin;
-
-  const afficheLienActif = (idx) => {
-    $('.lien-carte', $conteneurLiens).removeClass('actif');
-    const cible = $conteneurLiens.children('.lien-carte')[idx];
-    if (cible) $(cible).addClass('actif');
-  };
-
-  const glisseVers = (idx) => {
-    const decallage = margeDebut + idx * (largeurCarte + margeColonne);
-    $carrousel.scrollLeft(decallage);
-  };
-
-  $carrousel.on('scroll', () => {
-    const decalageCourant = $carrousel.scrollLeft();
-    const idxSlideActif = Math.round(decalageCourant / largeurTotale);
-    afficheLienActif(idxSlideActif);
-  });
-
-  $boutonsLienDirect.on('click', (e) => {
-    const bouton = $(e.target);
-    glisseVers(bouton.data('cible'));
-  });
+  const largeurCarte = $($carrousel.children()[0]).width();
 
   $('.precedent', $conteneurLiens).on('click', () => {
     const decalageCourant = $carrousel.scrollLeft();
@@ -61,4 +20,5 @@ const brancheComportementCarrousel = (selecteurCarrousel) => {
 
 $(() => {
   brancheComportementCarrousel('#carrousel-presentation');
+  brancheComportementCarrousel('#carrousel-temoignages');
 });
