2aa7cbd43598d06f579d9ea1ba4badfc816cc896
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-02-27 14:04:26 +0100

    Améliore la robustesse de l'abonné
    Avec le bus, les abonnés peuvent être déclenchés par divers événements.
    On pourrait donc les déclencher avec un événement qui ne contient pas les propriétés nécessaires.
    Il est plus prudent de vérifier explicitement ces propriétés dans l'abonné.

--- src/bus/abonnements/consigneAutorisationsModifieesDansJournal.js
+++ src/bus/abonnements/consigneAutorisationsModifieesDansJournal.js
@@ -2,8 +2,17 @@ const {
   EvenementCollaboratifServiceModifie,
 } = require('../../modeles/journalMSS/evenementCollaboratifServiceModifie');
 
+const leveException = (raison) => {
+  throw new Error(
+    `Impossible de consigner des autorisations modifées dans le journal MSS sans avoir ${raison} en paramètre.`
+  );
+};
+
 function consigneAutorisationsModifieesDansJournal({ adaptateurJournal }) {
   return async ({ idService, autorisations }) => {
+    if (!idService) leveException("l'ID du service");
+    if (!autorisations) leveException('les autorisations');
+
     const collaboratifServiceModifie = new EvenementCollaboratifServiceModifie({
       idService,
       autorisations,
