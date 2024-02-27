0023fb2e2550c1de62cf0228ef584918fbc517ed
ThibaudMZN <thibaud.marazano@gmail.com>
2024-02-27 14:22:46 +0100

    Ajoute la méthode `genereTamponHomologation` à l'adaptateur PDF
    ... en plus de générer les images `.png`, l'adaptateur génère des fichiers `.html` avec les images en base64, ainsi qu'un fichier texte d'instructions

--- src/adaptateurs/adaptateurPdf.js
+++ src/adaptateurs/adaptateurPdf.js
@@ -1,9 +1,16 @@
 const { PDFDocument } = require('pdf-lib');
 const pug = require('pug');
+const { readFile } = require('fs/promises');
+const { decode } = require('html-entities');
+const { resolve } = require('path');
 const { lanceNavigateur } = require('./adaptateurPdf.puppeteer');
 const {
   fabriqueAdaptateurGestionErreur,
 } = require('./fabriqueAdaptateurGestionErreur');
+const {
+  instructionsTamponHomologation,
+  configurationsDispositifs,
+} = require('./adaptateurPdf.configurationTamponHomologation');
 
 const formatPdfA4 = (enteteHtml, piedPageHtml) => ({
   format: 'A4',
@@ -141,8 +148,84 @@ const genereSyntheseSecurite = async (donnees) => {
   }
 };
 
+const genereTamponHomologation = async (donnees) => {
+  let navigateur = null;
+  try {
+    navigateur = await lanceNavigateur();
+    const compileImageEnHTMLBase64 = (buffer, largeur, hauteur) =>
+      pug.compileFile('src/pdf/modeles/tamponHomologation.base64.pug')({
+        base64: buffer.toString('base64'),
+        largeur,
+        hauteur,
+      });
+
+    const fichiers = [];
+    /* eslint-disable no-await-in-loop */
+    /* eslint-disable no-restricted-syntax */
+    for (const {
+      tailleDispositif,
+      taillesTexte,
+      largeur,
+      hauteur,
+    } of configurationsDispositifs) {
+      const corps = pug.compileFile('src/pdf/modeles/tamponHomologation.pug')({
+        ...donnees,
+        taillesTexte,
+        tailleDispositif,
+        decode,
+      });
+
+      const page = await navigateur.newPage();
+      await page.setContent(corps);
+      const screenshotBase64 = await page.screenshot({
+        encoding: 'base64',
+        type: 'png',
+        omitBackground: true,
+        clip: { width: largeur, height: hauteur, x: 0, y: 0, scale: 4 },
+      });
+
+      const bufferImage = Buffer.from(screenshotBase64, 'base64');
+      fichiers.push({
+        nom: `cartoucheHomologation.${tailleDispositif}.png`,
+        buffer: bufferImage,
+      });
+
+      fichiers.push({
+        nom: `cartoucheHomologation.${tailleDispositif}.html`,
+        buffer: compileImageEnHTMLBase64(bufferImage, largeur, hauteur),
+      });
+    }
+    /* eslint-enable no-await-in-loop */
+    /* eslint-enable no-restricted-syntax */
+    fichiers.push({
+      nom: 'instructions.txt',
+      buffer: instructionsTamponHomologation,
+    });
+
+    const imageTamponHomologation = await readFile(
+      resolve(__dirname, '../../public/assets/images/tampon_homologation.png')
+    );
+    fichiers.push({
+      nom: 'tamponHomologation.png',
+      buffer: imageTamponHomologation,
+    });
+    fichiers.push({
+      nom: `tamponHomologation.html`,
+      buffer: compileImageEnHTMLBase64(imageTamponHomologation, 214, 214),
+    });
+
+    return fichiers;
+  } catch (e) {
+    fabriqueAdaptateurGestionErreur().logueErreur(e);
+    throw e;
+  } finally {
+    if (navigateur !== null) await navigateur.close();
+  }
+};
+
 module.exports = {
   genereAnnexes,
   genereDossierDecision,
   genereSyntheseSecurite,
+  genereTamponHomologation,
 };
