c75b6a92665b2b2954f9ccfe96714160e7495252
ThibaudMZN <thibaud.marazano@gmail.com>
2024-02-28 12:01:41 +0100

    Ajoute le bouton d'ouverture du tiroir de téléchargement du tampon d'homologation

--- public/service/homologation/dossiers.js
+++ public/service/homologation/dossiers.js
@@ -1,5 +1,18 @@
 import brancheComportemenFormulaireEtape from './formulaireEtape.js';
+import ActionTelechargementTamponHomologation from '../../modules/tableauDeBord/actions/ActionTelechargementTamponHomologation.js';
+import { gestionnaireTiroir } from '../../modules/tableauDeBord/gestionnaireTiroir.mjs';
 
 $(() => {
+  const telechargementTamponHomologation =
+    new ActionTelechargementTamponHomologation();
+  const idService = $('.page-service').data('id-service');
+
+  $('#bouton-tampon-homologation').on('click', () => {
+    gestionnaireTiroir.afficheContenuAction(
+      { action: telechargementTamponHomologation },
+      { idService }
+    );
+  });
+
   brancheComportemenFormulaireEtape(() => Promise.resolve());
 });
