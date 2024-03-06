609142ed4773d4c0675ba0c05889300c1b8e670d
ThibaudMZN <thibaud.marazano@gmail.com>
2024-03-06 11:18:13 +0100

    Supprime les propriétés inutiles dans l'événement

--- src/modeles/journalMSS/erreurs.js
+++ src/modeles/journalMSS/erreurs.js
@@ -1,30 +1,20 @@
 class ErreurJournal extends Error {}
 class ErreurAutorisationsServiceManquantes extends ErreurJournal {}
 class ErreurDateHomologationManquante extends ErreurJournal {}
-class ErreurDetailMesuresManquant extends ErreurJournal {}
 class ErreurDureeHomologationManquante extends ErreurJournal {}
 class ErreurIdentifiantServiceManquant extends ErreurJournal {}
 class ErreurIdentifiantUtilisateurManquant extends ErreurJournal {}
 class ErreurIdentifiantMesureManquant extends ErreurJournal {}
 class ErreurIdentifiantRetourUtilisateurManquant extends ErreurJournal {}
-class ErreurIndiceCyberManquant extends ErreurJournal {}
-class ErreurNombreOrganisationsUtilisatricesManquant extends ErreurJournal {}
-class ErreurNombreMesuresCompletesManquant extends ErreurJournal {}
-class ErreurNombreTotalMesuresManquant extends ErreurJournal {}
 class ErreurServiceManquant extends ErreurJournal {}
 
 module.exports = {
   ErreurAutorisationsServiceManquantes,
   ErreurDateHomologationManquante,
-  ErreurDetailMesuresManquant,
   ErreurDureeHomologationManquante,
   ErreurIdentifiantServiceManquant,
   ErreurIdentifiantUtilisateurManquant,
   ErreurIdentifiantMesureManquant,
   ErreurIdentifiantRetourUtilisateurManquant,
-  ErreurIndiceCyberManquant,
-  ErreurNombreOrganisationsUtilisatricesManquant,
-  ErreurNombreMesuresCompletesManquant,
-  ErreurNombreTotalMesuresManquant,
   ErreurServiceManquant,
 };
