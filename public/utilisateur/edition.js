3166ccf21ba6de74902002cd177889a437d74fcd
Stéfanie LOISELEUR <stloiseleur@ippon.fr>
2024-04-04 14:43:30 +0200

    Sur le formulaire utilisateur, affiche une bannière informative lorsque le SIRET doit être mis à jour

--- public/utilisateur/edition.js
+++ public/utilisateur/edition.js
@@ -1,6 +1,17 @@
 import brancheSoumissionFormulaireUtilisateur from '../modules/interactions/brancheSoumissionFormulaireUtilisateur.js';
 
+const afficheBanniereMiseAJourSiret = () => {
+  const { completudeProfil } = JSON.parse($('#infos-completude-profil').text());
+  if (
+    completudeProfil.champsNonRenseignes.includes('siret') &&
+    !completudeProfil.champsNonRenseignes.includes('nom')
+  ) {
+    $('.banniere-avertissement').removeClass('invisible');
+  }
+};
+
 $(() => {
+  afficheBanniereMiseAJourSiret();
   const selecteurFormulaire = 'form.utilisateur#edition';
   const action = (donnees) =>
     axios
