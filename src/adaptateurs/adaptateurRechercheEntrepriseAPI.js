7ff1d8e68056c38cf9385d3e52afd201f76f0511
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-04-05 14:56:08 +0200

    [SOIN] Passe en `async/await`

--- src/adaptateurs/adaptateurRechercheEntrepriseAPI.js
+++ src/adaptateurs/adaptateurRechercheEntrepriseAPI.js
@@ -3,76 +3,84 @@ const {
   fabriqueAdaptateurGestionErreur,
 } = require('./fabriqueAdaptateurGestionErreur');
 
-const rechercheOrganisations = (terme, departement) =>
-  axios
-    .get('https://recherche-entreprises.api.gouv.fr/search', {
-      params: {
-        q: terme,
-        ...(departement && { departement }),
-        per_page: 25,
-        page: 1,
-        limite_matching_etablissements: 1,
-        est_entrepreneur_individuel: false,
-      },
-    })
-    .then((reponse) =>
-      reponse.data.results
-        .filter((r) => r.siege.departement !== null)
-        .map((r) => ({
-          nom: r.nom_complet,
-          departement: r.siege.departement,
-          siret: r.siege.siret,
-        }))
-    )
-    .catch((e) => {
-      fabriqueAdaptateurGestionErreur().logueErreur(e, {
-        'Erreur renvoyée par API recherche-entreprise': e.response.data,
-      });
-      return Promise.resolve([]);
+const rechercheOrganisations = async (terme, departement) => {
+  try {
+    const reponse = await axios.get(
+      'https://recherche-entreprises.api.gouv.fr/search',
+      {
+        params: {
+          q: terme,
+          ...(departement && { departement }),
+          per_page: 25,
+          page: 1,
+          limite_matching_etablissements: 1,
+          est_entrepreneur_individuel: false,
+        },
+      }
+    );
+
+    return reponse.data.results
+      .filter((r) => r.siege.departement !== null)
+      .map((r) => ({
+        nom: r.nom_complet,
+        departement: r.siege.departement,
+        siret: r.siege.siret,
+      }));
+  } catch (e) {
+    fabriqueAdaptateurGestionErreur().logueErreur(e, {
+      'Erreur renvoyée par API recherche-entreprise': e.response.data,
     });
 
-const recupereDetailsOrganisation = (siret) =>
-  axios
-    .get('https://recherche-entreprises.api.gouv.fr/search', {
-      params: {
-        q: siret,
-        per_page: 1,
-        page: 1,
-        limite_matching_etablissements: 1,
-      },
-    })
-    .then((reponse) => {
-      if (reponse.data.results.length !== 1) {
-        fabriqueAdaptateurGestionErreur().logueErreur(
-          new Error(
-            `Recherche entreprise: pas de résultat pour le siret ${siret}`
-          )
-        );
-        return Promise.resolve(undefined);
+    return [];
+  }
+};
+
+const recupereDetailsOrganisation = async (siret) => {
+  try {
+    const reponse = await axios.get(
+      'https://recherche-entreprises.api.gouv.fr/search',
+      {
+        params: {
+          q: siret,
+          per_page: 1,
+          page: 1,
+          limite_matching_etablissements: 1,
+        },
       }
-      return reponse.data.results.map((r) => ({
-        estServicePublic: r.complements.est_service_public,
-        estFiness: r.complements.est_finess,
-        estEss: r.complements.est_ess,
-        estEntrepreneurIndividuel: r.complements.est_entrepreneur_individuel,
-        collectiviteTerritoriale: r.complements.collectivite_territoriale,
-        estAssociation: r.complements.est_association,
-        categorieEntreprise: r.categorie_entreprise,
-        activitePrincipale: r.activite_principale,
-        trancheEffectifSalarie: r.tranche_effectif_salarie,
-        natureJuridique: r.nature_juridique,
-        sectionActivitePrincipale: r.section_activite_principale,
-        anneeTrancheEffectifSalarie: r.annee_tranche_effectif_salarie,
-        commune: r.siege.commune,
-        departement: r.siege.departement,
-      }))[0];
-    })
-    .catch((e) => {
-      fabriqueAdaptateurGestionErreur().logueErreur(e, {
-        'Erreur renvoyée par API recherche-entreprise': e.response.data,
-      });
-      return Promise.resolve(undefined);
+    );
+
+    if (reponse.data.results.length !== 1) {
+      fabriqueAdaptateurGestionErreur().logueErreur(
+        new Error(
+          `Recherche entreprise: pas de résultat pour le siret ${siret}`
+        )
+      );
+      return undefined;
+    }
+
+    return reponse.data.results.map((r) => ({
+      estServicePublic: r.complements.est_service_public,
+      estFiness: r.complements.est_finess,
+      estEss: r.complements.est_ess,
+      estEntrepreneurIndividuel: r.complements.est_entrepreneur_individuel,
+      collectiviteTerritoriale: r.complements.collectivite_territoriale,
+      estAssociation: r.complements.est_association,
+      categorieEntreprise: r.categorie_entreprise,
+      activitePrincipale: r.activite_principale,
+      trancheEffectifSalarie: r.tranche_effectif_salarie,
+      natureJuridique: r.nature_juridique,
+      sectionActivitePrincipale: r.section_activite_principale,
+      anneeTrancheEffectifSalarie: r.annee_tranche_effectif_salarie,
+      commune: r.siege.commune,
+      departement: r.siege.departement,
+    }))[0];
+  } catch (e) {
+    fabriqueAdaptateurGestionErreur().logueErreur(e, {
+      'Erreur renvoyée par API recherche-entreprise': e.response.data,
     });
+    return undefined;
+  }
+};
 
 module.exports = {
   rechercheOrganisations,
