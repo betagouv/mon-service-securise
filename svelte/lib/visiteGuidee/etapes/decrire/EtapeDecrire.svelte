e8562e9db716c918de66dc37c20b630fa2f38349
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-04-24 11:40:33 +0200

    Ajoute la pagination sur la modale de sous étape

--- svelte/lib/visiteGuidee/etapes/decrire/EtapeDecrire.svelte
+++ svelte/lib/visiteGuidee/etapes/decrire/EtapeDecrire.svelte
@@ -36,4 +36,6 @@
   titre="Décrivez votre service"
   description="Dans un premier temps, répondez à quelques questions afin d’obtenir une liste de mesures de sécurité personnalisée"
   animation="/statique/assets/images/visiteGuidee/decrire.gif"
+  nbEtapes={2}
+  numeroEtapeCourante={1}
 />
