0724865f22ff915cb57b7b87ae7dc77dbad4cc48
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-04-24 11:31:24 +0200

    Ajoute le `GIF` dans la modale de sous étape

--- svelte/lib/visiteGuidee/etapes/decrire/EtapeDecrire.svelte
+++ svelte/lib/visiteGuidee/etapes/decrire/EtapeDecrire.svelte
@@ -35,4 +35,5 @@
   bind:positionCible
   titre="Décrivez votre service"
   description="Dans un premier temps, répondez à quelques questions afin d’obtenir une liste de mesures de sécurité personnalisée"
+  animation="/statique/assets/images/visiteGuidee/decrire.gif"
 />
