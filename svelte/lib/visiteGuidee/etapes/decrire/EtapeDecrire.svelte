a009a696b20047347823bf78ca65175ecfb835b4
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-04-25 09:25:12 +0200

    [CORRECTION] Attend que les élements HTML soient correctement montés avant d'instancier la modale

--- svelte/lib/visiteGuidee/etapes/decrire/EtapeDecrire.svelte
+++ svelte/lib/visiteGuidee/etapes/decrire/EtapeDecrire.svelte
@@ -1,48 +1,53 @@
 <script lang="ts">
   import ModaleSousEtape from '../../kit/ModaleSousEtape.svelte';
+  import { onMount } from 'svelte';
 
-  const cibleNomService = document.getElementById('nom-service')!;
-  const cibleGererContributeurs = document.getElementById(
-    'gerer-contributeurs'
-  )!;
+  let cibleNomService: HTMLElement;
+  let cibleGererContributeurs: HTMLElement;
+  onMount(() => {
+    cibleNomService = document.getElementById('nom-service')!;
+    cibleGererContributeurs = document.getElementById('gerer-contributeurs')!;
+  });
 </script>
 
-<ModaleSousEtape
-  sousEtapes={[
-    {
-      cible: cibleNomService,
-      callbackInitialeCible: (cible) => (cible.style.width = '50%'),
-      callbackFinaleCible: (cible) => (cible.style.width = '100%'),
-      positionnementModale: 'MilieuDroite',
-      titre: 'Décrivez votre service',
-      description:
-        'Dans un premier temps, répondez à quelques questions afin d’obtenir une liste de mesures de sécurité personnalisée',
-      animation: '/statique/assets/images/visiteGuidee/decrire.gif',
-    },
-    {
-      cible: cibleGererContributeurs,
-      callbackInitialeCible: (cible) => {
-        cible.style.minWidth = '204px';
-        document.getElementsByClassName(
-          'inviter-contributeurs'
-        )[0].style.display = 'flex';
-        document.getElementsByClassName(
-          'nombre-contributeurs'
-        )[0].style.display = 'none';
-        document.body.dispatchEvent(
-          new CustomEvent('jquery-affiche-tiroir-contributeurs-visite-guidee')
-        );
-        document.getElementsByClassName('tiroir')[0].style.zIndex = '10001';
+{#if cibleNomService && cibleGererContributeurs}
+  <ModaleSousEtape
+    sousEtapes={[
+      {
+        cible: cibleNomService,
+        callbackInitialeCible: (cible) => (cible.style.width = '50%'),
+        callbackFinaleCible: (cible) => (cible.style.width = '100%'),
+        positionnementModale: 'MilieuDroite',
+        titre: 'Décrivez votre service',
+        description:
+          'Dans un premier temps, répondez à quelques questions afin d’obtenir une liste de mesures de sécurité personnalisée',
+        animation: '/statique/assets/images/visiteGuidee/decrire.gif',
       },
-      callbackFinaleCible: () =>
-        document
-          .getElementsByClassName('fermeture-tiroir')[0]
-          .dispatchEvent(new Event('click')),
-      positionnementModale: 'HautDroite',
-      titre: 'Collaborez avec votre équipe',
-      description:
-        'Vous pouvez travailler en équipe, inviter vos collègues mais également vos prestataires. Différents niveaux de droits sont disponibles (lecture, édition…).',
-      animation: '/statique/assets/images/visiteGuidee/decrire.gif',
-    },
-  ]}
-/>
+      {
+        cible: cibleGererContributeurs,
+        callbackInitialeCible: (cible) => {
+          cible.style.minWidth = '204px';
+          document.getElementsByClassName(
+            'inviter-contributeurs'
+          )[0].style.display = 'flex';
+          document.getElementsByClassName(
+            'nombre-contributeurs'
+          )[0].style.display = 'none';
+          document.body.dispatchEvent(
+            new CustomEvent('jquery-affiche-tiroir-contributeurs-visite-guidee')
+          );
+          document.getElementsByClassName('tiroir')[0].style.zIndex = '10001';
+        },
+        callbackFinaleCible: () =>
+          document
+            .getElementsByClassName('fermeture-tiroir')[0]
+            .dispatchEvent(new Event('click')),
+        positionnementModale: 'HautDroite',
+        titre: 'Collaborez avec votre équipe',
+        description:
+          'Vous pouvez travailler en équipe, inviter vos collègues mais également vos prestataires. Différents niveaux de droits sont disponibles (lecture, édition…).',
+        animation: '/statique/assets/images/visiteGuidee/decrire.gif',
+      },
+    ]}
+  />
+{/if}
