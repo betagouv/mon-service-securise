c3c6cf5aa634bf6e2123655f7222b133fc0df7c6
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-04-25 12:06:02 +0200

    Ajoute la sous-étape de téléchargement à "HOMOLOGUER"

--- svelte/lib/visiteGuidee/etapes/homologuer/EtapeHomologuer.svelte
+++ svelte/lib/visiteGuidee/etapes/homologuer/EtapeHomologuer.svelte
@@ -3,12 +3,14 @@
   import { onMount } from 'svelte';
 
   let cibleNouvelleHomologation: HTMLElement;
+  let cibleTelechargement: HTMLElement;
   onMount(() => {
     cibleNouvelleHomologation = document.getElementById('suivant')!;
+    cibleTelechargement = document.getElementById('voir-telechargement')!;
   });
 </script>
 
-{#if cibleNouvelleHomologation}
+{#if cibleNouvelleHomologation && cibleTelechargement}
   <ModaleSousEtape
     sousEtapes={[
       {
@@ -18,6 +20,31 @@
         titre: 'Mettez-vous en conformité',
         description:
           "Générez un dossier et un projet de décision d'homologation pour vous mettre en conformité avec la réglementation",
+        animation: '/statique/assets/images/visiteGuidee/decrire.gif',
+      },
+      {
+        cible: cibleTelechargement,
+        callbackInitialeCible: () => {
+          document.body.dispatchEvent(
+            new CustomEvent(
+              'jquery-affiche-tiroir-telechargement-visite-guidee'
+            )
+          );
+          document.getElementsByClassName('tiroir')[0].style.zIndex = '10001';
+          document.getElementsByClassName(
+            'fermeture-tiroir'
+          )[0].disabled = true;
+        },
+        callbackFinaleCible: () => {
+          document
+            .getElementsByClassName('fermeture-tiroir')[0]
+            .dispatchEvent(new Event('click'));
+        },
+        avecTrouRideauColle: true,
+        positionnementModale: 'MilieuDroite',
+        titre: 'Homologuez en toute simplicité',
+        description:
+          'Téléchargez en 1 clic, le dossier et le projet de décision d’homologation pour le faire signer.',
         animation: '/statique/assets/images/visiteGuidee/decrire.gif',
       },
     ]}
