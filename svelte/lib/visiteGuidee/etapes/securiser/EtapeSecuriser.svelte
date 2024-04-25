82426fc15184df5169dc864b254776d8d88bb5f0
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-04-25 11:11:31 +0200

    Ajoute la sous-étape d'indice cyber

--- svelte/lib/visiteGuidee/etapes/securiser/EtapeSecuriser.svelte
+++ svelte/lib/visiteGuidee/etapes/securiser/EtapeSecuriser.svelte
@@ -3,10 +3,13 @@
   import { onMount } from 'svelte';
 
   let cibleTiroirMesure: HTMLDivElement;
-
+  let cibleIndiceCyber: HTMLDivElement;
   onMount(() => {
     cibleTiroirMesure = document.getElementsByClassName(
       'tiroir'
+    )[0]! as HTMLDivElement;
+    cibleIndiceCyber = document.getElementsByClassName(
+      'conteneur-indice-cyber'
     )[0]! as HTMLDivElement;
   });
 </script>
@@ -29,11 +32,25 @@
         },
         delaiAvantAffichage: 200,
         avecTrouRideauColle: true,
-        callbackFinaleCible: () => {},
-        positionnementModale: 'HautGauche',
+        callbackFinaleCible: () => {
+          document
+            .getElementsByClassName('fermeture-tiroir')[0]
+            .dispatchEvent(new Event('click'));
+        },
+        positionnementModale: 'MilieuGauche',
         titre: 'Sécurisez, grâce à des mesures adaptées',
         description:
           'Chaque mesure est associée à son référentiel (ANSSI, CNIL) et son niveau d’importance (recommandée ou indispensable pour les mesures ANSSI).',
+        animation: '/statique/assets/images/visiteGuidee/decrire.gif',
+      },
+      {
+        cible: cibleIndiceCyber,
+        callbackInitialeCible: (cible) => cible.removeAttribute('href'),
+        avecTrouRideauColle: true,
+        positionnementModale: 'HautGauche',
+        titre: 'Indice cyber',
+        description:
+          "Mettez en œuvre les mesures proposées et obtenez une évaluation indicative du niveau de sécurisation du service : l'indice cyber.",
         animation: '/statique/assets/images/visiteGuidee/decrire.gif',
       },
     ]}
