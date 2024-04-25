171c11d2be027b7e519388dd64a729f6e4d89e64
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-04-25 15:37:40 +0200

    Ajoute la deuxième sous-étape de "Piloter"

--- svelte/lib/visiteGuidee/etapes/piloter/EtapePiloter.svelte
+++ svelte/lib/visiteGuidee/etapes/piloter/EtapePiloter.svelte
@@ -3,6 +3,7 @@
   import { onMount } from 'svelte';
 
   let cibleNomService: HTMLElement;
+  let cibleBandeauNouveaute: HTMLElement;
   onMount(() => {
     const intervalle = setInterval(() => {
       cibleNomService = document.getElementsByClassName(
@@ -10,10 +11,14 @@
       )[0]! as HTMLElement;
       if (cibleNomService) clearInterval(intervalle);
     }, 10);
+
+    cibleBandeauNouveaute = document.getElementsByClassName(
+      'bandeau-nouveautes'
+    )[0]! as HTMLElement;
   });
 </script>
 
-{#if cibleNomService}
+{#if cibleNomService && cibleBandeauNouveaute}
   <ModaleSousEtape
     sousEtapes={[
       {
@@ -31,6 +36,18 @@
         titre: 'Votre tableau de bord',
         description:
           'Vous pourrez piloter en équipe la sécurité de tous vos services numériques et les homologuer rapidement, grâce à un tableau complet présentant vos données de progressions.',
+        animation: '/statique/assets/images/visiteGuidee/decrire.gif',
+      },
+      {
+        cible: cibleBandeauNouveaute,
+        positionnementModale: 'BasMilieu',
+        avecTrouRideauColle: true,
+        callbackInitialeCible: (cible) => {
+          cible.disabled = true;
+        },
+        titre: 'Les nouveautés',
+        description:
+          'Ne ratez aucune évolution de MonServiceSécurisé. Nous vous partageons les nouveautés en matière de cybersécurité, et les mesures additionnelles qui peuvent impliquer des changement et/ou amélioration pour vos services.',
         animation: '/statique/assets/images/visiteGuidee/decrire.gif',
       },
     ]}
