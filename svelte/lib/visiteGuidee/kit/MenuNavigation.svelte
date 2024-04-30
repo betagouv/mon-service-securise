476fea23b4553088176135b7a78f6004d1c90937
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-04-30 11:30:14 +0200

    [VISITE GUIDEE] Ajoute le nouveau composant `MenuNavigation` de la visite guidée

---
+++ svelte/lib/visiteGuidee/kit/MenuNavigation.svelte
@@ -0,0 +1,19 @@
+<button class="declencheur-menu-navigation">
+  <img
+    src="/statique/assets/images/icone_liste_a_cocher.svg"
+    alt="Ouverture du menu de navigation de la visite guidée"
+  />
+</button>
+
+<style>
+  .declencheur-menu-navigation {
+    width: 44px;
+    height: 44px;
+    border-radius: 50%;
+    border: none;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
--- svelte/lib/visiteGuidee/visiteGuidee.ts
+++ svelte/lib/visiteGuidee/visiteGuidee.ts
@@ -1,14 +1,21 @@
 import VisiteGuidee from './VisiteGuidee.svelte';
 import type { VisiteGuideeProps } from './visiteGuidee.d';
 import { visiteGuidee } from './visiteGuidee.store';
+import MenuNavigation from './kit/MenuNavigation.svelte';
 
 document.body.addEventListener(
   'svelte-recharge-visite-guidee',
   (e: CustomEvent<VisiteGuideeProps>) => rechargeApp(e.detail)
 );
 
+let appMenuNavigation: MenuNavigation;
 let app: VisiteGuidee;
 const rechargeApp = (props: VisiteGuideeProps) => {
+  appMenuNavigation?.$destroy();
+  appMenuNavigation = new MenuNavigation({
+    target: document.getElementById('visite-guidee-menu-navigation')!,
+  });
+
   app?.$destroy();
   visiteGuidee.initialise(props.etapeCourante);
   app = new VisiteGuidee({
