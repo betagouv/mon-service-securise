f7ef40ddfb5a9b226f9c57cef199a98e1482fd96
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-05-07 09:33:09 +0200

    Déplace toutes les routes "Non connecté" vers le routeur dédié

--- src/routes/nonConnecte/routesNonConnectePage.js
+++ src/routes/nonConnecte/routesNonConnectePage.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const routesNonConnectePage = () => {
+const routesNonConnectePage = ({ referentiel }) => {
   const routes = express.Router();
 
   routes.get('/', (_requete, reponse) => {
@@ -9,6 +9,39 @@ const routesNonConnectePage = () => {
 
   routes.get('/aPropos', (_requete, reponse) => {
     reponse.render('aPropos');
+  });
+
+  routes.get('/securite', (_requete, reponse) => {
+    reponse.render('securite');
+  });
+
+  routes.get('/accessibilite', (_requete, reponse) => {
+    reponse.render('accessibilite');
+  });
+
+  routes.get('/cgu', (_requete, reponse) => {
+    reponse.render('cgu');
+  });
+
+  routes.get('/confidentialite', (_requete, reponse) => {
+    reponse.render('confidentialite');
+  });
+
+  routes.get('/mentionsLegales', (_requete, reponse) => {
+    reponse.render('mentionsLegales');
+  });
+
+  routes.get('/statistiques', (_requete, reponse) => {
+    reponse.render('statistiques');
+  });
+
+  routes.get('/inscription', (_requete, reponse) => {
+    const departements = referentiel.departements();
+    reponse.render('inscription', { departements });
+  });
+
+  routes.get('/activation', (_requete, reponse) => {
+    reponse.render('activation');
   });
 
   return routes;
