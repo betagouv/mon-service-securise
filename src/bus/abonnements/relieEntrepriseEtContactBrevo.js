a4bc2fe3c0b41186e3d50cb95395cf086a7efe98
Stéfanie LOISELEUR <stloiseleur@ippon.fr>
2024-05-21 14:59:33 +0200

    [SOIN] Utilise la nouvelle classe CRM Brevo dans l'abonné relieEntrepriseEtContactBrevo

--- src/bus/abonnements/relieEntrepriseEtContactBrevo.js
+++ src/bus/abonnements/relieEntrepriseEtContactBrevo.js
@@ -1,40 +1,6 @@
-function relieEntrepriseEtContactBrevo({
-  adaptateurRechercheEntreprise,
-  adaptateurMail,
-}) {
+function relieEntrepriseEtContactBrevo({ crmBrevo }) {
   return async ({ utilisateur }) => {
-    if (!utilisateur)
-      throw new Error(
-        "Impossible de relier une entreprise et un contact sans avoir l'utilisateur en paramètre."
-      );
-
-    if (!utilisateur.entite.siret) return;
-
-    const idUtilisateurBrevo = await adaptateurMail.recupereIdentifiantContact(
-      utilisateur.email
-    );
-
-    let idEntrepriseBrevo = await adaptateurMail.recupereEntreprise(
-      utilisateur.entite.siret
-    );
-
-    if (!idEntrepriseBrevo) {
-      const { entite } = utilisateur;
-      const entiteResponsable =
-        await adaptateurRechercheEntreprise.recupereDetailsOrganisation(
-          entite.siret
-        );
-      idEntrepriseBrevo = await adaptateurMail.creeEntreprise(
-        entite.siret,
-        entite.nom,
-        entiteResponsable.natureJuridique
-      );
-    }
-
-    await adaptateurMail.relieContactAEntreprise(
-      idUtilisateurBrevo,
-      idEntrepriseBrevo
-    );
+    await crmBrevo.creerLienEntrepriseContact(utilisateur);
   };
 }
 
