05ae17953ad70f757e449b84b3a48301bfce3dcc
Stéfanie LOISELEUR <stloiseleur@ippon.fr>
2024-05-21 14:58:57 +0200

    [SOIN] Déplace la logique de CRM Brevo dans une classe dédiée

---
+++ test/crm/crmBrevo.spec.js
@@ -0,0 +1,141 @@
+const expect = require('expect.js');
+const CrmBrevo = require('../../src/crm/crmBrevo');
+const {
+  fabriqueAdaptateurMailMemoire,
+} = require('../../src/adaptateurs/adaptateurMailMemoire');
+const fauxAdaptateurRechercheEntreprise = require('../mocks/adaptateurRechercheEntreprise');
+const { unUtilisateur } = require('../constructeurs/constructeurUtilisateur');
+
+describe('Le CRM Brevo', () => {
+  it("jette une erreur s'il n'est pas instancié avec les bons adaptateurs", () => {
+    expect(() => new CrmBrevo({})).to.throwError((e) => {
+      expect(e.message).to.be(
+        "Impossible d'instancier le CRM sans adaptateurs"
+      );
+    });
+  });
+  describe('sur demande de création du lien entre un utilisateur et son entreprise', () => {
+    let adaptateurMail;
+    let adaptateurRechercheEntreprise;
+    let crmBrevo;
+
+    beforeEach(() => {
+      adaptateurMail = fabriqueAdaptateurMailMemoire();
+      adaptateurRechercheEntreprise = fauxAdaptateurRechercheEntreprise();
+      crmBrevo = new CrmBrevo({
+        adaptateurRechercheEntreprise,
+        adaptateurMail,
+      });
+    });
+
+    it("lève une exception s'il ne reçoit pas d'utilisateur", async () => {
+      try {
+        await crmBrevo.creerLienEntrepriseContact(null);
+
+        expect().fail("L'instanciation aurait dû lever une exception.");
+      } catch (e) {
+        expect(e.message).to.be(
+          "Impossible de relier une entreprise et un contact sans avoir l'utilisateur en paramètre."
+        );
+      }
+    });
+
+    it("ne fait rien si l'utilisateur n'a pas de SIRET", async () => {
+      let adaptateurAppele = false;
+
+      adaptateurMail.recupereIdentifiantContact = async () => {
+        adaptateurAppele = true;
+      };
+
+      const utilisateur = unUtilisateur()
+        .quiTravaillePourUneEntiteAvecSiret('')
+        .construis();
+
+      await crmBrevo.creerLienEntrepriseContact(utilisateur);
+
+      expect(adaptateurAppele).to.be(false);
+    });
+
+    describe("si l'utilisateur a un SIRET", () => {
+      let utilisateur;
+
+      beforeEach(() => {
+        utilisateur = unUtilisateur()
+          .avecEmail('jean.dujardin@beta.gouv.com')
+          .quiTravaillePourUneEntiteAvecSiret('1234')
+          .avecNomEntite('MonServiceSécurisé')
+          .construis();
+        adaptateurMail.recupereIdentifiantContact = async () => 'C1';
+      });
+
+      it("recupère l'identifiant Brevo de l'utilisateur via son email", async () => {
+        let emailRecu;
+
+        adaptateurMail.recupereIdentifiantContact = async (email) => {
+          emailRecu = email;
+        };
+
+        await crmBrevo.creerLienEntrepriseContact(utilisateur);
+
+        expect(emailRecu).to.be('jean.dujardin@beta.gouv.com');
+      });
+
+      it("vérifie si l'entreprise existe déjà sur Brevo via son SIRET", async () => {
+        let siretRecu;
+
+        adaptateurMail.recupereEntreprise = async (siret) => {
+          siretRecu = siret;
+        };
+
+        await crmBrevo.creerLienEntrepriseContact(utilisateur);
+
+        expect(siretRecu).to.be('1234');
+      });
+
+      describe("si l'entreprise n'existe pas", () => {
+        it("créé l'entreprise", async () => {
+          let donneesRecuesCreationEntreprise;
+
+          adaptateurRechercheEntreprise.recupereDetailsOrganisation =
+            async () => ({
+              natureJuridique: 'NatureJuridique',
+            });
+          adaptateurMail.recupereEntreprise = async () => null;
+          adaptateurMail.creeEntreprise = async (
+            siret,
+            nom,
+            natureJuridique
+          ) => {
+            donneesRecuesCreationEntreprise = { siret, nom, natureJuridique };
+            return 'E1';
+          };
+
+          await crmBrevo.creerLienEntrepriseContact(utilisateur);
+
+          expect(donneesRecuesCreationEntreprise).to.eql({
+            siret: '1234',
+            nom: 'MonServiceSécurisé',
+            natureJuridique: 'NatureJuridique',
+          });
+        });
+      });
+
+      it("relie l'utilisateur à l'entreprise", async () => {
+        let donneesRecues;
+
+        adaptateurMail.recupereEntreprise = async () => 'E1';
+        adaptateurMail.relieContactAEntreprise = async (
+          idContact,
+          idEntreprise
+        ) => {
+          donneesRecues = { idContact, idEntreprise };
+        };
+
+        await crmBrevo.creerLienEntrepriseContact(utilisateur);
+
+        expect(donneesRecues.idContact).to.be('C1');
+        expect(donneesRecues.idEntreprise).to.be('E1');
+      });
+    });
+  });
+});
