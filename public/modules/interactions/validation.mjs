e305cde326a1d2487dff7c2632cda557c5d415c4
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-05-22 16:24:51 +0200

    Supprime l'utilisation de la classe `intouche` dans la validation de formulaire

--- public/modules/interactions/validation.mjs
+++ public/modules/interactions/validation.mjs
@@ -3,19 +3,8 @@ import {
   declencheValidationCasesACocher,
 } from './brancheValidationCasesACocher.mjs';
 
-const EVENEMENT_AFFICHE_ERREURS_SI_NECESSAIRE = 'afficheErreursSiNecessaire';
-
 const brancheConteneur = (selecteurConteneur) => {
   $('input, select, textarea', selecteurConteneur).each((_index, champ) => {
-    $(champ).addClass('intouche');
-    $(champ).on('input', () => {
-      $(champ).removeClass('intouche');
-    });
-    if (champ.type === 'radio' || champ.type === 'checkbox') {
-      $(champ).on('change', (evenement) =>
-        $(evenement.target).siblings().removeClass('intouche')
-      );
-    }
     $(champ).on('invalid', (evenement) => {
       evenement.preventDefault();
     });
@@ -23,15 +12,6 @@ const brancheConteneur = (selecteurConteneur) => {
 };
 
 const brancheValidation = (selecteurFormulaire) => {
-  $(selecteurFormulaire).on(
-    EVENEMENT_AFFICHE_ERREURS_SI_NECESSAIRE,
-    (evenement) => {
-      if (!evenement.target.reportValidity()) {
-        $('.intouche', selecteurFormulaire).removeClass('intouche');
-      }
-    }
-  );
-
   brancheConteneur(selecteurFormulaire);
   brancheValidationCasesACocher();
 };
@@ -53,7 +33,6 @@ const declencheScrollSurErreur = (selecteurFormulaire) => {
 
 const declencheValidation = (selecteurFormulaire) => {
   declencheValidationCasesACocher();
-  $(selecteurFormulaire).trigger(EVENEMENT_AFFICHE_ERREURS_SI_NECESSAIRE);
   declencheScrollSurErreur(selecteurFormulaire);
 };
 
