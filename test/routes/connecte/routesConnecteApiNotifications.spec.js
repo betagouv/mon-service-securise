884ae24cf03744944eb046aa2031f2caa6214d4b
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-06-05 17:05:50 +0200

    Ajoute une route permettant de marquer une notification de nouveauté à "lue"

--- test/routes/connecte/routesConnecteApiNotifications.spec.js
+++ test/routes/connecte/routesConnecteApiNotifications.spec.js
@@ -8,16 +8,17 @@ describe('Le serveur MSS des routes privées /api/notifications', () => {
   beforeEach(testeur.initialise);
   afterEach(testeur.arrete);
 
-  describe('quand requête GET sur `/api/notifications`', () => {
-    beforeEach(() => {
-      testeur.referentiel().recharge({
-        nouvellesFonctionnalites: [
-          { id: 'N1', dateDeDeploiement: '2024-01-01' },
-          { id: 'N2', dateDeDeploiement: '2024-02-02' },
-        ],
-      });
+  beforeEach(() => {
+    testeur.middleware().reinitialise({ idUtilisateur: 'U1' });
+    testeur.referentiel().recharge({
+      nouvellesFonctionnalites: [
+        { id: 'N1', dateDeDeploiement: '2024-01-01' },
+        { id: 'N2', dateDeDeploiement: '2024-02-02' },
+      ],
     });
+  });
 
+  describe('quand requête GET sur `/api/notifications`', () => {
     it("vérifie que l'utilisateur a accepté les CGU", (done) => {
       testeur
         .middleware()
@@ -34,6 +35,37 @@ describe('Le serveur MSS des routes privées /api/notifications', () => {
 
       expect(reponse.status).to.be(200);
       expect(reponse.data.notifications.length).to.be(2);
+    });
+  });
+
+  describe('quand requête POST sur `/api/notifications/nouveautes/:id`', () => {
+    it('délègue au centre de notification le marquage à "lue"', async () => {
+      let donneesRecues;
+      testeur.depotDonnees().marqueNouveauteLue = async (
+        idUtilisateur,
+        idNouveaute
+      ) => {
+        donneesRecues = { idUtilisateur, idNouveaute };
+      };
+
+      const reponse = await axios.post(
+        'http://localhost:1234/api/notifications/nouveautes/N1'
+      );
+
+      expect(reponse.status).to.be(200);
+      expect(donneesRecues.idUtilisateur).to.be('U1');
+    });
+
+    it("reste robuste en cas d'erreur", async () => {
+      try {
+        await axios.post(
+          'http://localhost:1234/api/notifications/nouveautes/ID_INCONNU'
+        );
+        expect().fail("L'appel aurait dû lever une exception");
+      } catch (e) {
+        expect(e.response.status).to.be(400);
+        expect(e.response.data).to.be('Identifiant de nouveauté inconnu');
+      }
     });
   });
 });
