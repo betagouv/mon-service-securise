3c1f0a419d0336195cc35b1aca1674d8a7cad7df
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-06-06 09:10:18 +0200

    Extrais la "Notification" dans un composant dédié

--- svelte/lib/centreNotifications/CentreNotifications.svelte
+++ svelte/lib/centreNotifications/CentreNotifications.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import FermetureSurClicEnDehors from '../ui/FermetureSurClicEnDehors.svelte';
-  import type { Notification } from '/centreNotifications.d';
-  import {
-    marqueNotificationCommeLue,
-    recupereNotifications,
-  } from './centreNotifications.api';
+  import ComposantNotification from './kit/Notification.svelte';
+  import type { Notification } from './centreNotifications.d';
+  import { recupereNotifications } from './centreNotifications.api';
 
   let ouvert = false;
   let elementCentreNotifications: HTMLDivElement;
@@ -21,21 +19,6 @@
   onMount(async () => {
     await rafraichisNotifications();
   });
-
-  const formatteDate = (chaineDate: string) => {
-    const dateDeploiement = new Date(chaineDate);
-    const maintenant = new Date();
-    const differenceEnJours = Math.round(
-      (dateDeploiement.getTime() - maintenant.getTime()) / (1000 * 3600 * 24)
-    );
-    const formatter = new Intl.RelativeTimeFormat('fr-FR', {
-      localeMatcher: 'best fit',
-      numeric: 'auto',
-      style: 'long',
-    });
-    const dateFormattee = formatter.format(differenceEnJours, 'day');
-    return dateFormattee.charAt(0).toUpperCase() + dateFormattee.slice(1);
-  };
 </script>
 
 <FermetureSurClicEnDehors
@@ -65,43 +48,10 @@
     </div>
     <div class="contenu-notifications">
       {#each notifications as notification (notification.id)}
-        <div class="notification">
-          <div class="conteneur-pictogramme">
-            {#if notification.statutLecture === 'nonLue'}
-              <div class="pastille-non-lue" />
-            {/if}
-            <img
-              src="/statique/assets/images/notifications/nouveaute.svg"
-              alt="Icône de nouveauté"
-            />
-          </div>
-          <div class="conteneur-notification">
-            <p class="type-notification">Nouveautés</p>
-            <p class="titre">{notification.titre}</p>
-            <div class="cartouche-cta">
-              <img
-                src="/statique/assets/images/notifications/illustrations/{notification.image}"
-                alt="Illustration de la nouveauté {notification.titre}"
-              />
-              <div>
-                <p class="sous-titre">{notification.sousTitre}</p>
-                <a
-                  href={notification.lien}
-                  class="cta"
-                  rel="noopener"
-                  target="_blank"
-                  on:click={async () => {
-                    await marqueNotificationCommeLue(notification.id);
-                    await rafraichisNotifications();
-                  }}>Découvrir</a
-                >
-              </div>
-            </div>
-            <div class="horodatage">
-              <span>{formatteDate(notification.dateDeDeploiement)}</span>
-            </div>
-          </div>
-        </div>
+        <ComposantNotification
+          {notification}
+          on:notificationMiseAJour={async () => await rafraichisNotifications()}
+        />
       {/each}
     </div>
   </div>
@@ -201,89 +151,5 @@
   .contenu-notifications {
     max-height: 626px;
     overflow-y: auto;
-  }
-
-  .conteneur-notifications img {
-    max-width: 86px;
-  }
-
-  .type-notification {
-    font-weight: bold;
-    margin: 0;
-    font-size: 16px;
-  }
-
-  .notification {
-    text-decoration: none;
-    color: var(--texte-fonce);
-    padding: 16px;
-    border-top: 1px solid var(--liseres-fonce);
-    display: flex;
-    flex-direction: row;
-    gap: 8px;
-  }
-
-  .conteneur-pictogramme {
-    --taille: 40px;
-    min-width: var(--taille);
-    min-height: var(--taille);
-    max-width: var(--taille);
-    max-height: var(--taille);
-    border-radius: 50%;
-    background: var(--fond-bleu-pale);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-  }
-
-  .cartouche-cta {
-    padding: 8px;
-    display: flex;
-    align-items: start;
-    justify-content: start;
-    gap: 10px;
-    border: 1px solid var(--liseres-fonce);
-    border-radius: 8px;
-  }
-
-  a.cta {
-    color: var(--bleu-mise-en-avant);
-    padding: 4px 8px;
-    border: 1px solid var(--bleu-mise-en-avant);
-    border-radius: 4px;
-    cursor: pointer;
-    font-size: 12px;
-  }
-
-  a.cta:hover {
-    background: var(--bleu-mise-en-avant) !important;
-    color: white;
-  }
-
-  p.sous-titre {
-    margin-top: 0;
-    font-size: 12px;
-  }
-
-  p.titre {
-    margin: 4px 0 8px 0;
-    font-size: 14px;
-  }
-
-  .horodatage {
-    color: var(--texte-clair);
-    font-size: 12px;
-    margin-top: 8px;
-  }
-
-  .pastille-non-lue {
-    width: 8px;
-    height: 8px;
-    background: #ca3535;
-    border-radius: 50%;
-    position: absolute;
-    top: 2px;
-    left: 2px;
   }
 </style>
