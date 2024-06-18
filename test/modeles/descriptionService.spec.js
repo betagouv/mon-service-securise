e251c0e6c9be341171e537352cdeecdff4ad22ed
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-06-18 12:40:43 +0200

    Rend le niveau de sécurité obligatoire

--- test/modeles/descriptionService.spec.js
+++ test/modeles/descriptionService.spec.js
@@ -78,6 +78,7 @@ describe('La description du service', () => {
       'statutDeploiement',
       'typeService',
       'nombreOrganisationsUtilisatrices',
+      'niveauSecurite',
     ]);
   });
 
