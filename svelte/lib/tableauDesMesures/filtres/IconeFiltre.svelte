482683692c9f2065b0d0dbd5450b07da62247b78
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-07-02 11:33:44 +0200

    Extrais `<IconeFiltre>`
    … pour mutualiser.
    
    Signed-off-by: Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>

---
+++ svelte/lib/tableauDesMesures/filtres/IconeFiltre.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let filtresActifs: boolean = false;
+</script>
+
+<picture class:filtres-actifs={filtresActifs}>
+  <img src="/statique/assets/images/icone_filtre.svg" alt="" />
+</picture>
+Filtres
+
+<style>
+  picture {
+    height: 24px;
+    width: 24px;
+  }
+
+  .filtres-actifs::after {
+    display: block;
+    content: '';
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    border: 1px solid white;
+    background: var(--bleu-mise-en-avant);
+    position: relative;
+    top: -28px;
+    left: 18px;
+  }
+</style>
