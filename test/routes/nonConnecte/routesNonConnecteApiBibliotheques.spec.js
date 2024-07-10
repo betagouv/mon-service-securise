30d09e77c6fcecec0a5720726066349373ac3b91
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-07-10 21:31:36 +0200

    [SOIN] Utilise la version `async` de la vérification d'erreur
    … pour être plus concis.

--- test/routes/nonConnecte/routesNonConnecteApiBibliotheques.spec.js
+++ test/routes/nonConnecte/routesNonConnecteApiBibliotheques.spec.js
@@ -7,15 +7,14 @@ describe('Le serveur MSS des routes /bibliotheques/*', () => {
 
   afterEach(testeur.arrete);
 
-  it('retourne une erreur HTTP 404 si la bibliothèque est inconnue', (done) => {
-    testeur.verifieRequeteGenereErreurHTTP(
+  it('retourne une erreur HTTP 404 si la bibliothèque est inconnue', async () => {
+    await testeur.verifieRequeteGenereErreurHTTPAsync(
       404,
       'Bibliothèque inconnue : bibliothequeInconnue.js',
       {
         method: 'get',
         url: 'http://localhost:1234/bibliotheques/bibliothequeInconnue.js',
-      },
-      done
+      }
     );
   });
 });
