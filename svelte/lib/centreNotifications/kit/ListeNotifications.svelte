b0082a2a8e81a39d0c1570b314776d79d3da270b
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-07-10 14:18:55 +0200

    Extrais un composant "ListeNotifications"
    ... pour pouvoir le réutiliser dans le cas des 3 listes de notifications "À faire", "Nouveautés" et "Toutes"

---
+++ svelte/lib/centreNotifications/kit/ListeNotifications.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import ComposantNotification from './Notification.svelte';
+  import type { Notification } from './centreNotifications.d';
+
+  export let notifications: Notification[];
+</script>
+
+<div class="contenu-notifications">
+  {#each notifications as notification (notification.id)}
+    <ComposantNotification {notification} on:notificationMiseAJour />
+  {/each}
+</div>
+
+<style>
+  .contenu-notifications {
+    max-height: 626px;
+    overflow-y: auto;
+  }
+</style>
