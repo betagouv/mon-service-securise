a73097d9834768b3f34f030ffb51b0a7671c5c81
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-07-11 15:18:30 +0200

    Ajoute la classe `CmsCrisp` afin de pouvoir orchestrer et retourner le contenu HTML d'un article

---
+++ src/cms/cmsCrisp.js
@@ -0,0 +1,27 @@
+const fabriqueCrispMarkdown = require('./fabriqueCrispMarkdown');
+
+class CmsCrisp {
+  constructor({
+    adaptateurCmsCrisp,
+    constructeurCrispMarkdown = fabriqueCrispMarkdown,
+  }) {
+    if (!adaptateurCmsCrisp) {
+      throw new Error("Impossible d'instancier le CMS sans adaptateur");
+    }
+    this.adaptateurCmsCrisp = adaptateurCmsCrisp;
+    this.constructeurCrispMarkdown = constructeurCrispMarkdown;
+  }
+
+  async recupereDevenirAmbassadeur() {
+    const { contenuMarkdown, titre } =
+      await this.adaptateurCmsCrisp.recupereDevenirAmbassadeur();
+    const contenu = this.constructeurCrispMarkdown(contenuMarkdown).versHTML();
+
+    return {
+      titre,
+      contenu,
+    };
+  }
+}
+
+module.exports = CmsCrisp;
