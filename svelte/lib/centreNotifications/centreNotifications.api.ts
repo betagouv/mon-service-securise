9ebb839a9bab4119f8d4bbe1cdd6ecbe1ebd0882
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-07-12 15:03:16 +0200

    Déplace la connaissance des routes dans le connecteur API

--- svelte/lib/centreNotifications/centreNotifications.api.ts
+++ svelte/lib/centreNotifications/centreNotifications.api.ts
@@ -1,8 +1,17 @@
+import type { TypeNotification } from './centreNotifications.d';
+
 export const recupereNotifications = async () => {
   const reponse = await axios.get('/api/notifications');
   return reponse.data.notifications;
 };
 
-export const marqueNotificationCommeLue = async (type: string, id: string) => {
-  await axios.put(`/api/notifications/${type}/${id}`);
+export const marqueNotificationCommeLue = async (
+  type: TypeNotification,
+  id: string
+) => {
+  const routes: Record<TypeNotification, string> = {
+    nouveaute: 'nouveautes',
+    tache: 'taches',
+  };
+  await axios.put(`/api/notifications/${routes[type]}/${id}`);
 };
