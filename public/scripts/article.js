e4c1a9432b56623a739986da1c8ad9f090c666e4
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-07-15 17:47:46 +0200

    Ajoute un scroll 'smooth' sur les liens

--- public/scripts/article.js
+++ public/scripts/article.js
@@ -1,5 +1,6 @@
 $(() => {
   const titres = $('h2,h3,h4');
+  const tousLesLiensVersTitres = $('.table-des-matieres a');
 
   const afficheLienActifSiTitreVisible = (entry) => {
     const id = entry.target.getAttribute('id');
@@ -21,5 +22,22 @@ $(() => {
 
   $(document).on('beforeunload', () => {
     observateurDeTitre.disconnect();
+  });
+
+  const scrollVersTitre = (e) => {
+    e.preventDefault();
+    const idCible = $(e.target).attr('href');
+    const cible = $(idCible);
+    if (cible) {
+      cible[0].scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+  };
+
+  tousLesLiensVersTitres.on('click', (e) => {
+    scrollVersTitre(e);
   });
 });
