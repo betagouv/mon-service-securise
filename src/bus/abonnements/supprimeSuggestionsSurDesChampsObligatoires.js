89b0e8fb2addea088e165606c460c6bdde568ce2
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-07-18 11:51:05 +0200

    Acquitte la suggestion « organisations utilisatrices » lors de la MAJ du service

--- src/bus/abonnements/supprimeSuggestionsSurDesChampsObligatoires.js
+++ src/bus/abonnements/supprimeSuggestionsSurDesChampsObligatoires.js
@@ -1,6 +1,14 @@
 function supprimeSuggestionsSurDesChampsObligatoires({ depotDonnees }) {
   return async ({ service }) => {
+    // Les données pointées par les suggestions d'actions sont des données obligatoires dans le modèle.
+    // Cet abonné est déclenché suite à une mise à jour du service…
+    // …or le service ne peut être sauvegardé que s'il a toutes les données obligatoires.
+    // Donc on acquitte les suggestions d'actions.
     await depotDonnees.acquitteSuggestionAction(service.id, 'miseAJourSiret');
+    await depotDonnees.acquitteSuggestionAction(
+      service.id,
+      'miseAJourNombreOrganisationsUtilisatrices'
+    );
   };
 }
 
