da711776c871dcda8ad0b8d07dfc9505da45f2e5
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-07-18 11:51:05 +0200

    Acquitte la suggestion « organisations utilisatrices » lors de la MAJ du service

--- test/bus/abonnements/supprimeSuggestionsSurDesChampsObligatoires.spec.js
+++ test/bus/abonnements/supprimeSuggestionsSurDesChampsObligatoires.spec.js
@@ -6,10 +6,10 @@ const { unService } = require('../../constructeurs/constructeurService');
 
 describe('L’abonnement qui supprime les suggestions portant sur des données obligatoires', () => {
   it('utilise le dépôt pour supprimer la suggestion de mise à jour du SIRET', async () => {
-    let suggestionSupprimee;
+    let idServiceAcquitte;
     const depotDonnees = {
       acquitteSuggestionAction: (idService, nature) => {
-        suggestionSupprimee = { idService, nature };
+        if (nature === 'miseAJourSiret') idServiceAcquitte = idService;
       },
     };
 
@@ -21,9 +21,22 @@ describe('L’abonnement qui supprime les suggestions portant sur des données o
     await abonne({
       service: unService().avecId('S1').construis(),
     });
-    expect(suggestionSupprimee).to.eql({
-      idService: 'S1',
-      nature: 'miseAJourSiret',
-    });
+    expect(idServiceAcquitte).to.be('S1');
+  });
+
+  it('utilise le dépôt pour supprimer la suggestion de mise à jour des organisations utilisatrices', async () => {
+    let idServiceAcquitte;
+    const depotDonnees = {
+      acquitteSuggestionAction: (idService, nature) => {
+        if (nature === 'miseAJourNombreOrganisationsUtilisatrices')
+          idServiceAcquitte = idService;
+      },
+    };
+
+    await supprimeSuggestionsSurDesChampsObligatoires({
+      depotDonnees,
+    })({ service: unService().avecId('S1').construis() });
+
+    expect(idServiceAcquitte).to.be('S1');
   });
 });
