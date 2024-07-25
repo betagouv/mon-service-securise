fb5a1ade15f70eeb853d0f64c2d01d59022db00b
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-07-25 09:12:39 +0200

    Ajoute le filtrage au sein du store de notifications
    ... pour différencier les notifications du centre et celles des pages

--- svelte/lib/ui/stores/notifications.store.ts
+++ svelte/lib/ui/stores/notifications.store.ts
@@ -1,7 +1,14 @@
 import { writable } from 'svelte/store';
 import type { Notification, TypeNotification } from '../types.d';
 
-const { subscribe, set } = writable<Notification[]>([]);
+type Notifications = {
+  pourCentreNotifications: Notification[];
+  pourPage: Notification[];
+};
+const { subscribe, set } = writable<Notifications>({
+  pourCentreNotifications: [],
+  pourPage: [],
+});
 
 const routes: Record<TypeNotification, string> = {
   nouveaute: 'nouveautes',
@@ -12,7 +19,15 @@ export const storeNotifications = {
   subscribe,
   rafraichis: async () => {
     const reponse = await axios.get('/api/notifications');
-    set(reponse.data.notifications);
+    const toutesNotifications = reponse.data.notifications;
+    set({
+      pourCentreNotifications: toutesNotifications.filter(
+        (n: Notification) => n.canalDiffusion === 'centreNotifications'
+      ),
+      pourPage: toutesNotifications.filter(
+        (n: Notification) => n.canalDiffusion === 'page'
+      ),
+    });
   },
   marqueLue: async (type: TypeNotification, id: string) => {
     await axios.put(`/api/notifications/${routes[type]}/${id}`);
