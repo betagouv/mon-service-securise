0fa6cee74e8aeb6299414f5deb9b36f7411be5f5
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2024-07-26 10:10:43 +0200

    [VISITE GUIDEE] Ajoute l'étape 3 de Décrire
    ... en seconde sous-étape de la première étape de la visite

--- svelte/lib/visiteGuidee/etapes/decrire/EtapeDecrire.svelte
+++ svelte/lib/visiteGuidee/etapes/decrire/EtapeDecrire.svelte
@@ -3,19 +3,35 @@
   import { onMount } from 'svelte';
 
   let cibleNomService: HTMLElement;
+  let cibleBesoinsSecurite: HTMLElement;
   let cibleGererContributeurs: HTMLElement;
   onMount(() => {
+    document.body.dispatchEvent(
+      new CustomEvent('svelte-recharge-niveaux-de-securite', {
+        detail: {
+          niveauDeSecuriteMinimal: 'niveau1',
+          lectureSeule: false,
+          avecSuggestionBesoinsSecuriteRetrogrades: false,
+          idService: 'ID',
+          modeVisiteGuidee: true,
+        },
+      })
+    );
     cibleNomService = document.getElementById('nom-service')!;
+    cibleBesoinsSecurite = document.getElementById('niveaux-de-securite')!;
     cibleGererContributeurs = document.getElementById('gerer-contributeurs')!;
   });
 </script>
 
-{#if cibleNomService && cibleGererContributeurs}
+{#if cibleNomService && cibleBesoinsSecurite && cibleGererContributeurs}
   <ModaleSousEtape
     sousEtapes={[
       {
         cible: cibleNomService,
         callbackInitialeCible: (cible) => {
+          document.body.dispatchEvent(
+            new CustomEvent('jquery-affiche-decrire-etape-1')
+          );
           cible.style.width = '50%';
           window.scrollTo(0, 0); // Ici on force le scroll pour ne pas être dérangé par "Erreur de saisie"
         },
@@ -26,6 +42,28 @@
           'Dans un premier temps, renseignez quelques informations afin d’obtenir une liste personnalisée de mesures de sécurité.',
         animation: '/statique/assets/images/visiteGuidee/decrire.gif',
         margeElementMisEnAvant: 28,
+      },
+      {
+        cible: cibleBesoinsSecurite,
+        callbackInitialeCible: (cible) => {
+          document.body.dispatchEvent(
+            new CustomEvent('jquery-replie-menu-navigation-visite-guidee')
+          );
+          document.body.dispatchEvent(
+            new CustomEvent('jquery-affiche-decrire-etape-3')
+          );
+          cible.scrollIntoView({ block: 'start' });
+        },
+        delaiAvantAffichage: 300,
+        callbackFinaleCible: () => {
+          window.scrollTo(0, 0);
+        },
+        positionnementModale: 'DeuxTiersCentre',
+        titre: 'Sélectionnez le besoin de sécurité',
+        description:
+          'Sélectionnez les besoins identifiés par l’ANSSI ou des besoins plus élevés pour découvrir la liste des mesures pour sécuriser votre service.',
+        animation: '/statique/assets/images/visiteGuidee/besoins_securite.gif',
+        margeElementMisEnAvant: 20,
       },
       {
         cible: cibleGererContributeurs,
