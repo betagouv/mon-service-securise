351a8ffbe164989b668b5023216f7a740501a497
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-08-02 10:46:37 +0200

    Renomme homologation -> service dans les objets PDF

--- src/modeles/objetsPDF/objetPDFAnnexeRisques.js
+++ src/modeles/objetsPDF/objetPDFAnnexeRisques.js
@@ -3,14 +3,14 @@ const Referentiel = require('../../referentiel');
 class ObjetPDFAnnexeRisques {
   constructor(homologation, referentiel = Referentiel.creeReferentielVide()) {
     this.referentiel = referentiel;
-    this.homologation = homologation;
+    this.service = homologation;
   }
 
   donnees() {
     return {
       niveauxGravite: this.referentiel.infosNiveauxGraviteConcernes(true),
-      nomService: this.homologation.nomService(),
-      risquesParNiveauGravite: this.homologation.risques.parNiveauGravite(),
+      nomService: this.service.nomService(),
+      risquesParNiveauGravite: this.service.risques.parNiveauGravite(),
     };
   }
 }
