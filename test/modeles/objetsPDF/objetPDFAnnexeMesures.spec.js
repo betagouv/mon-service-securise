a3b3c64943e51f89e5d3f006597dfcae6bab8871
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-08-02 16:15:56 +0200

    Renomme les variables homologation en service

--- test/modeles/objetsPDF/objetPDFAnnexeMesures.spec.js
+++ test/modeles/objetsPDF/objetPDFAnnexeMesures.spec.js
@@ -18,7 +18,7 @@ describe("L'objet PDF de l'annexe des mesures", () => {
   };
   const referentiel = Referentiel.creeReferentielVide();
   referentiel.enrichis(donneesReferentiel);
-  const homologation = new Service(
+  const service = new Service(
     {
       id: '123',
       idUtilisateur: '456',
@@ -31,10 +31,7 @@ describe("L'objet PDF de l'annexe des mesures", () => {
   );
 
   it('fournit les statuts provenant du référentiel', () => {
-    const vueAnnexePDFMesures = new VueAnnexePDFMesures(
-      homologation,
-      referentiel
-    );
+    const vueAnnexePDFMesures = new VueAnnexePDFMesures(service, referentiel);
 
     const donnees = vueAnnexePDFMesures.donnees();
 
@@ -43,10 +40,7 @@ describe("L'objet PDF de l'annexe des mesures", () => {
   });
 
   it('fournit les catégories provenant du référentiel', () => {
-    const vueAnnexePDFMesures = new VueAnnexePDFMesures(
-      homologation,
-      referentiel
-    );
+    const vueAnnexePDFMesures = new VueAnnexePDFMesures(service, referentiel);
 
     const donnees = vueAnnexePDFMesures.donnees();
 
@@ -55,10 +49,7 @@ describe("L'objet PDF de l'annexe des mesures", () => {
   });
 
   it("fournit le nom du service de l'homologation", () => {
-    const vueAnnexePDFMesures = new VueAnnexePDFMesures(
-      homologation,
-      referentiel
-    );
+    const vueAnnexePDFMesures = new VueAnnexePDFMesures(service, referentiel);
 
     const donnees = vueAnnexePDFMesures.donnees();
 
@@ -67,11 +58,8 @@ describe("L'objet PDF de l'annexe des mesures", () => {
   });
 
   it("fournit les mesures par statut et par catégorie de l'homologation", () => {
-    const vueAnnexePDFMesures = new VueAnnexePDFMesures(
-      homologation,
-      referentiel
-    );
-    homologation.mesuresParStatutEtCategorie = () => ({
+    const vueAnnexePDFMesures = new VueAnnexePDFMesures(service, referentiel);
+    service.mesuresParStatutEtCategorie = () => ({
       grave: { gouvernance: [{ id: 'mesure1' }] },
     });
 
@@ -84,11 +72,8 @@ describe("L'objet PDF de l'annexe des mesures", () => {
   });
 
   it("fournit le nombre de mesures à remplir toutes catégories de l'homologation", () => {
-    const vueAnnexePDFMesures = new VueAnnexePDFMesures(
-      homologation,
-      referentiel
-    );
-    homologation.nombreTotalMesuresARemplirToutesCategories = () => 6;
+    const vueAnnexePDFMesures = new VueAnnexePDFMesures(service, referentiel);
+    service.nombreTotalMesuresARemplirToutesCategories = () => 6;
 
     const donnees = vueAnnexePDFMesures.donnees();
 
@@ -97,15 +82,19 @@ describe("L'objet PDF de l'annexe des mesures", () => {
   });
 
   it('fournit une chaine de caractères indiquant les référentiels concernés par les mesures non renseignées', () => {
-    const service = unService().construis();
-    service.mesures.enrichiesAvecDonneesPersonnalisees = () => ({
-      mesuresGenerales: {
-        mesureA: { referentiel: 'ANSSI' },
-        mesureB: { referentiel: 'CNIL' },
-        mesureC: { referentiel: 'CNIL' },
-      },
-    });
-    const vueAnnexePDFMesures = new VueAnnexePDFMesures(service, referentiel);
+    const serviceAvecMesuresAnssiCnil = unService().construis();
+    serviceAvecMesuresAnssiCnil.mesures.enrichiesAvecDonneesPersonnalisees =
+      () => ({
+        mesuresGenerales: {
+          mesureA: { referentiel: 'ANSSI' },
+          mesureB: { referentiel: 'CNIL' },
+          mesureC: { referentiel: 'CNIL' },
+        },
+      });
+    const vueAnnexePDFMesures = new VueAnnexePDFMesures(
+      serviceAvecMesuresAnssiCnil,
+      referentiel
+    );
 
     const donnees = vueAnnexePDFMesures.donnees();
 
