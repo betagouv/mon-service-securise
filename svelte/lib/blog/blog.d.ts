031b5269e50f35d528d89f82ce3de10b319c206c
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2024-08-05 14:59:13 +0200

    [BLOG] Affiche les articles

--- svelte/lib/blog/blog.d.ts
+++ svelte/lib/blog/blog.d.ts
@@ -6,9 +6,17 @@ declare global {
 
 export type BlogProps = {
   sections: Section[];
+  articles: Article[];
 };
 
 export type Section = {
   id: string;
   nom: string;
+};
+
+export type Article = {
+  id: string;
+  titre: string;
+  url?: string;
+  section: Section;
 };
