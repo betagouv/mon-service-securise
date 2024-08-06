aa37c6a5a6ffdcb4bddfc88cddf0f0c3c9b24ced
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-08-06 11:11:11 +0200

    Exécute seulement le CRON sur l'instance principale
    … pour éviter les jobs en doublon.

--- scripts/taches/tacheEnvoieMailsNotificationExpiration.sh
+++ scripts/taches/tacheEnvoieMailsNotificationExpiration.sh
@@ -4,5 +4,12 @@
 # Il permet au process node d'avoir accès aux variables d'env
 # https://developers.clever-cloud.com/doc/administrate/cron/#access-environment-variables
 
+# On ne veut pas exécuter les CRON depuis une instance spawnée à cause d'un scaling.
+# https://developers.clever-cloud.com/doc/administrate/cron/#deduplicating-crons
+if [[ "$INSTANCE_NUMBER" != "0" ]]; then
+  echo "L'instance n'est pas la 0, c'est la ${INSTANCE_NUMBER}. On s'arrête là."
+  exit 0
+fi
+
 cd "${APP_HOME}" || exit
 node scripts/taches/tacheEnvoieMailsNotificationExpiration.js
