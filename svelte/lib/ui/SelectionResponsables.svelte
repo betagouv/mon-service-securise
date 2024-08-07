d3416db3e88773424424d6bd0b2df9fb6bf7ec22
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-08-07 17:52:59 +0200

    Affiche tous les contributeurs disponible pour une mesure

--- svelte/lib/ui/SelectionResponsables.svelte
+++ svelte/lib/ui/SelectionResponsables.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { ResponsableMesure } from '../tableauDesMesures/tableauDesMesures.d';
   import MenuFlottant from './MenuFlottant.svelte';
+  import { contributeurs } from '../tableauDesMesures/stores/contributeurs.store';
 
   export let responsables: ResponsableMesure[] | null;
   export let estLectureSeule: boolean;
@@ -21,6 +22,30 @@
       <span class="titre">Attribuer une mesure</span>
       <button class="fermeture" on:click={() => (menuOuvert = false)}>✕</button>
     </div>
+    <div>
+      {#each $contributeurs as contributeur (contributeur.id)}
+        <div class="conteneur-contributeur">
+          <div class="conteneur-nom">
+            <span class="initiales">
+              {#if contributeur.initiales}
+                {contributeur.initiales}
+              {:else}
+                <img
+                  src="/statique/assets/images/icone_utilisateur_trait.svg"
+                  alt=""
+                />
+              {/if}
+            </span>
+            <span class="nom-contributeur">{contributeur.prenomNom}</span>
+          </div>
+          <input
+            type="checkbox"
+            value={contributeur.id}
+            class="checkbox-contributeur"
+          />
+        </div>
+      {/each}
+    </div>
   </div>
 </MenuFlottant>
 
@@ -33,7 +58,7 @@
     flex-direction: column;
     gap: 4px;
     background: white;
-    width: 274px;
+    width: 290px;
   }
 
   .bouton-declencheur {
@@ -87,7 +112,7 @@
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    padding: 8px;
+    padding: 8px 0;
   }
 
   .entete .titre {
@@ -106,5 +131,46 @@
 
   .entete .fermeture:hover {
     color: var(--texte-fonce);
+  }
+
+  .conteneur-contributeur {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 9px 0;
+    gap: 4px;
+  }
+
+  .conteneur-nom {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 8px;
+  }
+
+  .initiales {
+    min-width: 32px;
+    min-height: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: var(--fond-bleu-pale);
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 16px;
+    border-radius: 50%;
+  }
+
+  .nom-contributeur {
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 24px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    max-width: 224px;
+  }
+
+  .checkbox-contributeur {
+    margin: 0;
   }
 </style>
