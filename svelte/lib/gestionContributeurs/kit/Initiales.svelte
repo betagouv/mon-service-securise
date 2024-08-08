1e007a847be007d82f4186351453c3961158a4ea
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-08-08 11:22:25 +0200

    Modifie les couleurs des niveaux de droits dans les initiales des contributeurs

--- svelte/lib/gestionContributeurs/kit/Initiales.svelte
+++ svelte/lib/gestionContributeurs/kit/Initiales.svelte
@@ -23,7 +23,6 @@
     align-items: center;
     justify-content: center;
     font-weight: 700;
-    color: #ffffff;
   }
 
   .initiales.persona::before {
@@ -33,21 +32,58 @@
     background: url('/statique/assets/images/persona_invitation_contributeur.svg')
       no-repeat;
     background-size: contain;
+    filter: brightness(0);
   }
 
   .PROPRIETAIRE {
-    background: #c19616;
+    background: var(--role-proprietaire);
+  }
+
+  .initiales.PROPRIETAIRE {
+    color: var(--role-proprietaire-texte);
+  }
+
+  .initiales.PROPRIETAIRE.persona::before {
+    filter: brightness(0) invert(75%) sepia(72%) saturate(1307%)
+      hue-rotate(334deg) brightness(98%) contrast(101%);
   }
 
   .ECRITURE {
-    background: linear-gradient(180deg, #326fc0 0%, #4d3dc5 100%);
+    background: var(--role-editeur);
+  }
+
+  .initiales.ECRITURE {
+    color: var(--role-editeur-texte);
+  }
+
+  .initiales.ECRITURE.persona::before {
+    filter: brightness(0) invert(37%) sepia(97%) saturate(2274%)
+      hue-rotate(184deg) brightness(85%) contrast(102%);
   }
 
   .LECTURE {
-    background: linear-gradient(180deg, #a226b8 0%, #8926c9 100%);
+    background: var(--role-lecteur);
+  }
+
+  .initiales.LECTURE {
+    color: var(--role-lecteur-texte);
+  }
+
+  .initiales.LECTURE.persona::before {
+    filter: brightness(0) invert(20%) sepia(89%) saturate(3120%)
+      hue-rotate(259deg) brightness(83%) contrast(106%);
   }
 
   .PERSONNALISE {
-    background: linear-gradient(180deg, #54b8f6 0%, #3479c9 100%);
+    background: var(--role-personnalise);
+  }
+
+  .initiales.PERSONNALISE {
+    color: var(--role-personnalise-texte);
+  }
+
+  .initiales.PERSONNALISE.persona::before {
+    filter: brightness(0) invert(30%) sepia(68%) saturate(1806%)
+      hue-rotate(112deg) brightness(92%) contrast(91%);
   }
 </style>
