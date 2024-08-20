4970b0e81e6e20973939813b0072203071f41d52
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-08-20 10:38:46 +0200

    Ajoute la deuxième sous-étape de la visite guidée du plan d'action

--- svelte/lib/visiteGuidee/etapes/securiser/EtapeSecuriser.svelte
+++ svelte/lib/visiteGuidee/etapes/securiser/EtapeSecuriser.svelte
@@ -3,10 +3,14 @@
   import { onMount } from 'svelte';
 
   let ciblePremiereMesure: HTMLDivElement;
+  let cibleOnglets: HTMLDivElement;
   let cibleIndiceCyber: HTMLDivElement;
   onMount(() => {
     ciblePremiereMesure = document.getElementsByClassName(
       'titre-mesure'
+    )[0]! as HTMLDivElement;
+    cibleOnglets = document.getElementsByClassName(
+      'conteneur-onglet'
     )[0]! as HTMLDivElement;
     cibleIndiceCyber = document.getElementsByClassName(
       'conteneur-indice-cyber'
@@ -14,7 +18,7 @@
   });
 </script>
 
-{#if ciblePremiereMesure && cibleIndiceCyber}
+{#if ciblePremiereMesure && cibleOnglets && cibleIndiceCyber}
   <ModaleSousEtape
     sousEtapes={[
       {
@@ -29,6 +33,20 @@
         description:
           'Chaque mesure est associée à son référentiel (ANSSI, CNIL) et son niveau d’importance (recommandée ou indispensable pour les mesures ANSSI).',
         animation: '/statique/assets/images/visiteGuidee/securiser_1.gif',
+      },
+      {
+        cible: cibleOnglets,
+        callbackInitialeCible: (cible) => {
+          const onglets = cible.childNodes;
+          for (let i = 0; i < onglets.length; i++) {
+            onglets[i].inert = true;
+          }
+        },
+        positionnementModale: 'HautDroite',
+        titre: 'Définissez un statut pour chaque mesure !',
+        description:
+          'En fonction du statut défini, les mesures sont catégorisées par onglets afin de faciliter le suivi de celles qui vous restent à faire.',
+        animation: '/statique/assets/images/visiteGuidee/securiser_2.gif',
       },
       {
         cible: cibleIndiceCyber,
