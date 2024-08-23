574de485cb520d1780831528e0d43a23406b7cb0
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-08-23 09:54:04 +0200

    Affiche la pastille quand le filtre `Mes mesures` est appliqué

--- svelte/test/tableauDesMesures/stores/nombreDeResultats.store.spec.ts
+++ svelte/test/tableauDesMesures/stores/nombreDeResultats.store.spec.ts
@@ -1,12 +1,28 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { get } from 'svelte/store';
 import { rechercheParPriorite } from '../../../lib/tableauDesMesures/stores/rechercheParPriorite.store';
 import { nombreResultats } from '../../../lib/tableauDesMesures/stores/nombreDeResultats.store';
+import { rechercheMesMesures } from '../../../lib/tableauDesMesures/stores/rechercheMesMesures.store';
 
 describe('Le store dérivé du nombre des résultats de recherche de mesure', () => {
+  beforeEach(() => {
+    rechercheParPriorite.set([]);
+    rechercheMesMesures.set(false);
+  });
+
   describe("sur application d'un filtre de priorité", () => {
     it("indique qu'un filtre est appliqué", () => {
       rechercheParPriorite.set(['p1']);
+
+      const { aDesFiltresAppliques } = get(nombreResultats);
+
+      expect(aDesFiltresAppliques).toBe(true);
+    });
+  });
+
+  describe('sur application du filtre `Mes mesures`', () => {
+    it("indique qu'un filtre est appliqué", () => {
+      rechercheMesMesures.set(true);
 
       const { aDesFiltresAppliques } = get(nombreResultats);
 
