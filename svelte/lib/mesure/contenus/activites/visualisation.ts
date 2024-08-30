83605d53c0a5a1351409255baa1fe1dfcbcd537d
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2024-08-30 10:08:51 +0200

    Affiche l'activité "ajoutEcheance"

--- svelte/lib/mesure/contenus/activites/visualisation.ts
+++ svelte/lib/mesure/contenus/activites/visualisation.ts
@@ -5,6 +5,7 @@ import ActiviteInconnue from './ActiviteInconnue.svelte';
 import type { SvelteComponent } from 'svelte';
 import ActiviteAjoutStatut from './ActiviteAjoutStatut.svelte';
 import ActiviteMiseAJourStatut from './ActiviteMiseAJourStatut.svelte';
+import ActiviteAjoutEcheance from './ActiviteAjoutEcheance.svelte';
 
 export type VisualisationActivite = {
   titre: string;
@@ -29,6 +30,10 @@ const visualisationsParType: Partial<
   miseAJourStatut: {
     titre: 'Modification du statut',
     composantContenu: ActiviteMiseAJourStatut,
+  },
+  ajoutEcheance: {
+    titre: 'Échéance',
+    composantContenu: ActiviteAjoutEcheance,
   },
 };
 
---
+++ svelte/lib/ui/TagEcheanceMesure.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let echeance: Date;
+  const echeanceFormattee = new Intl.DateTimeFormat('fr-FR', {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+  }).format(echeance);
+</script>
+
+<span class="dateEcheance">
+  {echeanceFormattee}
+</span>
+
+<style>
+  .dateEcheance::before {
+    content: '';
+    width: 24px;
+    height: 24px;
+    display: inline-block;
+    background: url('/statique/assets/images/icone_calendrier_sans_loupe.svg')
+      no-repeat;
+  }
+
+  .dateEcheance {
+    display: flex;
+    align-items: center;
+    margin: 2px 0;
+    gap: 6px;
+  }
+</style>
