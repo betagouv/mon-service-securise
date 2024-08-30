c69770168b0bd2009c72cfe7140fe9bf9ce4d2d4
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2024-08-30 10:08:51 +0200

    Affiche l'activité "ajoutEcheance"

---
+++ svelte/lib/mesure/contenus/activites/ActiviteAjoutEcheance.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { ActiviteMesure, DetailsAjoutPropriete } from '../../mesure.d';
+  import TagEcheanceMesure from '../../../ui/TagEcheanceMesure.svelte';
+
+  export let activite: ActiviteMesure;
+
+  const details = <DetailsAjoutPropriete>activite.details;
+  const echeance = new Date(details.nouvelleValeur);
+</script>
+
+<div>
+  L'échéance de la <b>mesure #{activite.identifiantNumeriqueMesure}</b> a été
+  fixée au
+  <TagEcheanceMesure {echeance} />
+</div>
