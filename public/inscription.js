a6ba94a1d89b52a2c101cd5042b935676d2c2d68
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2024-09-12 14:28:09 +0200

    Pré-charge les valeurs dans le formulaire d'inscription
    ...en cas d'utilisateur agent connect

--- public/inscription.js
+++ public/inscription.js
@@ -1,6 +1,23 @@
 import brancheSoumissionFormulaireUtilisateur from './modules/interactions/brancheSoumissionFormulaireUtilisateur.js';
 
 $(() => {
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has('ac')) {
+    const { selectize } = $('#siretEntite-selectize')[0];
+    $(document.body).on('recharge-siret-entite', (evt) => {
+      const { options } = evt.originalEvent.detail;
+      if (options && options.length > 0) {
+        selectize.setValue(options[0].label, true);
+      }
+    });
+    $('#prenom').val(urlParams.get('prenom'));
+    $('#nom').val(urlParams.get('nom'));
+    $('#email').val(urlParams.get('email'));
+    const siret = urlParams.get('siret');
+    $('#siretEntite').val(siret);
+    $('#siretEntite-selectize')[0].selectize.onSearchChange(siret);
+  }
+
   const selecteurFormulaire = 'form#inscription';
   const action = (donnees) =>
     axios
