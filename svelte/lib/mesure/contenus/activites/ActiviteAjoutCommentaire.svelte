e97b200f08ab1fea69d1663904bd21541a445207
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-09-20 10:41:26 +0200

    Affiche correctement les retours chariot dans les commentaires

--- svelte/lib/mesure/contenus/activites/ActiviteAjoutCommentaire.svelte
+++ svelte/lib/mesure/contenus/activites/ActiviteAjoutCommentaire.svelte
@@ -9,14 +9,13 @@
   const regexUUID =
     /@\[([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})\]/gm;
 
-  const contenu = decode(details.contenu).replaceAll(
-    regexUUID,
-    (_s, idUtilisateur) => {
+  const contenu = decode(details.contenu)
+    .replaceAll(regexUUID, (_s, idUtilisateur) => {
       const contributeur = $contributeurs.find((c) => c.id === idUtilisateur);
       const texte = contributeur ? contributeur.prenomNom : 'Utilisateur·rice';
       return `<span class="mention">@${texte}</span>`;
-    }
-  );
+    })
+    .replaceAll(/\n/gm, '<br>');
 </script>
 
 <div>
