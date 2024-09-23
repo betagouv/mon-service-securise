ca46b3dcb06f42294429b5ba27b00bbe9ced828d
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-09-23 14:16:59 +0200

    Utilise un `pool` minimum de 0
    ... pour éviter les `stales connexions`, suite à la migration vers CleverCloud

--- knexfile.js
+++ knexfile.js
@@ -2,14 +2,14 @@ module.exports = {
   development: {
     client: 'pg',
     connection: process.env.URL_SERVEUR_BASE_DONNEES,
-    pool: { min: 2, max: 10 },
+    pool: { min: 0, max: 10 },
     migrations: { tableName: 'knex_migrations' },
   },
 
   production: {
     client: 'pg',
     connection: process.env.URL_SERVEUR_BASE_DONNEES,
-    pool: { min: 2, max: 10 },
+    pool: { min: 0, max: 10 },
     migrations: { tableName: 'knex_migrations' },
   },
 };
