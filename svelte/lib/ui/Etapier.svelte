c8f31c3d0e3de2daf0d2535d3413a3c8c6f64620
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-09-24 15:03:51 +0200

    Affiche l’étapier
    ...et crée un composant Etapier au passage

---
+++ svelte/lib/ui/Etapier.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let etapeCourante: number;
+  export let nombreEtapes: number;
+</script>
+
+<div>
+  {#each new Array(nombreEtapes).fill(0) as _, i}
+    <span class:accessible={i + 1 <= etapeCourante} />
+  {/each}
+</div>
+
+<style>
+  div {
+    display: flex;
+    gap: 8px;
+  }
+
+  span {
+    flex-grow: 1;
+    height: 8px;
+    background-color: #eee;
+  }
+  .accessible {
+    background-color: var(--bleu-mise-en-avant);
+  }
+</style>
