a0646df26252fe2ae51200b1ae586ae44a8a8e21
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-09-26 09:17:33 +0200

    Extrais un composant pour le champ texte

---
+++ svelte/lib/ui/ChampTexte.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let nom: string;
+  export let id: string;
+</script>
+
+<input type="text" {id} name={nom} />
+
+<style>
+  input {
+    border-radius: 5px;
+    border: 1px solid var(--liseres-fonce);
+    font-size: 16px;
+    padding: 8px 16px;
+    line-height: 24px;
+  }
+</style>
