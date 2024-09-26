4c413ae8f4fc39848224b4090709ea3c0fe38f56
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-09-26 11:01:22 +0200

    Ajoute le squelette du composant Profil

---
+++ svelte/lib/profil/Profil.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import type { Departement, Utilisateur } from './profil.d';
+
+  export let departements: Departement[];
+  export let utilisateur: Utilisateur;
+</script>
---
+++ svelte/lib/profil/profil.ts
@@ -0,0 +1,18 @@
+import Profil from './Profil.svelte';
+import type { ProfilProps } from './profil.d';
+
+document.body.addEventListener(
+  'svelte-recharge-profil',
+  (e: CustomEvent<ProfilProps>) => rechargeApp({ ...e.detail })
+);
+
+let app: Profil;
+const rechargeApp = (props: ProfilProps) => {
+  app?.$destroy();
+  app = new Profil({
+    target: document.getElementById('conteneur-profil')!,
+    props: props,
+  });
+};
+
+export default app!;
