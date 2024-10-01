df9ec26464fa3e7cf554d69343afd6d6b723a71a
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-10-01 11:46:14 +0200

    Extrais la sélection du nombre de services dans un composant

---
+++ svelte/lib/inscription/SelectionNombreServices.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { validationChamp } from '../directives/validationChamp';
+  import type { EstimationNombreServices } from './inscription.d';
+
+  export let id: string;
+  export let nombreServices: string = '';
+  export let estimationNombreServices: EstimationNombreServices[];
+</script>
+
+<select
+  {id}
+  required
+  bind:value={nombreServices}
+  use:validationChamp={'Ce champ est obligatoire. Veuillez sélectionner une option.'}
+>
+  <option value="" disabled selected>Sélectionner un nombre</option>
+  {#each estimationNombreServices as estimation}
+    {@const valeur = `${estimation.borneBasse}_${estimation.borneHaute}`}
+    <option value={valeur}>{estimation.label}</option>
+  {/each}
+</select>
+
+<style>
+  select {
+    appearance: auto;
+  }
+</style>
