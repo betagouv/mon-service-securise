d016d58ff167ba452afa95dd835e388981e978e8
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-10-09 17:16:38 +0200

    Ajoute le tableau des risques
    ... qui ne permet pas pour l'instant de modifier les risques existants.

--- svelte/lib/risques/risques.d.ts
+++ svelte/lib/risques/risques.d.ts
@@ -4,7 +4,30 @@ declare global {
   }
 }
 
+export type DonneesRisque = {
+  niveauGravite: string;
+  id: string;
+  commentaire: string;
+  intitule: string;
+  categories: string[];
+  identifiantNumerique: string;
+};
+
+export type Risques = {
+  risquesGeneraux: DonneesRisque[];
+  risquesSpecifiques: DonneesRisque[];
+};
+export type TypeRisque = 'GENERAL' | 'SPECIFIQUE';
+export type Risque = DonneesRisque & { type: TypeRisque };
+
+export type NiveauGravite = {
+  position: number;
+};
+
 export type RisquesProps = {
   idService: string;
   estLectureSeule: boolean;
+  risques: Risques;
+  categories: Record<string, string>;
+  niveauxGravite: Record<string, NiveauGravite>;
 };
