2c6230caef1afeeb47102218365b372d0ab8667f
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-10-14 09:37:33 +0200

    [SOIN] Extrais la ligne de risque dans un composant

--- svelte/lib/risques/Risques.svelte
+++ svelte/lib/risques/Risques.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
   import type { Risque, NiveauGravite } from './risques.d';
-  import CartoucheReferentiel from '../ui/CartoucheReferentiel.svelte';
-  import { Referentiel } from '../ui/types.d';
   import TiroirRisque from './TiroirRisque.svelte';
+  import LigneRisque from './LigneRisque.svelte';
 
   export let idService: string;
   export let estLectureSeule: boolean;
@@ -47,44 +46,14 @@
   </thead>
   <tbody>
     {#each risques as risque (risque.id)}
-      {@const estSpecifiqueAMettreAJour =
-        risque.type === 'SPECIFIQUE' && !risque.categories.length}
-      <tr class:estSpecifiqueAMettreAJour on:click={() => ouvreRisque(risque)}>
-        <td>
-          <span class="identifiant-numerique"
-            >{risque.identifiantNumerique}</span
-          >
-        </td>
-        <td class="intitule">
-          <p class="intitule-risques" title="Ce risque doit être mis à jour">
-            {estSpecifiqueAMettreAJour ? '⚠️ ' : ''}{risque.intitule}
-          </p>
-          <p class="cartouches-intitule">
-            <CartoucheReferentiel
-              referentiel={risque.type === 'GENERAL'
-                ? Referentiel.ANSSI
-                : Referentiel.RISQUE_SPECIFIQUE}
-            />
-            {#each risque.categories as categorie, index (index)}
-              <span class="cartouche-categorie">{categories[categorie]}</span>
-            {/each}
-          </p>
-        </td>
-        <td>
-          <select
-            class="niveau-gravite {risque.niveauGravite}"
-            class:vide={!risque.niveauGravite}
-            bind:value={risque.niveauGravite}
-            disabled={estLectureSeule || estSpecifiqueAMettreAJour}
-            on:change={() => metAJourRisque(risque)}
-          >
-            <option label="+" value="" disabled />
-            {#each Object.entries(niveauxGravite) as [id, niveauGravite] (id)}
-              <option label={niveauGravite.position.toString()} value={id} />
-            {/each}
-          </select>
-        </td>
-      </tr>
+      <LigneRisque
+        {risque}
+        on:click={() => ouvreRisque(risque)}
+        {categories}
+        {niveauxGravite}
+        {estLectureSeule}
+        on:metAJourRisque={() => metAJourRisque(risque)}
+      />
     {/each}
   </tbody>
 </table>
@@ -113,113 +82,15 @@
     white-space: nowrap;
   }
 
-  thead tr th:first-of-type,
-  tbody tr td:first-of-type {
+  thead tr th:first-of-type {
     padding-left: 24px;
   }
 
-  thead tr th:last-of-type,
-  tbody tr td:last-of-type {
+  thead tr th:last-of-type {
     padding-right: 24px;
-  }
-
-  tbody td {
-    padding: 24px 24px;
   }
 
   tr {
     border: 1px solid #cbd5e1;
-  }
-
-  .identifiant-numerique {
-    font-size: 14px;
-    font-weight: 700;
-    line-height: 20px;
-    display: flex;
-    height: 24px;
-    padding: 0 10px;
-    align-items: center;
-    gap: 4px;
-    border-radius: 40px;
-    border: 1.5px solid var(--texte-fonce);
-    width: fit-content;
-  }
-
-  .intitule {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-  }
-
-  .intitule-risques {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 22px;
-    margin: 0;
-  }
-
-  .cartouches-intitule {
-    margin: 0;
-    display: flex;
-    gap: 8px;
-  }
-
-  .cartouche-categorie {
-    display: flex;
-    height: 24px;
-    padding: 1px 8px 3px 8px;
-    align-items: center;
-    gap: 6px;
-    border-radius: 40px;
-    background: #f1f5f9;
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 16px;
-  }
-
-  .niveau-gravite {
-    display: flex;
-    width: 23px;
-    padding: 0 4px;
-    justify-content: center;
-    align-items: center;
-    gap: 4px;
-    border-radius: 4px;
-    color: white;
-    font-size: 14px;
-    font-weight: 700;
-    line-height: 24px;
-    box-sizing: border-box;
-    margin: 0;
-    text-align: center;
-  }
-
-  .niveau-gravite.nonConcerne {
-    background: var(--liseres-fonce);
-  }
-
-  .niveau-gravite.minime {
-    background: var(--bleu-mise-en-avant);
-  }
-
-  .niveau-gravite.significatif {
-    background: var(--bleu-survol);
-  }
-
-  .niveau-gravite.grave {
-    background: var(--bleu-anssi);
-  }
-
-  .niveau-gravite.critique {
-    background: black;
-  }
-
-  .niveau-gravite.vide {
-    background: var(--fond-gris-pale);
-    color: var(--texte-clair);
-  }
-
-  .estSpecifiqueAMettreAJour {
-    background: var(--fond-ocre-pale);
   }
 </style>
