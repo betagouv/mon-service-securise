1697276056c27b51974e1e556917cef06fa8670d
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-10-15 14:10:08 +0200

    Affiche la sélection de la gravité avec un libellé

--- svelte/lib/risques/SelectionGravite.svelte
+++ svelte/lib/risques/SelectionGravite.svelte
@@ -4,18 +4,27 @@
   export let niveauGravite: IdentifiantGravite;
   export let referentielGravites: ReferentielGravites;
   export let estLectureSeule;
+  export let avecLibelleOption: boolean = false;
 </script>
 
 <select
   class="niveau-gravite {niveauGravite}"
   class:vide={!niveauGravite}
+  class:avecLibelleOption
   bind:value={niveauGravite}
   disabled={estLectureSeule}
   on:change
 >
-  <option label="+" value="" disabled />
+  <option
+    label={avecLibelleOption ? 'Définir la gravité' : '+'}
+    value=""
+    disabled
+  />
   {#each Object.entries(referentielGravites) as [id, niveau] (id)}
-    <option label={niveau.position.toString()} value={id} />
+    <option
+      label={`${niveau.position.toString()} - ${niveau.description}`}
+      value={id}
+    />
   {/each}
 </select>
 
@@ -23,7 +32,7 @@
   .niveau-gravite {
     display: flex;
     width: 23px;
-    padding: 0 4px;
+    padding: 0 6px 2px 7px;
     justify-content: center;
     align-items: center;
     gap: 4px;
@@ -31,10 +40,11 @@
     color: white;
     font-size: 14px;
     font-weight: 700;
-    line-height: 24px;
+    line-height: 22px;
     box-sizing: border-box;
     margin: 0;
     text-align: center;
+    border: none;
   }
 
   .niveau-gravite.nonConcerne {
@@ -60,5 +70,15 @@
   .niveau-gravite.vide {
     background: var(--fond-gris-pale);
     color: var(--texte-clair);
+    padding: 0 3px 3px;
+  }
+
+  select.avecLibelleOption {
+    width: fit-content;
+    appearance: auto;
+    text-align: left;
+    padding: 8px;
+    border-radius: 4px;
+    border: 1px solid transparent;
   }
 </style>
---
+++ svelte/lib/ui/ControleFormulaireTiroir.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let libelle: string;
+  export let requis: boolean = false;
+</script>
+
+<label>
+  <span class:requis class="libelle">{libelle}</span>
+  <span class="contenu">
+    <slot />
+  </span>
+</label>
+
+<style>
+  .contenu {
+    margin-top: 4px;
+  }
+
+  label {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    font-weight: normal;
+    margin: 0;
+  }
+
+  .libelle {
+    font-weight: bold;
+  }
+
+  .requis:before {
+    content: '*';
+    color: #e3271c;
+    margin-right: 4px;
+    font-size: 1rem;
+  }
+</style>
