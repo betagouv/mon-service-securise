3a7dc5a3ae747afad4f4ed483221c052314f967a
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-10-25 10:46:50 +0200

    Crée une classe `Contributeur`
    … qui sera utilisé dans `Service` pour valoriser `this.contributeurs`.
    
    Actuellement `this.contributeurs` contient des `Utilisateur`.
    
    Mais `Utilisateur` va évoluer pour intégrer un versionning des CGU acceptées. La dépendance entre `this.contributeurs` et `Utilisateur` complique cette évolution.
    
    On veut que le code de `this.contributeurs` reste indépendant de ce changement. Donc, on extrait une nouvelle abstraction qui ne sera pas impactée par les CGU versionnées.

---
+++ test/modeles/contributeur.spec.js
@@ -0,0 +1,30 @@
+const expect = require('expect.js');
+const { Contributeur } = require('../../src/modeles/contributeur');
+
+describe('Un contributeur', () => {
+  it('connaît son identifiant', () => {
+    const contributeur = new Contributeur({ id: 'C-1' });
+
+    expect(contributeur.id).to.be('C-1');
+  });
+
+  it('connaît son « prénom / nom »', () => {
+    const contributeur = new Contributeur({ prenom: 'Jean', nom: 'Dujardin' });
+
+    expect(contributeur.prenomNom()).to.be('Jean Dujardin');
+  });
+
+  it('connaît ses initiales', () => {
+    const contributeur = new Contributeur({ prenom: 'Jean', nom: 'Dujardin' });
+
+    expect(contributeur.initiales()).to.be('JD');
+  });
+
+  it("connaît les détails du poste qu'il occupe", () => {
+    const contributeur = new Contributeur({
+      postes: ['RSSI', 'DPO', 'Maire'],
+    });
+
+    expect(contributeur.posteDetaille()).to.eql('RSSI, DPO et Maire');
+  });
+});
