ef81566d7c2ddbb9c49dbfbea05580ea7577ff14
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-10-25 14:50:22 +0200

    [SOIN] Gagne des sauts de ligne

--- src/adaptateurs/adaptateurJWT.js
+++ src/adaptateurs/adaptateurJWT.js
@@ -6,10 +6,6 @@ const decode = (token) => (token ? jwt.verify(token, secret) : undefined);
 const signeDonnees = (donnees) => jwt.sign(donnees, secret);
 
 const genereToken = (idUtilisateur, cguAcceptees, source) =>
-  signeDonnees({
-    idUtilisateur,
-    cguAcceptees,
-    source,
-  });
+  signeDonnees({ idUtilisateur, cguAcceptees, source });
 
 module.exports = { decode, genereToken, signeDonnees };
