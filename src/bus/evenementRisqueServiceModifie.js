f0db4c0f78cd380c734c4b685bf0366c4201028a
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-10-30 10:54:21 +0100

    Contrôle que l’événement est construit avec un service

--- src/bus/evenementRisqueServiceModifie.js
+++ src/bus/evenementRisqueServiceModifie.js
@@ -1,5 +1,8 @@
 class EvenementRisqueServiceModifie {
   constructor({ service }) {
+    if (!service) {
+      throw new Error("Impossible d'instancier l'événement sans service");
+    }
     this.service = service;
   }
 }
