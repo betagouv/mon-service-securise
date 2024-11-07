2588d4245d3059ee9f892d435f44485964e2cd10
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-11-07 11:30:02 +0100

    Ajoute une méthode au dépôt permettant d'ajouter des établissements supervisés (via un SIRET)

--- src/depots/depotDonneesSuperviseurs.js
+++ src/depots/depotDonneesSuperviseurs.js
@@ -1,8 +1,11 @@
 const creeDepot = ({ adaptateurPersistance }) => {
+  const ajouteSiretAuSuperviseur = async (idSuperviseur, siret) =>
+    adaptateurPersistance.ajouteSiretAuSuperviseur(idSuperviseur, siret);
+
   const lisSuperviseurs = async (siret) =>
     adaptateurPersistance.lisSuperviseursConcernes(siret);
 
-  return { lisSuperviseurs };
+  return { ajouteSiretAuSuperviseur, lisSuperviseurs };
 };
 
 module.exports = { creeDepot };
