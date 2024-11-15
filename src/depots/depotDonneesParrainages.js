94877a7ed7e628fee0a40af0f8f9ce699c03fbbf
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2024-11-15 15:52:31 +0100

    [PARRAINAGE] Ajoute au dépôt de données la lecture et mise à jour d'un parrainage

--- src/depots/depotDonneesParrainages.js
+++ src/depots/depotDonneesParrainages.js
@@ -1,3 +1,5 @@
+const Parrainage = require('../modeles/parrainage');
+
 const creeDepot = (config = {}) => {
   const { adaptateurPersistance } = config;
 
@@ -8,8 +10,22 @@ const creeDepot = (config = {}) => {
       parrainage.filleulAFinaliseCompte
     );
 
+  const parrainagePour = async (idUtilisateurFilleul) => {
+    const donnees =
+      await adaptateurPersistance.parrainagePour(idUtilisateurFilleul);
+    return donnees ? new Parrainage(donnees) : undefined;
+  };
+
+  const metsAJourParrainage = async (parrainage) =>
+    adaptateurPersistance.metsAJourParrainage(
+      parrainage.idUtilisateurFilleul,
+      parrainage.filleulAFinaliseCompte
+    );
+
   return {
     ajouteParrainage,
+    parrainagePour,
+    metsAJourParrainage,
   };
 };
 module.exports = { creeDepot };
--- test/depots/depotDonneesParrainages.spec.js
+++ test/depots/depotDonneesParrainages.spec.js
@@ -36,4 +36,51 @@ describe('Le dépôt de données des parrainages', () => {
     expect(idUtilisateurParrainRecu).to.be('P1');
     expect(filleulAFinaliseCompteRecu).to.be(true);
   });
+
+  it("délègue à l'adaptateur persistance la mise à jour du parrainage", async () => {
+    let idUtilisateurFilleulRecu;
+    let filleulAFinaliseCompteRecu;
+    adaptateurPersistance = {
+      metsAJourParrainage: async (
+        idUtilisateurFilleul,
+        filleulAFinaliseCompte
+      ) => {
+        idUtilisateurFilleulRecu = idUtilisateurFilleul;
+        filleulAFinaliseCompteRecu = filleulAFinaliseCompte;
+      },
+    };
+
+    const parrainage = new Parrainage({
+      idUtilisateurFilleul: 'F1',
+      idUtilisateurParrain: 'P1',
+      filleulAFinaliseCompte: true,
+    });
+
+    await depot().metsAJourParrainage(parrainage);
+
+    expect(idUtilisateurFilleulRecu).to.be('F1');
+    expect(filleulAFinaliseCompteRecu).to.be(true);
+  });
+
+  it("délègue à l'adaptateur persistance la récupération d'un parrainage selon l'id du filleul", async () => {
+    let idUtilisateurFilleulRecu;
+    adaptateurPersistance = {
+      parrainagePour: async (idUtilisateurFilleul) => {
+        idUtilisateurFilleulRecu = idUtilisateurFilleul;
+        return {
+          idUtilisateurFilleul: 'F1',
+          idUtilisateurParrain: 'P1',
+          filleulAFinaliseCompte: true,
+        };
+      },
+    };
+
+    const resultat = await depot().parrainagePour('F1');
+
+    expect(idUtilisateurFilleulRecu).to.be('F1');
+    expect(resultat).to.be.a(Parrainage);
+    expect(resultat.filleulAFinaliseCompte).to.be(true);
+    expect(resultat.idUtilisateurFilleul).to.be('F1');
+    expect(resultat.idUtilisateurParrain).to.be('P1');
+  });
 });
