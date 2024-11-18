2163c925b1fdd244e23885aa01cfba6dc71a44cf
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-11-18 10:29:31 +0100

    [CORRECTION] Ne met pas à jour le parrainage si l'utilisateur est "encore" un invité
    ... car, lors de la récupération des informations depuis ProConnect, on enregistre les informations complémentaires (nom, prénom, siret) avant même d'avoir terminé le parcours d'inscription. On souhaite donc s'assurer que l'utilisateur a bien été au bout de ce parcours.

--- src/bus/abonnements/metAJourParrainage.js
+++ src/bus/abonnements/metAJourParrainage.js
@@ -1,7 +1,11 @@
 function metAJourParrainage({ depotDonnees }) {
   return async ({ utilisateur }) => {
     const parrainage = await depotDonnees.parrainagePour(utilisateur.id);
-    if (!parrainage || parrainage.compteFilleulEstFinalise()) {
+    if (
+      !parrainage ||
+      parrainage.compteFilleulEstFinalise() ||
+      utilisateur.estUnInvite()
+    ) {
       return;
     }
     parrainage.confirmeFinalisationCompteFilleul();
