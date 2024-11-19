93eedb7d750db1b161f9b86e5958c3c35d763a4e
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-11-19 09:59:22 +0100

    [CORRECTION] Affiche la modale de reconnexion si une erreur "CSRF mismatch" est interceptée par le front

--- public/scripts/csrf.js
+++ public/scripts/csrf.js
@@ -1,5 +1,17 @@
+import lanceDecompteDeconnexion from '../modules/deconnexion.js';
+
 $(() => {
   const $infos = $('#infos-csrf');
   const { token } = JSON.parse($infos.text());
   axios.defaults.headers.common['X-CSRF-Token'] = token;
+
+  axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response.data.includes('CSRF token mismatch')) {
+        lanceDecompteDeconnexion(0);
+      }
+      return Promise.reject(error);
+    }
+  );
 });
