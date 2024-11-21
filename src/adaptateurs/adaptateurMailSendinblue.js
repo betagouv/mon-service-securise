c50a5c11e629e88a3d466eb970a8ec2d54db52b9
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2024-11-21 13:41:17 +0100

    Active le mode `createOrUpdate` dans la création de contact Brevo
    ...ainsi, si le contact existe déjà avec cet email, il sera mis à jour

--- src/adaptateurs/adaptateurMailSendinblue.js
+++ src/adaptateurs/adaptateurMailSendinblue.js
@@ -76,6 +76,7 @@ const creeContact = (
     .post(
       `${urlBase}/contacts`,
       {
+        updateEnabled: true,
         email: destinataire,
         emailBlacklisted: bloqueMarketing,
         attributes: {
