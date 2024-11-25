11a4a2deefc4cf0807cc2414c39ddfd86b4ad7ed
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-11-26 00:12:21 +0100

    Ajoute une indication de chargement de l'indice cyber

---
+++ svelte/lib/ui/IconeChargementEnCours.svelte
@@ -0,0 +1,23 @@
+<div></div>
+
+<style>
+  div {
+    width: 12px;
+    height: 12px;
+    margin: 0;
+    border-radius: 50%;
+    border: 2px solid;
+    border-color: var(--bleu-mise-en-avant) transparent;
+    animation: rotation 1.2s linear infinite;
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
