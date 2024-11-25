d7c7d713fb26115da6407b129c0837059fd75e65
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-11-25 20:10:39 +0100

    Ajoute la visualisation de toutes les informations de service
    ... et le premier badge "Propriétaire"

--- svelte/lib/tableauDeBord/tableauDeBord.d.ts
+++ svelte/lib/tableauDeBord/tableauDeBord.d.ts
@@ -3,3 +3,51 @@ declare global {
     'svelte-recharge-tableau-de-bord': CustomEvent;
   }
 }
+
+type Contributeur = {
+  id: string;
+  prenomNom: string;
+  initiales: string;
+  poste: string;
+  estUtilisateurCourant: boolean;
+};
+
+export type Service = {
+  id: string;
+  nomService: string;
+  organisationResponsable: string;
+  contributeurs: Contributeur[];
+  statutHomologation: {
+    id: string;
+    enCoursEdition: boolean;
+    libelle: string;
+    ordre: number;
+  };
+  nombreContributeurs: number;
+  estProprietaire: boolean;
+  documentsPdfDisponibles: string[];
+  permissions: {
+    gestionContributeurs: boolean;
+  };
+  aUneSuggestionAction: boolean;
+};
+
+export type ReponseApiServices = {
+  services: Service[];
+  resume: {
+    nombreServices: number;
+    nombreServicesHomologues: number;
+  };
+};
+
+export type IndiceCyber = {
+  id: string;
+  indiceCyber: number;
+};
+
+export type ReponseApiIndicesCyber = {
+  services: IndiceCyber[];
+  resume: {
+    indiceCyberMoyen: number;
+  };
+};
