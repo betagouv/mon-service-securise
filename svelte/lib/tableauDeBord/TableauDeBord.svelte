5b736bec75efc66e9f5c1dd883518889bc17bea1
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2024-11-26 10:27:37 +0100

    [SOIN] Extrais un composant pour le tableau des services

--- svelte/lib/tableauDeBord/TableauDeBord.svelte
+++ svelte/lib/tableauDeBord/TableauDeBord.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { decode } from 'html-entities';
   import type {
     ReponseApiServices,
     ReponseApiIndicesCyber,
@@ -8,11 +7,7 @@
     IndiceCyber,
   } from './tableauDeBord.d';
   import ChargementEnCours from '../ui/ChargementEnCours.svelte';
-  import EtiquetteProprietaire from './elementsDeService/EtiquetteProprietaire.svelte';
-  import EtiquetteContributeurs from './elementsDeService/EtiquetteContributeurs.svelte';
-  import EtiquetteIndiceCyber from './elementsDeService/EtiquetteIndiceCyber.svelte';
-  import IconeChargementEnCours from '../ui/IconeChargementEnCours.svelte';
-  import EtiquetteHomologation from './elementsDeService/EtiquetteHomologation.svelte';
+  import TableauDesServices from './TableauDesServices.svelte';
 
   let enCoursChargement = false;
 
@@ -45,53 +40,7 @@
     <ChargementEnCours />
   </div>
 {:else}
-  <table>
-    <thead>
-      <tr>
-        <th>Nom du service</th>
-        <th>Contributeurs</th>
-        <th>Indice cyber</th>
-        <th>Homologation</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each services as service (service.id)}
-        {@const idService = service.id}
-        {@const indiceCyberDuService = indicesCybers.find(
-          (i) => i.id === idService
-        )?.indiceCyber}
-        <tr>
-          <td>
-            <a class="lien-service" href="/service/{idService}">
-              {#if service.estProprietaire}
-                <EtiquetteProprietaire />
-              {/if}
-              <span class="nom-service">{decode(service.nomService)}</span>
-            </a>
-          </td>
-          <td>
-            <EtiquetteContributeurs
-              nombreContributeurs={service.nombreContributeurs}
-            />
-          </td>
-          <td>
-            {#if indiceCyberDuService !== undefined}
-              <EtiquetteIndiceCyber score={indiceCyberDuService} {idService} />
-            {:else}
-              <IconeChargementEnCours />
-            {/if}
-          </td>
-          <td>
-            <EtiquetteHomologation
-              statutHomologation={service.statutHomologation.id}
-              label={service.statutHomologation.libelle}
-              {idService}
-            />
-          </td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
+  <TableauDesServices {services} {indicesCybers} />
 {/if}
 
 <style>
@@ -110,40 +59,5 @@
 
   h1 {
     margin: 0 0 32px;
-  }
-
-  table {
-    border-collapse: collapse;
-    border: 1px solid #ddd;
-    width: 100%;
-  }
-
-  table tr {
-    border: 1px solid #ddd;
-  }
-
-  table td,
-  table th {
-    padding: 8px 16px;
-  }
-
-  table th {
-    font-size: 14px;
-    font-weight: 700;
-    line-height: 24px;
-  }
-
-  .lien-service {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    font-size: 14px;
-    font-weight: 700;
-    line-height: 24px;
-    color: var(--texte-fonce);
-  }
-
-  .lien-service:hover .nom-service {
-    color: var(--bleu-mise-en-avant);
   }
 </style>
