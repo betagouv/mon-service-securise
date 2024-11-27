b87d54f61eab5283fa1d1d4c3e794ce70e8b7330
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-11-27 11:45:31 +0100

    Utilise le composant "Lien" pour l'ajout de service

--- svelte/lib/tableauDeBord/BandeauFiltres.svelte
+++ svelte/lib/tableauDeBord/BandeauFiltres.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
   import BarreDeRecherche from '../ui/BarreDeRecherche.svelte';
   import { rechercheTextuelle } from './stores/rechercheTextuelle.store';
+  import Lien from '../ui/Lien.svelte';
 </script>
 
 <div class="conteneur-filtres">
   <BarreDeRecherche bind:recherche={$rechercheTextuelle} />
+  <Lien
+    titre="Ajouter un service"
+    type="bouton-primaire"
+    icone="plus"
+    href="/service/creation"
+  />
 </div>
+
+<style>
+  .conteneur-filtres {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+</style>
