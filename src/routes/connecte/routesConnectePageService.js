771e0af07b831cb2e1a975d8e399c545c9c7de3a
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-11-28 16:11:32 +0100

    [SOIN] Renomme le PUG

--- src/routes/connecte/routesConnectePageService.js
+++ src/routes/connecte/routesConnectePageService.js
@@ -212,7 +212,7 @@ const routesConnectePageService = ({
       const risquesGenerauxAConsiderer = Object.keys(referentiel.risques())
         .map((id) => risquesGeneraux.find((r) => r.id === id) || { id })
         .map((donnees) => new RisqueGeneral(donnees, referentiel).toJSON());
-      reponse.render('service/risques-v2', {
+      reponse.render('service/risques', {
         InformationsService,
         referentiel,
         service,
