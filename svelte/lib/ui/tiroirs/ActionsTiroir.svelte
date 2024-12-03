e387181c2dcc0b3ca4cf8ec03ed49768a9aaa59c
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-12-03 16:54:18 +0100

    Ajoute des composants "de base" permettant de réaliser un "Composant Tiroir"

---
+++ svelte/lib/ui/tiroirs/ActionsTiroir.svelte
@@ -0,0 +1,19 @@
+<div class="conteneur-actions">
+  <slot />
+</div>
+
+<style>
+  .conteneur-actions {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    position: sticky;
+    bottom: 0;
+    border-top: 1px solid #cbd5e1;
+    padding: 19px 32px;
+    background: white;
+    flex-grow: 0;
+    flex-shrink: 0;
+    gap: 10px;
+  }
+</style>
---
+++ svelte/lib/ui/tiroirs/ContenuTiroir.svelte
@@ -0,0 +1,14 @@
+<div class="contenu-tiroir">
+  <slot />
+</div>
+
+<style>
+  .contenu-tiroir {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    text-align: left;
+    padding: 32px;
+    gap: 30px;
+  }
+</style>
