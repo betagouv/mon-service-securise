518d349810c8f91eb9d458a1a24cc25bbf11008d
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-12-04 16:57:21 +0100

    Ajoute le tiroir de gestion des contributeurs

---
+++ svelte/lib/ui/tiroirs/TiroirGestionContributeurs.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import ContenuTiroir from './ContenuTiroir.svelte';
+  import GestionContributeurs from '../../gestionContributeurs/GestionContributeurs.svelte';
+  import type { Service } from '../../gestionContributeurs/gestionContributeurs.d';
+  import { store } from '../../gestionContributeurs/gestionContributeurs.store';
+
+  export let services: Service[];
+
+  store.reinitialise(services);
+</script>
+
+<ContenuTiroir>
+  <div id="contenu-contributeurs">
+    <GestionContributeurs modeVisiteGuidee={false} />
+  </div>
+</ContenuTiroir>
