2f654f58a91dab7df787dc81181c243c7e292824
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2024-12-10 14:44:05 +0100

    Demande confirmation à la suppression d'un projet d'homologation
    ...via un message de confirmation à taper plutôt qu'un challenge mot de passe. En effet, les utilisateurs ProConnect n'ont pas de mot de passe dans MSS.
    On demande à l'utilisateur de copier une chaîne pour confirmer qu'il comprend l'action qu'il est en train de faire.

--- public/modules/tableauDeBord/actions/ActionSuppressionDossierCourant.mjs
+++ public/modules/tableauDeBord/actions/ActionSuppressionDossierCourant.mjs
@@ -13,50 +13,21 @@ class ActionSuppressionDossierCourant extends ActionAbstraite {
   initialise() {
     super.initialise();
     $('#action-suppression-dossier-courant').show();
-    const $msgErreurChallenge = $(
-      '#mot-de-passe-challenge-suppression-dossier-courant ~ .message-erreur-specifique'
-    );
-    const $champChallenge = $(
-      '#mot-de-passe-challenge-suppression-dossier-courant'
-    );
-    $msgErreurChallenge.hide();
-    $champChallenge.val('');
-
-    $champChallenge.off('input');
-    $champChallenge.on('input', () => $msgErreurChallenge.hide());
+    const champConfirmation = $('#confirmation-suppression');
+    champConfirmation.val('');
+    champConfirmation.removeClass('touche');
   }
 
   // eslint-disable-next-line consistent-return
   async execute({ idService }) {
     const $actionSuppression = $('#action-suppression-dossier-courant');
-    const motDePasseChallenge = $(
-      '#mot-de-passe-challenge-suppression-dossier-courant'
-    ).val();
 
     if (!this.formulaireEstValide) return Promise.reject();
 
     $actionSuppression.hide();
     this.basculeLoader(true);
 
-    try {
-      await axios.delete(
-        `/api/service/${idService}/homologation/dossierCourant`,
-        {
-          data: { motDePasseChallenge },
-        }
-      );
-    } catch (e) {
-      if (e.response.status === 401) {
-        const $msgErreurChallenge = $(
-          '#mot-de-passe-challenge-suppression-dossier-courant ~ .message-erreur-specifique'
-        );
-        $msgErreurChallenge.css('display', 'flex');
-        $actionSuppression.show();
-        this.basculeLoader(false);
-
-        throw e;
-      }
-    }
+    await axios.delete(`/api/service/${idService}/homologation/dossierCourant`);
   }
 }
 
