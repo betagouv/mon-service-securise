d41b7ac1d8b6faac7b500aff856772508a9c4226
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2024-12-19 16:20:10 +0100

    [SOIN] Extrais la config d'authentification dans une constante

--- src/adaptateurs/adaptateurProfilAnssi.js
+++ src/adaptateurs/adaptateurProfilAnssi.js
@@ -1,7 +1,12 @@
 const axios = require('axios');
 
+const CONFIGURATION_AUTHENTIFICATION = {
+  headers: {
+    Authorization: `Bearer ${process.env.PROFIL_ANSSI_JETON_API}`,
+  },
+};
+
 const inscris = async ({ nom, prenom, email, entite, telephone, postes }) => {
-  const jeton = process.env.PROFIL_ANSSI_JETON_API;
   const urlInscription = `${process.env.PROFIL_ANSSI_URL_BASE}/inscription`;
   await axios.post(
     urlInscription,
@@ -13,16 +18,11 @@ const inscris = async ({ nom, prenom, email, entite, telephone, postes }) => {
       telephone,
       domainesSpecialite: postes,
     },
-    {
-      headers: {
-        Authorization: `Bearer ${jeton}`,
-      },
-    }
+    CONFIGURATION_AUTHENTIFICATION
   );
 };
 
 const metsAJour = async ({ nom, prenom, email, entite, telephone, postes }) => {
-  const jeton = process.env.PROFIL_ANSSI_JETON_API;
   const urlProfil = `${process.env.PROFIL_ANSSI_URL_BASE}/profil/${email}`;
   await axios.put(
     urlProfil,
@@ -33,22 +33,13 @@ const metsAJour = async ({ nom, prenom, email, entite, telephone, postes }) => {
       telephone,
       domainesSpecialite: postes,
     },
-    {
-      headers: {
-        Authorization: `Bearer ${jeton}`,
-      },
-    }
+    CONFIGURATION_AUTHENTIFICATION
   );
 };
 
 const recupere = async (email) => {
-  const jeton = process.env.PROFIL_ANSSI_JETON_API;
   const urlProfil = `${process.env.PROFIL_ANSSI_URL_BASE}/profil/${email}`;
-  return axios.get(urlProfil, {
-    headers: {
-      Authorization: `Bearer ${jeton}`,
-    },
-  });
+  return axios.get(urlProfil, CONFIGURATION_AUTHENTIFICATION);
 };
 
 module.exports = {
