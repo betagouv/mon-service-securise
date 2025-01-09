6051fad31e2eecc15c224527e5ced33aeae87960
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-01-09 18:24:51 +0100

    [SOIN] Supprime du code inutile
    ... puisqu'on ne peut pas fermer la modale de session expirée

--- public/modules/interactions/modale.mjs
+++ public/modules/interactions/modale.mjs
@@ -3,21 +3,10 @@ const afficheModale = ($rideauModale) => {
   $rideauModale.addClass('visible');
 };
 
-const fermeModale = ($rideauModale) => {
-  $rideauModale.removeClass('visible');
-  $('body').removeClass('recouvert');
-};
-
 const initialiseComportementModale = ($rideauModale) => {
   $rideauModale
     .on('click', (e) => e.stopPropagation())
-    .on('afficheModale', () => afficheModale($rideauModale))
-    .on('fermeModale', () => fermeModale($rideauModale));
-
-  $('.fermeture-modale', $rideauModale).on('click', (eFermeture) => {
-    eFermeture.stopPropagation();
-    $rideauModale.trigger('fermeModale');
-  });
+    .on('afficheModale', () => afficheModale($rideauModale));
 };
 
 export default initialiseComportementModale;
