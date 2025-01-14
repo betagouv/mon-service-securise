28b95c273422eeddd9a460c9237fe4635b585100
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-01-14 17:27:26 +0100

    Ajoute les filtres de propriété et d'indice cyber sur le tableau de services

--- svelte/lib/tableauDeBord/BandeauFiltres.svelte
+++ svelte/lib/tableauDeBord/BandeauFiltres.svelte
@@ -3,10 +3,57 @@
   import { rechercheTextuelle } from './stores/rechercheTextuelle.store';
   import { services } from './stores/services.store';
   import Lien from '../ui/Lien.svelte';
+  import ListeDeroulanteRiche from '../ui/ListeDeroulanteRiche.svelte';
+  import { filtrageServices } from './stores/filtrageServices.store';
 </script>
 
 <div class="conteneur-filtres">
-  <BarreDeRecherche bind:recherche={$rechercheTextuelle} />
+  <div class="recherche">
+    <BarreDeRecherche bind:recherche={$rechercheTextuelle} />
+    <ListeDeroulanteRiche
+      bind:valeursSelectionnees={$filtrageServices}
+      id="filtres"
+      libelle="Filtrer"
+      options={{
+        categories: [
+          { id: 'indiceCyber', libelle: 'Indice cyber' },
+          { id: 'propriete', libelle: 'Propriété' },
+        ],
+        items: [
+          {
+            libelle: 'Inférieur à 1',
+            valeur: '<1',
+            idCategorie: 'indiceCyber',
+          },
+          {
+            libelle: 'Entre 1 et 2',
+            valeur: '1-2',
+            idCategorie: 'indiceCyber',
+          },
+          {
+            libelle: 'Entre 2 et 3',
+            valeur: '2-3',
+            idCategorie: 'indiceCyber',
+          },
+          {
+            libelle: 'Entre 3 et 4',
+            valeur: '3-4',
+            idCategorie: 'indiceCyber',
+          },
+          {
+            libelle: 'Entre 4 et 5',
+            valeur: '4-5',
+            idCategorie: 'indiceCyber',
+          },
+          {
+            libelle: 'Je suis propriétaire du service',
+            valeur: 'proprietaire',
+            idCategorie: 'propriete',
+          },
+        ],
+      }}
+    />
+  </div>
   {#if $services.length > 0}
     <Lien
       classe="nouveau-service"
@@ -23,5 +70,11 @@
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+  }
+
+  .recherche {
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
   }
 </style>
