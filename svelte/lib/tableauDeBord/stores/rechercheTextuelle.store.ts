3eb68b5561331723bea967c8564791385c64fab0
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-01-15 10:06:29 +0100

    [SOIN] Extrais la récupération des indices cyber
    ... pour les agréger aux données de service et faciliter leur utilisation par la suite

--- svelte/lib/tableauDeBord/stores/rechercheTextuelle.store.ts
+++ svelte/lib/tableauDeBord/stores/rechercheTextuelle.store.ts
@@ -1,11 +1,14 @@
 import { writable } from 'svelte/store';
-import type { Service } from '../tableauDeBord.d';
+import type { ServiceAvecIndiceCyber } from '../tableauDeBord.d';
 
 export const rechercheTextuelle = writable<string>('');
 
 const contientEnMinuscule = (champ: string | undefined, recherche: string) =>
   champ ? champ.toLowerCase().includes(recherche.toLowerCase()) : false;
 
-export const appliqueFiltreTextuel = (service: Service, valeur: string) =>
+export const appliqueFiltreTextuel = (
+  service: ServiceAvecIndiceCyber,
+  valeur: string
+) =>
   contientEnMinuscule(service.nomService, valeur) ||
   contientEnMinuscule(service.organisationResponsable, valeur);
--- svelte/lib/tableauDeBord/stores/services.store.ts
+++ svelte/lib/tableauDeBord/stores/services.store.ts
@@ -1,9 +1,24 @@
 import { writable } from 'svelte/store';
-import type { Service } from '../tableauDeBord.d';
+import type { IndiceCyber, ServiceAvecIndiceCyber } from '../tableauDeBord.d';
 
-const { set, subscribe, update } = writable<Service[]>([]);
+const { set, subscribe, update } = writable<ServiceAvecIndiceCyber[]>([]);
 
 export const services = {
-  reinitialise: (services: Service[]) => set(services),
+  reinitialise: (services: ServiceAvecIndiceCyber[]) => set(services),
+  ajouteIndicesCyber: (indicesCybers: IndiceCyber[]) => {
+    update((services: ServiceAvecIndiceCyber[]) => {
+      return services.map((service) => {
+        const indiceCyberDuService = indicesCybers.find(
+          (i) => i.id === service.id
+        );
+        return {
+          ...service,
+          ...(indiceCyberDuService && {
+            indiceCyber: parseFloat(indiceCyberDuService.indiceCyber),
+          }),
+        };
+      });
+    });
+  },
   subscribe,
 };
