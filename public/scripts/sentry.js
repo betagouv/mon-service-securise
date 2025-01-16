d88ae3988fc67027dec60f4a33ac5b8b9b717a10
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-16 14:18:57 +0100

    Ignore gentiment une erreur eslint

--- public/scripts/sentry.js
+++ public/scripts/sentry.js
@@ -1,7 +1,11 @@
+// La librairie Sentry est chargée via le tag de script de SDK. Nous savons (mieux qu’eslint)
+// que cette librairie est connue ici.
 $(() => {
+  // eslint-disable-next-line no-undef
   Sentry.init({
     dsn: $('#script-sentry').data('dsn'),
     environment: $('#script-sentry').data('environnement'),
   });
+  // eslint-disable-next-line no-undef
   Sentry.setTag('mss-source', 'frontend');
 });
