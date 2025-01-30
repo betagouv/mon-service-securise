db9d74903c5156c171ab79fad6bef80dbf10beb0
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-01-30 16:25:14 +0100

    Renomme le cookie de session en `session`
    ... plutôt que `token`. Ce nommage est très confusant, car le cookie contient lui même un champ `token` qui est le JWT d'authentification.
    On préfère donc `session`.

--- test/aides/cookie.js
+++ test/aides/cookie.js
@@ -11,7 +11,7 @@ const decodeTokenDuCookie = (reponse, indiceHeader) => {
   try {
     const headerCookie = reponse.headers['set-cookie'];
     const cookieSession = enObjet(headerCookie[indiceHeader]);
-    return JSON.parse(Buffer.from(cookieSession.token, 'base64').toString());
+    return JSON.parse(Buffer.from(cookieSession.session, 'base64').toString());
   } catch (e) {
     return undefined;
   }
