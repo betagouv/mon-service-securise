21f66d11eb5f9e20199997e58366930e0768f887
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-02-06 17:31:57 +0100

    Retourne un objet de l'adaptateur chiffrement
    ... plutôt qu'une chaine concaténée, afin de simplifier la lecture, et conserver le format `json` des données en base.

--- src/adaptateurs/adaptateurChiffrementChaCha20.js
+++ src/adaptateurs/adaptateurChiffrementChaCha20.js
@@ -6,13 +6,6 @@ const {
   nonce,
 } = require('./adaptateurChiffrement');
 
-const decoupeLaChaineChiffree = (chaineChiffree) => ({
-  chaineDonneesChiffrees: chaineChiffree.slice(56, -32),
-  chaineIV: chaineChiffree.slice(0, 24),
-  chaineDonneesAdditionnelles: chaineChiffree.slice(24, 56),
-  chaineTag: chaineChiffree.slice(-32),
-});
-
 const adaptateurChiffrementChaCha20 = ({ adaptateurEnvironnement }) => {
   const clefSecrete = Buffer.from(
     adaptateurEnvironnement.chiffrement().cleChaCha20Hex(),
@@ -21,55 +14,47 @@ const adaptateurChiffrementChaCha20 = ({ adaptateurEnvironnement }) => {
   return {
     chiffre: async (chaineOuObjet) => {
       const iv = randomBytes(12);
-      const donneesAdditionnelles = randomBytes(16);
-      const brut = JSON.stringify(chaineOuObjet);
-      const chiffrement = createCipheriv('chacha20-poly1305', clefSecrete, iv, {
+      const aad = randomBytes(16);
+      const donneesAChiffrer = JSON.stringify(chaineOuObjet);
+
+      const chiffreur = createCipheriv('chacha20-poly1305', clefSecrete, iv, {
         authTagLength: 16,
       });
-      chiffrement.setAAD(donneesAdditionnelles, {
-        plaintextLength: Buffer.byteLength(brut),
+      chiffreur.setAAD(aad, {
+        plaintextLength: Buffer.byteLength(donneesAChiffrer),
       });
 
       const donneesChiffrees = Buffer.concat([
-        chiffrement.update(brut, 'utf-8'),
-        chiffrement.final(),
+        chiffreur.update(donneesAChiffrer, 'utf-8'),
+        chiffreur.final(),
       ]);
-      const tag = chiffrement.getAuthTag();
+      const tag = chiffreur.getAuthTag();
 
-      return Buffer.concat([
-        iv,
-        donneesAdditionnelles,
-        donneesChiffrees,
-        tag,
-      ]).toString('hex');
+      return {
+        iv: iv.toString('hex'),
+        aad: aad.toString('hex'),
+        donnees: donneesChiffrees.toString('hex'),
+        tag: tag.toString('hex'),
+      };
     },
-    dechiffre: async (chaineChiffree) => {
-      const {
-        chaineDonneesChiffrees,
-        chaineIV,
-        chaineDonneesAdditionnelles,
-        chaineTag,
-      } = decoupeLaChaineChiffree(chaineChiffree);
-      const iv = Buffer.from(chaineIV, 'hex');
-      const donneesChiffrees = Buffer.from(chaineDonneesChiffrees, 'hex');
-      const tag = Buffer.from(chaineTag, 'hex');
+    dechiffre: async (donneesChiffrees) => {
+      const { iv, aad, donnees, tag } = donneesChiffrees;
 
-      const dechiffrement = createDecipheriv(
+      const dechiffreur = createDecipheriv(
         'chacha20-poly1305',
         clefSecrete,
-        iv,
+        Buffer.from(iv, 'hex'),
         { authTagLength: 16 }
       );
-      dechiffrement.setAAD(Buffer.from(chaineDonneesAdditionnelles, 'hex'), {
-        plaintextLength: chaineDonneesChiffrees.length,
+      dechiffreur.setAAD(Buffer.from(aad, 'hex'), {
+        plaintextLength: donnees.length,
       });
-      dechiffrement.setAuthTag(Buffer.from(tag));
+      dechiffreur.setAuthTag(Buffer.from(tag, 'hex'));
 
-      const donneesDechiffrees = dechiffrement.update(donneesChiffrees);
       const chaineDechiffree = Buffer.concat([
-        donneesDechiffrees,
-        dechiffrement.final(),
-      ]).toString();
+        dechiffreur.update(Buffer.from(donnees, 'hex')),
+        dechiffreur.final(),
+      ]);
       return JSON.parse(chaineDechiffree);
     },
     compareBCrypt,
