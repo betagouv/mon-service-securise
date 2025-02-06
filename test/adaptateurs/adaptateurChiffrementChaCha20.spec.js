d90c0b11845cb06ed7b1786a8b998e74bb402742
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-02-06 17:31:57 +0100

    Retourne un objet de l'adaptateur chiffrement
    ... plutôt qu'une chaine concaténée, afin de simplifier la lecture, et conserver le format `json` des données en base.

--- test/adaptateurs/adaptateurChiffrementChaCha20.spec.js
+++ test/adaptateurs/adaptateurChiffrementChaCha20.spec.js
@@ -19,11 +19,31 @@ describe("L'adaptateur qui chiffre et déchiffre avec l'algorithme ChaCha20", ()
       prenom: 'Poum',
       id: 12043,
     };
-    process.env.CHIFFREMENT_CLE_CHACHA20 = 'toto';
 
     const utilisateurChiffre = await adaptateur.chiffre(utilisateur);
     const utilisateurDechiffre = await adaptateur.dechiffre(utilisateurChiffre);
 
     expect(utilisateurDechiffre).to.eql(utilisateur);
+  });
+
+  it('retourne un objet contenant les metadonnées', async () => {
+    const adaptateur = adaptateurChiffrementChaCha20({
+      adaptateurEnvironnement: {
+        chiffrement: () => ({
+          cleChaCha20Hex: () =>
+            'f1e2d3c4b5a6978877665544332211ffeeddccbbaa9988776655443322110000',
+        }),
+      },
+    });
+
+    const utilisateurChiffre = await adaptateur.chiffre({ nom: 'Jean' });
+
+    const chaineHexaDeDouzeOctets = /^[0-9a-fA-F]{24}$/;
+    const chaineHexaDeSeizeOctets = /^[0-9a-fA-F]{32}$/;
+
+    expect(utilisateurChiffre.iv).to.match(chaineHexaDeDouzeOctets);
+    expect(utilisateurChiffre.aad).to.match(chaineHexaDeSeizeOctets);
+    expect(utilisateurChiffre.donnees).to.be.an('string');
+    expect(utilisateurChiffre.tag).to.match(chaineHexaDeSeizeOctets);
   });
 });
