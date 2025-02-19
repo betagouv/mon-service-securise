0c3f95c1a0f7d4698e3a516a405c89803b02b0ed
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-02-19 19:25:10 +0100

    Variabilise le pool de connexion maximum pour Postgres

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,17 @@
+const { baseDeDonnees } = require('./src/adaptateurs/adaptateurEnvironnement');
+
 module.exports = {
   development: {
     client: 'pg',
     connection: process.env.URL_SERVEUR_BASE_DONNEES,
-    pool: { min: 0, max: 10 },
+    pool: { min: 0, max: baseDeDonnees().poolMaximumConnexion() },
     migrations: { tableName: 'knex_migrations' },
   },
 
   production: {
     client: 'pg',
     connection: process.env.URL_SERVEUR_BASE_DONNEES,
-    pool: { min: 0, max: 10 },
+    pool: { min: 0, max: baseDeDonnees().poolMaximumConnexion() },
     migrations: { tableName: 'knex_migrations' },
   },
 };
