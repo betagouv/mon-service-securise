9ac9715b51f8b8bd3c3f29fd928f425c0f4f8eb7
Pascal Grange <pascal@grange.nom.fr>
2025-04-03 11:17:45 +0200

    Ajoute un script permettant d'initialiser l'env de dev avec un sel

---
+++ scripts/dev_init_sel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+usage() {
+  echo $0 >&2
+  echo Erreur: "$@" >&2
+  echo >&2
+  echo Initialise le sel, à utiliser uniquement en développement >&2
+  exit 1
+}
+
+grep -q CHIFFREMENT_SEL_DE_HASHAGE_1 ../.env && usage CHIFFREMENT_SEL_DE_HASHAGE_1 est déjà défini dans le fichier .env
+
+SEL="$(cat /dev/random | head -c 10 | xxd -p)"
+
+echo "CHIFFREMENT_SEL_DE_HASHAGE_1=$SEL" >> ../.env
+
+HASH_DU_SEL="$(node ./dev-init-sel.js "$SEL")"
+
+psql -h localhost -U postgres mss -c "insert into sels_de_hachage (version, empreinte) values (1, '$HASH_DU_SEL');"
