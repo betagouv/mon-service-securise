b3379f56acdb57f1eddc6af394347f8cb1fd0e4f
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-04-17 11:58:53 +0200

    Connecte un utilisateur si besoin
    ... et enregistre un évènement de connexion. On assigne aussi l'idToken ProConnect dans la session.

--- test/utilisateur/executeurApresAuthentification.spec.js
+++ test/utilisateur/executeurApresAuthentification.spec.js
@@ -2,6 +2,8 @@ const expect = require('expect.js');
 const {
   executeurApresAuthentification,
 } = require('../../src/utilisateur/executeurApresAuthentification');
+const { unUtilisateur } = require('../constructeurs/constructeurUtilisateur');
+const SourceAuthentification = require('../../src/modeles/sourceAuthentification');
 
 class MockReponse {
   redirect(cible) {
@@ -16,22 +18,24 @@ class MockReponse {
 
 describe("L'executeur après authentification", () => {
   let reponse;
+
   beforeEach(() => {
     reponse = new MockReponse();
   });
+
   describe("lorsque le type de l'ordre est une redirection", () => {
-    it('redirige', () => {
+    it('redirige', async () => {
       const ordre = {
         type: 'redirection',
         cible: '/une-url',
       };
 
-      executeurApresAuthentification(ordre, { reponse });
+      await executeurApresAuthentification(ordre, { reponse });
 
       expect(reponse.urlRedirection).to.be('/une-url');
     });
 
-    it("génère un token avec les données de l'ordre", () => {
+    it("génère un token avec les données de l'ordre", async () => {
       const ordre = {
         type: 'redirection',
         cible: '/une-url',
@@ -43,7 +47,7 @@ describe("L'executeur après authentification", () => {
         signeDonnees: (donnees) => `${donnees.champ}-jwt`,
       };
 
-      executeurApresAuthentification(ordre, { reponse, adaptateurJWT });
+      await executeurApresAuthentification(ordre, { reponse, adaptateurJWT });
 
       expect(reponse.urlRedirection).to.be('/une-url?token=valeur-jwt');
     });
@@ -60,13 +64,13 @@ describe("L'executeur après authentification", () => {
       };
     });
 
-    it('rend une page', () => {
+    it('rend une page', async () => {
       const ordre = {
         type: 'rendu',
         cible: 'un-pug',
       };
 
-      executeurApresAuthentification(ordre, { reponse });
+      await executeurApresAuthentification(ordre, { reponse });
 
       expect(reponse.pageRendue).to.be('un-pug');
     });
@@ -88,16 +92,16 @@ describe("L'executeur après authentification", () => {
         };
       });
 
-      it("génère un token avec les données de l'ordre", () => {
-        executeurApresAuthentification(ordre, { reponse, adaptateurJWT });
+      it("génère un token avec les données de l'ordre", async () => {
+        await executeurApresAuthentification(ordre, { reponse, adaptateurJWT });
 
         expect(reponse.donneesDeRenduDePage).to.eql({
           tokenDonneesInvite: 'valeur-jwt',
         });
       });
 
-      it("ajoute l'url de redirection si elle est fournie", () => {
-        executeurApresAuthentification(ordre, {
+      it("ajoute l'url de redirection si elle est fournie", async () => {
+        await executeurApresAuthentification(ordre, {
           reponse,
           adaptateurEnvironnement,
           urlRedirection: '/tableau-bord',
@@ -111,13 +115,13 @@ describe("L'executeur après authentification", () => {
       });
     });
     describe("lorsqu'aucune donnée n'est fournie", () => {
-      it("ajoute l'url de redirection si elle est fournie", () => {
+      it("ajoute l'url de redirection si elle est fournie", async () => {
         const ordre = {
           type: 'rendu',
           cible: 'un-pug',
         };
 
-        executeurApresAuthentification(ordre, {
+        await executeurApresAuthentification(ordre, {
           reponse,
           adaptateurEnvironnement,
           urlRedirection: '/tableau-bord',
@@ -127,6 +131,90 @@ describe("L'executeur après authentification", () => {
           urlRedirection: 'http://mss/tableau-bord',
         });
       });
+    });
+  });
+
+  describe("lorsqu'il y a un utilisateur à connecter", () => {
+    let utilisateurAConnecter;
+    let ordre;
+    let serviceGestionnaireSession;
+    let requete;
+    let depotDonnees;
+
+    beforeEach(() => {
+      utilisateurAConnecter = unUtilisateur()
+        .avecId('U1')
+        .quiEstComplet()
+        .construis();
+      ordre = {
+        utilisateurAConnecter,
+      };
+      serviceGestionnaireSession = {
+        enregistreSession: () => {},
+      };
+      requete = {
+        session: {},
+      };
+      depotDonnees = {
+        enregistreNouvelleConnexionUtilisateur: async () => {},
+      };
+    });
+
+    it('enregistre une session', async () => {
+      let sessionEnregistree;
+      serviceGestionnaireSession = {
+        enregistreSession: (req, utilisateur, source) => {
+          sessionEnregistree = { requete: req, utilisateur, source };
+        },
+      };
+      await executeurApresAuthentification(ordre, {
+        serviceGestionnaireSession,
+        requete,
+        depotDonnees,
+      });
+
+      expect(sessionEnregistree).not.to.be(undefined);
+      expect(sessionEnregistree.requete).to.eql(requete);
+      expect(sessionEnregistree.utilisateur).to.be(utilisateurAConnecter);
+      expect(sessionEnregistree.source).to.be(
+        SourceAuthentification.AGENT_CONNECT
+      );
+    });
+
+    it("enregistre l'idToken de ProConnect", async () => {
+      await executeurApresAuthentification(ordre, {
+        serviceGestionnaireSession,
+        requete,
+        agentConnectIdToken: 'Un token ProConnect',
+        depotDonnees,
+      });
+
+      expect(requete.session.AgentConnectIdToken).to.be('Un token ProConnect');
+    });
+
+    it('enregistre une nouvelle connexion', async () => {
+      let connexionEnregistree;
+      depotDonnees = {
+        enregistreNouvelleConnexionUtilisateur: async (
+          idUtilisateur,
+          source
+        ) => {
+          connexionEnregistree = { idUtilisateur, source };
+        },
+      };
+
+      await executeurApresAuthentification(ordre, {
+        serviceGestionnaireSession,
+        requete,
+        agentConnectIdToken: 'Un token ProConnect',
+        depotDonnees,
+      });
+
+      expect(connexionEnregistree).not.to.be(undefined);
+      expect(connexionEnregistree.idUtilisateur).to.be('U1');
+      expect(connexionEnregistree.source).to.be(
+        SourceAuthentification.AGENT_CONNECT
+      );
     });
   });
 });
