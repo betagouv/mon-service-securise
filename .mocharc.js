b88f7c2de4af9d6b6e509340bea770f2962cafbb
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-05-20 12:55:37 +0200

    Ajoute un rapport de tests dans la CI
    ... en utilisant le rapport `json` de mocha et `dorny`

---
+++ .mocharc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reporter: process.env.GITHUB_ACTIONS ? 'json' : 'spec',
+  'reporter-option': process.env.GITHUB_ACTIONS
+    ? ['output=./backend-test-report.json']
+    : null,
+  spec: './test*/**/*.spec.*js',
+};
