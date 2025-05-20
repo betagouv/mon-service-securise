a7a29ddc86b0ce7b355a27f507e656dbb6d04e10
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-05-20 23:58:48 +0200

    Affiche le rapport détaillé du téléversement

---
+++ svelte/lib/rapportTeleversement/composants/LigneService.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+  import {
+    MessagesErreur,
+    type RapportService,
+    type ErreurService,
+  } from '../rapportTeleversement.d';
+  import CelluleDonneeService from './CelluleDonneeService.svelte';
+  import TagEtat from './TagEtat.svelte';
+
+  export let service: RapportService;
+  export let numeroLigne: number;
+
+  const donneesService = service.service;
+  const aUneErreur = service.erreurs.length > 0;
+
+  const contientErreur = (erreur: ErreurService) =>
+    service.erreurs.includes(erreur);
+</script>
+
+<tr>
+  <th scope="row"><TagEtat valide={!aUneErreur} /></th>
+  <th scope="row" class="message-erreur" class:aUneErreur
+    >{aUneErreur ? MessagesErreur[service.erreurs[0]] : 'Aucune erreur'}</th
+  >
+  <CelluleDonneeService donnee={numeroLigne.toString()} />
+  <CelluleDonneeService
+    donnee={donneesService.nom}
+    enErreur={contientErreur('NOM_EXISTANT') || contientErreur('NOM_INVALIDE')}
+  />
+  <CelluleDonneeService
+    donnee={donneesService.siret}
+    enErreur={contientErreur('SIRET_INVALIDE')}
+  />
+  <CelluleDonneeService
+    donnee={donneesService.type}
+    enErreur={contientErreur('TYPE_INVALIDE')}
+    large
+  />
+  <CelluleDonneeService
+    donnee={donneesService.provenance}
+    enErreur={contientErreur('PROVENANCE_INVALIDE')}
+    large
+  />
+  <CelluleDonneeService
+    donnee={donneesService.statut}
+    enErreur={contientErreur('STATUT_INVALIDE')}
+    large
+  />
+  <CelluleDonneeService
+    donnee={donneesService.localisation}
+    enErreur={contientErreur('LOCALISATION_INVALIDE')}
+  />
+  <CelluleDonneeService
+    donnee={donneesService.delaiAvantImpactCritique}
+    enErreur={contientErreur('DELAI_AVANT_IMPACT_CRITIQUE_INVALIDE')}
+  />
+  <CelluleDonneeService
+    donnee={donneesService.dateHomologation}
+    enErreur={contientErreur('DOSSIER_HOMOLOGATION_INCOMPLET') ||
+      contientErreur('DATE_HOMOLOGATION_INVALIDE')}
+  />
+  <CelluleDonneeService
+    donnee={donneesService.dureeHomologation}
+    enErreur={contientErreur('DOSSIER_HOMOLOGATION_INCOMPLET') ||
+      contientErreur('DUREE_HOMOLOGATION_INVALIDE')}
+  />
+  <CelluleDonneeService
+    donnee={donneesService.nomAutoriteHomologation}
+    enErreur={contientErreur('DOSSIER_HOMOLOGATION_INCOMPLET')}
+    large
+  />
+  <CelluleDonneeService
+    donnee={donneesService.fonctionAutoriteHomologation}
+    enErreur={contientErreur('DOSSIER_HOMOLOGATION_INCOMPLET')}
+    dernier
+  />
+</tr>
+
+<style lang="scss">
+  th[scope='row'] {
+    padding: 8px 16px;
+    max-width: 280px;
+    text-align: left;
+    font-size: 0.875rem;
+    line-height: 1.5rem;
+    background: var(--fond-pale);
+    border-top: 1px solid #dddddd;
+    border-bottom: 1px solid #dddddd;
+  }
+
+  th:first-of-type {
+    border-left: 1px solid #dddddd;
+  }
+
+  th:last-of-type {
+    border-right: 1px solid #dddddd;
+  }
+
+  .message-erreur {
+    min-width: 224px;
+
+    &.aUneErreur {
+      color: var(--erreur-texte);
+    }
+  }
+</style>
---
+++ svelte/lib/rapportTeleversement/rapportTeleversement.d.ts
@@ -0,0 +1,55 @@
+declare global {
+  interface HTMLElementEventMap {
+    'svelte-recharge-rapport-televersement': CustomEvent;
+  }
+}
+
+export type ErreurService =
+  | 'NOM_INVALIDE'
+  | 'NOM_EXISTANT'
+  | 'SIRET_INVALIDE'
+  | 'TYPE_INVALIDE'
+  | 'PROVENANCE_INVALIDE'
+  | 'STATUT_INVALIDE'
+  | 'LOCALISATION_INVALIDE'
+  | 'DELAI_AVANT_IMPACT_CRITIQUE_INVALIDE'
+  | 'DOSSIER_HOMOLOGATION_INCOMPLET'
+  | 'DATE_HOMOLOGATION_INVALIDE'
+  | 'DUREE_HOMOLOGATION_INVALIDE';
+
+export type RapportService = {
+  service: {
+    dateHomologation: string;
+    delaiAvantImpactCritique: string;
+    dureeHomologation: string;
+    fonctionAutoriteHomologation: string;
+    localisation: string;
+    nom: string;
+    nomAutoriteHomologation: string;
+    provenance: string;
+    siret: string;
+    statut: string;
+    type: string;
+  };
+  erreurs: ErreurService[];
+};
+
+export type RapportDetaille = {
+  statut: 'VALIDE' | 'INVALIDE';
+  services: RapportService[];
+};
+
+export const MessagesErreur: Record<ErreurService, string> = {
+  NOM_INVALIDE: 'Le nom est manquant',
+  NOM_EXISTANT: 'Le nom est déjà utilisé',
+  SIRET_INVALIDE: 'Le SIRET est invalide',
+  TYPE_INVALIDE: 'Le type est invalide',
+  PROVENANCE_INVALIDE: 'La provenance est invalide',
+  STATUT_INVALIDE: 'Le statut est invalide',
+  LOCALISATION_INVALIDE: 'La localisation est invalide',
+  DELAI_AVANT_IMPACT_CRITIQUE_INVALIDE:
+    'La durée maximale de dysfonctionnement est invalide',
+  DOSSIER_HOMOLOGATION_INCOMPLET: "Le dossier d'homologation est incomplet",
+  DATE_HOMOLOGATION_INVALIDE: "La date d'homologation est invalide",
+  DUREE_HOMOLOGATION_INVALIDE: "La durée d'homologation est invalide",
+};
