917f1b41693556f7820d3298646fc17c68d0b378
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-05-21 14:23:26 +0200

    [SOIN] Extrais un composant "Toast" du Toaster
    ... afin de pouvoir le réutiliser dans le compte rendu du rapport de téléversement

---
+++ svelte/lib/ui/Toast.svelte
@@ -0,0 +1,108 @@
+<script lang="ts">
+  import { glisse } from './animations/transitions';
+
+  export let niveau: 'info' | 'succes' | 'erreur';
+  export let titre: string;
+  export let contenu: string;
+
+  export let avecOmbre: boolean = true;
+  export let avecAnimation: boolean = true;
+
+  const icones = {
+    info: 'icone_info',
+    succes: 'icone_succes',
+    erreur: 'icone_erreur',
+  };
+
+  const transitionConditionnelle = (
+    noeud: HTMLElement,
+    options: Record<string, any>
+  ) => {
+    if (avecAnimation) return options.fonction(noeud, options);
+  };
+</script>
+
+<article
+  class={niveau}
+  class:avecOmbre
+  transition:transitionConditionnelle|global={{
+    fonction: glisse,
+    depuis: 'right',
+    duree: 250,
+  }}
+>
+  <div class="conteneur-icone">
+    <div class="icone">
+      <img
+        src={`/statique/assets/images/toasts/${icones[niveau]}.svg`}
+        alt=""
+        width="24"
+        height="24"
+      />
+    </div>
+  </div>
+  <div class="conteneur-texte">
+    <p class="titre">{titre}</p>
+    <p class="texte">{@html contenu}</p>
+  </div>
+</article>
+
+<style lang="scss">
+  article {
+    font-size: 0.8em;
+    --couleur: transparent;
+    border: 1px solid var(--couleur);
+    display: flex;
+    flex-direction: row;
+    background: white;
+    align-items: stretch;
+    border-radius: 8px;
+    width: fit-content;
+    min-width: 430px;
+
+    &.avecOmbre {
+      box-shadow: 0 12px 20px 0 #0000001f;
+    }
+
+    &.info {
+      --couleur: var(--bleu-mise-en-avant);
+    }
+
+    &.succes {
+      --couleur: #0c8626;
+    }
+
+    &.erreur {
+      --couleur: #ce0500;
+    }
+
+    .conteneur-icone {
+      background: var(--couleur);
+      padding: 18px 10px;
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    .conteneur-texte {
+      text-align: left;
+      padding: 16px;
+
+      p {
+        margin: 0;
+      }
+
+      .titre {
+        font-size: 1.25rem;
+        font-weight: 700;
+        line-height: 1.75rem;
+        margin-bottom: 4px;
+      }
+
+      .texte {
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5rem;
+      }
+    }
+  }
+</style>
--- svelte/lib/ui/Toaster.svelte
+++ svelte/lib/ui/Toaster.svelte
@@ -1,32 +1,15 @@
 <script lang="ts">
   import { toasterStore } from './stores/toaster.store';
   import { glisse } from './animations/transitions';
+  import Toast from './Toast.svelte';
 
   const icones = { info: 'icone_info', succes: 'icone_succes' };
 </script>
 
 {#if $toasterStore.queue.length}
   <aside>
-    {#each $toasterStore.queue as toast (toast.id)}
-      <article
-        class={toast.niveau}
-        transition:glisse|global={{ depuis: 'right', duree: 250 }}
-      >
-        <div class="conteneur-icone">
-          <div class="icone">
-            <img
-              src={`/statique/assets/images/toasts/${icones[toast.niveau]}.svg`}
-              alt=""
-              width="24"
-              height="24"
-            />
-          </div>
-        </div>
-        <div class="conteneur-texte">
-          <p class="titre">{toast.titre}</p>
-          <p class="texte">{@html toast.contenu}</p>
-        </div>
-      </article>
+    {#each $toasterStore.queue as { niveau, contenu, titre, id } (id)}
+      <Toast {niveau} {titre} {contenu} />
     {/each}
   </aside>
 {/if}
@@ -43,65 +26,5 @@
     gap: 1em;
     padding-top: 2em;
     z-index: 1002;
-  }
-
-  article {
-    font-size: 0.8em;
-    --couleur: transparent;
-    border: 1px solid var(--couleur);
-    display: flex;
-    flex-direction: row;
-    background: white;
-    align-items: stretch;
-    border-radius: 8px;
-    overflow: hidden;
-    box-shadow: 0 12px 20px 0 #0000001f;
-  }
-
-  /* Donne un fond à l'article pour éviter les traces de blanc sur les `radius` de gauche */
-  article.info {
-    background: linear-gradient(
-      to right,
-      var(--bleu-mise-en-avant) 10px,
-      white 10px
-    );
-  }
-  article.succes {
-    background: linear-gradient(to right, #0c8626 10px, white 10px);
-  }
-
-  .conteneur-icone {
-    background: var(--couleur);
-    padding: 18px 10px;
-  }
-
-  .conteneur-texte {
-    text-align: left;
-    padding: 16px;
-  }
-
-  .conteneur-texte p {
-    margin: 0;
-  }
-
-  .conteneur-texte .titre {
-    font-size: 20px;
-    font-weight: 700;
-    line-height: 28px;
-    margin-bottom: 4px;
-  }
-
-  .conteneur-texte .texte {
-    font-size: 0.9rem;
-    font-weight: 400;
-    line-height: normal;
-  }
-
-  .info {
-    --couleur: var(--bleu-mise-en-avant);
-  }
-
-  .succes {
-    --couleur: #0c8626;
   }
 </style>
