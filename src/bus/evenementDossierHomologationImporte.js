ec17714cc1a98ef81c155f2cba3e8fb6f551a598
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-05-22 15:43:11 +0200

    Publie un évènement `DossierHomologationImporte` lors de la création d'un service avec dossier d'homologation
    ... qui servira à créer un évènement du journal, et déclencher les emails de rappel.

---
+++ src/bus/evenementDossierHomologationImporte.js
@@ -0,0 +1,15 @@
+class EvenementDossierHomologationImporte {
+  constructor({ idService, dossier }) {
+    if (!idService)
+      throw Error("Impossible d'instancier l'événement sans ID de service");
+
+    if (!dossier)
+      throw Error("Impossible d'instancier l'événement sans dossier");
+
+    this.idService = idService;
+    this.dossier = dossier;
+    this.importe = true;
+  }
+}
+
+module.exports = EvenementDossierHomologationImporte;
