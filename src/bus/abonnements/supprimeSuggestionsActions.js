fa0bc7bd999836544285fc65b66af1c94b446149
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-05-27 08:49:32 +0200

    Ajoute l'abonné permettant de supprimer les suggestions d'actions pour un service

---
+++ src/bus/abonnements/supprimeSuggestionsActions.js
@@ -0,0 +1,12 @@
+function supprimeSuggestionsActions({ depotDonnees }) {
+  return async ({ idService }) => {
+    if (!idService)
+      throw new Error(
+        "Impossible de supprimer les suggestions d'actions d'un service sans avoir l'ID du service en paramètre."
+      );
+
+    await depotDonnees.supprimeSuggestionsActionsPourService(idService);
+  };
+}
+
+module.exports = { supprimeSuggestionsActions };
---
+++ test/bus/abonnements/supprimeSuggestionsActions.spec.js
@@ -0,0 +1,40 @@
+const expect = require('expect.js');
+const {
+  supprimeSuggestionsActions,
+} = require('../../../src/bus/abonnements/supprimeSuggestionsActions');
+
+describe("L'abonnement qui supprime (en base de données) les suggestions d'actions d'un service", () => {
+  let depotDonnees;
+
+  beforeEach(() => {
+    depotDonnees = {
+      supprimeSuggestionsActionsPourService: async () => {},
+    };
+  });
+
+  it("lève une exception s'il ne reçoit pas l'ID du service", async () => {
+    try {
+      await supprimeSuggestionsActions({ depotDonnees })({
+        idService: null,
+      });
+      expect().fail("L'instanciation aurait dû lever une exception.");
+    } catch (e) {
+      expect(e.message).to.be(
+        "Impossible de supprimer les suggestions d'actions d'un service sans avoir l'ID du service en paramètre."
+      );
+    }
+  });
+
+  it("demande au dépôt de supprimer les suggestions d'actions pour ce service", async () => {
+    let depotAppele = false;
+    depotDonnees.supprimeSuggestionsActionsPourService = async () => {
+      depotAppele = true;
+    };
+
+    await supprimeSuggestionsActions({ depotDonnees })({
+      idService: '123',
+    });
+
+    expect(depotAppele).to.be(true);
+  });
+});
