f9062f64b9da1f7b48024e3df8fba03e1bd4998d
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-06-02 17:50:07 +0200

    [CORRECTION] Utilise `pmap` pour limiter les promesses concurrentes lors de l'enrichissement d'un service
    ... car, pour chaque service, on effectue :
    - une requête pour lire les contributeurs
    - une requête pour lire les suggestions d'actions
    
    Or, comme tous les services sont enrichis en même temps, toutes ces requêtes utilisent le pool de connection de la base de données.

--- src/utilitaires/pMap.js
+++ src/utilitaires/pMap.js
@@ -1,7 +1,15 @@
 const pmap = require('p-map');
 
+const avecPMap = (items, fonctionAAppliquer) =>
+  pmap(items, fonctionAAppliquer, { concurrency: 2 });
+
 const avecPMapPourChaqueElement = (itemsDansPromesse, fonctionAAppliquer) =>
-  itemsDansPromesse.then((items) =>
-    pmap(items, fonctionAAppliquer, { concurrency: 2 })
-  );
-module.exports = { avecPMapPourChaqueElement };
+  itemsDansPromesse.then((items) => avecPMap(items, fonctionAAppliquer));
+
+const avecPMapPourChaqueElementSansPromesse = (items, fonctionAAppliquer) =>
+  avecPMap(items, fonctionAAppliquer);
+
+module.exports = {
+  avecPMapPourChaqueElement,
+  avecPMapPourChaqueElementSansPromesse,
+};
