05056919b6b5760f579304be1c4e55c8b933431a
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-06-11 09:03:35 +0200

    Affiche correctement la "pastille" du nombre de services
    ... lorsque le nombre est supérieur à 99

--- svelte/lib/ui/Pastille.svelte
+++ svelte/lib/ui/Pastille.svelte
@@ -7,24 +7,27 @@
   {contenu}
 </span>
 
-<style>
+<style lang="scss">
   .pastille {
-    width: 16px;
-    height: 16px;
-    border-radius: 50%;
-    border: 1px solid white;
-    background: var(--fond-gris-pale);
-    color: var(--texte-clair);
     display: flex;
-    align-items: center;
+    width: fit-content;
+    min-width: 0.75rem;
+    height: 1rem;
+    padding: 2px 4px;
     justify-content: center;
-    font-size: 12px;
-    line-height: 12px;
-  }
+    align-items: center;
+    border-radius: 12px;
+    border: 1px solid white;
+    color: var(--texte-clair);
+    background: var(--fond-gris-pale);
+    font-size: 0.75rem;
+    line-height: 1.25rem;
+    font-weight: 400;
 
-  .pastille.active {
-    background: var(--bleu-mise-en-avant);
-    border: 1px solid transparent;
-    color: white;
+    &.active {
+      background: var(--bleu-mise-en-avant);
+      color: white;
+      border-color: transparent;
+    }
   }
 </style>
