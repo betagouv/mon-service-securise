b2221d8d9bb9b419269cd85a3e3b532a411449de
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-06-16 18:17:58 +0200

    Utilise le composant "liste articles" pour la page Conseils Cyber

--- public/scripts/conseilsCyber.mjs
+++ public/scripts/conseilsCyber.mjs
@@ -1,9 +1,10 @@
-import lisDonneesPartagees from '../modules/donneesPartagees.mjs';
-
 $(() => {
-  const sections = lisDonneesPartagees('sections-blog');
-  const articles = lisDonneesPartagees('articles-blog');
-  document.body.dispatchEvent(
-    new CustomEvent('svelte-recharge-blog', { detail: { sections, articles } })
-  );
+  const query = new URLSearchParams(window.location.search);
+  const sectionSelectionnee = query.get('section') ?? '';
+
+  if (sectionSelectionnee)
+    $('lab-anssi-liste-articles').attr(
+      'id-categorie-choisie',
+      sectionSelectionnee
+    );
 });
