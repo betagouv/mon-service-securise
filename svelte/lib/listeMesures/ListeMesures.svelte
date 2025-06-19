ff521d1814b13dde32879ac50e0b9ecd5f8c8208
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-06-19 15:20:55 +0200

    [LISTE MESURES] Mets en forme le tableau de mesures

--- svelte/lib/listeMesures/ListeMesures.svelte
+++ svelte/lib/listeMesures/ListeMesures.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import type { MesureReferentiel } from '../ui/types.d';
+  import CartoucheReferentiel from '../ui/CartoucheReferentiel.svelte';
+  import CartoucheIdentifiantMesure from '../ui/CartoucheIdentifiantMesure.svelte';
+  import CartoucheCategorieMesure from '../ui/CartoucheCategorieMesure.svelte';
 
   let mesuresReferentiel: Record<string, MesureReferentiel> = {};
 
@@ -21,11 +24,70 @@
   <tbody>
     {#each Object.values(mesuresReferentiel) as mesure}
       <tr>
-        <td>{mesure.description}</td>
+        <td>
+          <div>
+            <span>{mesure.description}</span>
+            <div>
+              <CartoucheReferentiel referentiel={mesure.referentiel} />
+              <CartoucheCategorieMesure categorie={mesure.categorie} />
+              <CartoucheIdentifiantMesure
+                identifiant={mesure.identifiantNumerique}
+              />
+            </div>
+          </div>
+        </td>
       </tr>
     {/each}
   </tbody>
 </table>
 
-<style>
+<style lang="scss">
+  :global(#liste-mesures) {
+    text-align: left;
+    width: 100%;
+    max-width: 1200px;
+  }
+
+  table {
+    border-collapse: collapse;
+    margin: 32px 0;
+    width: 100%;
+
+    thead {
+      border: 1px solid #dddddd;
+
+      th {
+        padding: 8px 16px;
+        color: #666666;
+        font-size: 0.875rem;
+        font-weight: bold;
+        line-height: 1.5rem;
+      }
+    }
+
+    tbody {
+      td {
+        padding: 8px 16px;
+        border: 1px solid #dddddd;
+
+        & > div {
+          display: flex;
+          flex-direction: column;
+          gap: 8px;
+
+          span {
+            font-size: 0.875rem;
+            font-weight: bold;
+            line-height: 1.5rem;
+          }
+
+          div {
+            display: flex;
+            flex-direction: row;
+            gap: 8px;
+          }
+        }
+      }
+    }
+  }
 </style>
