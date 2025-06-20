983af13b0baac252d5f3d263aa75779b7beb5791
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-06-20 10:13:12 +0200

    [SOIN] Extrais la ligne du tableau de mesures
    [SOIN] Extrais la ligne du tableau de mesures

---
+++ svelte/lib/listeMesures/LigneMesure.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import CartoucheReferentiel from '../ui/CartoucheReferentiel.svelte';
+  import CartoucheIdentifiantMesure from '../ui/CartoucheIdentifiantMesure.svelte';
+  import CartoucheCategorieMesure from '../ui/CartoucheCategorieMesure.svelte';
+
+  export let mesure;
+</script>
+
+<tr>
+  <td>
+    <div>
+      <span>{mesure.description}</span>
+      <div>
+        <CartoucheReferentiel referentiel={mesure.referentiel} />
+        <CartoucheCategorieMesure categorie={mesure.categorie} />
+        <CartoucheIdentifiantMesure identifiant={mesure.identifiantNumerique} />
+      </div>
+    </div>
+  </td>
+</tr>
+
+<style lang="scss">
+  td {
+    padding: 8px 16px;
+    border: 1px solid #dddddd;
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+
+      span {
+        font-size: 0.875rem;
+        font-weight: bold;
+        line-height: 1.5rem;
+      }
+
+      div {
+        display: flex;
+        flex-direction: row;
+        gap: 8px;
+      }
+    }
+  }
+</style>
