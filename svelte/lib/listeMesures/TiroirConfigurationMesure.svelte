e482333e05345bf7770871d169fb030e632d8a62
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-06-24 12:30:55 +0200

    [SOIN] Extrais le contenu de la première étape dans un composant dédié

--- svelte/lib/listeMesures/TiroirConfigurationMesure.svelte
+++ svelte/lib/listeMesures/TiroirConfigurationMesure.svelte
@@ -8,6 +8,7 @@
   import Lien from '../ui/Lien.svelte';
   import ActionsTiroir from '../ui/tiroirs/ActionsTiroir.svelte';
   import Bouton from '../ui/Bouton.svelte';
+  import PremiereEtape from './etapes/PremiereEtape.svelte';
 
   export const titre: string = 'Configurer la mesure';
   export const sousTitre: string =
@@ -27,34 +28,7 @@
   </div>
   <EtapierTiroir etapeCourante={1} />
   <hr />
-  <span class="explications"
-    >Adaptez le statut et/ou la précision de cette mesure selon vos besoins,
-    puis appliquez-les simultanément aux services de votre choix.</span
-  >
-  <div>
-    <SelectionStatut
-      id="configuration-mesure"
-      bind:statut={statutSelectionne}
-      referentielStatuts={statuts}
-      label="Statut de la mesure"
-      labelChoixVide="Ne pas modifier le statut"
-    />
-    {#if statutSelectionne === ''}
-      <span class="sans-modification-statut"
-        >Le statut de cette mesure ne sera pas modifié</span
-      >
-    {/if}
-  </div>
-  <div>
-    <label>
-      Précision sur la mesure
-      <textarea
-        bind:value={precision}
-        rows="3"
-        placeholder="Apportez des précisions sur la mesure, ses modalités de mise en oeuvre, etc."
-      />
-    </label>
-  </div>
+  <PremiereEtape {statuts} bind:statutSelectionne bind:precision />
 </ContenuTiroir>
 <ActionsTiroir>
   <Bouton
@@ -74,44 +48,5 @@
     width: 100%;
     border-top: none;
     border-bottom: 1px solid #dddddd;
-  }
-
-  .explications {
-    color: #3a3a3a;
-    font-size: 0.875rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 1.5rem;
-  }
-
-  :global(label[for='statut-configuration-mesure']) {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    margin-bottom: 8px;
-  }
-
-  .sans-modification-statut {
-    color: #3a3a3a;
-    font-size: 0.875rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 1.5rem;
-  }
-
-  textarea {
-    width: 100%;
-    border: none;
-    border-radius: 4px 4px 0 0;
-    border-bottom: 2px solid #3a3a3a;
-    background: #eee;
-    color: #3a3a3a;
-    font-family: Marianne;
-    font-size: 1rem;
-    font-weight: 400;
-    line-height: 1.5rem;
-    padding: 8px 16px;
-    box-sizing: border-box;
-    margin-top: 8px;
   }
 </style>
