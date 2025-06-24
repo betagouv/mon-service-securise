e133af3e7981a9c5744090d089d8c9e902592bba
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-06-24 09:37:57 +0200

    [REMANIEMENT] Extrais un composant "Description complète de mesure"
    ... permettant de le réutiliser dans le tiroir. Ce composant contient le nom, les badges et la description longue d'une mesure.

---
+++ svelte/lib/listeMesures/DescriptionCompleteMesure.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import CartoucheIdentifiantMesure from '../ui/CartoucheIdentifiantMesure.svelte';
+  import CartoucheCategorieMesure from '../ui/CartoucheCategorieMesure.svelte';
+  import CartoucheReferentiel from '../ui/CartoucheReferentiel.svelte';
+  import type { MesureReferentiel } from '../ui/types';
+  import DescriptionLongueMesure from '../ui/DescriptionLongueMesure.svelte';
+
+  export let mesure: MesureReferentiel;
+</script>
+
+<p class="description">{mesure.description}</p>
+<div class="cartouches">
+  <CartoucheReferentiel referentiel={mesure.referentiel} />
+  <CartoucheCategorieMesure categorie={mesure.categorie} />
+  <CartoucheIdentifiantMesure identifiant={mesure.identifiantNumerique} />
+</div>
+<DescriptionLongueMesure
+  description={mesure.descriptionLongue}
+  repliee={true}
+/>
+
+<style lang="scss">
+  .description {
+    font-size: 1rem;
+    line-height: 1.5rem;
+    color: #3a3a3a;
+    font-weight: bold;
+    margin-bottom: 8px;
+  }
+
+  .cartouches {
+    display: flex;
+    gap: 8px;
+    margin-bottom: 30px;
+  }
+</style>
