035c73208eb0210d4a220807f1bffe84e9879358
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-06-25 11:40:19 +0200

    [SOIN] Utilise le composant "Tableau" dans la seconde étape

--- svelte/lib/listeMesures/kit/tiroir/etapes/SecondeEtape.svelte
+++ svelte/lib/listeMesures/kit/tiroir/etapes/SecondeEtape.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import BarreDeRecherche from '../../../../ui/BarreDeRecherche.svelte';
-  import ListeDeroulanteRiche from '../../../../ui/ListeDeroulanteRiche.svelte';
   import type {
     MesureReferentiel,
     ReferentielStatut,
@@ -9,100 +7,64 @@
   import TagStatutMesure from '../../../../ui/TagStatutMesure.svelte';
   import { servicesAvecMesuresAssociees } from '../../../stores/servicesAvecMesuresAssociees.store';
   import { mesuresAvecServicesAssociesStore } from '../../../stores/mesuresAvecServicesAssocies.store';
-  import AucunResultat from '../../AucunResultat.svelte';
+  import Tableau from '../../../../ui/Tableau.svelte';
 
   export let statuts: ReferentielStatut;
   export let mesure: MesureReferentiel;
-  let recherche: string = '';
-  let filtrageStatut: Record<'statut', string[]> = { statut: [] };
-
-  const effaceRechercheEtFiltres = () => {
-    recherche = '';
-    filtrageStatut = { statut: [] };
-  };
 
   $: servicesAssocies =
     mesure &&
-    $servicesAvecMesuresAssociees.filter((s) => {
-      return $mesuresAvecServicesAssociesStore[mesure.id].includes(s?.id);
-    });
+    $servicesAvecMesuresAssociees
+      .filter((s) => {
+        return $mesuresAvecServicesAssociesStore[mesure.id].includes(s?.id);
+      })
+      .map(({ mesuresAssociees, ...autresDonnees }) => ({
+        ...mesuresAssociees[mesure.id],
+        ...autresDonnees,
+      }));
 
-  $: servicesFiltres = servicesAssocies
-    .filter((s) =>
-      recherche
-        ? s.nomService.toLowerCase().includes(recherche.toLowerCase()) ||
-          s.organisationResponsable
-            .toLowerCase()
-            .includes(recherche.toLowerCase())
-        : true
-    )
-    .filter((s) =>
-      filtrageStatut.statut.length > 0
-        ? filtrageStatut.statut.includes(
-            s.mesuresAssociees[mesure.id].statut || 'aDefinir'
-          )
-        : true
-    );
+  const optionsFiltrage = {
+    categories: [{ id: 'statut', libelle: 'Statuts' }],
+    items: [
+      { libelle: 'À définir', valeur: undefined, idCategorie: 'statut' },
+      ...Object.entries(statuts).map(([id, statut]) => ({
+        libelle: statut,
+        valeur: id,
+        idCategorie: 'statut',
+      })),
+    ],
+  };
 </script>
 
 <span>
   Sélectionnez les services concernés par ces modifications. Les données
   existantes seront remplacées.
 </span>
-<div class="filtres">
-  <BarreDeRecherche bind:recherche />
-  <ListeDeroulanteRiche
-    bind:valeursSelectionnees={filtrageStatut}
-    id="filtres"
-    libelle="Filtrer"
-    options={{
-      categories: [{ id: 'statut', libelle: 'Statuts' }],
-      items: [
-        { libelle: 'À définir', valeur: 'aDefinir', idCategorie: 'statut' },
-        ...Object.entries(statuts).map(([id, statut]) => ({
-          libelle: statut,
-          valeur: id,
-          idCategorie: 'statut',
-        })),
-      ],
-    }}
-  />
-</div>
-<table>
-  <thead>
-    <tr>
-      <th>Nom du service</th>
-      <th>Statut actuel</th>
-      <th>Précision actuelle</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each servicesFiltres as service}
-      <tr>
-        <td>
-          <div class="intitule-service">
-            <span class="nom">{decode(service.nomService)}</span>
-            <span class="organisation">{service.organisationResponsable}</span>
-          </div>
-        </td>
-        <td
-          ><TagStatutMesure
-            referentielStatuts={statuts}
-            statut={service.mesuresAssociees[mesure.id].statut}
-          /></td
-        >
-        <td>{decode(service.mesuresAssociees[mesure.id].modalites) || ''}</td>
-      </tr>
-    {/each}
-    {#if servicesFiltres.length === 0}
-      <tr>
-        <td colspan="3">
-          <AucunResultat on:click={effaceRechercheEtFiltres} />
-        </td>
-      </tr>
+<Tableau
+  colonnes={[
+    { cle: 'nom', libelle: 'Nom du service' },
+    { cle: 'statut', libelle: 'Statut actuel' },
+    { cle: 'modalites', libelle: 'Précision actuelle' },
+  ]}
+  donnees={servicesAssocies}
+  configurationRecherche={{
+    champsRecherche: ['nomService', 'organisationResponsable'],
+  }}
+  configurationFiltrage={{ options: optionsFiltrage }}
+>
+  <svelte:fragment slot="cellule" let:donnee let:colonne>
+    {#if colonne.cle === 'nom'}
+      <div class="intitule-service">
+        <span class="nom">{decode(donnee.nomService)}</span>
+        <span class="organisation">{donnee.organisationResponsable}</span>
+      </div>
+    {:else if colonne.cle === 'statut'}
+      <TagStatutMesure referentielStatuts={statuts} statut={donnee.statut} />
+    {:else if colonne.cle === 'modalites'}
+      {decode(donnee.modalites) || ''}
     {/if}
-  </tbody>
-</table>
+  </svelte:fragment>
+</Tableau>
 
 <style lang="scss">
   span {
@@ -110,46 +72,14 @@
     font-size: 0.875rem;
     line-height: 1.5rem;
   }
-  .filtres {
+
+  .intitule-service {
     display: flex;
-    flex-direction: row;
-    gap: 12px;
-  }
-  table {
-    border-collapse: collapse;
-    width: 100%;
-    color: #3a3a3a;
-    font-size: 0.875rem;
-    line-height: 1.5rem;
+    flex-direction: column;
+    gap: 4px;
 
-    thead {
-      border: 1px solid #dddddd;
-
-      th {
-        white-space: nowrap;
-        padding: 8px 16px;
-        color: #666666;
-        font-weight: bold;
-      }
-    }
-
-    tbody {
-      border: 1px solid #dddddd;
-
-      td {
-        padding: 8px 16px;
-        border-top: 1px solid #dddddd;
-      }
-    }
-
-    .intitule-service {
-      display: flex;
-      flex-direction: column;
-      gap: 4px;
-
-      .nom {
-        font-weight: bold;
-      }
+    .nom {
+      font-weight: bold;
     }
   }
 </style>
