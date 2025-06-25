cd617c4c13817bc41ffb392ad89bfe4709c4f1c9
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-06-25 16:53:27 +0200

    Réalise la modification via un appel API

---
+++ svelte/lib/listeMesures/listeMesures.api.ts
@@ -0,0 +1,19 @@
+import type { StatutMesure } from '../modeles/modeleMesure';
+
+export const enregistreModificationMesureSurServicesMultiple = async ({
+  idMesure,
+  idsServices,
+  statut,
+  modalites,
+}: {
+  idMesure: string;
+  idsServices: string[];
+  statut: StatutMesure | null;
+  modalites: string | null;
+}) => {
+  await axios.put(`/api/services/mesures/${idMesure}`, {
+    idsServices,
+    statut,
+    modalites,
+  });
+};
