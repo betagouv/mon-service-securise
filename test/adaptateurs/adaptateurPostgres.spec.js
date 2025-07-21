b78f608b0c0d4d1769f36b529ee8455fabe5d445
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-07-21 21:31:47 +0200

    Teste la jointure avec les contributeurs
    ...lors de la lecture d’un service.

--- test/adaptateurs/adaptateurPostgres.spec.js
+++ test/adaptateurs/adaptateurPostgres.spec.js
@@ -5,6 +5,7 @@ const {
   nouvelAdaptateur,
 } = require('../../src/adaptateurs/adaptateurPostgres');
 const { genereUUID } = require('../../src/adaptateurs/adaptateurUUID');
+const Autorisation = require('../../src/modeles/autorisations/autorisation');
 
 function patchKnexFirst(knexInstance) {
   const prototypeQueryBuilder = Object.getPrototypeOf(
@@ -50,15 +51,45 @@ describe("L'adaptateur persistance Postgres", () => {
     return idService;
   }
 
+  async function insereUtilisateur() {
+    const idUtilisateur = genereUUID();
+    await persistance.ajouteUtilisateur(idUtilisateur, {}, 'email');
+    return idUtilisateur;
+  }
+
+  async function insereAutorisation(idUtilisateur, idService) {
+    const idAutorisation = genereUUID();
+    const autorisation = Autorisation.NouvelleAutorisationProprietaire({
+      idUtilisateur,
+      idService,
+    });
+    await persistance.ajouteAutorisation(
+      idAutorisation,
+      autorisation.donneesAPersister()
+    );
+  }
+
   it("sait lire les suggestions d'action d'un service", async () => {
-    const idService = await insereService(persistance);
-    await knex('suggestions_actions').insert({
-      id_service: idService,
+    const idService = await insereService();
+    await persistance.ajouteSuggestionAction({
+      idService,
       nature: 'une nature',
     });
 
     const services = await persistance.servicesComplets({ idService });
 
     expect(services[0].suggestions).to.eql(['une nature']);
+  });
+
+  it("sait lire les contributeurs d'un service", async () => {
+    const idService = await insereService();
+    const idUtilisateur = await insereUtilisateur();
+    await insereAutorisation(idUtilisateur, idService);
+
+    const services = await persistance.servicesComplets({ idService });
+
+    const proprietaire = services[0].utilisateurs[0];
+    expect(proprietaire.id).to.be(idUtilisateur);
+    expect(proprietaire.email_hash).to.be('email');
   });
 });
