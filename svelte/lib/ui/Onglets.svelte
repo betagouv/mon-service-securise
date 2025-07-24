29b8fc5e2a0fe38f6ec8d1c44d862b70cb6e41b6
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-07-24 17:16:54 +0200

    Ajoute un composant "Onglets"
    ... permettant de regrouper plusieurs onglets en un seul.

---
+++ svelte/lib/ui/Onglets.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Onglet from './Onglet.svelte';
+
+  type ConfigurationOnglet = {
+    id: string;
+    label: string;
+    badge?: number;
+  };
+
+  export let onglets: ConfigurationOnglet[];
+  export let ongletActif: string;
+</script>
+
+<div class="conteneur-onglets">
+  {#each onglets as onglet (onglet.id)}
+    {@const { id, label, badge } = onglet}
+    <Onglet bind:ongletActif cetOnglet={id} labelOnglet={label} {badge} />
+  {/each}
+</div>
+
+<style lang="scss">
+  .conteneur-onglets {
+    display: flex;
+    gap: 8px;
+    margin-bottom: -1px;
+  }
+</style>
