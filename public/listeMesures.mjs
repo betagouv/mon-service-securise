0bde3c97779e208acad76a2c160336356fe82f86
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-07-28 17:51:08 +0200

    [SOIN] Passe la liste des catégories depuis le référentiel
    … pour éviter la duplication.

--- public/listeMesures.mjs
+++ public/listeMesures.mjs
@@ -2,13 +2,23 @@ import lisDonneesPartagees from './modules/donneesPartagees.mjs';
 
 $(() => {
   const statuts = lisDonneesPartagees('referentiel-statuts-mesures');
+  const categories = lisDonneesPartagees('referentiel-categories-mesures');
   const typesService = lisDonneesPartagees('referentiel-types-service');
   const afficheModelesMesureSpecifique = lisDonneesPartagees(
     'affiche-modeles-mesure-specifique'
   );
+
   document.body.dispatchEvent(
     new CustomEvent('svelte-recharge-liste-mesures', {
-      detail: { statuts, typesService, afficheModelesMesureSpecifique },
+      detail: {
+        statuts,
+        categories: Object.entries(categories).map(([id, label]) => ({
+          id,
+          label,
+        })),
+        typesService,
+        afficheModelesMesureSpecifique,
+      },
     })
   );
 });
