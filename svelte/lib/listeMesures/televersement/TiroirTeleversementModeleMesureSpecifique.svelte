44c9b52aba6beb67a2b5da27fdf547c93da5d58e
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-04 17:17:39 +0200

    Propose le template de mesures au téléchargement

--- svelte/lib/listeMesures/televersement/TiroirTeleversementModeleMesureSpecifique.svelte
+++ svelte/lib/listeMesures/televersement/TiroirTeleversementModeleMesureSpecifique.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
   import ContenuTiroir from '../../ui/tiroirs/ContenuTiroir.svelte';
   import ActionsTiroir from '../../ui/tiroirs/ActionsTiroir.svelte';
+  import KitDeTeleversement from '../../ui/televersement/KitDeTeleversement.svelte';
+  import { FormatAccepte } from '../../ui/televersement/KitDeTeleversement.types';
 
   export const titre: string = 'Téléverser des mesures';
-  export const sousTitre: string = '';
-  export const taille = 'large';
+  export const sousTitre: string =
+    'Importez votre liste de mesures, associez-les aux services de votre choix, puis ajustez leur statut ou leur précision simultanément.';
 </script>
 
 <ContenuTiroir>
-  <h4>Téléchargez et …</h4>
+  <KitDeTeleversement
+    etape1="Téléchargez et complétez le template de vos mesures"
+    template={{
+      href: '/statique/assets/fichiers/Template mesures - MSS.xlsx',
+      nom: 'Template mesures - MSS.xlsx',
+      titre: 'Template de mesures à télécharger',
+      sousTitre: 'XLSX - 56 kB',
+    }}
+    lesLimitations={['Format supporté : XLSX.']}
+    apiPostDuTeleversement="/api/televersement/modeles-de-mesure"
+    formatAccepte={FormatAccepte.Excel}
+    on:televersementChange={(e) => {
+      console.log(e);
+    }}
+  />
 </ContenuTiroir>
 <ActionsTiroir>
   <h4>Sauvegarder</h4>
