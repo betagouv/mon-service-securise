0332d4526a5fa7a66c736158e795bac23f15e750
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-08-05 17:23:38 +0200

    Ajoute l'étape de confirmation de modification de modèle de mesure spécifique

---
+++ svelte/lib/listeMesures/mesureSpecifique/modification/ConfirmationModificationModeleMesureSpecifique.svelte
@@ -0,0 +1,151 @@
+<script lang="ts">
+  import Toast from '../../../ui/Toast.svelte';
+  import type {
+    ModeleMesureSpecifique,
+    ReferentielTypesService,
+  } from '../../../ui/types.d';
+  import { servicesAvecMesuresAssociees } from '../../servicesAssocies/servicesAvecMesuresAssociees.store';
+  import Tableau from '../../../ui/Tableau.svelte';
+  import { decode } from 'html-entities';
+  import { referentielNiveauxSecurite } from '../../../ui/referentielNiveauxSecurite';
+
+  export let modeleMesure: ModeleMesureSpecifique;
+  export let referentielTypesService: ReferentielTypesService;
+
+  $: servicesAssocies =
+    modeleMesure &&
+    $servicesAvecMesuresAssociees.filter((s) =>
+      modeleMesure.idsServicesAssocies.includes(s.id)
+    );
+</script>
+
+<div class="entete">
+  <h3>Modifer les informations de la mesure</h3>
+  <div class="alerte-modification-indice-cyber">
+    <Toast
+      avecOmbre={false}
+      titre="Cette action est irréversible"
+      avecAnimation={false}
+      niveau="info"
+      contenu="Vous vous apprêtez à modifier cette mesure sur <b>{servicesAssocies.length}
+      {servicesAssocies.length === 1 ? 'service' : 'services'}</b>."
+    />
+  </div>
+  <hr />
+  <h5>
+    {servicesAssocies.length}
+    {servicesAssocies.length === 1 ? 'service concerné' : 'services concernés'} par
+    cette modification
+  </h5>
+</div>
+<Tableau
+  colonnes={[
+    { cle: 'nom', libelle: 'Nom du service' },
+    { cle: 'typeService', libelle: 'Type de service' },
+    { cle: 'niveauSecurite', libelle: 'Besoins de sécurité' },
+  ]}
+  donnees={servicesAssocies}
+>
+  <svelte:fragment slot="cellule" let:donnee let:colonne>
+    {#if colonne.cle === 'nom'}
+      <div class="contenu-nom-service">
+        <div class="intitule-service">
+          <span class="nom">{decode(donnee.nomService)}</span>
+          <span class="organisation">{donnee.organisationResponsable}</span>
+        </div>
+      </div>
+    {:else if colonne.cle === 'typeService'}
+      <div class="contenu-type-service">
+        <div>
+          <span class="type-service"
+            >{donnee.typeService
+              .map((t) => referentielTypesService[t].description)
+              .join(', ')}</span
+          >
+        </div>
+      </div>
+    {:else if colonne.cle === 'niveauSecurite'}
+      <div class="contenu-besoin-securite">
+        <div>
+          <span class="besoin-securite">
+            {referentielNiveauxSecurite[donnee.niveauSecurite]}
+          </span>
+        </div>
+      </div>
+    {/if}
+  </svelte:fragment>
+</Tableau>
+
+<style lang="scss">
+  h3 {
+    margin: 2px 0 0;
+    font-size: 1.25rem;
+    line-height: 1.75rem;
+  }
+
+  div {
+    color: #3a3a3a;
+    font-size: 0.875rem;
+    line-height: 1.5rem;
+  }
+
+  .contenu-type-service {
+    width: 158px;
+  }
+
+  .contenu-besoin-securite {
+    width: 182px;
+  }
+
+  .contenu-nom-service {
+    display: flex;
+    flex-direction: row;
+    gap: 8px;
+    align-items: center;
+  }
+
+  .intitule-service {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    max-width: 318px;
+
+    .nom {
+      font-weight: bold;
+    }
+
+    .nom,
+    .organisation {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+
+  .entete {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    margin-bottom: -6px;
+
+    .alerte-modification-indice-cyber {
+      max-width: 590px;
+      margin-top: 16px;
+    }
+
+    hr {
+      width: 100%;
+      border-top: none;
+      border-bottom: 1px solid #dddddd;
+      margin: 16px 0;
+    }
+
+    h5 {
+      margin: 0;
+      font-size: 1.375rem;
+      font-weight: bold;
+      line-height: 1.75rem;
+      color: #161616;
+    }
+  }
+</style>
