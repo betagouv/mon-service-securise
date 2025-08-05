8add868c5b79fe34f20e7654c346c99776b0cfee
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-05 17:16:57 +0200

    Ajoute 2 boutons d'actions au tiroir

--- svelte/lib/listeMesures/televersement/TiroirTeleversementModeleMesureSpecifique.svelte
+++ svelte/lib/listeMesures/televersement/TiroirTeleversementModeleMesureSpecifique.svelte
@@ -2,11 +2,23 @@
   import ContenuTiroir from '../../ui/tiroirs/ContenuTiroir.svelte';
   import ActionsTiroir from '../../ui/tiroirs/ActionsTiroir.svelte';
   import KitDeTeleversement from '../../ui/televersement/KitDeTeleversement.svelte';
-  import { FormatAccepte } from '../../ui/televersement/KitDeTeleversement.types';
+  import {
+    type EtatTeleversement,
+    FormatAccepte,
+  } from '../../ui/televersement/KitDeTeleversement.types';
+  import { tiroirStore } from '../../ui/stores/tiroir.store';
 
   export const titre: string = 'Téléverser des mesures';
   export const sousTitre: string =
     'Importez votre liste de mesures, associez-les aux services de votre choix, puis ajustez leur statut ou leur précision simultanément.';
+
+  let etatTeleversement: EtatTeleversement = 'EnAttente';
+
+  const afficheRapportDuTeleversement = async () => {
+    if (etatTeleversement !== 'Valide') return;
+
+    console.log('📋 Vers le rapport de téléversement… ');
+  };
 </script>
 
 <ContenuTiroir>
@@ -22,10 +34,31 @@
     apiPostDuTeleversement="/api/televersement/modelesMesureSpecifique"
     formatAccepte={FormatAccepte.Excel}
     on:televersementChange={(e) => {
-      console.log(e);
+      etatTeleversement = e.detail;
     }}
   />
 </ContenuTiroir>
 <ActionsTiroir>
-  <h4>Sauvegarder</h4>
+  <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+  <lab-anssi-bouton
+    titre="Annuler"
+    variante="tertiaire-sans-bordure"
+    taille="md"
+    positionIcone="sans"
+    actif={etatTeleversement !== 'EnCoursEnvoi'}
+    on:click={() => {
+      if (etatTeleversement !== 'EnCoursEnvoi') tiroirStore.ferme();
+    }}
+  />
+
+  <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+  <lab-anssi-bouton
+    titre="Valider le fichier"
+    variante="primaire"
+    taille="md"
+    icone="check-line"
+    positionIcone="gauche"
+    actif={etatTeleversement === 'Valide'}
+    on:click={afficheRapportDuTeleversement}
+  />
 </ActionsTiroir>
