7bfb529ba6a6d8366155285deefe054900552af9
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-07 10:37:16 +0200

    [REMANIEMENT] Rajoute les erreurs dans `<RapportTeleversementGenerique>`

--- svelte/lib/tableauDeBord/televersementServices/RapportTeleversementServices.svelte
+++ svelte/lib/tableauDeBord/televersementServices/RapportTeleversementServices.svelte
@@ -3,23 +3,31 @@
   import { onMount } from 'svelte';
   import { recupereRapportDetaille } from '../../rapportTeleversement/rapportTeleversement.api';
   import { singulierPluriel } from '../../ui/string';
+  import type { ResumeRapportTeleversement } from '../../rapportTeleversement/rapportTeleversementGenerique.types';
 
-  let resume: { elementsValide: { label: string } };
+  let resume: ResumeRapportTeleversement;
 
   onMount(async () => {
     const rapport = await recupereRapportDetaille();
     if (!rapport) return;
 
-    const nbOk = rapport?.services.filter((s) => s.erreurs.length === 0).length;
+    const nbOK = rapport.services.filter((s) => s.erreurs.length === 0).length;
+    const erreurs = rapport.services.filter((s) => s.erreurs.length > 0).length;
+
+    const labelOK = singulierPluriel(
+      '1 service valide',
+      `${nbOK} services valides`,
+      nbOK
+    );
+    const labelErreurs = singulierPluriel(
+      '1 service invalide',
+      `${erreurs} services invalides`,
+      erreurs
+    );
 
     resume = {
-      elementsValide: {
-        label: singulierPluriel(
-          '1 service valide',
-          `${nbOk} services valides`,
-          nbOk
-        ),
-      },
+      elementsValide: { label: labelOK },
+      elementsErreur: erreurs ? { label: labelErreurs } : null,
     };
   });
 </script>
