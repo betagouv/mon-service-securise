e1f7058036cab1dcf9c42c4fbbcf2eb74b6f441b
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-08 11:48:19 +0200

    Implémente une lecture naïve du Excel téléversé
    Les contrôles sur le nombre de mesures et les headers bien à leur place arrivent après.

--- src/adaptateurs/adaptateurTeleversementModelesMesureSpecifique.xls.js
+++ src/adaptateurs/adaptateurTeleversementModelesMesureSpecifique.xls.js
@@ -1,11 +1,27 @@
-async function extraisDonneesTeleversees() {
-  return [
-    {
-      description: '[EN DUR] Description',
-      descriptionLongue: '[EN DUR] Description longue',
-      categorie: 'gouvernance',
-    },
-  ];
+const { encode } = require('html-entities');
+const { LecteurExcel } = require('./excel/LecteurExcel');
+const { ErreurFichierXlsInvalide } = require('../erreurs');
+
+const ENTETE_INTITULE = 'Intitulé de la mesure *';
+const ENTETE_DESCRIPTION = 'Description de la mesure';
+const ENTETE_CATEGORIE = 'Catégorie *';
+
+async function extraisDonneesTeleversees(buffer) {
+  const lecteur = new LecteurExcel(buffer);
+  const donneesBrutes = lecteur.donneesDeFeuille('Template mesures', 6);
+
+  const lignesCompletes = lecteur.lesLignesSontCompletes(donneesBrutes, [
+    ENTETE_INTITULE,
+    ENTETE_CATEGORIE,
+  ]);
+
+  if (!lignesCompletes) throw new ErreurFichierXlsInvalide();
+
+  return donneesBrutes.map((ligne) => ({
+    description: encode(ligne[ENTETE_INTITULE]), // Chez MSS, l'intitulé va bien dans "description".
+    descriptionLongue: encode(ligne[ENTETE_DESCRIPTION]),
+    categorie: ligne[ENTETE_CATEGORIE],
+  }));
 }
 
 module.exports = { extraisDonneesTeleversees };
