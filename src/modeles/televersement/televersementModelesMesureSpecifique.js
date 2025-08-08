838e9bc8f4f9dceb18ae188e744405e5178e75d8
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-08 17:03:44 +0200

    Ajoute `rapportDetaille()` au téléversement de modèles de mesure spécifique
    Il manque encore des contrôles, mais on va l'enrichir plus tard.

--- src/modeles/televersement/televersementModelesMesureSpecifique.js
+++ src/modeles/televersement/televersementModelesMesureSpecifique.js
@@ -2,6 +2,28 @@ class TeleversementModelesMesureSpecifique {
   constructor(donnees) {
     this.modelesTeleverses = donnees;
   }
+
+  rapportDetaille() {
+    return {
+      modelesTeleverses: this.modelesTeleverses.map((m) => ({
+        modele: m,
+        erreurs: this.#controleUnModele(m),
+      })),
+    };
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  #controleUnModele(modele) {
+    const erreurs = [];
+
+    if (!modele.description || modele.description.trim() === '')
+      erreurs.push('INTITULE_MANQUANT');
+
+    if (!modele.categorie || modele.categorie.trim() === '')
+      erreurs.push('CATEGORIE_MANQUANTE');
+
+    return erreurs;
+  }
 }
 
 module.exports = TeleversementModelesMesureSpecifique;
