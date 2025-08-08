674ca96d22371c0a963a7dd7ffeb06f868860bda
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-08 11:48:19 +0200

    Implémente une lecture naïve du Excel téléversé
    Les contrôles sur le nombre de mesures et les headers bien à leur place arrivent après.

--- src/adaptateurs/adaptateurTeleversementModelesMesureSpecifique.xls.js
+++ src/adaptateurs/adaptateurTeleversementModelesMesureSpecifique.xls.js
@@ -1,11 +1,18 @@
-async function extraisDonneesTeleversees() {
-  return [
-    {
-      description: '[EN DUR] Description',
-      descriptionLongue: '[EN DUR] Description longue',
-      categorie: 'gouvernance',
-    },
-  ];
+const { encode } = require('html-entities');
+const { LecteurExcel } = require('./excel/LecteurExcel');
+
+const ENTETE_DESCRIPTION = 'Intitulé de la mesure *';
+const ENTETE_MODALITES = 'Description de la mesure';
+const ENTETE_CATEGORIE = 'Catégorie *';
+
+async function extraisDonneesTeleversees(buffer) {
+  const lecteur = new LecteurExcel(buffer);
+  const donneesBrutes = lecteur.donneesDeFeuille('Template mesures', 6);
+  return donneesBrutes.map((ligne) => ({
+    description: encode(ligne[ENTETE_DESCRIPTION]),
+    modalites: encode(ligne[ENTETE_MODALITES]),
+    categorie: ligne[ENTETE_CATEGORIE],
+  }));
 }
 
 module.exports = { extraisDonneesTeleversees };
