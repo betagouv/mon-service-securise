f60cc9683685250a773138ad663333a987fdee2a
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-08 17:18:39 +0200

    Protège l'adaptateur contre les null
    … pour le rendre plus robuste. On ne veut pas crasher ici à cause d'un null pointer.

--- src/adaptateurs/adaptateurTeleversementModelesMesureSpecifique.xls.js
+++ src/adaptateurs/adaptateurTeleversementModelesMesureSpecifique.xls.js
@@ -24,9 +24,9 @@ async function extraisDonneesTeleversees(buffer) {
   if (!lignesCompletes) throw new ErreurFichierXlsInvalide();
 
   return donneesBrutes.map((ligne) => ({
-    description: encode(ligne[ENTETE_INTITULE].trim()), // Chez MSS, l'intitulé va bien dans "description".
-    descriptionLongue: encode(ligne[ENTETE_DESCRIPTION].trim()),
-    categorie: ligne[ENTETE_CATEGORIE].trim(),
+    description: encode(ligne[ENTETE_INTITULE]?.trim()), // Chez MSS, l'intitulé va bien dans "description".
+    descriptionLongue: encode(ligne[ENTETE_DESCRIPTION]?.trim()),
+    categorie: ligne[ENTETE_CATEGORIE]?.trim(),
   }));
 }
 
