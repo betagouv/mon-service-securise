519d87b659fe259c67d019f6ef7d097ce0038d24
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-08-11 17:18:49 +0200

    Affiche la deuxième étape d'association de modèles au service

--- svelte/lib/tableauDesMesures/mesuresSpecifiques/TiroirAssociationModelesMesureSpecifiqueAuService.svelte
+++ svelte/lib/tableauDesMesures/mesuresSpecifiques/TiroirAssociationModelesMesureSpecifiqueAuService.svelte
@@ -7,6 +7,7 @@
   import CartoucheCategorieMesure from '../../ui/CartoucheCategorieMesure.svelte';
   import type { IdCategorie } from '../tableauDesMesures.d';
   import { tiroirStore } from '../../ui/stores/tiroir.store';
+  import SeparateurHorizontal from '../../ui/SeparateurHorizontal.svelte';
 
   export const titre: string = 'Ajouter des mesures depuis ma liste';
   export const sousTitre: string =
@@ -15,6 +16,7 @@
 
   export let categories: Record<IdCategorie, string>;
 
+  let etapeCourante: 1 | 2 = 1;
   let idsModelesSelectionnes: string[] = [];
 
   const itemsFiltrageCategories = Object.entries(categories).map(
@@ -27,52 +29,91 @@
 </script>
 
 <ContenuTiroir>
-  <div class="largeur-contrainte">
-    <Toast
-      avecOmbre={false}
-      avecAnimation={false}
-      titre="Une mesure ajoutée reçoit automatiquement le statut «&nbsp;À lancer&nbsp;»."
-      niveau="info"
-      contenu="Ce statut est obligatoire : il n’est pas possible d’ajouter une mesure sans statut."
-    />
-  </div>
-  <Tableau
-    colonnes={[
-      { cle: 'description', libelle: 'Intitulé de la mesure' },
-      { cle: 'descriptionLongue', libelle: 'Description' },
-      { cle: 'categorie', libelle: 'Catégorie' },
-    ]}
-    donnees={$modelesMesureSpecifique}
-    configurationRecherche={{
-      champsRecherche: ['description', 'descriptionLongue'],
-    }}
-    configurationFiltrage={{
-      options: {
-        categories: [{ id: 'categorie', libelle: 'Catégories' }],
-        items: itemsFiltrageCategories,
-      },
-    }}
-    configurationSelection={{
-      texteIndicatif: {
-        vide: 'Aucune mesure sélectionnée',
-        unique: 'mesure sélectionnée',
-        multiple: 'mesures sélectionnées',
-      },
-      champSelection: 'id',
-    }}
-    bind:selection={idsModelesSelectionnes}
-  >
-    <svelte:fragment slot="cellule" let:donnee let:colonne>
-      {@const { description, descriptionLongue, categorie } = donnee}
-      {#if colonne.cle === 'description'}
-        <b>{description}</b>
-      {:else if colonne.cle === 'descriptionLongue'}
-        {descriptionLongue}
-      {:else if colonne.cle === 'categorie'}
-        <CartoucheCategorieMesure {categorie} />
-      {/if}
-    </svelte:fragment>
-  </Tableau>
+  {#if etapeCourante === 1}
+    <div class="largeur-contrainte">
+      <Toast
+        avecOmbre={false}
+        avecAnimation={false}
+        titre="Une mesure ajoutée reçoit automatiquement le statut «&nbsp;À lancer&nbsp;»."
+        niveau="info"
+        contenu="Ce statut est obligatoire : il n’est pas possible d’ajouter une mesure sans statut."
+      />
+    </div>
+    <Tableau
+      colonnes={[
+        { cle: 'description', libelle: 'Intitulé de la mesure' },
+        { cle: 'descriptionLongue', libelle: 'Description' },
+        { cle: 'categorie', libelle: 'Catégorie' },
+      ]}
+      donnees={$modelesMesureSpecifique}
+      configurationRecherche={{
+        champsRecherche: ['description', 'descriptionLongue'],
+      }}
+      configurationFiltrage={{
+        options: {
+          categories: [{ id: 'categorie', libelle: 'Catégories' }],
+          items: itemsFiltrageCategories,
+        },
+      }}
+      configurationSelection={{
+        texteIndicatif: {
+          vide: 'Aucune mesure sélectionnée',
+          unique: 'mesure sélectionnée',
+          multiple: 'mesures sélectionnées',
+        },
+        champSelection: 'id',
+      }}
+      bind:selection={idsModelesSelectionnes}
+    >
+      <svelte:fragment slot="cellule" let:donnee let:colonne>
+        {@const { description, descriptionLongue, categorie } = donnee}
+        {#if colonne.cle === 'description'}
+          <b>{description}</b>
+        {:else if colonne.cle === 'descriptionLongue'}
+          {descriptionLongue}
+        {:else if colonne.cle === 'categorie'}
+          <CartoucheCategorieMesure {categorie} />
+        {/if}
+      </svelte:fragment>
+    </Tableau>
+  {:else}
+    {@const pluralise = idsModelesSelectionnes.length > 1 ? 's' : ''}
+    <div class="largeur-contrainte">
+      <Toast
+        avecOmbre={false}
+        avecAnimation={false}
+        titre="Cette action peut avoir un impact significatif."
+        niveau="info"
+        contenu="Vous vous apprêtez à associer <b>{idsModelesSelectionnes.length} mesure{pluralise}</b> à ce service. Cela aura un impact sur son indice cyber personnalisé."
+      />
+    </div>
+    <SeparateurHorizontal />
+    <h4>
+      {idsModelesSelectionnes.length} mesure{pluralise} concernée{pluralise} par
+      cette modification
+    </h4>
+    <Tableau
+      colonnes={[
+        { cle: 'description', libelle: 'Intitulé de la mesure' },
+        { cle: 'descriptionLongue', libelle: 'Description' },
+        { cle: 'categorie', libelle: 'Catégorie' },
+      ]}
+      donnees={$modelesMesureSpecifique.filter((m) =>
+        idsModelesSelectionnes.includes(m.id)
+      )}
+    >
+      <svelte:fragment slot="cellule" let:donnee let:colonne>
+        {@const { description, descriptionLongue, categorie } = donnee}
+        {#if colonne.cle === 'description'}
+          <b>{description}</b>
+        {:else if colonne.cle === 'descriptionLongue'}
+          {descriptionLongue}
+        {:else if colonne.cle === 'categorie'}
+          <CartoucheCategorieMesure {categorie} />
+        {/if}
+      </svelte:fragment>
+    </Tableau>
+  {/if}
 </ContenuTiroir>
 <ActionsTiroir>
   <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
@@ -82,6 +123,20 @@
     titre="Annuler"
     on:click={() => tiroirStore.ferme()}
   />
+  {#if etapeCourante === 1}
+    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+    <lab-anssi-bouton
+      variante="primaire"
+      taille="md"
+      titre={`Ajouter ${
+        idsModelesSelectionnes.length > 1 ? 'ces mesures' : 'cette mesure'
+      } à mon service`}
+      icone="add-line"
+      position-icone="gauche"
+      on:click={() => (etapeCourante = 2)}
+      actif={idsModelesSelectionnes.length > 0}
+    />
+  {/if}
 </ActionsTiroir>
 
 <style lang="scss">
@@ -91,5 +146,13 @@
 
   :global(.texte-tiroir) {
     display: inherit !important;
+  }
+
+  h4 {
+    font-size: 1.25rem;
+    font-weight: 700;
+    line-height: 1.75rem;
+    margin: 0;
+    max-width: 550px;
   }
 </style>
