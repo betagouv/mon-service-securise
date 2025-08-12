6f2b573f1eeaf2490e1fa9d0bf2924e7e3c62f46
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-12 12:09:44 +0200

    GET le rapport au moment du chargement du composant

---
+++ svelte/lib/listeMesures/televersement/RapportTeleversementModelesMesureSpecifique.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { recupereRapportDetaille } from './rapportTeleversementModelesMesureSpecifique.api';
+
+  onMount(async () => {
+    const resultat = await recupereRapportDetaille();
+    if (!resultat) return;
+
+    console.log(`📋 ${JSON.stringify(resultat)}`);
+  });
+</script>
