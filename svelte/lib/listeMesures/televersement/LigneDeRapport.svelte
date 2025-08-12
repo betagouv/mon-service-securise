3596adef2844bdee4f1d9e8cb4ba5f645505162e
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-12 17:15:27 +0200

    Affiche correctement les erreurs des lignes téléversées

--- svelte/lib/listeMesures/televersement/LigneDeRapport.svelte
+++ svelte/lib/listeMesures/televersement/LigneDeRapport.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-  import type {
-    ErreurModele,
-    ModeleTeleverse,
-  } from './rapportTeleversementModelesMesureSpecifique.types';
+  import {
+    type ErreurModele,
+    MessagesErreur,
+    type ModeleTeleverse,
+  } from './rapportTeleversementModelesMesureSpecifique.types.d';
   import TagEtat from '../../rapportTeleversement/composants/TagEtat.svelte';
   import CelluleDonnee from '../../rapportTeleversement/composants/CelluleDonnee.svelte';
   import { decode } from 'html-entities';
+  import TooltipErreursMultiple from '../../rapportTeleversement/composants/TooltipErreursMultiple.svelte';
 
   export let ligne: ModeleTeleverse;
 
   const aUneErreur = ligne.erreurs.length > 0;
+  const aDesErreurs = ligne.erreurs.length > 1;
 
   const contientErreur = (erreur: ErreurModele) =>
     ligne.erreurs.includes(erreur);
@@ -18,7 +21,16 @@
 <tr>
   <th scope="row"><TagEtat valide={!aUneErreur} /></th>
   <th scope="row" class="message-erreur" class:aUneErreur>
-    <div class="cellule-erreur">TODO DETAIL ERREUR</div>
+    <div class="cellule-erreur">
+      {aUneErreur
+        ? `${MessagesErreur[ligne.erreurs[0]]}${aDesErreurs ? '…' : ''}`
+        : 'Aucune erreur'}
+      {#if aDesErreurs}
+        <TooltipErreursMultiple
+          erreurs={ligne.erreurs.map((e) => MessagesErreur[e])}
+        />
+      {/if}
+    </div>
   </th>
   <CelluleDonnee contenu={ligne.numeroLigne.toString()} />
   <CelluleDonnee
