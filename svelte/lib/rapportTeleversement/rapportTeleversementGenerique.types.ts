01e7ed125fddbbc95adaf1f1024a2442194ea6b2
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-14 12:12:12 +0200

    Affiche en priorité l'erreur générale
    … si elle est présente.
    On continue d'afficher le rapport complet, en dessous, pour donner de l'information à l'utilisateur.

--- svelte/lib/rapportTeleversement/rapportTeleversementGenerique.types.ts
+++ svelte/lib/rapportTeleversement/rapportTeleversementGenerique.types.ts
@@ -1,5 +1,6 @@
 export type ResumeRapportTeleversement = {
   statut: 'VALIDE' | 'INVALIDE';
+  erreurGenerale: null | string;
   elementsValide: null | { label: string };
   elementsErreur: null | { label: string };
   labelValiderTeleversement: string;
