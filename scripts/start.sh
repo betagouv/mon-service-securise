bcbc9f8bee0f3895e114c3987ac7931f62cc847f
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-18 21:36:25 +0200

    Lance le conteneur en démon
    … pour régler, de nouveau, le souci des logs dupliqués. Le commit précédent n'a rien résolu : en relançant quelques fois le conteneur les logs dupliqués réapparaissent.
    
    Avec ce nouveau commit, on change de stratégie : un démon, puis consultation des logs avec `docker logs`.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-NODE_VERSION="$(cat .nvmrc)" docker compose \
-  up web --build
+NODE_VERSION="$(cat .nvmrc)" docker compose up web --build -d
+
+docker logs -f mon-service-securise-web-1
