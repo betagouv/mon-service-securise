eb527a1cb1eac3bf278b979b0c1958ed6744451e
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-19 12:39:07 +0200

    [REMANIEMENT] Supprime les erreurs devenues du code mort
    … depuis qu'on utilise `ErreurDonneeManquante` de façon générique.

--- src/modeles/journalMSS/erreurs.js
+++ src/modeles/journalMSS/erreurs.js
@@ -1,14 +1,5 @@
 class ErreurJournal extends Error {}
-class ErreurAutorisationsServiceManquantes extends ErreurJournal {}
-class ErreurDateDerniereConnexionManquante extends ErreurJournal {}
 class ErreurDateDerniereConnexionInvalide extends ErreurJournal {}
-class ErreurDateHomologationManquante extends ErreurJournal {}
-class ErreurDureeHomologationManquante extends ErreurJournal {}
-class ErreurIdentifiantServiceManquant extends ErreurJournal {}
-class ErreurIdentifiantUtilisateurManquant extends ErreurJournal {}
-class ErreurIdentifiantMesureManquant extends ErreurJournal {}
-class ErreurIdentifiantRetourUtilisateurManquant extends ErreurJournal {}
-class ErreurNombreServicesImportes extends ErreurJournal {}
 class ErreurServiceManquant extends ErreurJournal {}
 class ErreurUtilisateurManquant extends ErreurJournal {}
 class ErreurDonneeManquante extends ErreurJournal {
@@ -20,17 +11,8 @@ class ErreurDonneeManquante extends ErreurJournal {
 }
 
 module.exports = {
-  ErreurAutorisationsServiceManquantes,
-  ErreurDateDerniereConnexionManquante,
   ErreurDateDerniereConnexionInvalide,
-  ErreurDateHomologationManquante,
   ErreurDonneeManquante,
-  ErreurDureeHomologationManquante,
-  ErreurIdentifiantServiceManquant,
-  ErreurIdentifiantUtilisateurManquant,
-  ErreurIdentifiantMesureManquant,
-  ErreurIdentifiantRetourUtilisateurManquant,
-  ErreurNombreServicesImportes,
   ErreurServiceManquant,
   ErreurUtilisateurManquant,
 };
