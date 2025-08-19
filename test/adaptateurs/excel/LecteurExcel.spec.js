2eb86387dd65cfc5b33b8fff969a10c881456a64
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-08-19 10:21:59 +0200

    [SOIN] Ajoute des tests

---
+++ test/adaptateurs/excel/LecteurExcel.spec.js
@@ -0,0 +1,28 @@
+const expect = require('expect.js');
+const { LecteurExcel } = require('../../../src/adaptateurs/excel/LecteurExcel');
+
+describe('Le lecteur de fichier Excel', () => {
+  describe('concernant le contrôle de lignes complètes', () => {
+    it('dit que les lignes sont complètes quand toutes les propriétés sont présentes', () => {
+      const lecteur = new LecteurExcel();
+
+      const complete = lecteur.lesLignesSontCompletes(
+        [{ jour: 'lundi' }],
+        ['jour']
+      );
+
+      expect(complete).to.be(true);
+    });
+
+    it("dit que les lignes sont incomplètes dès qu'il manque une propriété", () => {
+      const lecteur = new LecteurExcel();
+
+      const complete = lecteur.lesLignesSontCompletes(
+        [{ jour: 'lundi' }],
+        ['jour', 'mois']
+      );
+
+      expect(complete).to.be(false);
+    });
+  });
+});
