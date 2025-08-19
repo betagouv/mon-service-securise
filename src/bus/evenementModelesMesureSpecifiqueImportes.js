3fb7d4bfaf7f09a3240a810e9c25b3a192de3f4a
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-08-19 11:00:47 +0200

    Publie un évènement de modèles de mesure spécifique importés

---
+++ src/bus/evenementModelesMesureSpecifiqueImportes.js
@@ -0,0 +1,18 @@
+class EvenementModelesMesureSpecifiqueImportes {
+  constructor({ idUtilisateur, nbModelesMesureSpecifiqueImportes }) {
+    if (!nbModelesMesureSpecifiqueImportes) {
+      throw new Error(
+        "Impossible d'instancier l'événement sans nombre de modèles de mesure spécifique importés"
+      );
+    }
+
+    if (!idUtilisateur) {
+      throw new Error(
+        "Impossible d'instancier l'événement sans identifiant utilisateur"
+      );
+    }
+    this.nbModelesMesureSpecifiqueImportes = nbModelesMesureSpecifiqueImportes;
+    this.idUtilisateur = idUtilisateur;
+  }
+}
+module.exports = EvenementModelesMesureSpecifiqueImportes;
