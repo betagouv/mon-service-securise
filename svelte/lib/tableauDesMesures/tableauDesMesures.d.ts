8a74d28353218598b30157384e656b299b3bd591
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-08-20 11:16:29 +0200

    Décode la description longue côté front
    ... même si dans l'idéal on aimerait le faire dans le backend. Côté front pour ne pas impacter toutes les utilisations côté Sécuriser.

--- svelte/lib/tableauDesMesures/tableauDesMesures.d.ts
+++ svelte/lib/tableauDesMesures/tableauDesMesures.d.ts
@@ -42,6 +42,7 @@ export type MesureSpecifique = {
   idModele?: string;
   categorie: string;
   description: string;
+  descriptionLongue?: string;
   statut: StatutMesure;
   modalites: string;
   identifiantNumerique: string;
