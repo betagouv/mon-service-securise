0fdf243dec5313690434006f36d1bc2d295271c4
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-09-03 15:31:10 +0200

    Retourne l'identifiant du brouillon depuis l'API

--- test/routes/connecte/routesConnecteApiBrouillonService.spec.ts
+++ test/routes/connecte/routesConnecteApiBrouillonService.spec.ts
@@ -1,4 +1,5 @@
 import testeurMSS from '../testeurMSS.js';
+import { unUUID } from '../../constructeurs/UUID.js';
 
 describe('Le serveur MSS des routes /api/brouillon-service/*', () => {
   const testeur = testeurMSS();
@@ -8,12 +9,13 @@ describe('Le serveur MSS des routes /api/brouillon-service/*', () => {
   describe('quand requête POST sur /api/brouillon-service', () => {
     it('crée un brouillon de service et retourne son id', async () => {
       testeur.middleware().reinitialise({ idUtilisateur: 'U1' });
+      testeur.depotDonnees().nouveauBrouillonService = async () => unUUID('3');
 
       const resultat = await testeur.post('/api/brouillon-service', {
         nomService: 'Le service',
       });
 
-      expect(resultat.body.id).toBeDefined();
+      expect(resultat.body.id).toBe(unUUID('3'));
     });
   });
 });
