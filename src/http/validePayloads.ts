591301fa4c52092ae8319c9f5279ed083c2adb84
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-09-04 21:38:54 +0200

    Type la `Request` renvoyée par `valideBody`
    … pour que `requete.body` soit typé dans la route qui utilise `valideBody()`.
    Inspiration : https://jakerobins.com/blog/using-zod-to-provide-typesafe-express-requests

--- src/http/validePayloads.ts
+++ src/http/validePayloads.ts
@@ -1,11 +1,23 @@
 import * as z from 'zod';
-import { NextFunction, Request, RequestHandler, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 export const valideBody =
-  (objet: z.ZodType): RequestHandler =>
-  async (requete: Request, reponse: Response, suite: NextFunction) => {
+  <TZod extends z.ZodType, TBody extends z.infer<TZod>>(objet: TZod) =>
+  async (
+    requete: Request<unknown, unknown, TBody, unknown, never>,
+    reponse: Response,
+    suite: NextFunction
+  ) => {
     const resultat = objet.safeParse(requete.body);
-    return resultat.success ? suite() : reponse.sendStatus(400);
+
+    if (!resultat.success) return reponse.sendStatus(400);
+
+    // Ici on veut bel et bien ré-écrire la requête, car c'est comme ça qu'expressjs est conçu.
+    // On réassigne `body` pour que les suivants récupèrent le contenu assaini par Zod.
+    // eslint-disable-next-line no-param-reassign
+    requete.body = resultat.data as TBody;
+
+    return suite();
   };
 
 export const valideParams =
