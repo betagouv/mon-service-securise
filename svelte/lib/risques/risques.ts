7ad8b1f146c40979be844551b60f754efd6e1584
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-09-04 10:39:48 +0200

    Supprime des `decode()` sur les risques
    Ces champs remontent jusqu'à la BDD, table `services`.
    
    On vise à supprimer toute utilisation du `@html` côté Svelte, qui est un smell de notre mauvaise gestion des encode()/decode().
    
    Objectif : cesser de `encode()` pour ne plus avoir besoin de `decode()` ni de `@html`

--- svelte/lib/risques/risques.ts
+++ svelte/lib/risques/risques.ts
@@ -18,7 +18,7 @@ export const convertisDonneesRisqueGeneral = (
   donneesRisque: DonneesRisque
 ) => ({
   ...donneesRisque,
-  commentaire: decode(donneesRisque.commentaire),
+  commentaire: donneesRisque.commentaire,
   niveauGravite: donneesRisque.niveauGravite ?? '',
   niveauVraisemblance: donneesRisque.niveauVraisemblance ?? '',
   type: 'GENERAL' as TypeRisque,
@@ -28,9 +28,9 @@ export const convertisDonneesRisqueSpecifique = (
   donneesRisque: DonneesRisque
 ) => ({
   ...donneesRisque,
-  intitule: decode(donneesRisque.intitule),
-  commentaire: decode(donneesRisque.commentaire),
-  description: decode(donneesRisque.description),
+  intitule: donneesRisque.intitule,
+  commentaire: donneesRisque.commentaire,
+  description: donneesRisque.description,
   niveauGravite: donneesRisque.niveauGravite ?? '',
   niveauVraisemblance: donneesRisque.niveauVraisemblance ?? '',
   type: 'SPECIFIQUE' as TypeRisque,
