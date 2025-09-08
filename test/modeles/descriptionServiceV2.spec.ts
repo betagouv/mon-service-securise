08b362ffe972df1472e02e354a1c9fd55985328c
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-09-08 14:51:23 +0200

    Donne un statut de saisie à la DescriptionServiceV2
    Ce qui permet d'afficher avec succès une page /mesures sur un service V2.
    Avant ça, `menuNavigation.pug` plantait, car ne trouvait pas `statutSaisie()`.

--- test/modeles/descriptionServiceV2.spec.ts
+++ test/modeles/descriptionServiceV2.spec.ts
@@ -16,4 +16,13 @@ describe('Une description service V2', () => {
     expect(descriptionV2.organisationResponsable.siret).toBe('1111111111111');
     expect(descriptionV2.organisationResponsable.departement).toBe('33');
   });
+
+  it('renvoie toujours un statut de saisie à « COMPLETES » car MSS ne permet pas de créer une Description V2 incomplète', async () => {
+    const descriptionV2 = new DescriptionServiceV2({
+      nomService: 'Mairie',
+      organisationResponsable: { siret: '1111111111111' },
+    });
+
+    expect(descriptionV2.statutSaisie()).toBe('completes');
+  });
 });
