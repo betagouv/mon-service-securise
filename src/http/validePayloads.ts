7af9f4031ba81c47bde5281a1f59de1e3a34ff66
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-09-09 10:51:28 +0200

    Type correctement la sortie de `ValideParams`
    … en reprenant la façon de faire de `ValideBody`.

--- src/http/validePayloads.ts
+++ src/http/validePayloads.ts
@@ -13,7 +13,7 @@ export const valideBody =
     if (!resultat.success) return reponse.sendStatus(400);
 
     // Ici on veut bel et bien ré-écrire la requête, car c'est comme ça qu'expressjs est conçu.
-    // On réassigne `body` pour que les suivants récupèrent le contenu assaini par Zod.
+    // On réassigne pour que les suivants récupèrent le contenu assaini par Zod.
     // eslint-disable-next-line no-param-reassign
     requete.body = resultat.data as TBody;
 
@@ -21,8 +21,20 @@ export const valideBody =
   };
 
 export const valideParams =
-  (objet: z.ZodType): RequestHandler =>
-  async (requete: Request, reponse: Response, suite: NextFunction) => {
+  <TZod extends z.ZodType, TParams extends z.infer<TZod>>(objet: TZod) =>
+  async (
+    requete: Request<TParams, unknown, unknown, unknown, never>,
+    reponse: Response,
+    suite: NextFunction
+  ) => {
     const resultat = objet.safeParse(requete.params);
-    return resultat.success ? suite() : reponse.sendStatus(400);
+
+    if (!resultat.success) return reponse.sendStatus(400);
+
+    // Ici on veut bel et bien ré-écrire la requête, car c'est comme ça qu'expressjs est conçu.
+    // On réassigne pour que les suivants récupèrent le contenu assaini par Zod.
+    // eslint-disable-next-line no-param-reassign
+    requete.params = resultat.data as TParams;
+
+    return suite();
   };
