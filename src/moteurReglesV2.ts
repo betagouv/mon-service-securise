8b6656629eac71aefbfc18d235e3258d50793653
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-09-09 23:03:30 +0200

    [MOTEUR] Extrais `evalueLigne()` pour augmenter la lisibilité

--- src/moteurReglesV2.ts
+++ src/moteurReglesV2.ts
@@ -2,11 +2,13 @@ import { Referentiel } from './referentiel.interface.js';
 import { DescriptionServiceV2 } from './modeles/descriptionServiceV2.js';
 
 type IdMesureV2 = string;
+
 export enum Modificateur {
   RendreIndispensable,
   RendreRecommandee,
   Ajouter,
 }
+
 type LigneDeMenu = {
   reference: IdMesureV2;
   dansSocleInitial: boolean;
@@ -29,45 +31,51 @@ export class MoteurReglesV2 {
 
     // eslint-disable-next-line no-restricted-syntax
     for (const ligne of this.menu) {
-      const modifiee: { aRajouter?: boolean; indispensable: boolean } = {
-        indispensable: false,
-      };
+      const modification = this.evalueLigne(ligne, descriptionService);
 
-      modifiee.aRajouter = ligne.dansSocleInitial;
-
-      const champ = Object.keys(ligne.modificateurs)[0];
-
-      if (champ) {
-        const [valeurMenu, modificateur] = ligne.modificateurs[champ];
-        const enRecord = descriptionService as unknown as Record<
-          string,
-          string
-        >;
-        const valeurReelle = enRecord[champ] as string;
-
-        if (valeurReelle === valeurMenu)
-          switch (modificateur) {
-            case Modificateur.RendreIndispensable:
-              modifiee.indispensable = true;
-              break;
-            case Modificateur.RendreRecommandee:
-              modifiee.indispensable = false;
-              break;
-            case Modificateur.Ajouter:
-              modifiee.aRajouter = true;
-              break;
-            default:
-              break;
-          }
-      }
-
-      if (modifiee.aRajouter)
+      if (modification.aRajouter)
         mesures.push([
           ligne.reference,
-          { indispensable: modifiee.indispensable },
+          { indispensable: modification.indispensable },
         ]);
     }
 
     return Object.fromEntries(mesures);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private evalueLigne(
+    ligne: LigneDeMenu,
+    descriptionService: DescriptionServiceV2
+  ) {
+    type ModificationMesure = { aRajouter?: boolean; indispensable: boolean };
+
+    const modification: ModificationMesure = { indispensable: false };
+
+    modification.aRajouter = ligne.dansSocleInitial;
+
+    const champ = Object.keys(ligne.modificateurs)[0];
+    if (champ) {
+      const [valeurMenu, modificateur] = ligne.modificateurs[champ];
+      const enRecord = descriptionService as unknown as Record<string, string>;
+      const valeurReelle = enRecord[champ] as string;
+
+      if (valeurReelle === valeurMenu)
+        switch (modificateur) {
+          case Modificateur.RendreIndispensable:
+            modification.indispensable = true;
+            break;
+          case Modificateur.RendreRecommandee:
+            modification.indispensable = false;
+            break;
+          case Modificateur.Ajouter:
+            modification.aRajouter = true;
+            break;
+          default:
+            break;
+        }
+    }
+
+    return modification;
   }
 }
