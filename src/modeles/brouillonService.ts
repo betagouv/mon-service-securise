8e6162c00fb697ae129872fe866e6703b55dc26b
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-09-09 17:19:00 +0200

    Ajoute le siret dans les données du brouillon
    ... et permet sa mise à jour

--- src/modeles/brouillonService.ts
+++ src/modeles/brouillonService.ts
@@ -3,25 +3,31 @@ import { VersionService } from './versionService.js';
 
 export type DonneesBrouillonService = {
   nomService: string;
+  siret?: string;
 };
 
 export class BrouillonService {
   readonly nomService: string;
+  protected siret?: string;
 
   constructor(
     readonly id: UUID,
     donnees: DonneesBrouillonService
   ) {
     this.nomService = donnees.nomService;
+    this.siret = donnees.siret;
+  }
+
+  metsAJourSiret(nouveauSiret: string) {
+    this.siret = nouveauSiret;
   }
 
   enDonneesCreationServiceV2() {
-    const siretFacticePourLeMoment = '13000766900018';
     return {
       versionService: VersionService.v2,
       descriptionService: {
         nomService: this.nomService,
-        organisationResponsable: { siret: siretFacticePourLeMoment },
+        organisationResponsable: { siret: this.siret },
       },
     };
   }
