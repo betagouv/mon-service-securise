82679c3f0bf3c0c0d6e080f6c4aaaacabcb81168
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-09-10 11:15:33 +0200

    Rend le formulaire de création plus flexible
    ... en utilisant un store qui pilote l'état du formulaire. Et ce afin de pouvoir ajouter facilement de nouvelles questions.

---
+++ svelte/lib/creationV2/etapes/etape1/QuestionNomService.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import ChampTexte from '../../../ui/ChampTexte.svelte';
+  import { createEventDispatcher } from 'svelte';
+
+  export let estComplete: boolean;
+  export let valeur: string;
+  const emetEvenement = createEventDispatcher<{
+    champModifie: string | number;
+  }>();
+
+  $: estComplete = valeur.length > 0;
+</script>
+
+<label for="nom-service">
+  Quel est le nom de votre service ?
+  <ChampTexte
+    id="nom-service"
+    nom="nom-service"
+    bind:valeur
+    on:blur={() => emetEvenement('champModifie', valeur)}
+  />
+</label>
---
+++ svelte/lib/creationV2/etapes/etape1/QuestionSiret.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import ChampTexte from '../../../ui/ChampTexte.svelte';
+  import { createEventDispatcher } from 'svelte';
+
+  export let estComplete: boolean;
+  export let valeur: string;
+  const emetEvenement = createEventDispatcher<{
+    champModifie: string | number;
+  }>();
+
+  $: estComplete = /^\d{14}$/.test(valeur);
+</script>
+
+<label for="nom-service">
+  Quel est le nom ou siret de l’organisation ?
+  <ChampTexte
+    id="siret"
+    nom="siret"
+    bind:valeur
+    on:blur={() => emetEvenement('champModifie', valeur)}
+  />
+</label>
