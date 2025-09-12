4288fd430401d418c0c5ae1f37ccd3f6379a15b7
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-09-12 09:22:43 +0200

    Reviens à un lancement classique en dev
    On annule 6b0609fae56bade0316554ca050d7d0121856974 car le bug des logs en double était en fait un bug Docker Compose
    résolu en version `2.39.2`.
    Donc, inutile de démarrer en mode démon.
    
    https://github.com/docker/compose/issues/13144

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-NODE_VERSION="$(cat .nvmrc)" docker compose up web --build -d
-
-docker logs -f mon-service-securise-web-1
+NODE_VERSION="$(cat .nvmrc)" docker compose \
+  up web --build
