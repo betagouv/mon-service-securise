d9a4b8a38b38a0144bf60cff804cc22c6a9dcd60
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-09-15 15:48:19 +0200

    Implémente les données à persister de la description V2
    Avec ce commit, on peut utiliser l'écran "/mesures" de MSS sur un service V2 !
    
    On introduit un builder pour que les tests opèrent sur des données qui compilent.

---
+++ test/constructeurs/constructeurDescriptionServiceV2.ts
@@ -0,0 +1,25 @@
+import {
+  DescriptionServiceV2,
+  DonneesDescriptionServiceV2,
+} from '../../src/modeles/descriptionServiceV2.js';
+
+class ConstructeurDescriptionServiceV2 {
+  private donnees: DonneesDescriptionServiceV2;
+
+  constructor() {
+    this.donnees = {
+      nomService: 'Service A',
+      niveauDeSecurite: 'niveau1',
+      organisationResponsable: { siret: '11112222333344' },
+      categorieDonneesTraitees: 'donneesSensibles',
+      volumetrieDonneesTraitees: 'moyen',
+    };
+  }
+
+  construis(): DescriptionServiceV2 {
+    return new DescriptionServiceV2(this.donnees);
+  }
+}
+
+export const uneDescriptionV2Valide = () =>
+  new ConstructeurDescriptionServiceV2();
--- test/modeles/descriptionServiceV2.spec.ts
+++ test/modeles/descriptionServiceV2.spec.ts
@@ -1,7 +1,11 @@
-import { DescriptionServiceV2 } from '../../src/modeles/descriptionServiceV2.ts';
+import {
+  DescriptionServiceV2,
+  DonneesDescriptionServiceV2,
+} from '../../src/modeles/descriptionServiceV2.ts';
+import { uneDescriptionV2Valide } from '../constructeurs/constructeurDescriptionServiceV2.js';
 
 describe('Une description service V2', () => {
-  it("peut s'instancier", () => {
+  it('connaît ses données à persister', () => {
     const descriptionV2 = new DescriptionServiceV2({
       nomService: 'Mairie',
       organisationResponsable: {
@@ -9,19 +13,28 @@ describe('Une description service V2', () => {
         siret: '1111111111111',
         departement: '33',
       },
+      niveauDeSecurite: 'niveau1',
+      categorieDonneesTraitees: 'documentsRHSensibles',
+      volumetrieDonneesTraitees: 'faible',
     });
 
-    expect(descriptionV2.nomService).toBe('Mairie');
-    expect(descriptionV2.organisationResponsable.nom).toBe('Ville V');
-    expect(descriptionV2.organisationResponsable.siret).toBe('1111111111111');
-    expect(descriptionV2.organisationResponsable.departement).toBe('33');
+    const aPersister = descriptionV2.donneesSerialisees();
+
+    expect<DonneesDescriptionServiceV2>(aPersister).toEqual({
+      nomService: 'Mairie',
+      organisationResponsable: {
+        departement: '33',
+        nom: 'Ville V',
+        siret: '1111111111111',
+      },
+      niveauDeSecurite: 'niveau1',
+      categorieDonneesTraitees: 'documentsRHSensibles',
+      volumetrieDonneesTraitees: 'faible',
+    });
   });
 
   it('renvoie toujours un statut de saisie à « COMPLETES » car MSS ne permet pas de créer une Description V2 incomplète', async () => {
-    const descriptionV2 = new DescriptionServiceV2({
-      nomService: 'Mairie',
-      organisationResponsable: { siret: '1111111111111' },
-    });
+    const descriptionV2 = uneDescriptionV2Valide().construis();
 
     expect(descriptionV2.statutSaisie()).toBe('completes');
   });
