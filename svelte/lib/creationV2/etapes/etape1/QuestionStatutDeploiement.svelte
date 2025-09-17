aa50ea9fa99dedcbc7bf58a218679f28c67dec35
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-09-17 16:40:13 +0200

    Utilise le nouveau composant Radio

--- svelte/lib/creationV2/etapes/etape1/QuestionStatutDeploiement.svelte
+++ svelte/lib/creationV2/etapes/etape1/QuestionStatutDeploiement.svelte
@@ -1,58 +1,32 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
+  import Radio from '../../Radio.svelte';
 
   export let estComplete: boolean;
   export let valeur: string;
 
   const dispatch = createEventDispatcher<{ champModifie: string }>();
-  const metsAJourValeur = () => {
-    if (valeur) {
-      dispatch('champModifie', valeur);
-    }
-  };
 
   $: estComplete = !!valeur;
+
+  $: if (valeur) dispatch('champModifie', valeur);
 </script>
 
 <label for="statut-deploiement">
   Quel est le statut de votre service ?*
 
   <span class="indication">Sélectionnez une réponse</span>
-  <div>
-    <input
-      type="radio"
-      id="enProjet"
-      name="statut-deploiement"
-      value="enProjet"
-      bind:group={valeur}
-      on:change={metsAJourValeur}
-    />
-    <label for="enProjet">En conception</label>
-  </div>
-
-  <div>
-    <input
-      type="radio"
-      id="enCours"
-      name="statut-deploiement"
-      value="enCours"
-      bind:group={valeur}
-      on:change={metsAJourValeur}
-    />
-    <label for="enCours">En cours de développement ou de déploiement</label>
-  </div>
-
-  <div>
-    <input
-      type="radio"
-      id="enLigne"
-      name="statut-deploiement"
-      value="enLigne"
-      bind:group={valeur}
-      on:change={metsAJourValeur}
-    />
-    <label for="enLigne">En ligne et accessible aux usagers et/ou agents</label>
-  </div>
+  <Radio id="enProjet" nom="En conception" bind:valeur />
+  <Radio
+    id="enCours"
+    nom="En cours de développement ou de déploiement"
+    bind:valeur
+  />
+  <Radio
+    id="enLigne"
+    nom="En ligne et accessible aux usagers et/ou agents"
+    bind:valeur
+  />
 </label>
 
 <style lang="scss">
@@ -63,21 +37,6 @@
       color: #666;
       font-weight: normal;
       margin-top: 8px;
-    }
-
-    div {
-      display: flex;
-      gap: 8px;
-      border: 1px solid #dddddd;
-      padding: 12px 24px;
-      border-radius: 8px;
-      cursor: pointer;
-
-      label {
-        font-size: 1rem !important;
-        line-height: 1.5rem !important;
-        font-weight: normal !important;
-      }
     }
   }
 </style>
