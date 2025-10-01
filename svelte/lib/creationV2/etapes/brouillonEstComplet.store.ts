f02892ba9a72ed9db0f2c63bda268f51ae504cdd
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-10-01 17:23:09 +0200

    Bloque le passage à l'étape suivante si le brouillon est incomplet

---
+++ svelte/lib/creationV2/etapes/brouillonEstComplet.store.ts
@@ -0,0 +1,19 @@
+import { derived } from 'svelte/store';
+import { leBrouillon } from './brouillon.store';
+
+export const brouillonEstCompletStore = derived<[typeof leBrouillon], boolean>(
+  [leBrouillon],
+  ([$b]) =>
+    !!$b.id &&
+    $b.nomService.length > 0 &&
+    /^\d{14}$/.test($b.siret) &&
+    $b.statutDeploiement.length > 0 &&
+    $b.presentation.length > 0 &&
+    $b.typeService.length > 0 &&
+    $b.typeHebergement.length > 0 &&
+    $b.ouvertureSysteme.length > 0 &&
+    $b.audienceCible.length > 0 &&
+    $b.dureeDysfonctionnementAcceptable.length > 0 &&
+    $b.volumetrieDonneesTraitees.length > 0 &&
+    $b.localisationsDonneesTraitees.length > 0
+);
