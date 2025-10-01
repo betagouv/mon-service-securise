957c2f9bf8384574192ad354321edc8d0a3797db
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-10-01 17:23:09 +0200

    Bloque le passage à l'étape suivante si le brouillon est incomplet

--- svelte/lib/creationV2/etapes/modeRapide/EtapeCreationEnModeRapide.svelte
+++ svelte/lib/creationV2/etapes/modeRapide/EtapeCreationEnModeRapide.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import BrouillonDeServiceEditable from '../BrouillonDeServiceEditable.svelte';
+  import { brouillonEstCompletStore } from '../brouillonEstComplet.store';
+
+  export let estComplete: boolean;
+  $: estComplete = $brouillonEstCompletStore;
 </script>
 
 <BrouillonDeServiceEditable />
