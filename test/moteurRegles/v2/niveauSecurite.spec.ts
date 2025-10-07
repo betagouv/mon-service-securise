4975cecf88bcc0966701723e4df5c2a852af4e12
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-10-07 14:34:35 +0200

    Ajoute le cas de test "S.I. 4"

--- test/moteurRegles/v2/niveauSecurite.spec.ts
+++ test/moteurRegles/v2/niveauSecurite.spec.ts
@@ -128,6 +128,18 @@ describe('Le moteur de règles de choix de niveau de sécurité V2', () => {
         ouvertureSysteme: 'accessibleSurInternet',
       } as Partial<DonneesDescriptionServiceV2>,
     },
+    {
+      nomCasTest: 'S.I. 4',
+      niveauRequis: 'niveau3',
+      descriptionService: {
+        volumetrieDonneesTraitees: 'eleve',
+        categoriesDonneesTraitees: ['documentsIdentifiants'],
+        categoriesDonneesTraiteesSupplementaires: [],
+        dureeDysfonctionnementAcceptable: 'moinsDe24h',
+        audienceCible: 'limitee',
+        ouvertureSysteme: 'accessibleSurInternet',
+      } as Partial<DonneesDescriptionServiceV2>,
+    },
   ])(
     `évalue le niveau de sécurité requis pour les besoins de $nomCasTest`,
     ({ descriptionService: d, niveauRequis }) => {
