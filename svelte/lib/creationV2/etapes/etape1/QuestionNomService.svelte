fbab842012dafef9fb6903a98c809e2b5b649e8b
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-10-08 15:37:28 +0200

    Remplace l'input par un input dsfr pour le nom de service

--- svelte/lib/creationV2/etapes/etape1/QuestionNomService.svelte
+++ svelte/lib/creationV2/etapes/etape1/QuestionNomService.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-  import ChampTexte from '../../../ui/ChampTexte.svelte';
   import { createEventDispatcher } from 'svelte';
-  import type { MiseAJour } from '../../creationV2.api';
+  import { type MiseAJour } from '../../creationV2.api';
   import { leBrouillon } from '../brouillon.store';
 
   export let estComplete: boolean;
+
+  let touche = false;
 
   const emetEvenement = createEventDispatcher<{
     champModifie: MiseAJour;
@@ -13,13 +14,19 @@
   $: estComplete = $leBrouillon.nomService.trim().length > 0;
 </script>
 
-<label for="nom-service" class="titre-question">
-  Quel est le nom du service à sécuriser ?
-  <ChampTexte
-    id="nom-service"
-    nom="nom-service"
-    bind:valeur={$leBrouillon.nomService}
-    on:blur={() =>
-      emetEvenement('champModifie', { nomService: $leBrouillon.nomService })}
-  />
-</label>
+<dsfr-input
+  label="Nom du service à sécuriser*"
+  type="text"
+  id="nom-service"
+  nom="nom-service"
+  value={$leBrouillon.nomService}
+  status={touche && $leBrouillon.nomService.length < 1 ? 'error' : 'default'}
+  errorMessage="Le nom du service est obligatoire."
+  on:valuechanged={(e) => {
+    $leBrouillon.nomService = e.detail;
+    touche = true;
+  }}
+  on:blur={async () => {
+    emetEvenement('champModifie', { nomService: $leBrouillon.nomService });
+  }}
+/>
