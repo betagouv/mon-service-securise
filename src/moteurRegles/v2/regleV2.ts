03e1b02854081e690a6da22e44f3704bfca173fd
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-10-09 11:10:04 +0200

    Déplace l'évaluation de la règle dans une classe
    … pour séparer les responsabilités.

---
+++ src/moteurRegles/v2/regleV2.ts
@@ -0,0 +1,33 @@
+import { Modifications } from './modifications.js';
+import {
+  CaracteristiquesDuService,
+  ModificateursDeRegles,
+} from './moteurReglesV2.js';
+
+export class RegleV2 {
+  constructor(
+    readonly reference: string,
+    private readonly dansSocleInitial: boolean,
+    private readonly modificateurs: ModificateursDeRegles // eslint-disable-next-line no-empty-function
+  ) {}
+
+  public evalue(descriptionService: Record<string, string>): Modifications {
+    const collecte = new Modifications(this.dansSocleInitial);
+
+    Object.keys(this.modificateurs).forEach((champDeDecrire) => {
+      const modifications =
+        this.modificateurs[champDeDecrire as CaracteristiquesDuService];
+      for (let i = 0; i < modifications!.length; i += 1) {
+        const [valeurRegle, modificateur] = modifications![i];
+
+        const valeurReelle = descriptionService[champDeDecrire];
+
+        if (Array.isArray(valeurReelle) && valeurReelle.includes(valeurRegle))
+          collecte.ajoute(modificateur);
+        else if (valeurReelle === valeurRegle) collecte.ajoute(modificateur);
+      }
+    });
+
+    return collecte;
+  }
+}
