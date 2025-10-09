29833f0f5ef3feeb531ed769f4531f51651627cf
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-10-09 10:54:09 +0200

    Ajoute un champ DSFR de recherche d'organisation par SIRET

--- svelte/lib/creationV2/etapes/etape1/QuestionSiret.svelte
+++ svelte/lib/creationV2/etapes/etape1/QuestionSiret.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-  import ChampTexte from '../../../ui/ChampTexte.svelte';
   import { createEventDispatcher } from 'svelte';
   import type { MiseAJour } from '../../creationV2.api';
   import { leBrouillon } from '../brouillon.store';
+  import ChampOrganisation from '../../../ui/ChampOrganisation.svelte';
 
   export let estComplete: boolean;
 
   const emetEvenement = createEventDispatcher<{ champModifie: MiseAJour }>();
 
   $: estComplete = /^\d{14}$/.test($leBrouillon.siret);
+
+  $: {
+    if (estComplete)
+      emetEvenement('champModifie', { siret: $leBrouillon.siret });
+  }
 </script>
 
-<label for="siret" class="titre-question">
-  Quel est le nom ou siret de l’organisation ?
-  <ChampTexte
-    id="siret"
-    nom="siret"
-    bind:valeur={$leBrouillon.siret}
-    on:blur={() => emetEvenement('champModifie', { siret: $leBrouillon.siret })}
-  />
-</label>
+<ChampOrganisation
+  label="Quel est le nom ou siret de l’organisation ?*"
+  bind:siret={$leBrouillon.siret}
+/>
