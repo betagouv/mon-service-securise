26aa5a103ff81a3d1392468423dcfd3891e31e9e
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-10-15 17:04:09 +0200

    Affiche le résumé du service

--- svelte/lib/decrireV2/DecrireV2.svelte
+++ svelte/lib/decrireV2/DecrireV2.svelte
@@ -1,11 +1,48 @@
 <script lang="ts">
-  export let service: Record<string, unknown>;
+  import ResumeDuServiceLectureSeule from '../creationV2/etapes/resume/ResumeDuServiceLectureSeule.svelte';
+  import { convertisDonneesDescriptionEnLibelles } from '../creationV2/etapes/resume/resume.store';
+  import type { ServiceV2 } from './decrireV2.d';
+
+  export let service: ServiceV2;
+  let lectureSeule: boolean = true;
 </script>
 
-<pre>{JSON.stringify(service, null, 2)}</pre>
+<div class="conteneur-decrire-v2">
+  {#if lectureSeule}
+    <div class="conteneur-bouton-modifier">
+      <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+      <lab-anssi-bouton
+        titre="Modifier le service"
+        variante="tertiaire"
+        taille="md"
+        icone="edit-line"
+        positionIcone="droite"
+        on:click={() => (lectureSeule = false)}
+      />
+    </div>
+    <ResumeDuServiceLectureSeule
+      donnees={convertisDonneesDescriptionEnLibelles({
+        ...service.descriptionService,
+        siret: service.descriptionService.organisationResponsable.siret,
+        pointsAcces: service.descriptionService.pointsAcces.map(
+          (p) => p.description
+        ),
+      })}
+    />
+  {:else}
+    <p>Formulaire éditable</p>
+  {/if}
+</div>
 
 <style lang="scss">
-  pre {
+  .conteneur-decrire-v2 {
     text-align: left;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+
+    .conteneur-bouton-modifier {
+      margin-left: auto;
+    }
   }
 </style>
