d615410f70ab18b29b8d8ad4ccce76457ee48dd9
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-10-16 14:59:02 +0200

    Masque le bouton d'édition en lecture seule

--- public/service/decrireV2.mjs
+++ public/service/decrireV2.mjs
@@ -2,9 +2,10 @@ import lisDonneesPartagees from '../modules/donneesPartagees.mjs';
 
 $(() => {
   const descriptionService = lisDonneesPartagees('donnees-description-service');
+  const lectureSeule = lisDonneesPartagees('donnees-lecture-seule');
   document.body.dispatchEvent(
     new CustomEvent('svelte-recharge-decrire-v2', {
-      detail: { descriptionService },
+      detail: { descriptionService, lectureSeule },
     })
   );
 });
