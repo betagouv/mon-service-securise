59acf0fc9c174d8575ff2c419f4b31158b605bbe
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-10-16 15:56:19 +0200

    Ajoute des tests d'acceptance pour l'ensemble des moteurs V2

---
+++ test/moteurRegles/v2/mesuresCas1.ts
@@ -0,0 +1,353 @@
+export const mesuresCas1 = {
+  'RECENSEMENT.1': {
+    indispensable: true,
+  },
+  'RECENSEMENT.2': {
+    indispensable: true,
+  },
+  'RECENSEMENT.3': {
+    indispensable: true,
+  },
+  'CONFORMITE.1': {
+    indispensable: true,
+  },
+  'CONFORMITE.3': {
+    indispensable: false,
+  },
+  'PSSI.5': {
+    indispensable: true,
+  },
+  'CONTRAT.1': {
+    indispensable: true,
+  },
+  'CONTRAT.2': {
+    indispensable: true,
+  },
+  'DEV.1': {
+    indispensable: true,
+  },
+  'ECOSYSTEME.1': {
+    indispensable: true,
+  },
+  'ECOSYSTEME.2': {
+    indispensable: true,
+  },
+  'ECOSYSTEME.3': {
+    indispensable: false,
+  },
+  'ECOSYSTEME.4': {
+    indispensable: false,
+  },
+  'ECOSYSTEME.5': {
+    indispensable: false,
+  },
+  'ECOSYSTEME.6': {
+    indispensable: false,
+  },
+  'RH.2': {
+    indispensable: true,
+  },
+  'RH.4': {
+    indispensable: true,
+  },
+  'CARTO.2': {
+    indispensable: true,
+  },
+  'DONNEES.1': {
+    indispensable: true,
+  },
+  'DONNEES.2': {
+    indispensable: false,
+  },
+  'MCO_MCS.1': {
+    indispensable: true,
+  },
+  'MCO_MCS.11': {
+    indispensable: true,
+  },
+  'MCO_MCS.17': {
+    indispensable: true,
+  },
+  'MCO_MCS.2': {
+    indispensable: true,
+  },
+  'MCO_MCS.3': {
+    indispensable: true,
+  },
+  'MCO_MCS.4': {
+    indispensable: true,
+  },
+  'MCO_MCS.5': {
+    indispensable: true,
+  },
+  'MCO_MCS.6': {
+    indispensable: true,
+  },
+  'MCO_MCS.7': {
+    indispensable: true,
+  },
+  'MCO_MCS.8': {
+    indispensable: true,
+  },
+  'MCO_MCS.9': {
+    indispensable: true,
+  },
+  'RGPD.1': {
+    indispensable: true,
+  },
+  'RGPD.2': {
+    indispensable: true,
+  },
+  'RGPD.3': {
+    indispensable: true,
+  },
+  'RGPD.4': {
+    indispensable: true,
+  },
+  'RGPD.5': {
+    indispensable: true,
+  },
+  'RGPD.6': {
+    indispensable: true,
+  },
+  'PHYS.1': {
+    indispensable: true,
+  },
+  'PHYS.2': {
+    indispensable: true,
+  },
+  'CLOISON.1': {
+    indispensable: true,
+  },
+  'CLOISON.2': {
+    indispensable: true,
+  },
+  'CLOISON.3': {
+    indispensable: true,
+  },
+  'CLOISON.4': {
+    indispensable: true,
+  },
+  'CLOISON.5': {
+    indispensable: true,
+  },
+  'CLOISON.6': {
+    indispensable: true,
+  },
+  'FILTRE.1': {
+    indispensable: true,
+  },
+  'FILTRE.2': {
+    indispensable: true,
+  },
+  'FILTRE.3': {
+    indispensable: true,
+  },
+  'FILTRE.4': {
+    indispensable: true,
+  },
+  'FILTRE.5': {
+    indispensable: true,
+  },
+  'FILTRE.7': {
+    indispensable: true,
+  },
+  'MAIL.1': {
+    indispensable: true,
+  },
+  'DISTANCE.1': {
+    indispensable: true,
+  },
+  'DISTANCE.2': {
+    indispensable: true,
+  },
+  'DISTANCE.4': {
+    indispensable: true,
+  },
+  'MALWARE.3': {
+    indispensable: true,
+  },
+  'AUTH.1': {
+    indispensable: true,
+  },
+  'AUTH.10': {
+    indispensable: true,
+  },
+  'AUTH.11': {
+    indispensable: true,
+  },
+  'AUTH.2': {
+    indispensable: true,
+  },
+  'AUTH.3': {
+    indispensable: true,
+  },
+  'AUTH.4': {
+    indispensable: true,
+  },
+  'AUTH.8': {
+    indispensable: false,
+  },
+  'AUTH.9': {
+    indispensable: true,
+  },
+  'DROITS.1': {
+    indispensable: true,
+  },
+  'DROITS.2': {
+    indispensable: true,
+  },
+  'DROITS.3': {
+    indispensable: true,
+  },
+  'ID.1': {
+    indispensable: true,
+  },
+  'ID.2': {
+    indispensable: true,
+  },
+  'ANNUAIRE.1': {
+    indispensable: true,
+  },
+  'ANNUAIRE.2': {
+    indispensable: true,
+  },
+  'ANNUAIRE.3': {
+    indispensable: true,
+  },
+  'ANNUAIRE.5': {
+    indispensable: true,
+  },
+  'COMPADMIN.1': {
+    indispensable: true,
+  },
+  'COMPADMIN.2': {
+    indispensable: true,
+  },
+  'COMPADMIN.3': {
+    indispensable: true,
+  },
+  'COMPADMIN.4': {
+    indispensable: true,
+  },
+  'COMPADMIN.5': {
+    indispensable: true,
+  },
+  'COMPADMIN.6': {
+    indispensable: true,
+  },
+  'COMPADMIN.8': {
+    indispensable: false,
+  },
+  'COMPADMIN.9': {
+    indispensable: true,
+  },
+  'INCIDENT.1': {
+    indispensable: true,
+  },
+  'INCIDENT.2': {
+    indispensable: true,
+  },
+  'INCIDENT.3': {
+    indispensable: true,
+  },
+  'INCIDENT.4': {
+    indispensable: true,
+  },
+  'INCIDENT.6': {
+    indispensable: true,
+  },
+  'INCIDENT.8': {
+    indispensable: true,
+  },
+  'CONTINU.1': {
+    indispensable: true,
+  },
+  'CONTINU.2': {
+    indispensable: true,
+  },
+  'CONTINU.3': {
+    indispensable: true,
+  },
+  'CONTINU.5': {
+    indispensable: true,
+  },
+  'CONTINU.6': {
+    indispensable: false,
+  },
+  'RISQUE.1': {
+    indispensable: true,
+  },
+  'AUDIT.1': {
+    indispensable: true,
+  },
+  'AUDIT.2': {
+    indispensable: true,
+  },
+  'AUDIT.3': {
+    indispensable: true,
+  },
+  'AUDIT.4': {
+    indispensable: true,
+  },
+  'AUDIT.5': {
+    indispensable: true,
+  },
+  'AUDIT.6': {
+    indispensable: true,
+  },
+  'CONFIG.1': {
+    indispensable: true,
+  },
+  'CONFIG.2': {
+    indispensable: true,
+  },
+  'CONFIG.3': {
+    indispensable: true,
+  },
+  'CONFIG.4': {
+    indispensable: true,
+  },
+  'CONFIG.5': {
+    indispensable: true,
+  },
+  'CONFIG.6': {
+    indispensable: true,
+  },
+  'CONFIG.8': {
+    indispensable: true,
+  },
+  'ADMIN.1': {
+    indispensable: true,
+  },
+  'ADMIN.2': {
+    indispensable: true,
+  },
+  'ADMIN.3': {
+    indispensable: true,
+  },
+  'ADMIN.4': {
+    indispensable: true,
+  },
+  'ADMIN.5': {
+    indispensable: true,
+  },
+  'ADMIN.6': {
+    indispensable: true,
+  },
+  'ADMIN.7': {
+    indispensable: true,
+  },
+  'SUPERVISION.4': {
+    indispensable: true,
+  },
+  'SUPERVISION.5': {
+    indispensable: true,
+  },
+  'SUPERVISION.6': {
+    indispensable: false,
+  },
+  'SUPERVISION.7': {
+    indispensable: false,
+  },
+};
---
+++ test/moteurRegles/v2/mesuresCas3.ts
@@ -0,0 +1,125 @@
+export const mesuresCas3 = {
+  'RECENSEMENT.1': {
+    indispensable: true,
+  },
+  'RECENSEMENT.2': {
+    indispensable: false,
+  },
+  'RECENSEMENT.3': {
+    indispensable: true,
+  },
+  'CONTRAT.1': {
+    indispensable: true,
+  },
+  'DEV.1': {
+    indispensable: false,
+  },
+  'ECOSYSTEME.1': {
+    indispensable: false,
+  },
+  'ECOSYSTEME.5': {
+    indispensable: false,
+  },
+  'RH.2': {
+    indispensable: true,
+  },
+  'DONNEES.2': {
+    indispensable: false,
+  },
+  'MCO_MCS.13': {
+    indispensable: true,
+  },
+  'MCO_MCS.15': {
+    indispensable: true,
+  },
+  'MCO_MCS.16': {
+    indispensable: true,
+  },
+  'MCO_MCS.3': {
+    indispensable: false,
+  },
+  'MCO_MCS.5': {
+    indispensable: true,
+  },
+  'MCO_MCS.6': {
+    indispensable: true,
+  },
+  'MCO_MCS.9': {
+    indispensable: false,
+  },
+  'CLOISON.5': {
+    indispensable: false,
+  },
+  'FILTRE.1': {
+    indispensable: false,
+  },
+  'DISTANCE.2': {
+    indispensable: false,
+  },
+  'DISTANCE.4': {
+    indispensable: false,
+  },
+  'AUTH.11': {
+    indispensable: false,
+  },
+  'AUTH.5': {
+    indispensable: true,
+  },
+  'AUTH.6': {
+    indispensable: true,
+  },
+  'AUTH.7': {
+    indispensable: true,
+  },
+  'DROITS.2': {
+    indispensable: false,
+  },
+  'DROITS.3': {
+    indispensable: true,
+  },
+  'ID.2': {
+    indispensable: false,
+  },
+  'ID.3': {
+    indispensable: true,
+  },
+  'COMPADMIN.1': {
+    indispensable: true,
+  },
+  'COMPADMIN.2': {
+    indispensable: false,
+  },
+  'COMPADMIN.6': {
+    indispensable: true,
+  },
+  'COMPADMIN.9': {
+    indispensable: true,
+  },
+  'INCIDENT.7': {
+    indispensable: true,
+  },
+  'CONTINU.1': {
+    indispensable: false,
+  },
+  'CONTINU.2': {
+    indispensable: true,
+  },
+  'AUDIT.6': {
+    indispensable: true,
+  },
+  'CONFIG.4': {
+    indispensable: false,
+  },
+  'CONFIG.6': {
+    indispensable: false,
+  },
+  'SUPERVISION.5': {
+    indispensable: false,
+  },
+  'SUPERVISION.6': {
+    indispensable: false,
+  },
+  'SUPERVISION.7': {
+    indispensable: false,
+  },
+};
