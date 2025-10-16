3605a5c72c3a544de346654d2edbe7ac45c733ba
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-10-16 17:44:22 +0200

    Crée le service en mode rapide
    … depuis le composant « Étape mode rapide », car ce n'est plus fait dans le BrouillonDeServiceEditable suite à la remontée des appels API dans les composants parents.

--- svelte/lib/creationV2/etapes/modeRapide/EtapeCreationEnModeRapide.svelte
+++ svelte/lib/creationV2/etapes/modeRapide/EtapeCreationEnModeRapide.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
   import BrouillonDeServiceEditable from '../BrouillonDeServiceEditable.svelte';
   import { brouillonEstCompletStore } from '../brouillonEstComplet.store';
-  import { metsAJourBrouillonService } from '../../creationV2.api';
-  import { leBrouillon } from '../brouillon.store';
+  import {
+    creeBrouillonService,
+    metsAJourBrouillonService,
+    type MiseAJour,
+  } from '../../creationV2.api';
+  import { entiteDeUtilisateur, leBrouillon } from '../brouillon.store';
+  import { ajouteParametreAUrl } from '../../../outils/url';
 
   export let estComplete: boolean;
   $: estComplete = $brouillonEstCompletStore;
+
+  const enregistre = async (maj: MiseAJour) => {
+    const doitCreerLeBrouillon = !$leBrouillon.id && maj.nomService;
+    if (doitCreerLeBrouillon) {
+      const nom = maj.nomService as string;
+      const idBrouillon = await creeBrouillonService(nom);
+      ajouteParametreAUrl('id', idBrouillon);
+
+      let siret = $entiteDeUtilisateur ? $entiteDeUtilisateur.siret : '';
+      if (siret) await metsAJourBrouillonService(idBrouillon, { siret });
+
+      leBrouillon.chargeDonnees({ id: idBrouillon, nomService: nom, siret });
+
+      return;
+    }
+
+    await metsAJourBrouillonService($leBrouillon.id!, maj);
+  };
 </script>
 
 <BrouillonDeServiceEditable
   on:champModifie={async (e) => {
-    await metsAJourBrouillonService($leBrouillon.id, e.detail);
+    await enregistre(e.detail);
   }}
 />
