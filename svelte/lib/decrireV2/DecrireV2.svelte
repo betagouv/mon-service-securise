53563587c046ce4ed166d8ceca295e09de77d131
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-10-21 16:22:48 +0200

    Affiche le résumé du besoin de sécurité

--- svelte/lib/decrireV2/DecrireV2.svelte
+++ svelte/lib/decrireV2/DecrireV2.svelte
@@ -13,6 +13,8 @@
   import { questionsV2 } from '../../../donneesReferentielMesuresV2.js';
   import { toasterStore } from '../ui/stores/toaster.store';
   import type { UUID } from '../typesBasiquesSvelte';
+  import donneesNiveauxDeSecurite from '../niveauxDeSecurite/donneesNiveauxDeSecurite';
+  import ResumeNiveauSecurite from '../ui/ResumeNiveauSecurite.svelte';
 
   type ModeAffichage = 'Résumé' | 'Édition';
 
@@ -31,6 +33,7 @@
   export let lectureSeule: boolean;
 
   let mode: ModeAffichage = 'Résumé';
+  let ongletActif: 'informations' | 'besoinsSecurite' = 'informations';
 
   const copiePourRestauration = structuredClone(descriptionService);
   let descriptionEditable: DescriptionServiceV2 =
@@ -41,19 +44,44 @@
 </script>
 
 <div class="conteneur-decrire-v2">
-  <div class="conteneur-resume">
-    {#if mode === 'Résumé'}
-      <ResumeDuServiceLectureSeule donnees={descriptionAffichable} />
-    {:else}
-      <BrouillonDeServiceEditable
-        bind:donnees={descriptionEditable}
-        seulementNomServiceEditable={false}
-        on:champModifie={(e) => {
-          descriptionEditable = { ...descriptionEditable, ...e.detail };
-        }}
-      />
-    {/if}
+  <div class="conteneur-onglets">
+    <button
+      on:click={() => (ongletActif = 'informations')}
+      class:actif={ongletActif === 'informations'}
+    >
+      Informations sur le service
+    </button>
+    <button
+      on:click={() => (ongletActif = 'besoinsSecurite')}
+      class:actif={ongletActif === 'besoinsSecurite'}
+    >
+      Besoins de sécurité
+    </button>
   </div>
+  {#if ongletActif === 'informations'}
+    <div class="conteneur-resume">
+      {#if mode === 'Résumé'}
+        <ResumeDuServiceLectureSeule donnees={descriptionAffichable} />
+      {:else}
+        <BrouillonDeServiceEditable
+          bind:donnees={descriptionEditable}
+          seulementNomServiceEditable={false}
+          on:champModifie={(e) => {
+            descriptionEditable = { ...descriptionEditable, ...e.detail };
+          }}
+        />
+      {/if}
+    </div>
+  {:else}
+    {@const niveau = descriptionService.niveauSecurite}
+    {@const donneesNiveau = donneesNiveauxDeSecurite.find(
+      (d) => d.id === niveau
+    )}
+    <div class="conteneur-besoins-securite">
+      <h5>{donneesNiveau.nom}</h5>
+      <ResumeNiveauSecurite {niveau} />
+    </div>
+  {/if}
 
   {#if !lectureSeule && mode === 'Résumé'}
     <BarreActions
@@ -106,7 +134,32 @@
     max-width: unset;
   }
 
-  .conteneur-resume {
+  .conteneur-onglets {
+    display: flex;
+    flex-direction: row;
+    max-width: 1000px;
+    margin: 24px auto 0;
+    border-bottom: 2px solid #ddd;
+
+    button {
+      background: none;
+      border: none;
+      font-size: 0.875rem;
+      line-height: 1.5rem;
+      cursor: pointer;
+      padding: 12px 12px 10px 12px;
+      transform: translateY(2px);
+      border-bottom: 2px solid transparent;
+
+      &.actif {
+        color: #000091;
+        border-bottom: 2px solid #000091;
+      }
+    }
+  }
+
+  .conteneur-resume,
+  .conteneur-besoins-securite {
     text-align: left;
     display: flex;
     flex-direction: column;
@@ -115,5 +168,26 @@
     max-width: 1000px;
     padding: 0 54px;
     margin: 24px auto;
+  }
+
+  .conteneur-besoins-securite {
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    padding: 24px;
+    box-sizing: border-box;
+
+    :global(hr) {
+      display: none;
+    }
+  }
+
+  h5 {
+    grid-area: titre;
+    padding: 0;
+    align-self: baseline;
+    font-weight: 700;
+    font-size: 1.375rem;
+    line-height: 1.75rem;
+    margin: 0 8px 0 0;
   }
 </style>
