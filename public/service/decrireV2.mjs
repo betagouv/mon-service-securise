574049eb5a9c0b8071294900f3030d409163945f
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-10-21 15:17:06 +0200

    Extrais l'idService de la description de service
    ...lors de l'instanciation du composant Svelte

--- public/service/decrireV2.mjs
+++ public/service/decrireV2.mjs
@@ -1,11 +1,13 @@
 import lisDonneesPartagees from '../modules/donneesPartagees.mjs';
 
 $(() => {
-  const descriptionService = lisDonneesPartagees('donnees-description-service');
+  const { idService, descriptionService } = lisDonneesPartagees(
+    'donnees-description-service'
+  );
   const lectureSeule = lisDonneesPartagees('donnees-lecture-seule');
   document.body.dispatchEvent(
     new CustomEvent('svelte-recharge-decrire-v2', {
-      detail: { descriptionService, lectureSeule },
+      detail: { idService, descriptionService, lectureSeule },
     })
   );
 });
