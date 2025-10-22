b47d1573ab156bf1bac8672c5ab1253a45972020
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-10-22 12:30:58 +0200

    Déplace `brancheMenuSandwich()`
    … pour la rapprocher du seul endroit où il est utilisé.

--- public/entete.js
+++ public/entete.js
@@ -1,4 +1,20 @@
-import brancheMenuSandwich from './modules/interactions/brancheMenuSandwich.js';
+const brancheMenuSandwich = () => {
+  const $menu = $('header nav');
+  const $sandwich = $('.sandwich');
+  const $boutonFermer = $('.bouton-fermer', $menu);
+
+  const interrupteurVisible = (visible) => {
+    $menu.toggleClass('visible', visible);
+    $('body').toggleClass('sans-ascenseur', visible);
+  };
+
+  $sandwich.on('click', () => {
+    interrupteurVisible(true);
+    window.scrollTo(0, 0);
+  });
+
+  $boutonFermer.on('click', () => interrupteurVisible(false));
+};
 
 $(() => {
   brancheMenuSandwich();
