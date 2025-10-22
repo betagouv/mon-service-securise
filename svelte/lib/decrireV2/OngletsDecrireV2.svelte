b563d6cbf664051188a22b94fa871b1404f86b5c
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-10-22 14:47:53 +0200

    Extrais `OngletsDecrireV2`
    … pour la concision du composant `<DecrireV2>`.

---
+++ svelte/lib/decrireV2/OngletsDecrireV2.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  export let ongletActif: 'informations' | 'besoinsSecurite';
+</script>
+
+<div class="conteneur-onglets">
+  <button
+    on:click={() => (ongletActif = 'informations')}
+    class:actif={ongletActif === 'informations'}
+  >
+    Informations sur le service
+  </button>
+  <button
+    on:click={() => (ongletActif = 'besoinsSecurite')}
+    class:actif={ongletActif === 'besoinsSecurite'}
+  >
+    Besoins de sécurité
+  </button>
+</div>
+
+<style lang="scss">
+  .conteneur-onglets {
+    display: flex;
+    flex-direction: row;
+    max-width: 1000px;
+    margin: 24px auto 0;
+    border-bottom: 2px solid #ddd;
+
+    button {
+      background: none;
+      border: none;
+      font-size: 0.875rem;
+      line-height: 1.5rem;
+      cursor: pointer;
+      padding: 12px 12px 10px 12px;
+      transform: translateY(2px);
+      border-bottom: 2px solid transparent;
+
+      &.actif {
+        color: #000091;
+        border-bottom: 2px solid #000091;
+      }
+    }
+  }
+</style>
