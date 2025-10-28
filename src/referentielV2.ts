9c233d633e3ecce326dc2e61b5d2b438fc854095
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-10-28 11:05:50 +0100

    Surcharge la définition de `mesure(id)` dans le référentiel v2

--- src/referentielV2.ts
+++ src/referentielV2.ts
@@ -1,7 +1,19 @@
 import { creeReferentiel } from './referentiel.js';
 import { Referentiel } from './referentiel.interface.js';
+import { mesuresV2 } from '../donneesReferentielMesuresV2.js';
+import { IdMesureV2 } from './moteurRegles/v2/moteurReglesV2.js';
 
-export const creeReferentielV2 = (): Referentiel => ({
-  ...creeReferentiel(),
-  version: () => 'v2',
-});
+export type DonneesReferentielV2 = {
+  mesures: typeof mesuresV2;
+};
+
+export const creeReferentielV2 = (
+  donnees: DonneesReferentielV2 = { mesures: mesuresV2 }
+): Referentiel => {
+  const mesure = (idMesure: IdMesureV2) => donnees.mesures[idMesure];
+  return {
+    ...creeReferentiel(),
+    mesure,
+    version: () => 'v2',
+  };
+};
