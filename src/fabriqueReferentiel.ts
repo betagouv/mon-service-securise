0e7ea5ed4cbfaba3122ed9c92fee24a78e62ceb7
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-10-28 11:47:54 +0100

    Déplace les méthodes spécifiques au référentiel V2
    ... en prenant soin de s'assurer que le moteur de règles V2 est chargé correctement avec les règles du moteur

--- src/fabriqueReferentiel.ts
+++ src/fabriqueReferentiel.ts
@@ -1,21 +1,3 @@
-import * as fs from 'node:fs';
-import { LecteurDeCSVDeReglesV2 } from './moteurRegles/v2/parsing/lecteurDeCSVDeReglesV2.js';
-import { mesuresV2 } from '../donneesReferentielMesuresV2.js';
 import { creeReferentiel } from './referentiel.js';
 
-export const fabriqueReferentiel = () => {
-  const fichierMesuresProd = `${
-    import.meta.dirname
-  }/moteurRegles/v2/mesures_V2_prod_30-09-2025.csv`;
-  if (!fs.existsSync(fichierMesuresProd))
-    throw new Error(
-      `Le fichier ${fichierMesuresProd} n'existe pas, impossible d'initialiser le moteur de règles V2.`
-    );
-
-  const regles = new LecteurDeCSVDeReglesV2(mesuresV2).lis(fichierMesuresProd);
-
-  const referentiel = creeReferentiel();
-  referentiel.enregistreReglesMoteurV2(regles);
-
-  return referentiel;
-};
+export const fabriqueReferentiel = () => creeReferentiel();
