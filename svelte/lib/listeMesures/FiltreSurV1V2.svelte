5ee2e26e8fa75c5e70ae59728276439a27a46953
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-11-18 11:38:24 +0100

    Intègre l'UI de la maquette pour le filtre de référentiel V1/V2

--- svelte/lib/listeMesures/FiltreSurV1V2.svelte
+++ svelte/lib/listeMesures/FiltreSurV1V2.svelte
@@ -2,29 +2,37 @@
   import type { VersionService } from '../../../src/modeles/versionService';
 
   export let value: VersionService;
+
+  const selectionneVersion = (version: string) => {
+    value = version as VersionService;
+  };
 </script>
 
 <th colspan="3">
   <div>
-    <label for="version-v1">
-      <input
-        type="radio"
-        id="version-v1"
-        name="version-service"
-        value="v1"
-        bind:group={value}
-      />
-      v1
-    </label>
-    <label for="version-v2">
-      <input
-        type="radio"
+    <label for="version-v2" class:actif={value === 'v2'}>
+      <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+      <lab-anssi-bouton
+        titre="Nouveau référentiel"
+        variante={value === 'v2' ? 'secondaire' : 'tertiaire'}
+        taille="sm"
+        icone=""
+        positionIcone="sans"
         id="version-v2"
-        name="version-service"
-        value="v2"
-        bind:group={value}
+        on:click={() => selectionneVersion('v2')}
       />
-      v2
+    </label>
+    <label for="version-v1" class:actif={value === 'v1'}>
+      <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+      <lab-anssi-bouton
+        titre="Ancien référentiel"
+        variante={value === 'v1' ? 'secondaire' : 'tertiaire'}
+        taille="sm"
+        icone=""
+        positionIcone="sans"
+        id="version-v1"
+        on:click={() => selectionneVersion('v1')}
+      />
     </label>
   </div>
 </th>
@@ -33,13 +41,21 @@
   th {
     padding: 12px;
     color: var(--bleu-mise-en-avant);
+    border-bottom: 1px solid #dddddd;
 
     div {
       display: flex;
-      gap: 12px;
 
       label {
         cursor: pointer;
+
+        &.actif {
+          z-index: 1;
+        }
+      }
+
+      label[for='version-v1'] {
+        margin-left: -4px;
       }
     }
   }
