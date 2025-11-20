c745d2f00a1caed5d3b6eed96bb064a4bfc1a796
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-11-20 10:23:16 +0100

    Extrais la logique de mise à jour des champs dans CreationV2

--- svelte/lib/creationV2/CreationV2.svelte
+++ svelte/lib/creationV2/CreationV2.svelte
@@ -2,10 +2,18 @@
   import AssistantServiceV2 from './AssistantServiceV2.svelte';
   import type { Entite } from '../ui/types';
   import type { UUID } from '../typesBasiquesSvelte';
-  import { lisBrouillonService } from './creationV2.api';
+  import {
+    creeBrouillonService,
+    lisBrouillonService,
+    metsAJourBrouillonService,
+    type MiseAJour,
+  } from './creationV2.api';
   import { entiteDeUtilisateur, leBrouillon } from './etapes/brouillon.store';
   import { navigationStore } from './etapes/navigation.store';
   import { onMount } from 'svelte';
+  import { ajouteParametreAUrl } from '../outils/url';
+  import type { BrouillonServiceV2 } from './creationV2.types';
+  import { etapeCourante } from './etapes/etapeCourante.store';
 
   export let entite: Entite | undefined;
 
@@ -21,6 +29,32 @@
     }
     if (entite) $entiteDeUtilisateur = entite;
   });
+
+  const metsAJourPropriete = async (e: CustomEvent<MiseAJour>) => {
+    const doitCreerBrouillon =
+      !$leBrouillon.id && $etapeCourante.estPremiereQuestion;
+    if (doitCreerBrouillon) {
+      const nomService = e.detail.nomService as string;
+      const idBrouillon = await creeBrouillonService(nomService);
+      ajouteParametreAUrl('id', idBrouillon);
+      leBrouillon.chargeDonnees({ id: idBrouillon, nomService });
+      return;
+    }
+
+    await metsAJourBrouillonService($leBrouillon.id!, e.detail);
+
+    const nomChampModifie = Object.keys(
+      e.detail
+    )[0] as keyof BrouillonServiceV2;
+    const onEstToujoursSurLaQuestionQuiAEnvoyeLaMaj =
+      $etapeCourante.questionCourante.clesPropriete.includes(nomChampModifie);
+    // si on n'est plus sur la question mise à jour, c'est que "suivant()" a déjà été appelé
+    if (
+      onEstToujoursSurLaQuestionQuiAEnvoyeLaMaj &&
+      $etapeCourante.questionCourante.avecAvanceRapide
+    )
+      navigationStore.suivant();
+  };
 </script>
 
-<AssistantServiceV2 />
+<AssistantServiceV2 on:champModifie={metsAJourPropriete} />
