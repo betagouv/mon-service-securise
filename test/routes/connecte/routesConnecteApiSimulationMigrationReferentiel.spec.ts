0c7d28371caaf1b415d1f200f66ac84b40ae450c
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-11-20 12:01:15 +0100

    Ajoute une route pour lire une simulation depuis un id de service

---
+++ test/routes/connecte/routesConnecteApiSimulationMigrationReferentiel.spec.ts
@@ -0,0 +1,50 @@
+import { beforeEach } from 'vitest';
+import testeurMSS from '../testeurMSS.js';
+import { unUUIDRandom } from '../../constructeurs/UUID.js';
+import { BrouillonService } from '../../../src/modeles/brouillonService.js';
+import { ErreurSimulationInexistante } from '../../../src/erreurs.js';
+
+describe('Le serveur MSS des routes /api/service/:id/simulation-migration-referentiel/*', () => {
+  const testeur = testeurMSS();
+
+  beforeEach(testeur.initialise);
+
+  describe('quand requête GET sur `/api/service/:id/simulation-migration-referentiel`', () => {
+    it("renvoie une erreur 400 si l'ID passé n'est pas un UUID, pour toutes les routes", async () => {
+      const reponse = await testeur.get(
+        '/api/service/PAS_UN_UUID/simulation-migration-referentiel'
+      );
+
+      expect(reponse.status).toBe(400);
+    });
+
+    it('retourne la simulation pour le service correspondant', async () => {
+      const idService = unUUIDRandom();
+
+      testeur.depotDonnees().lisSimulationMigrationReferentiel = async () =>
+        new BrouillonService(idService, { nomService: 'Une simulation' });
+
+      const reponse = await testeur.get(
+        `/api/service/${idService}/simulation-migration-referentiel`
+      );
+
+      expect(reponse.status).toBe(200);
+      expect(reponse.body).toEqual({
+        id: idService,
+        nomService: 'Une simulation',
+      });
+    });
+
+    it("renvoie une erreur 404 si la simulation n'existe pas", async () => {
+      testeur.depotDonnees().lisSimulationMigrationReferentiel = async () => {
+        throw new ErreurSimulationInexistante();
+      };
+
+      const reponse = await testeur.get(
+        `/api/service/${unUUIDRandom()}/simulation-migration-referentiel`
+      );
+
+      expect(reponse.status).toBe(404);
+    });
+  });
+});
