c1a63c8d6766706832048b405edd1ad9bcdc5e37
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-11-20 10:10:23 +0100

    Englobe l'assistant v2 dans la créationV2

---
+++ svelte/lib/creationV2/CreationV2.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import AssistantServiceV2 from './AssistantServiceV2.svelte';
+  import type { Entite } from '../ui/types';
+
+  export let entite: Entite | undefined;
+</script>
+
+<AssistantServiceV2 {entite} />
