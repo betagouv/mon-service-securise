318fd35e500af3daae518b9be87f7a76fd84f5c9
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-11-26 14:57:04 +0100

    Teste l'équivalence des mesures inchangées en injectant une équivalence
    On injecte l'équivalence minimum qui nous permet de raisonner sur le test.

--- test/moteurRegles/simulationMigration/equivalencesMesuresV1V2.aide.ts
+++ test/moteurRegles/simulationMigration/equivalencesMesuresV1V2.aide.ts
@@ -3,10 +3,12 @@ import {
   EquivalencesMesuresV1V2,
 } from '../../../donneesConversionReferentielMesures.js';
 
-export const queDesInchangees = (): EquivalencesMesuresV1V2 =>
+export const toutesEquivalencesAvecStatut = (
+  statut: 'inchangee' | 'modifiee' | 'supprimee'
+): EquivalencesMesuresV1V2 =>
   Object.fromEntries(
     Object.entries(conversionMesuresV1versV2).map(([cle, valeur]) => [
       cle,
-      { ...valeur, statut: 'inchangee' },
+      { ...valeur, statut },
     ])
   ) as EquivalencesMesuresV1V2;
