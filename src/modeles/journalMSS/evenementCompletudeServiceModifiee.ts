2ba07d47b890b966a9a6356ba6b21f3f4cf95b41
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-12-04 16:53:14 +0100

    Extrais la production des données pour le v1
    … afin de créer une place naturelle pour la logique v2.

--- src/modeles/journalMSS/evenementCompletudeServiceModifiee.ts
+++ src/modeles/journalMSS/evenementCompletudeServiceModifiee.ts
@@ -1,24 +1,8 @@
 import Evenement from './evenement.js';
 import Service from '../service.js';
-import DescriptionService from '../descriptionService.js';
-import PointsAcces from '../pointsAcces.js';
-import FonctionnalitesSpecifiques from '../fonctionnalitesSpecifiques.js';
-import DonneesSensiblesSpecifiques from '../donneesSensiblesSpecifiques.js';
-import { CategorieMesure, DonneesIndiceCyber } from '../indiceCyber.type.js';
-
-type TableauIndiceCyber = Array<{ categorie: CategorieMesure; indice: number }>;
-type BornesOrganisations = { borneBasse: number; borneHaute: number };
-
-const nombreOuUn = (nombre: number) => Number(nombre) || 1;
-
-const enTableau = (donneesIndiceCyber: DonneesIndiceCyber) =>
-  Object.entries(donneesIndiceCyber).reduce<TableauIndiceCyber>(
-    (acc, [categorie, indice]) => [
-      ...acc,
-      { categorie: categorie as CategorieMesure, indice },
-    ],
-    []
-  );
+import { completudeV1 } from './evenementCompletudeServiceModifiee.serviceV1.js';
+import { VersionService } from '../versionService.js';
+import { ErreurJournal } from './erreurs.js';
 
 class EvenementCompletudeServiceModifiee extends Evenement {
   constructor(donnees: { service: Service }, options = {}) {
@@ -27,57 +11,14 @@ class EvenementCompletudeServiceModifiee extends Evenement {
     Evenement.verifieProprietesRenseignees(donnees, ['service']);
 
     const { service } = donnees;
-    const niveauSecuriteMinimal = service.estimeNiveauDeSecurite();
-    const { indiceCyber, ...autreDonneesCompletude } =
-      service.completudeMesures();
 
-    const {
-      typeService,
-      nombreOrganisationsUtilisatrices,
-      provenanceService,
-      statutDeploiement,
-      pointsAcces,
-      fonctionnalites,
-      fonctionnalitesSpecifiques,
-      donneesCaracterePersonnel,
-      donneesSensiblesSpecifiques,
-      localisationDonnees,
-      delaiAvantImpactCritique,
-      niveauSecurite,
-    } = service.descriptionService as DescriptionService;
-    const { borneBasse, borneHaute } =
-      nombreOrganisationsUtilisatrices as BornesOrganisations;
+    if (service.version() !== VersionService.v1)
+      throw new ErreurJournal(
+        `Impossible de générer la complétude pour ${service.version()}. Seule v1 est supportée.`
+      );
 
-    super(
-      'COMPLETUDE_SERVICE_MODIFIEE',
-      {
-        idService: adaptateurChiffrement.hacheSha256(service.id),
-        detailIndiceCyber: enTableau(indiceCyber),
-        versionIndiceCyber: 'v2',
-        ...autreDonneesCompletude,
-        nombreOrganisationsUtilisatrices: {
-          borneBasse: nombreOuUn(borneBasse),
-          borneHaute: nombreOuUn(borneHaute),
-        },
-        typeService,
-        provenanceService,
-        statutDeploiement,
-        pointsAcces: (pointsAcces as PointsAcces).nombre(),
-        fonctionnalites,
-        fonctionnalitesSpecifiques: (
-          fonctionnalitesSpecifiques as FonctionnalitesSpecifiques
-        ).nombre(),
-        donneesCaracterePersonnel,
-        donneesSensiblesSpecifiques: (
-          donneesSensiblesSpecifiques as DonneesSensiblesSpecifiques
-        ).nombre(),
-        localisationDonnees,
-        delaiAvantImpactCritique,
-        niveauSecurite,
-        niveauSecuriteMinimal,
-      },
-      date
-    );
+    const donneesJournal = completudeV1(service, adaptateurChiffrement);
+    super('COMPLETUDE_SERVICE_MODIFIEE', donneesJournal, date);
   }
 }
 
