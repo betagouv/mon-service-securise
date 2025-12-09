c67ddecbe80154414f5c758b348536217f5cf6fa
Xavier Maso <xavier.maso@beta.gouv.fr>
2025-12-09 17:18:38 +0100

    Explicite les échecs de parsing de fichier XLS
    ... pour bien faire apparaitre la nature invalide d'autres type de
    fichiers qui seraient téléversés.

--- test/adaptateurs/excel/LecteurExcel.spec.ts
+++ test/adaptateurs/excel/LecteurExcel.spec.ts
@@ -1,6 +1,28 @@
+import path from 'node:path';
+import { readFileSync } from 'node:fs';
+
+import { ErreurFichierXlsInvalide } from '../../../src/erreurs.js';
 import { LecteurExcel } from '../../../src/adaptateurs/excel/LecteurExcel.js';
 
 describe('Le lecteur de fichier Excel', () => {
+  describe('concernant la construction', () => {
+    describe('valide le format du contenu attendu', () => {
+      it.each(['anssi.pptx', 'anssi.ppt', 'logo_mss.png'])(
+        `lève une exception si le format du fichier %s n'est pas le bon`,
+        (fichier: string) => {
+          const contenuFichier = readFileSync(
+            path.join(__dirname, '..', '..', 'mocks', fichier)
+          );
+
+          expect(() => {
+            // eslint-disable-next-line no-new
+            new LecteurExcel(contenuFichier);
+          }).toThrowError(ErreurFichierXlsInvalide);
+        }
+      );
+    });
+  });
+
   describe('concernant le contrôle de lignes complètes', () => {
     it('dit que les lignes sont complètes quand toutes les propriétés sont présentes', () => {
       const lecteur = new LecteurExcel(Buffer.from(''));
