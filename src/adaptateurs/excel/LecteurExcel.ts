a805312e40363426d2517dba2426e9e0e004244b
Xavier Maso <xavier.maso@beta.gouv.fr>
2025-12-09 17:18:38 +0100

    Explicite les échecs de parsing de fichier XLS
    ... pour bien faire apparaitre la nature invalide d'autres type de
    fichiers qui seraient téléversés.

--- src/adaptateurs/excel/LecteurExcel.ts
+++ src/adaptateurs/excel/LecteurExcel.ts
@@ -4,8 +4,29 @@ import { ErreurFichierXlsInvalide } from '../../erreurs.js';
 class LecteurExcel {
   readonly fichierXLS: WorkBook;
 
+  private readonly messagesErreurs: string[] = [
+    'is not a spreadsheet',
+    'Cannot find Workbook stream',
+    'Could not find workbook',
+  ];
+
   constructor(buffer: Buffer) {
-    this.fichierXLS = xlsx.read(buffer, { type: 'buffer' });
+    try {
+      this.fichierXLS = xlsx.read(buffer, { type: 'buffer' });
+    } catch (e) {
+      // La validation du type fichier attendu (`xls`) s'opère au moment du parsing, et `throw` en cas d'échec ;
+      // _c.f._ https://git.sheetjs.com/sheetjs/sheetjs/src/commit/8a7cfd47bde8258c0d91df6a737bf0136699cdf8/bits/18_cfb.js#L252 ;
+      // L'implémentation de la validation effectue une vérification des octets d'en-têtes, spécifiques au format `xls`.
+      // Et d'autres fichiers partagent les même octets d'en-têtes : `xls`, `doc`, `ppt`, `pptx` ;
+      // _c.f._ https://en.wikipedia.org/wiki/List_of_file_signatures .
+      if (
+        e instanceof Error &&
+        this.messagesErreurs.some((m) => (e as Error).message.includes(m))
+      ) {
+        throw new ErreurFichierXlsInvalide();
+      }
+      throw e;
+    }
   }
 
   donneesDeFeuille(nomFeuille: string, indexLigneDuHeader: number) {
