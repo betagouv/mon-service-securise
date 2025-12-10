3b46afe7976ae7b59c04e4a9e4af265b1bb4a47d
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-12-10 12:12:33 +0100

    Affiche une modale de confirmation lors de la finalisation d'une simulation
    ...de migration de référentiel

--- svelte/lib/simulationV2/SimulationV2.svelte
+++ svelte/lib/simulationV2/SimulationV2.svelte
@@ -16,8 +16,10 @@
   } from './simulationv2.api';
   import type { BrouillonServiceV2 } from '../creationV2/creationV2.types';
   import { etapeCourante } from '../creationV2/etapes/etapeCourante.store';
+  import Modale from '../ui/Modale.svelte';
 
   export let idService: UUID;
+  export let elementModaleConfirmationMigration: Modale;
 
   let enCoursDeChargement = false;
 
@@ -57,7 +59,7 @@
 
 <AssistantServiceV2
   on:champModifie={metsAJourPropriete}
-  on:finalise={finalise}
+  on:finalise={() => elementModaleConfirmationMigration.affiche()}
   bind:enCoursDeChargement
   titreAssistant="Actualiser votre service"
   titreBoutonFinalise="Passer au nouveau référentiel"
@@ -75,3 +77,69 @@
     {/if}
   </svelte:fragment>
 </AssistantServiceV2>
+
+<Modale
+  id="modale-confirmation-migration"
+  bind:this={elementModaleConfirmationMigration}
+>
+  <svelte:fragment slot="contenu">
+    <div class="contenu-modale">
+      <h4>Êtes-vous sûr de vouloir passer au nouveau référentiel ?</h4>
+      <dsfr-alert
+        type="warning"
+        size="md"
+        hasTitle
+        hasDescription
+        title="Cette action est irréversible."
+        text="Il ne sera pas possible de revenir en arrière une fois la bascule effectuée. Cependant, les données déjà renseignées (questions inchangées dans l’ajout d’un service, mesures de sécurité, statuts, commentaires, plans d'action, etc) seront automatiquement reprises."
+      />
+    </div>
+  </svelte:fragment>
+  <svelte:fragment slot="actions">
+    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+    <lab-anssi-bouton
+      titre="Annuler"
+      variante="secondaire"
+      taille="md"
+      icone=""
+      positionIcone="sans"
+      on:click={() => elementModaleConfirmationMigration.ferme()}
+    />
+    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+    <lab-anssi-bouton
+      titre="Confirmer le changement"
+      variante="primaire"
+      taille="md"
+      icone=""
+      positionIcone="sans"
+      on:click={() => finalise()}
+    />
+  </svelte:fragment>
+</Modale>
+
+<style lang="scss">
+  :global(#modale-confirmation-migration) {
+    max-width: 556px;
+    max-height: 480px;
+
+    :global(.contenu-modale) {
+      display: flex;
+      flex-direction: column;
+      gap: 24px;
+      padding-bottom: 24px;
+      margin-top: 0;
+    }
+
+    :global(.conteneur-actions) {
+      border: none;
+    }
+
+    h4 {
+      font-weight: bold;
+      font-size: 1.5rem;
+      line-height: 2rem;
+      color: #161616;
+      margin: 16px 0;
+    }
+  }
+</style>
