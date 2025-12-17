8b66b1adaaf8898c3e3892de65875b162d32ab33
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-12-17 11:12:05 +0100

    Ajoute un mode dégradé de gestion d'erreur
    … pour compenser le Sentry qui ne fonctionne pas toujours bien.

--- src/adaptateurs/fabriqueAdaptateurGestionErreur.js
+++ src/adaptateurs/fabriqueAdaptateurGestionErreur.js
@@ -1,6 +1,19 @@
 import { sentry } from './adaptateurEnvironnement.js';
 import * as adaptateurSentry from './adaptateurGestionErreurSentry.js';
 import { fabriqueAdaptateurGestionErreurVide } from './adaptateurGestionErreurVide.js';
+import { fabriqueAdaptateurGestionErreurMattermost } from './adaptateurGestionErreurMattermost.js';
 
-export const fabriqueAdaptateurGestionErreur = () =>
-  sentry().dsn() ? adaptateurSentry : fabriqueAdaptateurGestionErreurVide();
+export const fabriqueAdaptateurGestionErreur = () => {
+  const modeDegradeMattermost = Boolean(
+    process.env.GESTION_ERREUR_URL_WEBHOOK_MATTERMOST
+  );
+  if (modeDegradeMattermost) {
+    // eslint-disable-next-line no-console
+    console.log('ℹ️ 🪶 MODE DÉGRADÉ VIA MATTERMOST ACTIVÉ');
+    return fabriqueAdaptateurGestionErreurMattermost();
+  }
+
+  return sentry().dsn()
+    ? adaptateurSentry
+    : fabriqueAdaptateurGestionErreurVide();
+};
