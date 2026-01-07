5fd9530a7ca800f57d1bd90130202f5b09417b46
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-01-07 11:28:21 +0100

    Supprime l'utilisation de l'adaptateur de gestion d'erreur Mattermost

--- src/adaptateurs/fabriqueAdaptateurGestionErreur.js
+++ src/adaptateurs/fabriqueAdaptateurGestionErreur.js
@@ -1,19 +1,6 @@
 import { sentry } from './adaptateurEnvironnement.js';
 import * as adaptateurSentry from './adaptateurGestionErreurSentry.js';
 import { fabriqueAdaptateurGestionErreurVide } from './adaptateurGestionErreurVide.js';
-import { fabriqueAdaptateurGestionErreurMattermost } from './adaptateurGestionErreurMattermost.js';
 
-export const fabriqueAdaptateurGestionErreur = () => {
-  const modeDegradeMattermost = Boolean(
-    process.env.GESTION_ERREUR_URL_WEBHOOK_MATTERMOST
-  );
-  if (modeDegradeMattermost) {
-    // eslint-disable-next-line no-console
-    console.log('ℹ️ 🪶 MODE DÉGRADÉ VIA MATTERMOST ACTIVÉ');
-    return fabriqueAdaptateurGestionErreurMattermost();
-  }
-
-  return sentry().dsn()
-    ? adaptateurSentry
-    : fabriqueAdaptateurGestionErreurVide();
-};
+export const fabriqueAdaptateurGestionErreur = () =>
+  sentry().dsn() ? adaptateurSentry : fabriqueAdaptateurGestionErreurVide();
