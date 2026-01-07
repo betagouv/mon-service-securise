a85ba7f1ddcba2dec883d89e239417e27d265481
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-01-07 15:03:47 +0100

    [SOIN] Type correctement le fichier Typescript

--- test/modeles/journalMSS/evenementConnexionUtilisateur.spec.ts
+++ test/modeles/journalMSS/evenementConnexionUtilisateur.spec.ts
@@ -1,80 +1,76 @@
-import expect from 'expect.js';
 import EvenementConnexionUtilisateur from '../../../src/modeles/journalMSS/evenementConnexionUtilisateur.js';
 import {
   ErreurDateDerniereConnexionInvalide,
   ErreurDonneeManquante,
 } from '../../../src/modeles/journalMSS/erreurs.js';
+import { unUUID } from '../../constructeurs/UUID.ts';
+import { SourceAuthentification } from '../../../src/modeles/sourceAuthentification.ts';
 
 describe('Un événement de connexion utilisateur', () => {
-  const hacheEnMajuscules = { hacheSha256: (valeur) => valeur?.toUpperCase() };
+  const hacheEnMajuscules = {
+    hacheSha256: (valeur: string) => valeur?.toUpperCase(),
+  };
+
+  const donneesEvenement = () => ({
+    idUtilisateur: unUUID('a'),
+    dateDerniereConnexion: '2022-07-07',
+    source: SourceAuthentification.MSS,
+  });
 
   it("hache l'identifiant de l'utilisateur qui lui est donné", () => {
-    const evenement = new EvenementConnexionUtilisateur(
-      { idUtilisateur: 'abc', dateDerniereConnexion: '2022-07-07' },
-      { adaptateurChiffrement: hacheEnMajuscules }
-    );
+    const evenement = new EvenementConnexionUtilisateur(donneesEvenement(), {
+      adaptateurChiffrement: hacheEnMajuscules,
+    });
 
-    expect(evenement.toJSON().donnees.idUtilisateur).to.be('ABC');
+    expect(evenement.toJSON().donnees.idUtilisateur).toBe(unUUID('A'));
   });
 
   it('sait se convertir en JSON', () => {
-    const evenement = new EvenementConnexionUtilisateur(
-      { idUtilisateur: 'abc', dateDerniereConnexion: '2022-11-17T13:05:02' },
-      { date: '17/11/2022', adaptateurChiffrement: hacheEnMajuscules }
-    );
+    const evenement = new EvenementConnexionUtilisateur(donneesEvenement(), {
+      date: '17/11/2022',
+      adaptateurChiffrement: hacheEnMajuscules,
+    });
 
     expect(evenement.toJSON()).to.eql({
       type: 'CONNEXION_UTILISATEUR',
       donnees: {
-        idUtilisateur: 'ABC',
-        dateDerniereConnexion: '2022-11-17T13:05:02',
+        idUtilisateur: unUUID('A'),
+        dateDerniereConnexion: '2022-07-07',
+        source: SourceAuthentification.MSS,
       },
       date: '17/11/2022',
     });
   });
 
   it("exige que l'identifiant utilisateur soit renseigné", () => {
-    try {
-      new EvenementConnexionUtilisateur(
-        { idUtilisateur: null, dateDerniereConnexion: '2022-05-06' },
-        { adaptateurChiffrement: hacheEnMajuscules }
-      );
-
-      expect().fail(
-        Error("L'instanciation de l'événement aurait dû lever une exception")
-      );
-    } catch (e) {
-      expect(e).to.be.an(ErreurDonneeManquante);
-    }
+    expect(
+      () =>
+        new EvenementConnexionUtilisateur(
+          // @ts-expect-error On force volontairement la valeur `null` pour déclencher une erreur
+          { ...donneesEvenement(), idUtilisateur: null },
+          { adaptateurChiffrement: hacheEnMajuscules }
+        )
+    ).toThrowError(ErreurDonneeManquante);
   });
 
   it('exige que la date de dernière connexion soit renseignée', () => {
-    try {
-      new EvenementConnexionUtilisateur(
-        { idUtilisateur: '123', dateDerniereConnexion: null },
-        { adaptateurChiffrement: hacheEnMajuscules }
-      );
-
-      expect().fail(
-        Error("L'instanciation de l'événement aurait dû lever une exception")
-      );
-    } catch (e) {
-      expect(e).to.be.an(ErreurDonneeManquante);
-    }
+    expect(
+      () =>
+        new EvenementConnexionUtilisateur(
+          // @ts-expect-error On force volontairement la valeur `null` pour déclencher une erreur
+          { ...donneesEvenement(), dateDerniereConnexion: null },
+          { adaptateurChiffrement: hacheEnMajuscules }
+        )
+    ).toThrowError(ErreurDonneeManquante);
   });
 
   it('exige que la date de dernière connexion soit valide', () => {
-    try {
-      new EvenementConnexionUtilisateur(
-        { idUtilisateur: '123', dateDerniereConnexion: 'pasValide' },
-        { adaptateurChiffrement: hacheEnMajuscules }
-      );
-
-      expect().fail(
-        Error("L'instanciation de l'événement aurait dû lever une exception")
-      );
-    } catch (e) {
-      expect(e).to.be.an(ErreurDateDerniereConnexionInvalide);
-    }
+    expect(
+      () =>
+        new EvenementConnexionUtilisateur(
+          { ...donneesEvenement(), dateDerniereConnexion: 'pasValide' },
+          { adaptateurChiffrement: hacheEnMajuscules }
+        )
+    ).toThrowError(ErreurDateDerniereConnexionInvalide);
   });
 });
