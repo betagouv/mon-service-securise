4b5cd2cc398ca21cc804226d4fd079dc578552d0
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-01-07 16:28:07 +0100

    [SOIN] Convertis les fichiers `.js` en `.ts`

--- test/bus/abonnements/consigneConnexionUtilisateurDansJournal.spec.ts
+++ test/bus/abonnements/consigneConnexionUtilisateurDansJournal.spec.ts
@@ -1,25 +1,32 @@
-import expect from 'expect.js';
 import * as AdaptateurJournalMSSMemoire from '../../../src/adaptateurs/adaptateurJournalMSSMemoire.js';
 import { consigneConnexionUtilisateurDansJournal } from '../../../src/bus/abonnements/consigneConnexionUtilisateurDansJournal.js';
+import {
+  AdaptateurJournalMSS,
+  EvenementJournal,
+} from '../../../src/adaptateurs/adaptateurJournalMSS.interface.ts';
+import { unUUID } from '../../constructeurs/UUID.ts';
+import { SourceAuthentification } from '../../../src/modeles/sourceAuthentification.ts';
 
 describe("L'abonnement qui consigne (dans le journal MSS) la connexion d'un utilisateur", () => {
-  let adaptateurJournal;
+  let adaptateurJournal: AdaptateurJournalMSS;
 
   beforeEach(() => {
     adaptateurJournal = AdaptateurJournalMSSMemoire.nouvelAdaptateur();
   });
 
   it('consigne un événement de "connexion utilisateur"', async () => {
-    let evenementRecu = {};
+    let evenementRecu: EvenementJournal;
     adaptateurJournal.consigneEvenement = async (evenement) => {
       evenementRecu = evenement;
     };
 
     await consigneConnexionUtilisateurDansJournal({ adaptateurJournal })({
-      idUtilisateur: '123',
+      idUtilisateur: unUUID('1'),
       dateDerniereConnexion: '2022-09-02',
+      source: SourceAuthentification.MSS,
+      connexionAvecMFA: false,
     });
 
-    expect(evenementRecu.type).to.be('CONNEXION_UTILISATEUR');
+    expect(evenementRecu!.type).toBe('CONNEXION_UTILISATEUR');
   });
 });
