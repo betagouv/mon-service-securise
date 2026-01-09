6d345f3a3e83976a9c7c425009963d789e8f7c96
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-01-09 15:43:19 +0100

    Supprime la lecture de Excel de téléversement v1
    … car seule la v2 est désormais utile en PROD.

--- src/adaptateurs/adaptateurTeleversementServices.xls.js
+++ src/adaptateurs/adaptateurTeleversementServices.xls.js
@@ -2,36 +2,6 @@ import { ErreurFichierXlsInvalide } from '../erreurs.js';
 import { chaineDateFrEnChaineDateISO } from '../utilitaires/date.js';
 import { LecteurExcel } from './excel/LecteurExcel.js';
 
-const ENTETE_NOM = 'Nom du service numérique *';
-const ENTETE_SIRET = "Siret de l'organisation *";
-const ENTETE_NB_ORGANISATIONS_UTILISATRICES =
-  "Nombre d'organisations utilisatrices *";
-const ENTETE_TYPE = 'Type(s) *';
-const ENTETE_PROVENANCE = 'Provenance *';
-const ENTETE_STATUT = 'Statut *';
-const ENTETE_LOCALISATION = 'Localisation des données *';
-const ENTETE_DELAI_AVANT_IMPACT_CRITIQUE =
-  'Estimation de la durée maximale acceptable de dysfonctionnement grave du service *';
-const ENTETE_DATE_HOMOLOGATION = "Date d'homologation";
-const ENTETE_DUREE_HOMOLOGATION = "Durée d'homologation";
-const ENTETE_NOM_AUTORITE = 'Autorité\nNom Prénom';
-const ENTETE_FONCTION_AUTORITE = 'Autorité\nFonction';
-
-const toutesLesEntetesNecessaires = [
-  ENTETE_NOM,
-  ENTETE_SIRET,
-  ENTETE_NB_ORGANISATIONS_UTILISATRICES,
-  ENTETE_TYPE,
-  ENTETE_PROVENANCE,
-  ENTETE_STATUT,
-  ENTETE_LOCALISATION,
-  ENTETE_DELAI_AVANT_IMPACT_CRITIQUE,
-  ENTETE_DATE_HOMOLOGATION,
-  ENTETE_DUREE_HOMOLOGATION,
-  ENTETE_NOM_AUTORITE,
-  ENTETE_FONCTION_AUTORITE,
-];
-
 const ENTETE_V2_NOM = 'Nom du service numérique *';
 const ENTETE_V2_SIRET = "Siret de l'organisation *";
 const ENTETE_V2_STATUT = 'Statut *';
@@ -72,38 +42,6 @@ const toutesLesEntetesV2Necessaires = [
   ENTETE_V2_NOM_AUTORITE,
   ENTETE_V2_FONCTION_AUTORITE,
 ];
-
-const extraisTeleversementServices = async (buffer) => {
-  const lecteur = new LecteurExcel(buffer);
-  const donneesBrutes = lecteur.donneesDeFeuille('Template services', 6);
-
-  const toutesLignesValides = lecteur.lesLignesSontCompletes(
-    donneesBrutes,
-    toutesLesEntetesNecessaires
-  );
-
-  if (donneesBrutes.length > 250 || !toutesLignesValides) {
-    throw new ErreurFichierXlsInvalide();
-  }
-
-  return donneesBrutes.map((service) => ({
-    nom: service[ENTETE_NOM],
-    siret: service[ENTETE_SIRET],
-    nombreOrganisationsUtilisatrices:
-      service[ENTETE_NB_ORGANISATIONS_UTILISATRICES],
-    type: service[ENTETE_TYPE],
-    provenance: service[ENTETE_PROVENANCE],
-    statut: service[ENTETE_STATUT],
-    localisation: service[ENTETE_LOCALISATION],
-    delaiAvantImpactCritique: service[ENTETE_DELAI_AVANT_IMPACT_CRITIQUE],
-    dateHomologation: service[ENTETE_DATE_HOMOLOGATION]
-      ? chaineDateFrEnChaineDateISO(service[ENTETE_DATE_HOMOLOGATION])
-      : undefined,
-    dureeHomologation: service[ENTETE_DUREE_HOMOLOGATION],
-    nomAutoriteHomologation: service[ENTETE_NOM_AUTORITE],
-    fonctionAutoriteHomologation: service[ENTETE_FONCTION_AUTORITE],
-  }));
-};
 
 const extraisTeleversementServicesV2 = async (buffer) => {
   const lecteur = new LecteurExcel(buffer);
@@ -146,4 +84,4 @@ const extraisTeleversementServicesV2 = async (buffer) => {
   }));
 };
 
-export { extraisTeleversementServices, extraisTeleversementServicesV2 };
+export { extraisTeleversementServicesV2 };
