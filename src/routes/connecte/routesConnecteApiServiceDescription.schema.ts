d3a7994dc25ceaf8b7b7928fed91f029e8c9e9f2
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-01-21 14:19:59 +0100

    Utilise zod pour valider la route `POST /estimationNiveauSecurite`

--- src/routes/connecte/routesConnecteApiServiceDescription.schema.ts
+++ src/routes/connecte/routesConnecteApiServiceDescription.schema.ts
@@ -21,3 +21,26 @@ export const schemaPutDescriptionService = (referentielV2: ReferentielV2) => ({
   statutDeploiement: schemaService.statutDeploiement(referentielV2),
   typeService: schemaService.typeService(referentielV2),
 });
+
+export const schemaPostEstimationNiveauSecurite = (
+  referentielV2: ReferentielV2
+) => ({
+  delaiAvantImpactCritique:
+    schemaService.delaiAvantImpactCritique(referentielV2),
+  donneesCaracterePersonnel:
+    schemaService.donneesCaracterePersonnel(referentielV2),
+  donneesSensiblesSpecifiques: schemaService.donneesSensiblesSpecifiques(),
+  fonctionnalites: schemaService.fonctionnalites(referentielV2),
+  fonctionnalitesSpecifiques: schemaService.fonctionnalitesSpecifiques(),
+  localisationDonnees: schemaService.localisationDonnees(referentielV2),
+  niveauSecurite: schemaService.niveauSecurite(referentielV2),
+  nomService: schemaService.nomService(),
+  nombreOrganisationsUtilisatrices:
+    schemaService.nombreOrganisationsUtilisatrices(),
+  organisationResponsable: schemaService.organisationResponsable(),
+  pointsAcces: schemaService.pointsAcces(),
+  presentation: schemaService.presentation(),
+  provenanceService: schemaService.provenanceService(referentielV2),
+  statutDeploiement: schemaService.statutDeploiement(referentielV2),
+  typeService: schemaService.typeService(referentielV2),
+});
