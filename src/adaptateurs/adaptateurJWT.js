db28ca4865059d223cc85c40cbc5cbd1612eb25b
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-01-23 15:52:04 +0100

    [JWT] Émets un JWT valide jusqu'à la fin du jour
    … au lieu d'avoir une validité d'une heure.
    Voir le commit JWT précédent qui explique la raison d'avoir un seul JWT par jour.

--- src/adaptateurs/adaptateurJWT.js
+++ src/adaptateurs/adaptateurJWT.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { ErreurJWTInvalide, ErreurJWTManquant } from '../erreurs.js';
 import * as environnement from './adaptateurEnvironnement.js';
+import { secondesJusqua23h59m59s } from '../utilitaires/date.js';
 
 const adaptateurJWT = ({ adaptateurEnvironnement }) => {
   const secret = adaptateurEnvironnement.JWT().secret();
@@ -18,7 +19,9 @@ const adaptateurJWT = ({ adaptateurEnvironnement }) => {
   };
 
   const signeDonnees = (donnees) =>
-    jwt.sign(donnees, secret, { expiresIn: '1h' });
+    jwt.sign(donnees, secret, {
+      expiresIn: secondesJusqua23h59m59s(new Date()),
+    });
 
   const genereToken = (idUtilisateur, source, estInvite) =>
     signeDonnees({ idUtilisateur, source, estInvite });
