40dea9a257112cfe417df579f8657a389483ddb3
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-02-09 10:28:28 +0100

    [SOIN] Extrait un composant pour le "Cartouche" de la thématique de mesure

---
+++ svelte/lib/ui/CartoucheThematique.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let thematique: string;
+</script>
+
+<span class="cartouche">{thematique}</span>
+
+<style lang="scss">
+  .cartouche {
+    padding: 1px 8px 3px;
+    font-size: 0.9em;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: fit-content;
+    background: #eee;
+    color: #161616;
+    font-weight: 400;
+    white-space: nowrap;
+  }
+</style>
