12aea0749c2712efc241d2f461a67ef8aacb7f52
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-02-16 11:51:08 +0100

    Génère l'intitulé d'un risque
    ... en fonction de son vecteur de risque et des objectifs visés.

--- src/moteurRisques/v2/risqueV2.ts
+++ src/moteurRisques/v2/risqueV2.ts
@@ -2,13 +2,39 @@ import { IdVecteurRisque } from './selectionVecteurs.types.js';
 import { IdObjectifVise } from './selectionObjectifsVises.types.js';
 import { Gravite } from './graviteObjectifsVises.js';
 
+export type ConfigurationRisqueV2 = Record<
+  IdVecteurRisque,
+  {
+    intitule: string;
+    intitulesObjectifsVises: Partial<Record<IdObjectifVise, string>>;
+  }
+>;
+
 export class RisqueV2 {
   gravite: Gravite;
+  intitule: string;
 
   constructor(
     private readonly idVecteur: IdVecteurRisque,
-    private readonly objectifsVises: Partial<Record<IdObjectifVise, Gravite>>
+    private readonly objectifsVises: Partial<Record<IdObjectifVise, Gravite>>,
+    private readonly configuration: ConfigurationRisqueV2
   ) {
     this.gravite = Math.max(...Object.values(objectifsVises)) as Gravite;
+    this.intitule = this.genereIntitule();
+  }
+
+  private genereIntitule() {
+    const formatteurListe = new Intl.ListFormat('fr', {
+      style: 'long',
+      type: 'conjunction',
+    });
+
+    const vecteur = this.configuration[this.idVecteur];
+    const idsOV = Object.keys(this.objectifsVises) as IdObjectifVise[];
+    const intitulesOv = formatteurListe.format(
+      idsOV.map((id) => vecteur.intitulesObjectifsVises[id]!)
+    );
+
+    return `${vecteur.intitule} ${intitulesOv}`;
   }
 }
--- test/moteurRisques/v2/risqueV2.spec.ts
+++ test/moteurRisques/v2/risqueV2.spec.ts
@@ -1,17 +1,93 @@
-import { RisqueV2 } from '../../../src/moteurRisques/v2/risqueV2.ts';
+import {
+  ConfigurationRisqueV2,
+  RisqueV2,
+} from '../../../src/moteurRisques/v2/risqueV2.ts';
 
 describe('Un risque V2', () => {
+  const intitulesOVs = () => ({
+    OV1: 'avec OV1',
+    OV2: 'avec OV2',
+    OV3: 'avec OV3',
+    OV4: 'avec OV4',
+  });
+
+  const configurationRisque = (
+    surchargeVecteur?: Partial<ConfigurationRisqueV2>
+  ): ConfigurationRisqueV2 => ({
+    V1: { intitule: 'V1', intitulesObjectifsVises: intitulesOVs() },
+    V2: { intitule: 'V2', intitulesObjectifsVises: intitulesOVs() },
+    V3: { intitule: 'V3', intitulesObjectifsVises: intitulesOVs() },
+    V4: { intitule: 'V4', intitulesObjectifsVises: intitulesOVs() },
+    V5: { intitule: 'V5', intitulesObjectifsVises: intitulesOVs() },
+    V6: { intitule: 'V6', intitulesObjectifsVises: intitulesOVs() },
+    V7: { intitule: 'V7', intitulesObjectifsVises: intitulesOVs() },
+    V8: { intitule: 'V8', intitulesObjectifsVises: intitulesOVs() },
+    V9: { intitule: 'V9', intitulesObjectifsVises: intitulesOVs() },
+    V10: { intitule: 'V10', intitulesObjectifsVises: intitulesOVs() },
+    V11: { intitule: 'V11', intitulesObjectifsVises: intitulesOVs() },
+    V12: { intitule: 'V12', intitulesObjectifsVises: intitulesOVs() },
+    V13: { intitule: 'V13', intitulesObjectifsVises: intitulesOVs() },
+    V14: { intitule: 'V14', intitulesObjectifsVises: intitulesOVs() },
+    ...surchargeVecteur,
+  });
+
   describe('concernant sa gravité', () => {
     it('connait son niveau de gravité', () => {
-      const risque = new RisqueV2('V1', { OV1: 3 });
+      const risque = new RisqueV2('V1', { OV1: 3 }, configurationRisque());
 
       expect(risque.gravite).toBe(3);
     });
 
     it("retourne la gravité maximale de tous les OV (même s'il devrait tous avoir la même valeur)", () => {
-      const risque = new RisqueV2('V1', { OV2: 3, OV3: 3 });
+      const risque = new RisqueV2(
+        'V1',
+        { OV2: 3, OV3: 3 },
+        configurationRisque()
+      );
 
       expect(risque.gravite).toBe(3);
+    });
+  });
+
+  describe('concernant son intitulé', () => {
+    it("joins l'intitulé du vecteur et de l'objectif visé", () => {
+      const configurationV1 = {
+        V1: {
+          intitule: 'Intitulé du risque V1',
+          intitulesObjectifsVises: { OV1: "avec l'OV1" },
+        },
+      };
+
+      const risque = new RisqueV2(
+        'V1',
+        { OV1: 3 },
+        configurationRisque(configurationV1)
+      );
+
+      expect(risque.intitule).toBe("Intitulé du risque V1 avec l'OV1");
+    });
+
+    it('joins les intitulés de tous les objectifs visés avec un connecteur de conjonction', () => {
+      const configurationV1 = {
+        V1: {
+          intitule: 'Intitulé du risque V1',
+          intitulesObjectifsVises: {
+            OV1: "avec l'OV1",
+            OV2: "avec l'OV2",
+            OV3: "avec l'OV3",
+          },
+        },
+      };
+
+      const risque = new RisqueV2(
+        'V1',
+        { OV1: 3, OV2: 3, OV3: 3 },
+        configurationRisque(configurationV1)
+      );
+
+      expect(risque.intitule).toBe(
+        "Intitulé du risque V1 avec l'OV1, avec l'OV2 et avec l'OV3"
+      );
     });
   });
 });
