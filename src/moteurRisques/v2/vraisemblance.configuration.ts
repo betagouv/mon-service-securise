a96694fe30fbaa4bb9d4823f11f89fbb93c8fadd
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-02-17 14:03:46 +0100

    Ajoute une configuration qui regroupe les 14 autres
    ... afin de pouvoir l'utiliser dans le moteur de risques

---
+++ src/moteurRisques/v2/vraisemblance.configuration.ts
@@ -0,0 +1,31 @@
+import { V1 } from './vraisemblance.V1.configuration.js';
+import { V2 } from './vraisemblance.V2.configuration.js';
+import { V3 } from './vraisemblance.V3.configuration.js';
+import { V4 } from './vraisemblance.V4.configuration.js';
+import { V5 } from './vraisemblance.V5.configuration.js';
+import { V6 } from './vraisemblance.V6.configuration.js';
+import { V7 } from './vraisemblance.V7.configuration.js';
+import { V8 } from './vraisemblance.V8.configuration.js';
+import { V9 } from './vraisemblance.V9.configuration.js';
+import { V10 } from './vraisemblance.V10.configuration.js';
+import { V11 } from './vraisemblance.V11.configuration.js';
+import { V12 } from './vraisemblance.V12.configuration.js';
+import { V13 } from './vraisemblance.V13.configuration.js';
+import { V14 } from './vraisemblance.V14.configuration.js';
+
+export const configurationVraisemblance = {
+  V1,
+  V2,
+  V3,
+  V4,
+  V5,
+  V6,
+  V7,
+  V8,
+  V9,
+  V10,
+  V11,
+  V12,
+  V13,
+  V14,
+};
