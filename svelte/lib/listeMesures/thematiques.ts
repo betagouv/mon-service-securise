b794addb8d0f90f192f7b97beaa2c2a051312bf6
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-02-18 11:24:52 +0100

    Ajoute la thématique dans le filtre de la liste de mesures
    ... uniquement pour les mesures V2.

---
+++ svelte/lib/listeMesures/thematiques.ts
@@ -0,0 +1,8 @@
+export const thematiques = [
+  'Gouvernance et gestion des risques',
+  "Gestion de l'écosystème",
+  'Sécurité applicative et protection des données',
+  'Gestion des identités et des accès',
+  'Surveillance, maintien et réponse aux incidents',
+  'Protection des systèmes et réseaux',
+] as const;
