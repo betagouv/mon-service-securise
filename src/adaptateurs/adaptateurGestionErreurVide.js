e77ab11c9da3980e66724171c3197466ed960459
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-02-19 15:30:47 +0100

    [SOIN] Convertis l'adaptateur Gestion Erreur Vide en Typescript

--- src/adaptateurs/adaptateurGestionErreurVide.js
+++
@@ -1,6 +0,0 @@
-export const fabriqueAdaptateurGestionErreurVide = () => ({
-  initialise: () => {},
-  identifieUtilisateur: () => {},
-  controleurErreurs: (erreur, _requete, _reponse, suite) => suite(erreur),
-  logueErreur: (_erreur) => {},
-});
