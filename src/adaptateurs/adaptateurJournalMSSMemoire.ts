fa0c7294fb0d72df9a31191fa5fe2c1f98cdb9d3
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-02-19 15:26:07 +0100

    [SOIN] Convertis l'adaptateur Journal Mémoire en Typescript

--- src/adaptateurs/adaptateurJournalMSSMemoire.ts
+++ src/adaptateurs/adaptateurJournalMSSMemoire.ts
@@ -1,16 +1,18 @@
 import { journalMSS } from './adaptateurEnvironnement.js';
+import {
+  AdaptateurJournalMSS,
+  EvenementJournal,
+} from './adaptateurJournalMSS.interface.js';
 
-const nouvelAdaptateur = () => {
-  const consigneEvenement = (donneesEvenements) => {
+const nouvelAdaptateur = (): AdaptateurJournalMSS => {
+  const consigneEvenement = async (evenement: EvenementJournal) => {
     /* eslint-disable no-console */
     const logDansConsole = journalMSS().logEvenementDansConsole();
     if (logDansConsole)
       console.log(
-        `[JOURNAL MSS] Nouvel événement\n${JSON.stringify(donneesEvenements)}`
+        `[JOURNAL MSS] Nouvel événement\n${JSON.stringify(evenement)}`
       );
     /* eslint-enable no-console */
-
-    return Promise.resolve();
   };
 
   return {
