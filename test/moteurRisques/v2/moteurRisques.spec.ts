670d128ebdd44de0fa8a10d1998bb86b5ec59b8f
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-02-24 16:26:57 +0100

    [SOIN] Met en évidence la réduction de la vraisemblance
    ... afin de pouvoir comparer les risques résiduels et les bruts. On va utiliser le même service et montrer que les risques bruts utilisent des mesures au statut "non fait".

--- test/moteurRisques/v2/moteurRisques.spec.ts
+++ test/moteurRisques/v2/moteurRisques.spec.ts
@@ -1,6 +1,12 @@
-import { uneDescriptionV2Valide } from '../../constructeurs/constructeurDescriptionServiceV2.ts';
+import {
+  uneDescriptionDeNiveauDeSecuriteEstime1,
+  uneDescriptionV2Valide,
+} from '../../constructeurs/constructeurDescriptionServiceV2.ts';
 import { MoteurRisquesV2 } from '../../../src/moteurRisques/v2/moteurRisques.ts';
 import { RisqueV2 } from '../../../src/moteurRisques/v2/risqueV2.ts';
+import MesureGenerale from '../../../src/modeles/mesureGenerale.js';
+import { creeReferentielV2 } from '../../../src/referentielV2.ts';
+import { IdMesureV2 } from '../../../donneesReferentielMesuresV2.ts';
 
 describe('Le moteur de risques V2', () => {
   const uneAPISimple = () =>
@@ -32,11 +38,29 @@ describe('Le moteur de risques V2', () => {
   });
 
   it('sait donner les risques pour le service (en utilisant les vecteurs et objectifs visés pertinents)', () => {
-    const moteur = new MoteurRisquesV2(uneAPISimple(), {});
+    const referentiel = creeReferentielV2();
+    const mesureFaite = (id: IdMesureV2) => ({
+      [id]: new MesureGenerale({ statut: 'fait', id }, referentiel),
+    });
+
+    const moteur = new MoteurRisquesV2(
+      // Un service dont le premier risque est "R3"
+      uneDescriptionDeNiveauDeSecuriteEstime1()
+        .avecNiveauSecurite('niveau1')
+        .construis(),
+      {
+        // La mesure du groupe "g" qui fait baisser la vraisemblance de 1
+        ...mesureFaite('CONTRAT.1'),
+        // Les mesures du groupe "a" qui font baisser la vraisemblance de 2
+        ...mesureFaite('MCO_MCS.14'),
+        ...mesureFaite('MCO_MCS.5'),
+        ...mesureFaite('MCO_MCS.6'),
+      }
+    );
 
     const risques = moteur.risques();
 
     expect(risques).toBeInstanceOf(Array<RisqueV2>);
-    expect(risques[0].vraisemblance).toBe(4);
+    expect(risques[0].vraisemblance).toBe(1);
   });
 });
