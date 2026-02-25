9d091c402311cb9209dbe400316deb5803890229
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-02-25 17:14:56 +0100

    Créer la matrice (vide) des risques

---
+++ svelte/lib/risquesV2/MatriceRisquesV2.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+</script>
+
+<table class="relatif">
+  <caption class="legende"><span>Gravité</span></caption>
+  <caption class="legende bas-droite"><span>Vraisemblance</span></caption>
+  <tbody>
+    {#each new Array(4).fill(0) as _ligne, indexLigne (indexLigne)}
+      {@const gravite = indexLigne * -1 + 4}
+      <tr>
+        <th scope="row">
+          <span>{indexLigne * -1 + 4}</span>
+        </th>
+        {#each new Array(4).fill(0) as _colonne, indexColonne (indexColonne)}
+          {@const vraisemblance = indexColonne + 1}
+          {@const niveauRisque = gravite * vraisemblance}
+          {@const couleur =
+            niveauRisque <= 4 && vraisemblance < 3 && gravite < 4
+              ? 'vert'
+              : niveauRisque >= 8 && vraisemblance > 2 && gravite >= 2
+              ? 'rouge'
+              : 'orange'}
+          <td>
+            <div class="contenu-cellule {couleur}">
+              <span></span>
+            </div>
+          </td>
+        {/each}
+      </tr>
+    {/each}
+    <tr class="legende-x">
+      <th aria-hidden="true"></th>
+      {#each new Array(4).fill(0) as _colonne, indexColonne (indexColonne)}
+        <th scope="col">
+          <span>{indexColonne + 1}</span>
+        </th>
+      {/each}
+    </tr>
+  </tbody>
+</table>
+
+<style lang="scss">
+  table {
+    border-collapse: collapse;
+    --bordure: #929292;
+    --rouge: #e1000f;
+    --orange: #fa7a35;
+    --vert: #77b645;
+
+    th {
+      color: #929292;
+      font-size: 1rem;
+      font-weight: 400;
+      line-height: 1.5rem;
+
+      span {
+        margin-right: 10px;
+      }
+    }
+
+    tr {
+      td {
+        padding: 2.5px;
+        border-top: 2px dashed var(--bordure);
+
+        &:first-of-type {
+          border-left: 2px solid var(--bordure);
+          padding-left: 6px;
+        }
+
+        .contenu-cellule {
+          width: 123px;
+          height: 72px;
+
+          &.vert {
+            background: var(--vert);
+          }
+
+          &.orange {
+            background: var(--orange);
+          }
+
+          &.rouge {
+            background: var(--rouge);
+          }
+        }
+      }
+    }
+
+    .legende-x {
+      th:not([aria-hidden='true']) {
+        border-top: 2px solid var(--bordure);
+      }
+    }
+  }
+
+  .legende {
+    position: absolute;
+    top: 12px;
+    left: 12px;
+    transform: translateX(-100%) translateY(-100%);
+    color: #929292;
+    font-size: 1rem;
+    line-height: 1.5rem;
+
+    &.bas-droite {
+      top: unset;
+      left: unset;
+      bottom: 0;
+      right: 12px;
+      transform: translateX(100%);
+    }
+  }
+
+  .relatif {
+    position: relative;
+  }
+</style>
--- svelte/lib/risquesV2/RisquesV2.svelte
+++ svelte/lib/risquesV2/RisquesV2.svelte
@@ -1,5 +1,40 @@
 <script lang="ts">
-  export let idService: string;
+  import MatriceRisquesV2 from './MatriceRisquesV2.svelte';
 </script>
 
-<h1>{idService}</h1>
+<div class="conteneur">
+  <div class="entete">
+    <h2>Cartographie des risques usuels</h2>
+  </div>
+  <div class="conteneur-matrice">
+    <MatriceRisquesV2 />
+  </div>
+</div>
+
+<style lang="scss">
+  .conteneur {
+    box-shadow: 0 2px 6px 0 rgba(0, 0, 18, 0.16);
+    display: flex;
+    flex-direction: column;
+
+    h2 {
+      width: fit-content;
+      margin: 0;
+      color: #161616;
+      font-size: 1.25rem;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 1.75rem;
+    }
+
+    .entete {
+      padding: 1rem;
+    }
+
+    .conteneur-matrice {
+      padding: 1rem;
+      display: flex;
+      justify-content: center;
+    }
+  }
+</style>
