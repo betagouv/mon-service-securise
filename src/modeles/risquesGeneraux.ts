76170d196c90b36d95034f6ece15f18b4516c315
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-02-25 11:05:20 +0100

    [SOIN] Convertis le modèle "RisquesGeneraux" en Typescript

---
+++ src/modeles/risquesGeneraux.ts
@@ -0,0 +1,16 @@
+import ListeRisques from './listeRisques.js';
+import RisqueGeneral, { DonneesRisqueGeneral } from './risqueGeneral.js';
+import { Referentiel } from '../referentiel.interface.js';
+
+type DonneesRisquesGeneraux = {
+  risquesGeneraux: DonneesRisqueGeneral[];
+};
+
+class RisquesGeneraux extends ListeRisques {
+  constructor(donnees: DonneesRisquesGeneraux, referentiel: Referentiel) {
+    const { risquesGeneraux } = donnees;
+    super(RisqueGeneral, { items: risquesGeneraux }, referentiel);
+  }
+}
+
+export default RisquesGeneraux;
