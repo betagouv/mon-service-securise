c1be159b9ccce3f6b6576fab418bb90851d82df9
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-02-25 11:03:42 +0100

    [SOIN] Convertis le modèle "ListeRisques" en Typescript

--- src/modeles/listeRisques.ts
+++ src/modeles/listeRisques.ts
@@ -1,39 +1,45 @@
 import ElementsConstructibles from './elementsConstructibles.js';
 import { ErreurRisqueInconnu } from '../erreurs.js';
+import Risque from './risque.js';
+import { IdNiveauGravite } from './niveauGravite.js';
 
-class ListeRisques extends ElementsConstructibles {
+class ListeRisques extends ElementsConstructibles<Risque> {
   principaux() {
     return this.items.filter((risque) => risque.important());
   }
 
   parNiveauGravite(accumulateur = {}) {
-    return this.tous().reduce((acc, risque) => {
-      acc[risque.niveauGravite] ||= [];
-      acc[risque.niveauGravite].push(risque.toJSON());
-      return acc;
-    }, accumulateur);
+    return this.tous().reduce(
+      (acc, risque) => {
+        // eslint-disable-next-line no-param-reassign
+        acc[risque.niveauGravite] ||= [];
+        acc[risque.niveauGravite].push(risque.toJSON());
+        return acc;
+      },
+      accumulateur as Record<IdNiveauGravite, Array<object>>
+    );
   }
 
-  ajouteRisque(risque) {
+  ajouteRisque(risque: Risque) {
     this.items.push(risque);
   }
 
-  supprimeRisque(idRisque) {
+  supprimeRisque(idRisque: string) {
     this.items = this.items.filter((r) => r.id !== idRisque);
   }
 
-  trouveIndex(idRisque) {
+  trouveIndex(idRisque: string) {
     const index = this.items.findIndex((r) => r.id === idRisque);
     if (index === -1)
       throw new ErreurRisqueInconnu(`Le risque "${idRisque}" est introuvable.`);
     return index;
   }
 
-  trouveParId(idRisque) {
+  trouveParId(idRisque: string) {
     return this.items[this.trouveIndex(idRisque)];
   }
 
-  metsAJourRisque(risque) {
+  metsAJourRisque(risque: Risque) {
     const index = this.trouveIndex(risque.id);
     this.items[index] = risque;
   }
