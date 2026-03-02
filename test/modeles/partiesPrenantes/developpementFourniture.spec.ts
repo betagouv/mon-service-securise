8dfaaeb0d4eb86f04e7ad52a890fa9f3130c9e65
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-03-02 12:08:07 +0100

    [SOIN] Convertis le modèle "DeveloppementFourniture" en Typescript

--- test/modeles/partiesPrenantes/developpementFourniture.spec.ts
+++ test/modeles/partiesPrenantes/developpementFourniture.spec.ts
@@ -1,10 +1,9 @@
-import expect from 'expect.js';
 import DeveloppementFourniture from '../../../src/modeles/partiesPrenantes/developpementFourniture.js';
 
 describe('Un développement / fourniture', () => {
   it('sait se décrire en JSON', () => {
     const developpementFourniture = new DeveloppementFourniture({ nom: 'Mss' });
-    expect(developpementFourniture.toJSON()).to.eql({
+    expect(developpementFourniture.toJSON()).toEqual({
       type: 'DeveloppementFourniture',
       nom: 'Mss',
     });
