21bb56e7f0e7ee85acc0f899639e42472c27f2d1
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-03-02 12:08:54 +0100

    [SOIN] Convertis le modèle "Hebergement" en Typescript

--- test/modeles/partiesPrenantes/hebergement.spec.ts
+++ test/modeles/partiesPrenantes/hebergement.spec.ts
@@ -1,10 +1,9 @@
-import expect from 'expect.js';
 import Hebergement from '../../../src/modeles/partiesPrenantes/hebergement.js';
 
 describe('Un hébergement', () => {
   it('sait se décrire en JSON', () => {
     const hebergement = new Hebergement({ nom: 'hébergeur' });
-    expect(hebergement.toJSON()).to.eql({
+    expect(hebergement.toJSON()).toEqual({
       type: 'Hebergement',
       nom: 'hébergeur',
     });
