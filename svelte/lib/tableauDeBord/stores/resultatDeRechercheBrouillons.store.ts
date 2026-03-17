437567a4f3f91e4e35633976eb0d507cc282532f
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-03-17 10:01:14 +0100

    N'applique pas les filtres et "onglets" aux brouillons
    ... car ils ne doivent apparaitre que sur l'onglet "Tous", sans filtre

--- svelte/lib/tableauDeBord/stores/resultatDeRechercheBrouillons.store.ts
+++ svelte/lib/tableauDeBord/stores/resultatDeRechercheBrouillons.store.ts
@@ -2,13 +2,38 @@ import { derived } from 'svelte/store';
 import { rechercheTextuelle } from './rechercheTextuelle.store';
 import type { BrouillonService } from '../tableauDeBord.d';
 import { brouillonsService } from './brouillonsService.store';
+import { filtrageServices } from './filtrageServices.store';
+import { affichageParStatutHomologationSelectionne } from './affichageParStatutHomologation';
 
 export const resultatsDeRechercheBrouillons = derived<
-  [typeof brouillonsService, typeof rechercheTextuelle],
+  [
+    typeof brouillonsService,
+    typeof rechercheTextuelle,
+    typeof filtrageServices,
+    typeof affichageParStatutHomologationSelectionne,
+  ],
   BrouillonService[]
 >(
-  [brouillonsService, rechercheTextuelle],
-  ([$brouillonsService, $rechercheTextuelle]) => {
+  [
+    brouillonsService,
+    rechercheTextuelle,
+    filtrageServices,
+    affichageParStatutHomologationSelectionne,
+  ],
+  ([
+    $brouillonsService,
+    $rechercheTextuelle,
+    $filtrageServices,
+    $affichageParStatutHomologationSelectionne,
+  ]) => {
+    if (
+      $filtrageServices.indiceCyber.length > 0 ||
+      $filtrageServices.propriete.length > 0 ||
+      $filtrageServices.niveauSecurite.length > 0 ||
+      $filtrageServices.completude.length > 0 ||
+      $affichageParStatutHomologationSelectionne !== 'tous'
+    )
+      return [];
     return $brouillonsService.filter((brouillon) =>
       brouillon.nomService
         .toLowerCase()
