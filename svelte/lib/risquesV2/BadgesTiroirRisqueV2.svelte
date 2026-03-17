96a82f7e36d9429cf3da20c115034906497c2f19
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-03-17 14:55:10 +0100

    Permet de passer des props au composant de l'entête

--- svelte/lib/risquesV2/BadgesTiroirRisqueV2.svelte
+++ svelte/lib/risquesV2/BadgesTiroirRisqueV2.svelte
@@ -1,1 +1,20 @@
-<span>Un sous texte</span>
+<script lang="ts">
+  import CartoucheIdentifiantRisque from './CartoucheIdentifiantRisque.svelte';
+  import type { Risque } from './risquesV2.d';
+
+  interface Props {
+    risque: Risque;
+  }
+
+  let { risque }: Props = $props();
+</script>
+
+<div>
+  <CartoucheIdentifiantRisque {risque} />
+</div>
+
+<style lang="scss">
+  div {
+    margin-top: 16px;
+  }
+</style>
