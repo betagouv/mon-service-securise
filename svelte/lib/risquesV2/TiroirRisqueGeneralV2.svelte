6f0b20e55458bb3834bf14c136bfe4dc24a92684
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-03-17 16:04:27 +0100

    Mets en forme le contenu du tiroir de risque v2

--- svelte/lib/risquesV2/TiroirRisqueGeneralV2.svelte
+++ svelte/lib/risquesV2/TiroirRisqueGeneralV2.svelte
@@ -31,56 +31,58 @@
 </script>
 
 <ContenuTiroir>
-  <Onglets
-    bind:ongletActif
-    onglets={[
-      {
-        id: 'infos',
-        label: 'Information sur le risque',
-      },
-      {
-        id: 'mesuresAssociees',
-        label: 'Mesures associées',
-      },
-    ]}
-  />
-  {#if ongletActif === 'infos'}
-    <div>
-      <p>
-        <b>Description du risque</b>
-        <br />
-        <span><i>Temporaire: Lorem Ipsum</i></span>
-      </p>
-      <p>
-        <b>Exemple de service numérique</b>
-        <br />
-        <span><i>Temporaire: Lorem Ipsum</i></span>
-      </p>
+  <div class="conteneur-onglets">
+    <Onglets
+      bind:ongletActif
+      onglets={[
+        {
+          id: 'infos',
+          label: 'Information sur le risque',
+        },
+        {
+          id: 'mesuresAssociees',
+          label: 'Mesures associées',
+        },
+      ]}
+    />
+  </div>
+  <div class="contenu-onglet">
+    {#if ongletActif === 'infos'}
       <div>
-        <div class="ligne-niveau-risque">
-          <span><b>Gravité potentielle :</b></span>
-          <span>{mappingNiveauGravite[risque.gravite]}</span>
+        <p>
+          <b>Description du risque</b>
+          <span><i>Temporaire: Lorem Ipsum</i></span>
+        </p>
+        <p>
+          <b>Exemple de service numérique</b>
+          <span><i>Temporaire: Lorem Ipsum</i></span>
+        </p>
+        <div class="niveaux-risque">
+          <div class="ligne-niveau-risque">
+            <span><b>Gravité potentielle :</b></span>
+            <span>{mappingNiveauGravite[risque.gravite]}</span>
+          </div>
+          <div class="ligne-niveau-risque">
+            <span><b>Vraisemblance au départ :</b></span>
+            <span>{mappingNiveauVraisemblance[risqueBrut.vraisemblance]}</span>
+          </div>
         </div>
-        <div class="ligne-niveau-risque">
-          <span><b>Vraisemblance au départ :</b></span>
-          <span>{mappingNiveauVraisemblance[risqueBrut.vraisemblance]}</span>
+        <div>
+          <label for="commentaire"><b>Commentaire</b></label>
+          <dsfr-input
+            type="text"
+            id="commentaire"
+            nom="commentaire"
+            value={commentaire}
+            placeholder="Apportez des précisions sur le risque"
+            onvaluechanged={metsAJourCommentaire}
+          ></dsfr-input>
         </div>
       </div>
-      <div>
-        <label for="commentaire"><b>Commentaire</b></label>
-        <dsfr-input
-          type="text"
-          id="commentaire"
-          nom="commentaire"
-          value={commentaire}
-          placeholder="Apportez des précisions sur le risque"
-          onvaluechanged={metsAJourCommentaire}
-        ></dsfr-input>
-      </div>
-    </div>
-  {:else if ongletActif === 'mesuresAssociees'}
-    <p>Mesures</p>
-  {/if}
+    {:else if ongletActif === 'mesuresAssociees'}
+      <p>Mesures</p>
+    {/if}
+  </div>
 </ContenuTiroir>
 <ActionsTiroir>
   <div class="actions">
@@ -97,13 +99,52 @@
 </ActionsTiroir>
 
 <style lang="scss">
-  .ligne-niveau-risque {
-    display: flex;
-  }
-
   .actions {
     display: flex;
     gap: 24px;
     align-items: center;
+  }
+
+  .contenu-onglet {
+    padding: 32px;
+    border: 1px solid #ddd;
+    margin-top: -30px;
+    border-top: none;
+
+    label {
+      margin-bottom: 8px;
+      display: block;
+    }
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+    }
+    p {
+      margin: 0;
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+    }
+
+    .niveaux-risque {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+
+      .ligne-niveau-risque {
+        display: flex;
+
+        & > * {
+          flex: 1;
+        }
+      }
+    }
+  }
+
+  .conteneur-onglets {
+    border-bottom: 1px solid #ddd;
+    padding-left: 16px;
   }
 </style>
