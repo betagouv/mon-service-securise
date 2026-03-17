bdf9a484dac8f4e548ebfe739f15d2c706642808
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-03-17 15:01:24 +0100

    Ajoute les badges dans l'entête du tiroir de risques

--- svelte/lib/risquesV2/BadgesTiroirRisqueV2.svelte
+++ svelte/lib/risquesV2/BadgesTiroirRisqueV2.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import CartoucheIdentifiantRisque from './CartoucheIdentifiantRisque.svelte';
   import type { Risque } from './risquesV2.d';
+  import CartouchesRisqueV2 from './CartouchesRisqueV2.svelte';
 
   interface Props {
     risque: Risque;
@@ -11,10 +12,14 @@
 
 <div>
   <CartoucheIdentifiantRisque {risque} />
+  <CartouchesRisqueV2 {risque} />
 </div>
 
 <style lang="scss">
   div {
+    display: flex;
+    align-items: center;
+    gap: 8px;
     margin-top: 16px;
   }
 </style>
