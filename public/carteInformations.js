06dcc1bd4540aaf03c141bb23938b1d544e6af0f
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-03-18 15:40:58 +0100

    [SOIN] Supprime du code mort

--- public/carteInformations.js
+++
@@ -1,8 +0,0 @@
-$(() => {
-  $('.carte .details').hide();
-  $('.carte .moins-details').hide();
-  $('.carte .plus-details, .carte .moins-details').on('click', (e) => {
-    $(e.target).parent().find('.details').toggle();
-    $(e.target).parent().find('.plus-details, .moins-details').toggle();
-  });
-});
