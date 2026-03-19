0a9564dda4a81752bedf68e9fe81b860c22c7186
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-03-19 10:19:30 +0100

    Ajoute les 3 matrices à la modale

--- svelte/lib/risquesV2/ModaleCartographies.svelte
+++ svelte/lib/risquesV2/ModaleCartographies.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
   import Modale from '../ui/Modale.svelte';
+  import MatriceRisquesV2 from './MatriceRisquesV2.svelte';
+  import type { TousRisques } from './risquesV2.d';
   let elementModale: Modale | undefined;
+
+  interface Props {
+    risques: TousRisques;
+  }
+
+  let { risques }: Props = $props();
 
   export const affiche = () => {
     elementModale?.affiche();
   };
 </script>
 
-<Modale id="modale-confirmation-fermeture" bind:this={elementModale}>
+<Modale id="modale-cartographies-risques-v2" bind:this={elementModale}>
   {#snippet contenu()}
-    <div class="contenu-modale">Hello</div>
+    <h4>Cartographie des risques usuels</h4>
+    <div class="contenu-modale">
+      <div class="conteneur-matrices">
+        <div class="conteneur-matrice">
+          <MatriceRisquesV2 risques={risques.risquesBruts} taille="sm" />
+        </div>
+        <div class="conteneur-matrice">
+          <MatriceRisquesV2 risques={risques.risques} taille="sm" />
+        </div>
+        <div class="conteneur-matrice">
+          <MatriceRisquesV2 risques={risques.risquesCibles} taille="sm" />
+        </div>
+      </div>
+    </div>
   {/snippet}
   {#snippet actions()}
     <!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
@@ -23,4 +44,24 @@
 </Modale>
 
 <style lang="scss">
+  :global(#modale-cartographies-risques-v2) {
+    max-width: 1400px;
+    max-height: 800px;
+    width: calc(100% - 48px);
+
+    :global(.contenu-modale) {
+      margin-top: 0;
+    }
+  }
+
+  .conteneur-matrices {
+    text-align: center;
+    display: flex;
+    gap: 155px;
+    padding-left: 45px;
+  }
+
+  .conteneur-matrice {
+    max-width: 574px;
+  }
 </style>
