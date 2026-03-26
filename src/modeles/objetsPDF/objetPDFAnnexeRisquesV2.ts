ce373bf383add6e246a93474264fd57345be0afd
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-03-26 16:21:49 +0100

    Implémente l'objet de données pour l'annexe pdf des risques v2

--- src/modeles/objetsPDF/objetPDFAnnexeRisquesV2.ts
+++ src/modeles/objetsPDF/objetPDFAnnexeRisquesV2.ts
@@ -1,24 +1,34 @@
 import type Service from '../service.js';
-import { ReferentielV2 } from '../../referentiel.interface.js';
 import { RisquesV2 } from '../../moteurRisques/v2/risquesV2.js';
+import { RisqueV2 } from '../../moteurRisques/v2/risqueV2.js';
 
-class ObjetPDFAnnexeRisques {
-  private readonly referentiel: ReferentielV2;
+export class ObjetPDFAnnexeRisquesV2 {
   private readonly service: Service;
   private readonly risques: RisquesV2;
 
-  constructor(service: Service, referentiel: ReferentielV2) {
-    this.referentiel = referentiel;
+  constructor(service: Service) {
     this.service = service;
     this.risques = this.service.risquesV2!;
   }
 
   donnees() {
+    const { risques, risquesSpecifiques, risquesBruts, risquesCibles } =
+      this.risques.toJSON();
+
+    function sansRisquesDesactives(
+      risquesAFiltrer: ReturnType<InstanceType<typeof RisqueV2>['toJSON']>[]
+    ) {
+      return risquesAFiltrer.filter((r) => !r.desactive);
+    }
+
     return {
       nomService: this.service.nomService(),
-      risques: this.risques.toJSON(),
+      risques: {
+        risques: sansRisquesDesactives(risques),
+        risquesBruts: sansRisquesDesactives(risquesBruts),
+        risquesCibles: sansRisquesDesactives(risquesCibles),
+        risquesSpecifiques,
+      },
     };
   }
 }
-
-export default ObjetPDFAnnexeRisques;
