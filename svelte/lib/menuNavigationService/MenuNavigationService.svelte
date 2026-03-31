cbcf8eca5965285110ed14d2aae3294aafa9866f
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-03-31 10:04:34 +0200

    Ajoute le bouton de gestion des contributeurs

--- svelte/lib/menuNavigationService/MenuNavigationService.svelte
+++ svelte/lib/menuNavigationService/MenuNavigationService.svelte
@@ -5,13 +5,17 @@
     type DonneesServicePourTelechargementDocuments,
   } from '../ui/tiroirs/TiroirTelechargementDocumentsService.svelte';
   import { onMount } from 'svelte';
+  import TiroirGestionContributeurs from '../ui/tiroirs/TiroirGestionContributeurs.svelte';
+  import type { DonneesServicePourTiroirContributeurs } from '../gestionContributeurs/gestionContributeurs.d';
 
   let { visible, etapeActive, idService }: MenuNavigationServiceProps =
     $props();
 
-  let service: DonneesServicePourTelechargementDocuments;
+  type ServicePourMenuNavigation = DonneesServicePourTelechargementDocuments &
+    DonneesServicePourTiroirContributeurs;
+  let service: ServicePourMenuNavigation;
   onMount(async () => {
-    const reponse = await axios.get<DonneesServicePourTelechargementDocuments>(
+    const reponse = await axios.get<ServicePourMenuNavigation>(
       `/api/service/${idService}`
     );
     service = reponse.data;
@@ -19,80 +23,97 @@
 </script>
 
 <div class="menu-navigation-service">
-  <ul>
-    {#if visible.mesures}
+  <div class="conteneur-menu-navigation-service">
+    <ul>
+      {#if visible.mesures}
+        <li>
+          <a
+            class:actif={etapeActive === 'mesures'}
+            class="lien-navigation"
+            href="/service/{idService}/mesures"
+          >
+            <lab-anssi-icone nom="list-unordered" taille="sm"></lab-anssi-icone>
+            Sécuriser
+          </a>
+        </li>
+      {/if}
+      {#if visible.dossiers}
+        <li>
+          <a
+            class:actif={etapeActive === 'dossiers'}
+            class="lien-navigation"
+            href="/service/{idService}/dossiers"
+          >
+            <lab-anssi-icone nom="award-line" taille="sm"></lab-anssi-icone>
+            Homologuer
+          </a>
+        </li>
+      {/if}
+      {#if visible.risques}
+        <li>
+          <a
+            class:actif={etapeActive === 'risques'}
+            class="lien-navigation"
+            href="/service/{idService}/risques"
+          >
+            <lab-anssi-icone nom="warning-line" taille="sm"></lab-anssi-icone>
+            Risques
+          </a>
+        </li>
+      {/if}
+      {#if visible.contactsUtiles}
+        <li>
+          <a
+            class:actif={etapeActive === 'contactsUtiles'}
+            class="lien-navigation"
+            href="/service/{idService}/rolesResponsabilites"
+          >
+            <lab-anssi-icone nom="mail-line" taille="sm"></lab-anssi-icone>
+            Contacts utiles
+          </a>
+        </li>
+      {/if}
       <li>
-        <a
-          class:actif={etapeActive === 'mesures'}
+        <button
           class="lien-navigation"
-          href="/service/{idService}/mesures"
+          onclick={() =>
+            tiroirStore.afficheContenu(TiroirTelechargementDocumentsService, {
+              service,
+            })}
         >
-          <lab-anssi-icone nom="list-unordered" taille="sm"></lab-anssi-icone>
-          Sécuriser
-        </a>
+          <lab-anssi-icone nom="file-download-line" taille="sm"
+          ></lab-anssi-icone>
+          Documents
+        </button>
       </li>
-    {/if}
-    {#if visible.dossiers}
-      <li>
-        <a
-          class:actif={etapeActive === 'dossiers'}
-          class="lien-navigation"
-          href="/service/{idService}/dossiers"
-        >
-          <lab-anssi-icone nom="award-line" taille="sm"></lab-anssi-icone>
-          Homologuer
-        </a>
-      </li>
-    {/if}
-    {#if visible.risques}
-      <li>
-        <a
-          class:actif={etapeActive === 'risques'}
-          class="lien-navigation"
-          href="/service/{idService}/risques"
-        >
-          <lab-anssi-icone nom="warning-line" taille="sm"></lab-anssi-icone>
-          Risques
-        </a>
-      </li>
-    {/if}
-    {#if visible.contactsUtiles}
-      <li>
-        <a
-          class:actif={etapeActive === 'contactsUtiles'}
-          class="lien-navigation"
-          href="/service/{idService}/rolesResponsabilites"
-        >
-          <lab-anssi-icone nom="mail-line" taille="sm"></lab-anssi-icone>
-          Contacts utiles
-        </a>
-      </li>
-    {/if}
-    <li>
-      <button
-        class="lien-navigation"
-        onclick={() =>
-          tiroirStore.afficheContenu(TiroirTelechargementDocumentsService, {
-            service,
-          })}
-      >
-        <lab-anssi-icone nom="file-download-line" taille="sm"></lab-anssi-icone>
-        Documents
-      </button>
-    </li>
-    {#if visible.descriptionService}
-      <li>
-        <a
-          class:actif={etapeActive === 'descriptionService'}
-          class="lien-navigation"
-          href="/service/{idService}/descriptionService"
-        >
-          <lab-anssi-icone nom="survey-line" taille="sm"></lab-anssi-icone>
-          Récapitulatif</a
-        >
-      </li>
-    {/if}
-  </ul>
+      {#if visible.descriptionService}
+        <li>
+          <a
+            class:actif={etapeActive === 'descriptionService'}
+            class="lien-navigation"
+            href="/service/{idService}/descriptionService"
+          >
+            <lab-anssi-icone nom="survey-line" taille="sm"></lab-anssi-icone>
+            Récapitulatif</a
+          >
+        </li>
+      {/if}
+    </ul>
+    <dsfr-button
+      label="Gérer les contributeurs"
+      kind="tertiary"
+      size="sm"
+      icon="group-line"
+      icon-place="left"
+      markup="button"
+      type="button"
+      has-icon
+      onclick={() =>
+        tiroirStore.afficheContenu(TiroirGestionContributeurs, {
+          services: [service],
+        })}
+    ></dsfr-button>
+  </div>
 </div>
 
 <style lang="scss">
@@ -104,54 +125,62 @@
     box-shadow: -1px 0 0 0 #ddd inset;
     height: 100%;
 
-    ul {
-      list-style: none;
-      padding: 0;
-      margin: 0;
+    .conteneur-menu-navigation-service {
       position: sticky;
       top: 0;
 
-      li {
-        display: flex;
-      }
-
-      .lien-navigation {
-        font-size: 1rem;
-        font-weight: bold;
-        line-height: 1.5rem;
-        color: #161616;
-        text-decoration: none;
-        border: none;
-        background: none;
-        cursor: pointer;
+      ul {
+        list-style: none;
+        padding: 0;
         margin: 0;
-        padding: 12px 8px 12px 0;
-        width: 100%;
-        text-align: left;
-        display: flex;
-        flex-direction: row;
-        gap: 8px;
 
-        &:hover {
-          background: #f6f6f6;
+        li {
+          display: flex;
         }
 
-        &:active {
-          background: #ededed;
-        }
+        .lien-navigation {
+          font-size: 1rem;
+          font-weight: bold;
+          line-height: 1.5rem;
+          color: #161616;
+          text-decoration: none;
+          border: none;
+          background: none;
+          cursor: pointer;
+          margin: 0;
+          padding: 12px 8px 12px 0;
+          width: 100%;
+          text-align: left;
+          display: flex;
+          flex-direction: row;
+          gap: 8px;
 
-        lab-anssi-icone {
-          border-left: 2px solid transparent;
-          padding-left: 8px;
-        }
+          &:hover {
+            background: #f6f6f6;
+          }
 
-        &.actif {
-          color: var(--bleu-mise-en-avant);
+          &:active {
+            background: #ededed;
+          }
 
           lab-anssi-icone {
-            border-left-color: var(--bleu-mise-en-avant);
+            border-left: 2px solid transparent;
+            padding-left: 8px;
+          }
+
+          &.actif {
+            color: var(--bleu-mise-en-avant);
+
+            lab-anssi-icone {
+              border-left-color: var(--bleu-mise-en-avant);
+            }
           }
         }
+      }
+
+      dsfr-button {
+        white-space: nowrap;
+        margin-top: 60px;
       }
     }
   }
