bdb6e061ef266c733cdfd439d1f7b0330863c5af
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-03-31 10:28:50 +0200

    Ajoute le nom du service dans l'entête des pages de service

---
+++ svelte/lib/entetePageService/EntetePageService.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import type { EntetePageServiceProps } from './entetePageService.d';
+
+  let { nomService, organisationResponsable }: EntetePageServiceProps =
+    $props();
+</script>
+
+<div class="entete-service">
+  <div class="conteneur-nom">
+    <h1>{nomService}</h1>
+    <h2>{organisationResponsable}</h2>
+  </div>
+</div>
+
+<style lang="scss">
+  .entete-service {
+    background: #f6f6f6;
+    padding: 32px 24px;
+
+    .conteneur-nom {
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+
+      h1 {
+        font-weight: bold;
+        font-size: 1.75rem;
+        line-height: 2.25rem;
+        color: #161616;
+        margin: 0;
+      }
+
+      h2 {
+        font-weight: 400;
+        font-size: 1rem;
+        line-height: 1.5rem;
+        color: #161616;
+        margin: 0;
+      }
+    }
+  }
+</style>
