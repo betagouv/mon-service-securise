e34d3ea7e11bf0ed99e227afc1b5d4c13ca3d39b
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-04-01 15:58:19 +0200

    [CORRECTION] Ne crash pas si les données partagées ne sont pas trouvables dans la page
    ... suite à de nombreuses erreurs inexplicables en Production.

--- public/modules/donneesPartagees.mjs
+++ public/modules/donneesPartagees.mjs
@@ -1,7 +1,9 @@
 import { decode } from '../bibliotheques/html-entities-2.6.0.min.js';
 
 const lisDonneesPartagees = (id) => {
-  const donneesEncodees = JSON.parse(document.getElementById(id).textContent);
+  const element = document.getElementById(id);
+  if (!element) return {};
+  const donneesEncodees = JSON.parse(element.textContent);
   const decodees = JSON.stringify(donneesEncodees, (_cle, valeur) =>
     typeof valeur === 'string' ? decode(valeur) : valeur
   );
