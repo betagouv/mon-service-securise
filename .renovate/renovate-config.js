d43498ad67e4f72d9c5ff3874ddb011151a50ef6
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-04-13 15:57:42 +0200

    POC Renovate

---
+++ .renovate/renovate-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  branchPrefix: 'test-renovate/',
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: false,
+  platform: 'github',
+  forkProcessing: 'enabled',
+  dryRun: 'full',
+  repositories: ['betagouv/mon-service-securise'],
+  packageRules: [
+    {
+      description: 'lockFileMaintenance',
+      matchUpdateTypes: [
+        'pin',
+        'digest',
+        'patch',
+        'minor',
+        'major',
+        'lockFileMaintenance',
+      ],
+      dependencyDashboardApproval: false,
+      minimumReleaseAge: null,
+    },
+  ],
+};
