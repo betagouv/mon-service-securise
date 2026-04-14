d6e887f69083bb61cba6876e54ea36d925198f68
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-04-14 16:54:57 +0200

    Crée un utilisateur et un service dans le global setup des tests d'accessibilité
    ...afin de pouvoir lancer ces tests indépendamment, via une bdd en mémoire

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,13 +7,14 @@ export default defineConfig({
   use: {
     baseURL: 'http://localhost:3000',
     viewport: { width: 1550, height: 900 },
-    headless: false,
   },
   webServer: {
-    command: 'pnpm dev:back',
+    command: 'pnpm build:front && pnpm dev:back',
     url: 'http://localhost:3000',
     reuseExistingServer: true,
     env: {
+      EMAIL_CONNEXION: 'test@fia1.fr',
+      DOSSIER_RAPPORT: 'test_accessibilite/rapport',
       CHIFFREMENT_SEL_DE_HASHAGE_1: 'd322524cb14933e9f8ab',
       NODE_ENV: 'test_accessibilite',
       NB_REQUETES_TRUST_PROXY: '0',
@@ -21,10 +22,11 @@ export default defineConfig({
       NB_REQUETES_MAX_PAR_MINUTE_ENDPOINT_SENSIBLE: '1000',
       NB_REQUETES_MAX_PAR_MINUTE: '1000',
       CACHE_CONTROL_FICHIERS_STATIQUES: 'public, max-age=0',
+      AVEC_JOURNAL_MEMOIRE_QUI_LOG_CONSOLE: 'false',
     },
     stdout: 'pipe',
     stderr: 'pipe',
-    timeout: 10_000,
+    timeout: 15_000,
     ignoreHTTPSErrors: true,
   },
   projects: [
--- test_accessibilite/globalSetup.ts
+++ test_accessibilite/globalSetup.ts
@@ -1,5 +1,98 @@
 import { rmSync } from 'fs';
+import { chromium, FullConfig, Page } from '@playwright/test';
+import {
+  fermeModale2FASiPresente,
+  fermeModaleVisiteGuideeSiPresente,
+} from './aideAuxTests.js';
 
-export default async () => {
+const cliquerSuivant = (page: Page) =>
+  page.getByRole('button', { name: 'Suivant' }).click();
+
+const remplirChamp = async (idInput: string, valeur: string, page: Page) => {
+  await page.locator(`input#${idInput}`).click();
+  await page.keyboard.type(valeur);
+  await page.keyboard.press('Tab');
+};
+
+export default async (config: FullConfig) => {
   rmSync('test_accessibilite/rapport/violations.jsonl', { force: true });
+
+  const urlBase = config.projects[0].use.baseURL;
+  const emailUtilisateurTest = config.webServer!.env!.EMAIL_CONNEXION;
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  const creerUtilisateur = async () => {
+    await page.goto(`${urlBase}/connexion`);
+    await page.click('a[href^="/oidc/connexion"]');
+    await page.waitForURL(/dev-agentconnect/);
+    await page.fill('input[type="email"]', emailUtilisateurTest);
+    await page.click('button[type="submit"]');
+    await page.waitForURL(/dev-agentconnect/);
+    await page.click('button[type="submit"]');
+    await page.waitForURL(/creation-compte/);
+    await page.click('text=Suivant');
+    await page.click('.declencheur');
+    await page.click('#RSSI');
+    await page.click('body');
+    await page.selectOption('#estimation-nombre-services', '1_10');
+    await page.click('text=Suivant');
+    await page.click('#cguAcceptees');
+    await page.click('text=Valider');
+  };
+
+  const creerService = async () => {
+    await page.waitForURL(/tableauDeBord/);
+    await fermeModale2FASiPresente(page);
+    await fermeModaleVisiteGuideeSiPresente(page);
+    await page.waitForLoadState('domcontentloaded');
+    await page.click('text=Ajouter votre premier service');
+    await page.click('text=Ajouter un service');
+
+    const nomService = `Mon service test ${new Date().getTime()}`;
+    await remplirChamp('nom-service', nomService, page);
+    await page.waitForResponse(
+      (r) => r.url().includes('/api/brouillon-service') && r.status() === 200
+    );
+    await cliquerSuivant(page);
+
+    const siretPrerempli = await page.evaluate(
+      () =>
+        !!(document.querySelector('input.valeur-cache') as HTMLInputElement)
+          ?.value
+    );
+    if (!siretPrerempli) {
+      await page.locator('input#siret').click();
+      await page.keyboard.type('ANSSI');
+      await page.waitForSelector('.liste-suggestions.visible');
+      await page.click('.liste-suggestions .option:first-child');
+    }
+    await cliquerSuivant(page);
+    await page.click('label[for="enProjet"]');
+    await cliquerSuivant(page);
+    await cliquerSuivant(page);
+    await page.click('label[for="portailInformation"]');
+    await cliquerSuivant(page);
+    await cliquerSuivant(page);
+    await page.click('label[for="onPremise"]');
+    await cliquerSuivant(page);
+    await page.click('label[for="interneRestreint"]');
+    await page.click('label[for="limitee"]');
+    await page.click('label[for="plusDe24h"]');
+    await cliquerSuivant(page);
+    await page.click('label[for="faible"]');
+    await page.click('label[for="UE"]');
+    await cliquerSuivant(page);
+    await page.click('text=Commencer à sécuriser le service');
+    await page.waitForURL(/mesures/);
+    const urlPageMesures = page.url();
+    const id = urlPageMesures.split('service/')[1].split('/')[0];
+    process.env.ID_SERVICE = id;
+    process.env.EMAIL_CONNEXION = emailUtilisateurTest;
+    process.env.DOSSIER_RAPPORT = config.webServer!.env!.DOSSIER_RAPPORT;
+  };
+
+  await creerUtilisateur();
+  await creerService();
+  await browser.close();
 };
