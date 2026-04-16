752b5773571cda129f1b1bd2e98756ff9a2f84ee
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-04-16 10:18:49 +0200

    Ajoute le contenu du composant de page Contact utiles

---
+++ svelte/lib/pagesService/pages/contactsUtiles/InputDSFR.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  interface Props {
+    label: string;
+    value: string;
+  }
+  let { label, value = $bindable() }: Props = $props();
+
+  const metsAJour = (event: CustomEvent<string>) => {
+    value = event.detail;
+  };
+</script>
+
+<dsfr-input {label} type="text" {value} onvaluechanged={metsAJour}></dsfr-input>
