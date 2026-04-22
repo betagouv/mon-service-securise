63eb0eb046f7cd88f812633c68f851e95f159140
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-04-22 12:39:05 +0200

    Désactive la navigation dans les carousel au premier et dernier élément

--- public/home.js
+++ public/home.js
@@ -5,6 +5,20 @@ const brancheComportementCarrousel = (selecteurCarrousel) => {
 
   const largeurCarte = $($carrousel.children()[0]).width();
 
+  $carrousel.on('scroll', (e) => {
+    const cible = $(e.currentTarget);
+    const scroll = cible.scrollLeft();
+    const total = $carrousel.children().length * largeurCarte;
+
+    if (scroll < largeurCarte / 2)
+      $('.precedent', $conteneurLiens).attr('disabled', 'disabled');
+    else $('.precedent', $conteneurLiens).removeAttr('disabled');
+
+    if (scroll + largeurCarte > total - largeurCarte / 2)
+      $('.suivant', $conteneurLiens).attr('disabled', 'disabled');
+    else $('.suivant', $conteneurLiens).removeAttr('disabled');
+  });
+
   $('.precedent', $conteneurLiens).on('click', () => {
     const decalageCourant = $carrousel.scrollLeft();
     $carrousel.scrollLeft(decalageCourant - largeurCarte);
