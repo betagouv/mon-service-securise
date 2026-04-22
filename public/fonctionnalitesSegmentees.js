f723ab78a4d45435a921b7d95a446e6847977568
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-04-22 15:47:51 +0200

    Termine la home en tablette et desktop

--- public/fonctionnalitesSegmentees.js
+++ public/fonctionnalitesSegmentees.js
@@ -3,10 +3,28 @@ $(() => {
 
   $('.fonctionnalite', conteneur).on('click', (e) => {
     const fonctionnalite = $(e.currentTarget);
+    const image = fonctionnalite.find('img');
 
     $('.fonctionnalite img', conteneur).addClass('invisible');
-    fonctionnalite.find('img').removeClass('invisible');
-    fonctionnalite[0].scrollIntoView({ behavior: 'smooth', block: 'start' });
+    image.removeClass('invisible');
+
+    $('.fonctionnalite', conteneur).removeClass('active');
+    fonctionnalite.addClass('active');
+
+    const estMobile = window.matchMedia('(max-width: 1200px)');
+
+    if (estMobile.matches) {
+      fonctionnalite[0].scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      $('.illustration-fonctionnalites-segmentees-desktop').attr(
+        'src',
+        image.attr('src')
+      );
+      $('.illustration-fonctionnalites-segmentees-desktop').attr(
+        'alt',
+        image.attr('alt')
+      );
+    }
   });
 
   $('.controle-segmente-fonctionnalites').on('valuechanged', (e) => {
@@ -14,6 +32,22 @@ $(() => {
     $('.conteneur-fonctionnalites').addClass('invisible');
     $(`.conteneur-fonctionnalites[data-index="${index}"]`).removeClass(
       'invisible'
+    );
+
+    $('.fonctionnalite', conteneur).removeClass('active');
+    const premiereFonctionnalite = $(
+      `.conteneur-fonctionnalites[data-index="${index}"] .fonctionnalite:first-of-type`,
+      conteneur
+    );
+    premiereFonctionnalite.addClass('active');
+    const image = premiereFonctionnalite.find('img');
+    $('.illustration-fonctionnalites-segmentees-desktop').attr(
+      'src',
+      image.attr('src')
+    );
+    $('.illustration-fonctionnalites-segmentees-desktop').attr(
+      'alt',
+      image.attr('alt')
     );
   });
 });
