48620b42337d7471c5a743a8a1dcd0217a1fdab6
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-04-23 12:14:55 +0200

    [SOIN] Supprime l'ancien tiroir de téléchargement du tampon d'homologation

--- public/service/homologation/dossiers.js
+++ public/service/homologation/dossiers.js
@@ -1,24 +1,14 @@
-import ActionTelechargementTamponHomologation from '../../modules/tableauDeBord/actions/ActionTelechargementTamponHomologation.js';
 import { gestionnaireTiroir } from '../../modules/tableauDeBord/gestionnaireTiroir.mjs';
 import ActionTelechargement from '../../modules/tableauDeBord/actions/ActionTelechargement.mjs';
 import ActionSuppressionDossierCourant from '../../modules/tableauDeBord/actions/ActionSuppressionDossierCourant.mjs';
 
 $(() => {
-  const telechargementTamponHomologation =
-    new ActionTelechargementTamponHomologation();
   const telechargement = new ActionTelechargement();
   const suppressionDossierCourant = new ActionSuppressionDossierCourant();
 
   const idService = $('.page-service').data('id-service');
   const chargeDonneesService = async () =>
     (await axios.get(`/api/service/${idService}`)).data;
-
-  $('#bouton-tampon-homologation').on('click', () => {
-    gestionnaireTiroir.afficheContenuAction(
-      { action: telechargementTamponHomologation },
-      { idService }
-    );
-  });
 
   $('#affiche-documents').on('click', async () => {
     const donneesService = await chargeDonneesService();
