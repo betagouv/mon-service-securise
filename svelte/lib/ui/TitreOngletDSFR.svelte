97d8b57e131b46c9a84233a47285e609d1d363f4
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-04-28 15:38:09 +0200

    [SOIN] Extrais <TitreOngletDSFR />
    … pour pouvoir le réutiliser dans la page des homologations.

---
+++ svelte/lib/ui/TitreOngletDSFR.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Pastille from './Pastille.svelte';
+
+  interface Props {
+    active: boolean;
+    libelle: string;
+    libellePastille: string;
+  }
+
+  let props: Props = $props();
+</script>
+
+<div class="label-onglet">
+  <span>{props.libelle}</span>
+  <Pastille active={props.active} contenu={props.libellePastille} />
+</div>
+
+<style lang="scss">
+  .label-onglet {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+  }
+</style>
