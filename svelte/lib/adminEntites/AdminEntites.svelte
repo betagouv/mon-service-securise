5e5ffa71e1e4e931a8cef764b49eaae7096cf36d
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-05-06 16:40:19 +0200

    Ajoute une coquille vide Svelte pour la page Admin des entités
    Dans AdminEntites.svelte on a un $props() vide pour forcer le mode rune de Svelte5 et ne pas se faire insulter par `mount()` dans adminEntites.ts.

---
+++ svelte/lib/adminEntites/AdminEntites.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  // eslint-disable-next-line no-empty-pattern
+  let {} = $props();
+</script>
+
+<h1>Entités</h1>
+
+<style lang="scss">
+  :global(#conteneur-admin-entites) {
+    text-align: left;
+    background: #fff;
+    width: 100%;
+    padding: 32px 48px;
+  }
+
+  h1 {
+    font-size: 2.5rem;
+    line-height: 3rem;
+    margin: 0;
+  }
+</style>
