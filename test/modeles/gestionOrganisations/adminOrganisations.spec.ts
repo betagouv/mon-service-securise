debe7f5a712d3558b033d2bd313413ef135f3cc0
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-05-13 10:41:20 +0200

    Ajoute un objet AdminOrganisations dans le domaine

---
+++ test/modeles/gestionOrganisations/adminOrganisations.spec.ts
@@ -0,0 +1,29 @@
+import { AdminOrganisations } from '../../../src/modeles/gestionOrganisations/adminOrganisations.ts';
+import { unUUID } from '../../constructeurs/UUID.ts';
+import Entite from '../../../src/modeles/entite.ts';
+
+describe("Un admin d'organisations", () => {
+  it('représente un utilisateur', () => {
+    const admin = AdminOrganisations.nouveau(unUUID('U'));
+
+    expect(admin.donnees().idUtilisateur).toBe(unUUID('U'));
+  });
+
+  it('administre des organisations', () => {
+    const admin = AdminOrganisations.nouveau(unUUID('U'));
+
+    admin.administreCetteEntite(
+      new Entite({ siret: 'SIRET-123', nom: 'Un nom', departement: '75' })
+    );
+
+    expect(admin.donnees().entitesAdministrees[0]).toEqual({
+      siret: 'SIRET-123',
+      nom: 'Un nom',
+      departement: '75',
+    });
+  });
+
+  it.todo(
+    "accepte silencieusement d'administrer une entité deux fois : elle n'est pas doublonnée"
+  );
+});
