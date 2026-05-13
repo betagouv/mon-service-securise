a7fdbab92516f64cfe6addd01ca885e1f43394c7
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-05-13 10:52:09 +0200

    Accepte silencieusement d'administrer une entité deux fois

--- test/modeles/gestionOrganisations/adminOrganisations.spec.ts
+++ test/modeles/gestionOrganisations/adminOrganisations.spec.ts
@@ -23,7 +23,18 @@ describe("Un admin d'organisations", () => {
     });
   });
 
-  it.todo(
-    "accepte silencieusement d'administrer une entité deux fois : elle n'est pas doublonnée"
-  );
+  it("accepte silencieusement d'administrer une entité deux fois : elle n'est pas doublonnée", () => {
+    const admin = AdminOrganisations.nouveau(unUUID('U'));
+
+    const entite = new Entite({
+      siret: 'SIRET-123',
+      nom: 'Un nom',
+      departement: '75',
+    });
+
+    admin.administreCetteEntite(entite);
+    admin.administreCetteEntite(entite);
+
+    expect(admin.donnees().entitesAdministrees).toHaveLength(1);
+  });
 });
