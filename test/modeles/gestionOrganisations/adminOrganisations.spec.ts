04b775fda65fc391589a4f908ad3b6d56b4f8b8e
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-05-13 11:00:02 +0200

    Implémente le retrait d'une entité administrée

--- test/modeles/gestionOrganisations/adminOrganisations.spec.ts
+++ test/modeles/gestionOrganisations/adminOrganisations.spec.ts
@@ -9,32 +9,69 @@ describe("Un admin d'organisations", () => {
     expect(admin.donnees().idUtilisateur).toBe(unUUID('U'));
   });
 
-  it('administre des organisations', () => {
-    const admin = AdminOrganisations.nouveau(unUUID('U'));
+  describe("sur l'ajout d'une entité administrée", () => {
+    it("administre l'entité", () => {
+      const admin = AdminOrganisations.nouveau(unUUID('U'));
 
-    admin.administreCetteEntite(
-      new Entite({ siret: 'SIRET-123', nom: 'Un nom', departement: '75' })
-    );
+      admin.administreCetteEntite(
+        new Entite({ siret: 'SIRET-123', nom: 'Un nom', departement: '75' })
+      );
 
-    expect(admin.donnees().entitesAdministrees[0]).toEqual({
-      siret: 'SIRET-123',
-      nom: 'Un nom',
-      departement: '75',
+      expect(admin.donnees().entitesAdministrees[0]).toEqual({
+        siret: 'SIRET-123',
+        nom: 'Un nom',
+        departement: '75',
+      });
+    });
+
+    it("accepte silencieusement d'administrer une entité deux fois : elle n'est pas doublonnée", () => {
+      const admin = AdminOrganisations.nouveau(unUUID('U'));
+
+      const entite = new Entite({
+        siret: 'SIRET-123',
+        nom: 'Un nom',
+        departement: '75',
+      });
+
+      admin.administreCetteEntite(entite);
+      admin.administreCetteEntite(entite);
+
+      expect(admin.donnees().entitesAdministrees).toHaveLength(1);
     });
   });
 
-  it("accepte silencieusement d'administrer une entité deux fois : elle n'est pas doublonnée", () => {
-    const admin = AdminOrganisations.nouveau(unUUID('U'));
+  describe("sur le retrait d'une entité administrée", () => {
+    it("n'administre plus cette entité", () => {
+      const admin = AdminOrganisations.nouveau(unUUID('U'));
+      const entite = new Entite({
+        siret: 'SIRET-123',
+        nom: 'Un nom',
+        departement: '75',
+      });
 
-    const entite = new Entite({
-      siret: 'SIRET-123',
-      nom: 'Un nom',
-      departement: '75',
+      admin.administreCetteEntite(entite);
+      admin.cesseDAdministrer(entite);
+
+      expect(admin.donnees().entitesAdministrees).toHaveLength(0);
     });
 
-    admin.administreCetteEntite(entite);
-    admin.administreCetteEntite(entite);
+    it('accepte silencieusement de retirer un entité non administrée', () => {
+      const admin = AdminOrganisations.nouveau(unUUID('U'));
+      const entiteA = new Entite({
+        siret: 'SIRET-A',
+        nom: 'Un nom',
+        departement: '75',
+      });
+      const entiteB = new Entite({
+        siret: 'SIRET-B',
+        nom: 'Un nom',
+        departement: '75',
+      });
 
-    expect(admin.donnees().entitesAdministrees).toHaveLength(1);
+      admin.administreCetteEntite(entiteA);
+      admin.cesseDAdministrer(entiteB);
+
+      expect(admin.donnees().entitesAdministrees).toHaveLength(1);
+    });
   });
 });
