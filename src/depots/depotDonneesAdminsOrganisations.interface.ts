573ea5307d4c2c13d46e63e27b08b26a67c16a8c
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-05-18 11:38:46 +0200

    [SOIN] Supprime du code devenu inutile
    ...car on lit maintenant un admin via le dépôt TS

--- src/depots/depotDonneesAdminsOrganisations.interface.ts
+++ src/depots/depotDonneesAdminsOrganisations.interface.ts
@@ -1,8 +1,6 @@
 import { UUID } from '../typesBasiques.js';
-import Entite from '../modeles/entite.js';
 
 export type DepotDonneesAdminsOrganisations = {
   lisAdminsPour: (siret: string) => Promise<Array<UUID>>;
-  entitesAdministreesPar: (idUtilisateur: UUID) => Promise<Array<Entite>>;
   ajouteSiretAAdmin: (idUtilisateur: UUID, siret: string) => Promise<void>;
 };
--- src/depots/depotDonneesAdminsOrganisations.ts
+++ src/depots/depotDonneesAdminsOrganisations.ts
@@ -2,7 +2,7 @@ import { AdaptateurPersistance } from '../adaptateurs/adaptateurPersistance.inte
 import { UUID } from '../typesBasiques.js';
 import { AdaptateurChiffrement } from '../adaptateurs/adaptateurChiffrement.interface.js';
 import { DepotDonneesAdminsOrganisations } from './depotDonneesAdminsOrganisations.interface.js';
-import Entite, { DonneesEntite } from '../modeles/entite.js';
+import Entite from '../modeles/entite.js';
 import { AdaptateurRechercheEntreprise } from '../adaptateurs/adaptateurRechercheEntreprise.interface.js';
 
 export const creeDepot = ({
@@ -17,17 +17,6 @@ export const creeDepot = ({
   const lisAdminsPour = async (siret: string): Promise<Array<UUID>> => {
     const siretHache = chiffrement.hacheSha256(siret);
     return persistance.lisAdminsPour(siretHache);
-  };
-
-  const entitesAdministreesPar = async (
-    idUtilisateur: UUID
-  ): Promise<Array<Entite>> => {
-    const donneesChiffrees =
-      await persistance.lisEntitesAdministreesPar(idUtilisateur);
-    const donneesEntites = await Promise.all<DonneesEntite>(
-      donneesChiffrees.map(chiffrement.dechiffre)
-    );
-    return donneesEntites.map((donneesEntite) => new Entite(donneesEntite));
   };
 
   const ajouteSiretAAdmin = async (idAdmin: UUID, siret: string) => {
@@ -48,7 +37,6 @@ export const creeDepot = ({
 
   return {
     ajouteSiretAAdmin,
-    entitesAdministreesPar,
     lisAdminsPour,
   };
 };
