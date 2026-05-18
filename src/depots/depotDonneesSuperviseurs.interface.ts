077fde597678ad4fe2c8f2e73d3386023fea577e
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-05-18 16:04:23 +0200

    [SOIN] Corrige des typages

--- src/depots/depotDonneesSuperviseurs.interface.ts
+++ src/depots/depotDonneesSuperviseurs.interface.ts
@@ -2,8 +2,11 @@ import { UUID } from '../typesBasiques.js';
 import Superviseur from '../modeles/superviseur.js';
 
 export type DepotDonneesSuperviseurs = {
-  ajouteSiretAuSuperviseur: (siret: string) => Promise<void>;
-  estSuperviseur: (siret: string) => Promise<boolean>;
+  ajouteSiretAuSuperviseur: (
+    idSuperviseur: UUID,
+    siret: string
+  ) => Promise<void>;
+  estSuperviseur: (idUtilisateur: UUID) => Promise<boolean>;
   lisSuperviseurs: (siret: string) => Promise<UUID[]>;
   superviseur: (id: UUID) => Promise<Superviseur | undefined>;
   revoqueSuperviseur: (id: UUID) => Promise<void>;
