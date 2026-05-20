daeeb184e269abe4ad0471d12c0cfdc7bbfefe33
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-05-20 09:17:21 +0200

    [ACCESSIBILITE] Ajoute les articles Crisp aux pages testées

--- test_accessibilite/pagesPubliques.spec.ts
+++ test_accessibilite/pagesPubliques.spec.ts
@@ -66,3 +66,46 @@ test("La page creation-compte n'a aucune violation grave d'accessibilité", asyn
   await page.click('#cguAcceptees');
   await page.click('text=Valider');
 });
+
+const articlesCrisp = [
+  'realiser-une-analyse-de-risques-de-la-securite-du-service',
+  'lhomologation-simplifiee-identifier-la-demarche-adaptee',
+  'tout-savoir-sur-lhomologation-de-securite',
+  'lhomologation-simplifiee-les-grandes-etapes-vers-lhomologation',
+  'lhomologation-simplifiee-perimetre-et-strategies-dhomologation-possibles',
+  'lhomologation-simplifiee-quest-ce-quune-homologation-de-securite',
+  'lhomologation-simplifiee-quand-homologuer-et-pour-quelle-duree',
+  'realiser-un-audit-de-la-securite-du-service',
+  'lhomologation-simplifiee-quels-sont-les-acteurs-de-lhomologation-de-securite',
+  'lhomologation-simplifiee-pourquoi-homologuer',
+  'lhomologation-simplifiee-les-facteurs-cles-de-succes',
+  'je-narrive-a-homologuer-que-3-services-par-an',
+  'jai-deja-oublie-de-renouveler-une-homologation',
+  'la-securite-nest-pas-valorisee',
+  'mon-fournisseur-de-service-me-dit-que-cest-securise',
+  'je-ne-sais-pas-comment-constituer-un-dossier-dhomologation',
+  'chiffrer-le-trafic-des-donnees-avec-un-certificat-de-securite-conforme-a-la-reglementation',
+  'organiser-un-exercice-de-gestion-de-crise',
+  'quelles-aides-sont-disponibles-pour-la-realisation-dune-homologation-de-securite',
+  'activer-lauthentification-multifacteur-pour-lacces-des-administrateurs-au-service',
+  'proteger-les-mots-de-passe-stockes-sur-le-service',
+  'encourager-les-administrateurs-a-utiliser-un-coffre-fort-de-mots-de-passe',
+  'fixer-des-contraintes-de-longueur-et-de-complexite-des-mots-de-passe',
+  'mais-a-quoi-ca-sert-dhomologuer',
+  'definir-une-politique-de-gestion-des-incidents-de-securite',
+  'mais-qui-doit-etre-lautorite-dhomologation-au-juste',
+  'installer-un-certificat-de-signature-electronique-conforme-a-la-reglementation',
+  'heberger-le-service-et-les-donnees-aupres-dun-prestataire-secnumcloud',
+];
+
+for (const article of articlesCrisp) {
+  test(`L'article Crisp ${article} n'a aucune violation grave d'accessibilité`, async ({
+    page,
+  }) => {
+    await page.goto(`/articles/${article}`);
+
+    const problemes = await problemesDAccessibiliteDeLaPage(page);
+
+    expect(problemes.length, messageDErreur(problemes)).toBe(0);
+  });
+}
