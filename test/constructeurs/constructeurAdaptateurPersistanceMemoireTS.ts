55efe211276375052bd6617431c3f68f86ca154d
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-05-20 16:43:58 +0200

    Ajoute un dépôt de données OO pour les superviseurs
    ..avec la méthode de lecture, qui utilise la nouvelle persistance et le chiffrement côté persistance

--- test/constructeurs/constructeurAdaptateurPersistanceMemoireTS.ts
+++ test/constructeurs/constructeurAdaptateurPersistanceMemoireTS.ts
@@ -1,13 +1,16 @@
 import { AdaptateurPersistanceMemoireTS } from '../../src/adaptateurs/adaptateurPersistanceMemoireTS.js';
 import { DonneesAdminOrganisations } from '../../src/modeles/gestionOrganisations/adminOrganisations.js';
+import { DonneesSuperviseur } from '../../src/modeles/superviseur.js';
 import { UUID } from '../../src/typesBasiques.js';
 import { DonneesEntite } from '../../src/modeles/entite.js';
 
 class ConstructeurAdaptateurPersistanceMemoireTS {
   private readonly adminsOrganisations: DonneesAdminOrganisations[];
+  private readonly superviseurs: DonneesSuperviseur[];
 
   constructor() {
     this.adminsOrganisations = [];
+    this.superviseurs = [];
   }
 
   ajouteAdminSurPerimetre(idAdmin: UUID, entitesDuPerimetre: DonneesEntite[]) {
@@ -19,9 +22,22 @@ class ConstructeurAdaptateurPersistanceMemoireTS {
     return this;
   }
 
+  ajouteSuperviseurSurPerimetre(
+    idSuperviseur: UUID,
+    entitesSupervisees: DonneesEntite[]
+  ) {
+    this.superviseurs.push({
+      idUtilisateur: idSuperviseur,
+      entitesSupervisees,
+    });
+
+    return this;
+  }
+
   construis() {
     return new AdaptateurPersistanceMemoireTS({
       adminsOrganisations: this.adminsOrganisations,
+      superviseurs: this.superviseurs,
     });
   }
 }
