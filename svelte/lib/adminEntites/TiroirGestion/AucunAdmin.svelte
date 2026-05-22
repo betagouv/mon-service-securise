5a9912a30ac1d58c6d4c19e2ad48ec1f1b9be0b0
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-05-22 16:38:50 +0200

    Affiche une illustration en cas d'absence d'admins

---
+++ svelte/lib/adminEntites/TiroirGestion/AucunAdmin.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+</script>
+
+<div class="etat-vide">
+  <img
+    src="/statique/assets/images/illustration_recherche_vide.svg"
+    alt=""
+    width="128"
+    height="128"
+  />
+  <div class="contenu">
+    <h3>Aucun admin ajouté à cette entité</h3>
+    <p>
+      Ajoutez un premier admin pour déléguer la gestion et le suivi de vos
+      entités.
+    </p>
+  </div>
+</div>
+
+<style lang="scss">
+  .etat-vide {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+    padding-bottom: 8px;
+    text-align: center;
+    margin-top: 2rem;
+
+    .contenu {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+
+      h3 {
+        font-size: 1.5rem;
+        line-height: 2rem;
+        font-weight: 700;
+        color: #161616;
+        margin: 0;
+      }
+
+      p {
+        font-size: 1.125rem;
+        line-height: 1.75rem;
+        color: #3a3a3a;
+        margin: 0;
+      }
+    }
+  }
+</style>
--- svelte/lib/adminEntites/TiroirGestion/ListeAdmins.svelte
+++ svelte/lib/adminEntites/TiroirGestion/ListeAdmins.svelte
@@ -9,11 +9,6 @@
   let { administrateurs }: Props = $props();
 </script>
 
-<hr />
-<h3>Administrateurs de l’entité</h3>
-<span class="sous-titre"
-  >Consultez la liste des utilisateurs disposant des droits admins.</span
->
 <div class="conteneur-cartouches">
   {#each administrateurs as admin, i (i)}
     <CartoucheAdmin
@@ -25,22 +20,6 @@
 </div>
 
 <style>
-  hr {
-    border: none;
-    border-top: 1px solid #dddddd;
-  }
-  h3 {
-    font-size: 1rem;
-    line-height: 1.5rem;
-    font-weight: 700;
-    margin-bottom: 4px;
-  }
-  .sous-titre {
-    font-size: 0.75rem;
-    line-height: 1.125rem;
-    color: #666666;
-  }
-
   .conteneur-cartouches {
     margin-top: 32px;
     display: flex;
