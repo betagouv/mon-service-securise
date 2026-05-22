3b627ea45173fde1ef3be8b1ca1ca80ae963bf34
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-05-22 14:55:32 +0200

    Extrais <SaisieEmailAdmin />
    et y ajoute la validation de l'adresse email à nommer.

---
+++ svelte/lib/adminEntites/TiroirGestion/SaisieEmailAdmin.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { api } from '../adminEntites.api';
+
+  interface Props {
+    onemailvalide: (email: string) => void;
+  }
+
+  let { onemailvalide }: Props = $props();
+
+  let nouvelAdmin = $state('');
+  let emailValide = $state(true);
+
+  const metAJourEmail = (email: string) => {
+    nouvelAdmin = email;
+    emailValide = true;
+  };
+
+  const valide = async () => {
+    emailValide = (await api.verifieEmail(nouvelAdmin)).existe;
+    if (!emailValide) return;
+
+    onemailvalide(nouvelAdmin);
+    nouvelAdmin = '';
+  };
+</script>
+
+<dsfr-input
+  value={nouvelAdmin}
+  onvaluechanged={(e: CustomEvent<string>) => metAJourEmail(e.detail)}
+  type="email"
+  label="Ajouter un administrateur"
+  hint="Vous pouvez ajouter un administrateur via son adresse e-mail."
+  status={emailValide ? 'info' : 'error'}
+  infoMessage="L’utilisateur doit toutefois déjà disposer d’un compte sur MonServiceSécurisé ; dans le cas contraire, il ne pourra pas être ajouté en tant qu’administrateur."
+></dsfr-input>
+<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
+<dsfr-button label="Nommer admin" size="sm" onclick={async () => await valide()}
+></dsfr-button>
