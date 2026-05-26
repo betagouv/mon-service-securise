1c8cdcec2bef0e7ecb602c8cec5ca786311f48de
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-05-26 15:06:54 +0200

    [SOIN] Extrais des composants
    … pour gagner en lisibilité.

---
+++ svelte/lib/adminEntites/LignesDuTableau/BadgeAdministrateur.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  interface Props {
+    prenomNom: string;
+  }
+
+  let { prenomNom }: Props = $props();
+</script>
+
+<dsfr-badge label={prenomNom} type="accent" accent="blue-ecume" size="sm"
+></dsfr-badge>
---
+++ svelte/lib/adminEntites/LignesDuTableau/NombreServices.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  interface Props {
+    nombreServices: number;
+  }
+
+  let { nombreServices }: Props = $props();
+</script>
+
+<span>
+  {#if nombreServices === 0}
+    Aucun service
+  {:else}
+    {nombreServices} service{nombreServices > 1 ? 's' : ''}
+  {/if}
+</span>
