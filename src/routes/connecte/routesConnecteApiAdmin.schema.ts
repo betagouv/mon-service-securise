64816c1dbdd601b509f721c04e2af0e69987bcff
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-06-02 17:35:45 +0200

    Utilise le service dans la route

--- src/routes/connecte/routesConnecteApiAdmin.schema.ts
+++ src/routes/connecte/routesConnecteApiAdmin.schema.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { schemaSiret } from '../../http/schemas/siret.schema.js';
+import { Autorisation } from '../../modeles/autorisations/autorisation.js';
 
 export const schemaPostAdminNomme = z.strictObject({
   emails: z.array(z.email()).min(1).max(50),
@@ -9,4 +10,13 @@ export const schemaPostAdminNomme = z.strictObject({
 export const schemaDeleteAdmin = z.strictObject({
   siret: schemaSiret.siret(),
   idUtilisateur: z.uuid(),
+});
+
+export const schemaAttributionRoleServices = z.strictObject({
+  role: z.enum([
+    Autorisation.RESUME_NIVEAU_DROIT.PROPRIETAIRE,
+    Autorisation.RESUME_NIVEAU_DROIT.ECRITURE,
+    Autorisation.RESUME_NIVEAU_DROIT.LECTURE,
+  ]),
+  idsServices: z.array(z.uuid()).min(1),
 });
