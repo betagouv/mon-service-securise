3c703d5ea824dd93bb690b4ef27ef8e05b3bd798
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-06-04 15:09:55 +0200

    [SOIN] Extrait les composants d'alerte de seul propriétaire
    ... afin de pouvoir les afficher aussi dans l'action d'attribution de rôle. On remonte aussi la logique de `derived` dans le tiroir pour passer les services "seulProprietaire" en props.

---
+++ svelte/lib/adminUtilisateurs/TiroirGestionUtilisateurAdministre/AlerteSeulProprietaire.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { singulierPluriel } from '../../outils/string';
+  import type { ServiceAdministre } from '../adminUtilisateurs.types';
+
+  interface Props {
+    servicesSeulProprietaire: ServiceAdministre[];
+  }
+
+  let { servicesSeulProprietaire }: Props = $props();
+</script>
+
+{#if servicesSeulProprietaire.length > 0}
+  <dsfr-alert size="sm" type="warning" has-description>
+    <div slot="description" class="alerte">
+      <span
+        >Cet utilisateur est actuellement le <b>seul propriétaire</b> de {servicesSeulProprietaire.length}
+        {singulierPluriel(
+          'service',
+          'services',
+          servicesSeulProprietaire.length
+        )}. Après cette action, seul les administrateurs du périmètre auront
+        accès {singulierPluriel(
+          'au service',
+          'aux services',
+          servicesSeulProprietaire.length
+        )}. <b>Cette action est irréversible.</b></span
+      >
+    </div>
+  </dsfr-alert>
+{/if}
+
+<style lang="scss">
+  dsfr-alert {
+    margin-bottom: 24px;
+
+    .alerte {
+      padding-bottom: 0.75rem;
+      display: block;
+    }
+  }
+</style>
---
+++ svelte/lib/adminUtilisateurs/TiroirGestionUtilisateurAdministre/IndicateurSeulProprietaire.svelte
@@ -0,0 +1,11 @@
+<span class="seul-proprietaire">
+  <lab-anssi-icone taille="sm" nom="warning-line"></lab-anssi-icone>
+  Seul propriétaire de ce service
+</span>
+
+<style lang="scss">
+  .seul-proprietaire {
+    display: flex;
+    gap: 8px;
+  }
+</style>
