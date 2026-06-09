8c6945c03875eaa2df51fb8fbe3a25adb4a9e5f5
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-06-09 15:30:25 +0200

    Trace la nomination d'admin dans l'audit

--- src/modeles/gestionOrganisations/traceAudit.ts
+++ src/modeles/gestionOrganisations/traceAudit.ts
@@ -3,11 +3,15 @@ import Service from '../service.js';
 import Entite from '../entite.js';
 import { Role } from '../autorisations/autorisation.js';
 
-export type TypeActionAudit = 'ATTRIBUTION_ROLE' | 'RETRAIT_ACCES';
+export type TypeActionAudit =
+  | 'ATTRIBUTION_ROLE'
+  | 'RETRAIT_ACCES'
+  | 'NOMINATION_ADMIN';
 
 interface DonneesAuditMap extends Record<TypeActionAudit, unknown> {
   ATTRIBUTION_ROLE: { role: Role };
   RETRAIT_ACCES: undefined;
+  NOMINATION_ADMIN: undefined;
 }
 
 export type DonneesAudit<T extends TypeActionAudit> = DonneesAuditMap[T];
