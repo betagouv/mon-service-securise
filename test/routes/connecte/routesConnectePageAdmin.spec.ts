aeff36649b388a678705f53a95f7f87ec433dc08
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-06-10 17:14:15 +0200

    Ajoute la page des administrateurs supervisés par un superviseur

--- test/routes/connecte/routesConnectePageAdmin.spec.ts
+++ test/routes/connecte/routesConnectePageAdmin.spec.ts
@@ -2,53 +2,69 @@ import testeurMSS from '../testeurMSS.js';
 import { unUtilisateur } from '../../constructeurs/constructeurUtilisateur.js';
 import { unUUID } from '../../constructeurs/UUID.ts';
 import { AdminOrganisations } from '../../../src/modeles/gestionOrganisations/adminOrganisations.ts';
+import Superviseur from '../../../src/modeles/superviseur.ts';
 
 describe("Le serveur MSS des pages d'admin", () => {
   const testeur = testeurMSS();
 
   beforeEach(() => testeur.initialise());
 
-  ['/admin/entites', '/admin/utilisateurs'].forEach((route) => {
-    describe(`quand GET sur ${route}`, () => {
-      beforeEach(() => {
-        const utilisateur = unUtilisateur().construis();
-        testeur.depotDonnees().utilisateur = async () => utilisateur;
-        testeur.depotDonnees().lisAdminOrganisations = async () =>
-          AdminOrganisations.hydrate({
-            idUtilisateur: unUUID('U'),
-            entitesAdministrees: [{ siret: '1234' }],
-          });
-      });
-
-      it("vérifie que l'utilisateur a accepté les CGU", async () => {
-        await testeur
-          .middleware()
-          .verifieRequeteExigeAcceptationCGU(testeur.app(), `${route}`);
-      });
-
-      it('sert le contenu HTML de la page', async () => {
-        const reponse = await testeur.get(`${route}`);
-
-        expect(reponse.status).to.equal(200);
-        expect(reponse.headers['content-type']).to.contain('text/html');
-      });
-
-      it("jette une erreur 404 si le feature flag de gestion des orgas n'est pas activé", async () => {
-        testeur.adaptateurEnvironnement().featureFlag = () => ({
-          avecGestionDesOrganisations: () => false,
+  ['/admin/entites', '/admin/utilisateurs', '/admin/administrateurs'].forEach(
+    (route) => {
+      describe(`quand GET sur ${route}`, () => {
+        beforeEach(() => {
+          const utilisateur = unUtilisateur().construis();
+          testeur.depotDonnees().utilisateur = async () => utilisateur;
+          testeur.depotDonnees().lisAdminOrganisations = async () =>
+            AdminOrganisations.hydrate({
+              idUtilisateur: unUUID('U'),
+              entitesAdministrees: [{ siret: '1234' }],
+            });
+          testeur.depotDonnees().lisSuperviseur = async () =>
+            Superviseur.hydrate({
+              idUtilisateur: unUUID('U'),
+              entitesSupervisees: [{ siret: '1234' }],
+            });
         });
 
-        const reponse = await testeur.get(`${route}`);
+        it("vérifie que l'utilisateur a accepté les CGU", async () => {
+          await testeur
+            .middleware()
+            .verifieRequeteExigeAcceptationCGU(testeur.app(), `${route}`);
+        });
 
-        expect(reponse.status).to.equal(404);
+        it('sert le contenu HTML de la page', async () => {
+          const reponse = await testeur.get(`${route}`);
+
+          expect(reponse.status).to.equal(200);
+          expect(reponse.headers['content-type']).to.contain('text/html');
+        });
+
+        it("jette une erreur 404 si le feature flag de gestion des orgas n'est pas activé", async () => {
+          testeur.adaptateurEnvironnement().featureFlag = () => ({
+            avecGestionDesOrganisations: () => false,
+          });
+
+          const reponse = await testeur.get(`${route}`);
+
+          expect(reponse.status).to.equal(404);
+        });
       });
-    });
-  });
+    }
+  );
 
   it("ne donne accès à la page admin/utilisateurs qu'aux admins", async () => {
     testeur.depotDonnees().lisAdminOrganisations = async () => undefined;
 
     const reponse = await testeur.get('/admin/utilisateurs');
+
+    expect(reponse.status).to.equal(404);
+  });
+
+  it("ne donne accès à la page admin/administrateurs qu'aux superviseurs", async () => {
+    testeur.depotDonnees().lisSuperviseur = async () => undefined;
+
+    const reponse = await testeur.get('/admin/administrateurs');
 
     expect(reponse.status).to.equal(404);
   });
