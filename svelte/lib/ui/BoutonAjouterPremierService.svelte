3d2cc0dd5252fb8218f01e92705ca8e115dbf223
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-06-10 09:40:16 +0200

    [SOIN] Extrais le bouton de création de premier service
    ...afin de le réutiliser dans la page admin/utilisateurs

---
+++ svelte/lib/ui/BoutonAjouterPremierService.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { tiroirStore } from './stores/tiroir.store.js';
+  import TiroirTeleversementServicesV2 from '../tableauDeBord/televersementServices/TiroirTeleversementServicesV2.svelte';
+  import BoutonAvecListeDeroulante from './BoutonAvecListeDeroulante.svelte';
+</script>
+
+<BoutonAvecListeDeroulante
+  titre="Ajouter votre premier service"
+  options={[
+    {
+      label: 'Ajouter un service',
+      icone: 'plus',
+      href: '/service/v2/creation',
+    },
+    {
+      label: 'Téléverser des services',
+      icone: 'televerser',
+      action: () =>
+        tiroirStore.afficheContenu(TiroirTeleversementServicesV2, {}),
+    },
+  ]}
+/>
