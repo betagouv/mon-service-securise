3fc6ab1bd7d787039908156a3eea905dec151329
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-06-11 15:11:44 +0200

    Ajoute la recherche sur les pages d'admin

--- svelte/lib/adminEntites/AdminEntites.svelte
+++ svelte/lib/adminEntites/AdminEntites.svelte
@@ -9,8 +9,12 @@
   import NombreUtilisateurs from './LignesDuTableau/NombreUtilisateurs.svelte';
   import NommerUnAdmin from './LignesDuTableau/NommerUnAdmin.svelte';
   import GererLesAdmins from './LignesDuTableau/GererLesAdmins.svelte';
+  import { chaineNormalisee } from '../outils/string';
+  import ChampRecherche from '../ui/ChampRecherche.svelte';
+  import AucunResultatRecherche from '../ui/AucunResultatRecherche.svelte';
 
   let mesEntites: Array<EntiteSupervisee> = $state([]);
+  let recherche = $state('');
 
   onMount(async () => {
     await rafraichis();
@@ -19,51 +23,68 @@
   const rafraichis = async () => {
     mesEntites = await api.entitesDansMonPerimetre();
   };
+
+  let rechercheNormalisee = $derived(chaineNormalisee(recherche));
+
+  const mesEntitesFiltrees = $derived(
+    mesEntites.filter(
+      (e) =>
+        (e.nom && chaineNormalisee(e.nom).includes(rechercheNormalisee)) ||
+        e.siret.includes(rechercheNormalisee)
+    )
+  );
 </script>
 
 <Toaster />
+
 <svelte:document on:admins-entites-modifiees={rafraichis} />
 
 <h1>Entités</h1>
 
 <Tuiles {mesEntites} />
 
-<dsfr-table
-  columns={[
-    { key: 'nom', label: 'Entité' },
-    { key: 'admins', label: 'Admin(s)' },
-    { key: 'nombreServices', label: 'Nombre de services' },
-    { key: 'nombreUtilisateurs', label: "Nombre d'utilisateurs" },
-    { key: 'actions', label: 'Actions' },
-  ]}
-  rows={mesEntites}
-  rich
-  multiline
->
-  {#each mesEntites as entite, i (entite.siret)}
-    <div slot="cell:nom:{i}">
-      <span><b>{entite.nom}</b></span>
-    </div>
-    <div slot="cell:admins:{i}" class="conteneur-admins">
-      {#each entite.administrateurs as { prenomNom }, j (j)}
-        <BadgeAdministrateur {prenomNom} />
-      {/each}
-    </div>
-    <div slot="cell:nombreServices:{i}">
-      <NombreServices nombreServices={entite.nombreServices} />
-    </div>
-    <div slot="cell:nombreUtilisateurs:{i}">
-      <NombreUtilisateurs nombreUtilisateurs={entite.nombreUtilisateurs} />
-    </div>
-    <div slot="cell:actions:{i}">
-      {#if entite.administrateurs.length === 0}
-        <NommerUnAdmin {entite} />
-      {:else}
-        <GererLesAdmins {entite} />
-      {/if}
-    </div>
-  {/each}
-</dsfr-table>
+<ChampRecherche bind:valeur={recherche} />
+
+{#if recherche.length > 0 && mesEntitesFiltrees.length === 0}
+  <AucunResultatRecherche onclick={() => (recherche = '')} />
+{:else}
+  <dsfr-table
+    columns={[
+      { key: 'nom', label: 'Entité' },
+      { key: 'admins', label: 'Admin(s)' },
+      { key: 'nombreServices', label: 'Nombre de services' },
+      { key: 'nombreUtilisateurs', label: "Nombre d'utilisateurs" },
+      { key: 'actions', label: 'Actions' },
+    ]}
+    rows={mesEntitesFiltrees}
+    rich
+    multiline
+  >
+    {#each mesEntitesFiltrees as entite, i (entite.siret)}
+      <div slot="cell:nom:{i}">
+        <span><b>{entite.nom}</b></span>
+      </div>
+      <div slot="cell:admins:{i}" class="conteneur-admins">
+        {#each entite.administrateurs as { prenomNom }, j (j)}
+          <BadgeAdministrateur {prenomNom} />
+        {/each}
+      </div>
+      <div slot="cell:nombreServices:{i}">
+        <NombreServices nombreServices={entite.nombreServices} />
+      </div>
+      <div slot="cell:nombreUtilisateurs:{i}">
+        <NombreUtilisateurs nombreUtilisateurs={entite.nombreUtilisateurs} />
+      </div>
+      <div slot="cell:actions:{i}">
+        {#if entite.administrateurs.length === 0}
+          <NommerUnAdmin {entite} />
+        {:else}
+          <GererLesAdmins {entite} />
+        {/if}
+      </div>
+    {/each}
+  </dsfr-table>
+{/if}
 
 <style lang="scss">
   :global(#conteneur-admin-entites) {
---
+++ svelte/lib/ui/AucunResultatRecherche.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  interface Props {
+    onclick: () => unknown;
+  }
+
+  let { onclick }: Props = $props();
+</script>
+
+<div class="aucun-resultat">
+  <img src="/statique/assets/images/illustration_recherche_vide.svg" alt="" />
+  <h4>Aucun résultat ne correspond à votre recherche</h4>
+  <!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
+  <dsfr-button
+    kind="secondary"
+    icon="restart-line"
+    has-icon
+    label="Effacer la recherche"
+    {onclick}
+  ></dsfr-button>
+</div>
+
+<style lang="scss">
+  .aucun-resultat {
+    padding: 36px 0;
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    flex-direction: column;
+    color: #161616;
+    max-width: 588px;
+    margin: 0 auto;
+
+    h4 {
+      margin: 0;
+      font-size: 1.5rem;
+      line-height: 2rem;
+      font-weight: bold;
+    }
+
+    img {
+      max-width: 128px;
+      transform: scaleX(-1);
+    }
+
+    dsfr-button {
+      margin-top: 16px;
+    }
+  }
+</style>
