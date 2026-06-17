6ab3e3f67bf39f2fa7274be614dd14cc2f744ff9
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-06-17 10:23:26 +0200

    Mets le bouton d'ajout d'email en mode icone seul et sur la même ligne que l'input

--- svelte/lib/adminEntites/TiroirGestion/SaisieEmailAdmin.svelte
+++ svelte/lib/adminEntites/TiroirGestion/SaisieEmailAdmin.svelte
@@ -24,18 +24,33 @@
   };
 </script>
 
-<dsfr-input
-  value={nouvelAdmin}
-  onvaluechanged={(e: CustomEvent<string>) => metAJourEmail(e.detail)}
-  type="email"
-  label="Ajouter un administrateur"
-  hint="Vous pouvez ajouter un administrateur via son adresse e-mail."
-  status={emailValide ? 'info' : 'error'}
-  infoMessage="L’utilisateur doit toutefois déjà disposer d’un compte sur MonServiceSécurisé ; dans le cas contraire, il ne pourra pas être ajouté en tant qu’administrateur."
-></dsfr-input>
-<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
-<dsfr-button
-  label="Ajouter cet admin"
-  size="sm"
-  onclick={async () => await valide()}
-></dsfr-button>
+<div class="conteneur-saisie">
+  <dsfr-input
+    value={nouvelAdmin}
+    onvaluechanged={(e: CustomEvent<string>) => metAJourEmail(e.detail)}
+    type="email"
+    label="Ajouter un administrateur"
+    hint="Vous pouvez ajouter un administrateur via son adresse e-mail."
+    status={emailValide ? 'info' : 'error'}
+    infoMessage="L’utilisateur doit déjà disposer d’un compte sur MonServiceSécurisé ; dans le cas contraire, il ne pourra pas être ajouté en tant qu’administrateur."
+  ></dsfr-input>
+  <!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
+  <dsfr-button
+    title="Ajouter cet admin à la liste à inviter"
+    label="Ajouter cet admin"
+    size="md"
+    has-icon
+    icon="add-line"
+    icon-place="only"
+    disabled={nouvelAdmin.length === 0}
+    onclick={async () => await valide()}
+  ></dsfr-button>
+</div>
+
+<style lang="scss">
+  .conteneur-saisie {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+  }
+</style>
