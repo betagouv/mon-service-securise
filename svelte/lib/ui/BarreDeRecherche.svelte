15c2626b05387f2989fd0e7678f254203ac8a142
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-06-18 11:36:12 +0200

    Remplace la barre de recherche par un composant DSFR

--- svelte/lib/ui/BarreDeRecherche.svelte
+++ svelte/lib/ui/BarreDeRecherche.svelte
@@ -1,153 +1,24 @@
 <script lang="ts">
   interface Props {
     recherche: string;
-    taille?: 'moyen' | 'grand';
-    desactive?: boolean;
   }
 
-  let {
-    recherche = $bindable(),
-    taille = 'moyen',
-    desactive = false,
-  }: Props = $props();
+  // eslint-disable-next-line no-useless-assignment
+  let { recherche = $bindable() }: Props = $props();
 
-  const supprimeRecherche = () => {
-    recherche = '';
-  };
+  const metsAJourRecherche = (e: CustomEvent<string>) => (recherche = e.detail);
 </script>
 
-<div role="search" class={taille} class:desactive>
-  <input
-    type="search"
-    placeholder="Rechercher"
-    bind:value={recherche}
-    disabled={desactive}
-    aria-label="Rechercher"
-  />
-  {#if recherche && !desactive}
-    <button class="suppression-recherche" onclick={supprimeRecherche}>
-      <img
-        src="/statique/assets/images/ui-kit/suppression_champ.svg"
-        alt="Suppression de la recherche"
-      />
-    </button>
-  {/if}
-  <button class="conteneur-icone" disabled={desactive}>
-    <img
-      src="/statique/assets/images/icone_loupe.svg"
-      alt="Icône de recherche"
-    />
-    {#if taille === 'grand'}
-      <span>Rechercher</span>
-    {/if}
-  </button>
-</div>
+<dsfr-search
+  placeholder="Rechercher"
+  onvaluechanged={metsAJourRecherche}
+  input-id="recherche"
+  input-label="Rechercher"
+  button-label="Rechercher"
+></dsfr-search>
 
 <style>
-  div[role='search'] {
-    display: flex;
-    flex-direction: row;
-    gap: 0;
-    position: relative;
-  }
-
-  div[role='search'].moyen {
-    width: 328px;
-  }
-
-  div[role='search'].grand {
-    width: 792px;
-  }
-
-  div[role='search']:hover .conteneur-icone {
-    background: var(--btn-primaire-survol);
-  }
-
-  input[type='search'] {
-    padding: 8px 16px;
-    font-family: 'Marianne';
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 24px;
-    border: none;
-    border-radius: 4px 0 0 0;
-    border-top: 1px solid var(--systeme-design-etat-contour-champs);
-    border-left: 1px solid var(--systeme-design-etat-contour-champs);
-    border-bottom: 2px solid var(--bleu-mise-en-avant);
-    width: 100%;
-  }
-
-  input[type='search']::-webkit-search-cancel-button {
-    -webkit-appearance: none;
-    appearance: none;
-  }
-
-  input[type='search']::placeholder {
-    font-style: italic;
-    color: #666666;
-  }
-
-  input[type='search']:focus-visible {
-    outline: 2px solid var(--bleu-mise-en-avant);
-    outline-offset: 2px;
-  }
-
-  input[type='search']:disabled {
-    cursor: not-allowed;
-    border: 1px solid var(--fond-gris-fonce);
-    border-right: none;
-  }
-
-  input[type='search']:disabled::placeholder {
-    color: var(--gris-inactif);
-  }
-
-  .conteneur-icone {
-    display: flex;
-    padding: 8px;
-    justify-content: center;
-    align-items: center;
-    border-radius: 0 4px 0 0;
-    background: var(--bleu-mise-en-avant);
-    border: none;
-    cursor: pointer;
-  }
-
-  div[role='search'] .conteneur-icone:active {
-    background: var(--btn-primaire-clique);
-  }
-
-  div[role='search'] .conteneur-icone:disabled {
-    cursor: not-allowed;
-    background: var(--fond-gris-fonce);
-  }
-
-  div[role='search'].grand .conteneur-icone {
-    padding: 10px 24px 10px 18px;
-    justify-content: center;
-    align-items: center;
-    gap: 8px;
-    color: white;
-    font-size: 18px;
-    font-weight: 500;
-    line-height: 28px;
-  }
-
-  .suppression-recherche {
-    border: none;
-    background: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    position: absolute;
-    top: 13px;
-    right: 56px;
-    padding: 0;
-  }
-
-  div[role='search'].grand .suppression-recherche {
-    right: 190px;
-    top: 17px;
+  dsfr-search {
+    min-width: 328px;
   }
 </style>
