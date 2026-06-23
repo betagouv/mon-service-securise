a2f2becdd9feb0451396018cbd9098720bea8f6d
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-06-23 10:50:59 +0200

    Permet au modèle RisqueV2 de surcharger sa gravité
    ... qui est actuellement calculée par le moteur de risque. On introduit un `getter` pour la première fois, car le contexe s'y prete fortement

--- src/moteurRisques/v2/risquesV2.types.ts
+++ src/moteurRisques/v2/risquesV2.types.ts
@@ -1,6 +1,7 @@
 import { IdVecteurRisque } from './selectionVecteurs.types.js';
 import { IdObjectifVise } from './selectionObjectifsVises.types.js';
 import { idsRisquesV2 } from '../../../donneesReferentielRisquesV2.js';
+import { Gravite } from './graviteObjectifsVises.js';
 
 export type ConfigurationRisqueV2 = Record<
   IdVecteurRisque,
@@ -21,6 +22,7 @@ export type CategorieRisque =
 export type DonneesRisqueV2 = {
   desactive?: boolean;
   commentaire?: string;
+  graviteSurchargee?: Gravite;
 };
 
 export type DonneesRisquesV2 = Partial<Record<IdRisqueV2, DonneesRisqueV2>>;
