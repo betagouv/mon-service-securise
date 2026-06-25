f0a5f012d78fea1020534c7d03e0850e4d2bdf86
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-06-25 10:02:18 +0200

    [SOIN] Convertis un évènement du bus en typescript

--- src/bus/evenementRisqueServiceModifie.ts
+++ src/bus/evenementRisqueServiceModifie.ts
@@ -1,5 +1,9 @@
+import Service from '../modeles/service.js';
+
 class EvenementRisqueServiceModifie {
-  constructor({ service }) {
+  readonly service: Service;
+
+  constructor({ service }: { service: Service }) {
     if (!service) {
       throw new Error("Impossible d'instancier l'événement sans service");
     }
