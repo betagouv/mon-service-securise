cd12cedf0ebcd292642ff112db134f108872252f
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-06-29 22:09:45 +0200

    Corrige l'appelant qui rattache les services au moment de la création

--- src/bus/abonnements/rattacheServiceEtAdmins.ts
+++ src/bus/abonnements/rattacheServiceEtAdmins.ts
@@ -1,13 +1,21 @@
 import Service from '../../modeles/service.js';
 import { ServiceAdministrationOrganisations } from '../../supervision/serviceAdministrationOrganisations.js';
+import Utilisateur from '../../modeles/utilisateur.js';
 
 function rattacheServiceEtAdmins({
   serviceAdministrationOrganisations,
 }: {
   serviceAdministrationOrganisations: ServiceAdministrationOrganisations;
 }) {
-  return async ({ service }: { service: Service }) => {
+  return async ({
+    service,
+    utilisateur,
+  }: {
+    service: Service;
+    utilisateur: Utilisateur;
+  }) => {
     await serviceAdministrationOrganisations.rattacheLesAdministrateursDe(
+      utilisateur.id,
       service
     );
   };
--- test/bus/abonnements/rattacheServiceEtAdmins.spec.ts
+++ test/bus/abonnements/rattacheServiceEtAdmins.spec.ts
@@ -2,22 +2,32 @@ import { unService } from '../../constructeurs/constructeurService.js';
 import Service from '../../../src/modeles/service.js';
 import { ServiceAdministrationOrganisations } from '../../../src/supervision/serviceAdministrationOrganisations.ts';
 import { rattacheServiceEtAdmins } from '../../../src/bus/abonnements/rattacheServiceEtAdmins.ts';
+import { UUID } from '../../../src/typesBasiques.ts';
+import { unUtilisateur } from '../../constructeurs/constructeurUtilisateur.js';
 
 describe("L'abonné en charge de rattacher un nouveau service à ses administrateurs", () => {
   it("délègue le rattachement au service d'administration des organisations", async () => {
     let serviceRecu;
+    let idRecu;
     const serviceAdministrationOrganisations = {
-      rattacheLesAdministrateursDe: async (service: Service) => {
+      rattacheLesAdministrateursDe: async (
+        idActeur: UUID,
+        service: Service
+      ) => {
         serviceRecu = service;
+        idRecu = idActeur;
       },
     } as ServiceAdministrationOrganisations;
 
     const service = unService().avecId('S1').construis();
+    const utilisateur = unUtilisateur().avecId('U1').construis();
 
     await rattacheServiceEtAdmins({ serviceAdministrationOrganisations })({
       service,
+      utilisateur,
     });
 
     expect(serviceRecu!.id).toEqual('S1');
+    expect(idRecu).toEqual('U1');
   });
 });
