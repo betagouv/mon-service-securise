9727227ff614395c1d06d880a1035c45278d3eb0
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-07-01 11:26:04 +0200

    Ajoute la font Marianne et un semblant de pied de page

--- poc-pdf/feedback.ts
+++ poc-pdf/feedback.ts
@@ -1,9 +1,14 @@
 import { AdaptateurPdfTypst } from '../src/adaptateurs/adaptateurPdf.typst.js';
+import { unServiceV2 } from '../test/constructeurs/constructeurService.js';
 import fs from 'node:fs';
 
 const adaptateur = new AdaptateurPdfTypst();
 
-const buffer = await adaptateur.genereSyntheseSecurite();
+const s = unServiceV2().avecNomService('Mairie de Bordeaux').construis();
+
+const buffer = await adaptateur.genereSyntheseSecurite({
+  nomService: s.nomService(),
+});
 
 fs.writeFileSync('pocTypst.pdf', buffer, 'utf8');
 console.log('done');
