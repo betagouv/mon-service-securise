f6074f7ef7be8678f526a99d653d270cd9b3957d
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-07-01 18:10:24 +0200

    Implémente la première page du PDF "Annexes" en Typst
    On force `genereAnnexes` à respecter la même interface que l'implémentation puppeteer actuelle… donc ça fait un `DonneesPdfAnnexes` assez moche… mais ça a le mérite de l'expliciter.

--- src/adaptateurs/adaptateurPdf.interface.ts
+++ src/adaptateurs/adaptateurPdf.interface.ts
@@ -1,7 +1,31 @@
 import Service from '../modeles/service.js';
+import { VersionService } from '../modeles/versionService.js';
+import { DonneesPourAnnexeDescriptionDeServiceV2 } from '../modeles/objetsPDF/objetPDFAnnexeDescriptionV2.js';
+
+export type DonneesPourAnnexeDescriptionDeServiceV1 = {
+  nomService: string;
+  versionService: VersionService;
+  fonctionnalites: string[];
+  donneesStockees: string[];
+  dureeDysfonctionnementMaximumAcceptable: string;
+};
+
+export type DonneesPdfAnnexes = {
+  donneesDescription:
+    | {
+        versionService: VersionService.v1 &
+          DonneesPourAnnexeDescriptionDeServiceV1;
+      }
+    | {
+        versionService: VersionService.v2 &
+          DonneesPourAnnexeDescriptionDeServiceV2;
+      };
+};
 
 export interface AdaptateurPdf {
   genereSyntheseSecurite: (donnees: {
     service: Service;
   }) => Promise<Buffer<ArrayBuffer>>;
+
+  genereAnnexes: (donnees: DonneesPdfAnnexes) => Promise<Buffer<ArrayBuffer>>;
 }
