183c7c088cc5593928db33dcf4836575ed740cb7
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-07-02 15:06:59 +0200

    Affiche l'identifiant numérique des risques spécifiques dans leurs badges

---
+++ svelte/lib/risquesV2/kit/CartoucheIdentifiantRisqueSpecifiqueV2.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { RisqueSpecifiqueV2 } from '../risquesV2';
+
+  interface Props {
+    risque?: RisqueSpecifiqueV2;
+  }
+
+  let { risque }: Props = $props();
+</script>
+
+<dsfr-badge
+  label="Risque spécifique {risque ? `(${risque.identifiantNumerique})` : ''}"
+  type="statut"
+></dsfr-badge>
+
+<style lang="scss">
+  dsfr-badge {
+    white-space: nowrap;
+  }
+</style>
