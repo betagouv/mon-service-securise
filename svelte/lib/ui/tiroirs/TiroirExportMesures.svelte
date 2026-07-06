542cebd036660b59bbcf24ea1a50cee4890fe6ea
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-07-06 18:08:31 +0200

    Ajoute le tiroir d'export de mesures en Svelte

---
+++ svelte/lib/ui/tiroirs/TiroirExportMesures.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import ContenuTiroir from './ContenuTiroir.svelte';
+  import DocumentTelechargeable from './DocumentTelechargeable.svelte';
+
+  interface Props {
+    idService: string;
+  }
+
+  let { idService }: Props = $props();
+
+  export const titre = 'Exporter la liste des mesures de sécurité';
+  export const sousTitre = 'Obtenir la liste complète des mesures de sécurité.';
+</script>
+
+<ContenuTiroir>
+  <DocumentTelechargeable
+    type="CSV"
+    nom="Liste sans données additionnelles renseignées"
+    description="Cette liste ne contient ni statut ni commentaire ajoutés aux mesures."
+    href="/service/{idService}/mesures/export.csv?timestamp={Date.now()}"
+  />
+  <DocumentTelechargeable
+    type="CSV"
+    nom="Liste avec données additionnelles renseignées"
+    description="Cette liste contient les statuts et les commentaires ajoutés aux mesures."
+    href="/service/{idService}/mesures/export.csv?avecDonneesAdditionnelles=true&timestamp={Date.now()}"
+  />
+</ContenuTiroir>
