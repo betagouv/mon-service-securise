32d4b3fbffa80df123f85d47f25bd7b246adadd4
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-07-09 17:02:11 +0200

    [SOIN] Convertis le modèle "ElementsFabricables" en Typescript

--- src/modeles/elementsFabricables.js
+++
@@ -1,9 +0,0 @@
-import ListeItems from './listeItems.js';
-
-class ElementsFabricables extends ListeItems {
-  constructor(Fabrique, donnees) {
-    super(Fabrique.cree, donnees);
-  }
-}
-
-export default ElementsFabricables;
