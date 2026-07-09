5424b53ef01c147cbd58c25f37395637d38b4fe0
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-07-09 16:38:34 +0200

    [SOIN] Convertis le modèle "ActeursHomologation" en Typescript

---
+++ src/modeles/acteursHomologation.ts
@@ -0,0 +1,29 @@
+import ElementsConstructibles from './elementsConstructibles.js';
+import ActeurHomologation, {
+  DonneesActeurHomologation,
+} from './acteurHomologation.js';
+import { TousReferentiels } from '../referentiel.interface.js';
+import { creeReferentielVide } from '../referentiel.js';
+
+type DonneesActeursHomologation = {
+  acteursHomologation: DonneesActeurHomologation[];
+};
+
+class ActeursHomologation extends ElementsConstructibles<ActeurHomologation> {
+  constructor(
+    donnees: DonneesActeursHomologation,
+    referentiel: TousReferentiels = creeReferentielVide()
+  ) {
+    super(
+      ActeurHomologation,
+      { items: donnees.acteursHomologation },
+      referentiel
+    );
+  }
+
+  static proprietesItem() {
+    return ActeurHomologation.proprietes();
+  }
+}
+
+export default ActeursHomologation;
