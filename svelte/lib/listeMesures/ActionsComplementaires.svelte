264b5df24440625e2910ab080257dfb723adc086
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-07-16 11:23:03 +0200

    Ajoute le toggle qui décide d'afficher ou pas les référentiels externes
    Dans ce commit le toggle ne fait rien : on fait seulement l'affichage.

--- svelte/lib/listeMesures/ActionsComplementaires.svelte
+++ svelte/lib/listeMesures/ActionsComplementaires.svelte
@@ -25,42 +25,68 @@
   );
 </script>
 
-<div class="conteneur-actions-complementaires">
-  <Lien
-    type="bouton-tertiaire"
-    href="/mesures/export.csv?version={$storeVersionsDeService.versionSelectionnee}"
-    titre="Télécharger la liste de mesures"
-    target="_blank"
-    icone="telecharger"
-  />
-  <div class="action-ajout-modeles-mesure-specifique">
-    <BoutonAvecListeDeroulante
-      titre="Ajouter une / des mesures"
-      aligneADroite
-      options={[
-        {
-          label: 'Ajouter une mesure',
-          icone: 'add-line',
-          action: onajouterunemesureclick,
-        },
-        {
-          label: 'Téléverser des mesures',
-          icone: 'upload-2-line',
-          action: onteleverserdesmesuresclick,
-        },
-      ]}
-      disabled={!$peutAjouterModelesMesureSpecifique}
+<div class="conteneur-actions">
+  <dsfr-toggle
+    label="Afficher les référentiels d'exigences associés"
+    id="affiche-referentiels-externes"
+  ></dsfr-toggle>
+  <div class="ligne-2">
+    <Lien
+      type="bouton-tertiaire"
+      href="/mesures/export.csv?version={$storeVersionsDeService.versionSelectionnee}"
+      titre="Télécharger la liste de mesures"
+      target="_blank"
+      icone="telecharger"
     />
-    {#if !$peutAjouterModelesMesureSpecifique}
-      <Infobulle
-        contenu="Vous avez atteint la limite maximale de ${capaciteAjoutDeMesure.nombreMaximum} mesures. Pour ajouter des mesures, veuillez d'abord en supprimer."
+    <div class="action-ajout-modeles-mesure-specifique">
+      <BoutonAvecListeDeroulante
+        titre="Ajouter une / des mesures"
+        aligneADroite
+        options={[
+          {
+            label: 'Ajouter une mesure',
+            icone: 'add-line',
+            action: onajouterunemesureclick,
+          },
+          {
+            label: 'Téléverser des mesures',
+            icone: 'upload-2-line',
+            action: onteleverserdesmesuresclick,
+          },
+        ]}
+        disabled={!$peutAjouterModelesMesureSpecifique}
       />
-    {/if}
+      {#if !$peutAjouterModelesMesureSpecifique}
+        <Infobulle
+          contenu="Vous avez atteint la limite maximale de ${capaciteAjoutDeMesure.nombreMaximum} mesures. Pour ajouter des mesures, veuillez d'abord en supprimer."
+        />
+      {/if}
+    </div>
   </div>
 </div>
 
 <style lang="scss">
-  .conteneur-actions-complementaires {
+  /* <Tableau> est utilisé à au moins 8 endroits… pour ne pas casser l'existant,
+   on vient modifier en flex-start seulement ici, via un global.
+   */
+  :global(.conteneur-tableau .filtres) {
+    align-items: flex-start;
+  }
+
+  .conteneur-actions {
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    padding-top: 8px;
+
+    dsfr-toggle {
+      width: fit-content;
+      white-space: nowrap;
+      margin-left: auto;
+    }
+  }
+
+  .ligne-2 {
     margin-left: auto;
     display: flex;
     gap: 12px;
