072e9dc89dcdffd543ec3dabdd043552fc489d46
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-07-28 17:32:51 +0200

    Ajoute le graphiques sur les types de service

--- svelte/lib/adminStatistiques/adminStatistiques.types.ts
+++ svelte/lib/adminStatistiques/adminStatistiques.types.ts
@@ -1,5 +1,6 @@
-import type { IdNiveauDeSecurite } from '../ui/types.d';
+import type { IdNiveauDeSecurite, IdTypeService } from '../ui/types.d';
 
 export type Statistiques = {
   servicesParNiveauSecurite: Partial<Record<IdNiveauDeSecurite, number>>;
+  servicesParType: Partial<Record<IdTypeService, number>>;
 };
