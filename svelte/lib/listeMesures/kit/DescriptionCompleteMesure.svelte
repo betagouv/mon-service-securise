7a790d6bf1d82ead46524d40110f78dc163309b6
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-07-28 09:56:26 +0200

    Déplace le contenu du tiroir de modèle de mesure générale dans des onglets dédiés

--- svelte/lib/listeMesures/kit/DescriptionCompleteMesure.svelte
+++ svelte/lib/listeMesures/kit/DescriptionCompleteMesure.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-  import CartoucheIdentifiantMesure from '../../ui/CartoucheIdentifiantMesure.svelte';
-  import CartoucheCategorieMesure from '../../ui/CartoucheCategorieMesure.svelte';
-  import CartoucheReferentiel from '../../ui/CartoucheReferentiel.svelte';
   import DescriptionLongueMesure from '../../ui/DescriptionLongueMesure.svelte';
   import type { ModeleDeMesure } from '../listeMesures.d';
-  import CartoucheThematique from '../../ui/CartoucheThematique.svelte';
 
   interface Props {
     modeleDeMesure: Omit<ModeleDeMesure, 'type' | 'idsServicesAssocies'>;
@@ -13,36 +9,4 @@
   let { modeleDeMesure }: Props = $props();
 </script>
 
-<p class="description">{modeleDeMesure.description}</p>
-<div class="cartouches">
-  <CartoucheReferentiel referentiel={modeleDeMesure.referentiel} />
-  <CartoucheCategorieMesure categorie={modeleDeMesure.categorie} />
-  {#if modeleDeMesure.thematique}
-    <CartoucheThematique thematique={modeleDeMesure.thematique} />
-  {/if}
-  <CartoucheIdentifiantMesure
-    identifiant={modeleDeMesure.identifiantNumerique}
-  />
-</div>
-<DescriptionLongueMesure
-  description={modeleDeMesure.descriptionLongue}
-  repliee={true}
-/>
-
-<style lang="scss">
-  .description {
-    font-size: 1rem;
-    line-height: 1.5rem;
-    color: #3a3a3a;
-    font-weight: bold;
-    margin-bottom: 8px;
-    margin-top: 0;
-  }
-
-  .cartouches {
-    display: flex;
-    gap: 8px;
-    margin-bottom: 30px;
-    align-items: baseline;
-  }
-</style>
+<DescriptionLongueMesure description={modeleDeMesure.descriptionLongue} />
