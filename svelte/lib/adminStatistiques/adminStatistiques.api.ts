b34499d84611b4f5e68fee8060448fb7611a88bd
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-07-29 15:31:51 +0200

    Ajoute les filtres sur le front

--- svelte/lib/adminStatistiques/adminStatistiques.api.ts
+++ svelte/lib/adminStatistiques/adminStatistiques.api.ts
@@ -1,6 +1,21 @@
+import type { IdNiveauDeSecurite } from '../ui/types';
 import type { Statistiques } from './adminStatistiques.types';
 
 export const api = {
-  statistiques: async () =>
-    (await axios.get<Statistiques>('/api/admin/statistiques')).data,
+  statistiques: async (filtres: {
+    filtreNiveauxSecurite: IdNiveauDeSecurite[];
+    filtreEntites: string[];
+  }) => {
+    const params = new URLSearchParams();
+    filtres.filtreNiveauxSecurite.forEach((niveau) =>
+      params.append('filtreNiveauxSecurite', niveau)
+    );
+    filtres.filtreEntites.forEach((entite) =>
+      params.append('filtreEntites', entite)
+    );
+
+    return (
+      await axios.get<Statistiques>('/api/admin/statistiques', { params })
+    ).data;
+  },
 };
