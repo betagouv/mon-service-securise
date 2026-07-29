c59af793ecdaec48c4334eb14c49f09968642579
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-07-29 10:53:35 +0200

    [SOIN] Convertis l'adaptateur Journal Postgres en classe
    ... afin de pouvoir le tester plus simplement par la suite

--- src/adaptateurs/fabriqueAdaptateurJournalMSS.js
+++ src/adaptateurs/fabriqueAdaptateurJournalMSS.js
@@ -1,13 +1,11 @@
 import * as adaptateurJournalMSSMemoire from './adaptateurJournalMSSMemoire.js';
-import * as adaptateurJournalMSSPostgres from './adaptateurJournalMSSPostgres.js';
+import { AdaptateurJournalMSSPostgres } from './adaptateurJournalMSSPostgres.js';
 
 const fabriqueAdaptateurJournal = () => {
-  const adaptateur =
-    process.env.AVEC_JOURNAL_EN_MEMOIRE === 'true'
-      ? adaptateurJournalMSSMemoire
-      : adaptateurJournalMSSPostgres;
+  if (process.env.AVEC_JOURNAL_EN_MEMOIRE === 'true')
+    return adaptateurJournalMSSMemoire.nouvelAdaptateur();
 
-  return adaptateur.nouvelAdaptateur();
+  return new AdaptateurJournalMSSPostgres();
 };
 
 export default fabriqueAdaptateurJournal;
