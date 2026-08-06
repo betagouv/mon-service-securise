1a81252c0d76be1b528cb0b667202b53b0b1b086
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-08-06 15:38:10 +0200

    Affiche une modale avec un texte explicatif
    ...pour l'instant cette modale n'est pas positionnée

--- svelte/lib/visiteGuideeSPA/VisiteGuideeSPA.svelte
+++ svelte/lib/visiteGuideeSPA/VisiteGuideeSPA.svelte
@@ -1,74 +1,87 @@
 <script lang="ts">
   import PagesService from '../pagesService/PagesService.svelte';
   import type {
-    VisiteGuideeSPAProps,
+    DonneesEtapeVisiteGuidee,
+    EtapeVisiteGuidee,
     PageFondVisiteGuidee,
+    VisiteGuideeSPAProps,
   } from './visiteGuideeSPA.d';
+  import { donneesEtapesVisiteGuidee } from './visiteGuideeSPA.d';
   import AssistantServiceV2 from '../creationV2/AssistantServiceV2.svelte';
   import TableauDeBord from '../tableauDeBord/TableauDeBord.svelte';
   import CreationV2 from '../creationV2/CreationV2.svelte';
+  import ModaleExplicative from './ModaleExplicative.svelte';
 
   let { referentiel, featureFlags, nonce }: VisiteGuideeSPAProps = $props();
-
-  let pageFondVisiteGuidee: PageFondVisiteGuidee | undefined = $state();
+  let etapeVisiteGuidee: EtapeVisiteGuidee = $state(1);
+  let donneesEtape: DonneesEtapeVisiteGuidee = $derived(
+    donneesEtapesVisiteGuidee[etapeVisiteGuidee]
+  );
+  let pageFondVisiteGuidee: PageFondVisiteGuidee = $derived(
+    donneesEtape.pageFond
+  );
 </script>
 
-{#if pageFondVisiteGuidee === 'creationV2'}
-  <AssistantServiceV2
-    titreAssistant="Ajouter un service"
-    titreBoutonFinalise="Commencer à sécuriser le service"
-    enCoursDeChargement={false}
-  />
-{:else if pageFondVisiteGuidee === 'besoinsSecuriteV2'}
-  <CreationV2
-    modeVisiteGuidee={true}
-    entite={{ siret: '1234', nom: '', departement: '75' }}
-  />
-{:else if pageFondVisiteGuidee === 'tableauDeBord'}
-  <div class="avec-padding">
-    <TableauDeBord
-      estSuperviseur={false}
-      estAdmin={false}
-      avecGestionOrganisations={false}
-      modeVisiteGuidee={true}
-      profilUtilisateurComplet={true}
+<ModaleExplicative bind:etape={etapeVisiteGuidee} />
+
+<div>
+  {#if pageFondVisiteGuidee === 'creationV2'}
+    <AssistantServiceV2
+      titreAssistant="Ajouter un service"
+      titreBoutonFinalise="Commencer à sécuriser le service"
+      enCoursDeChargement={false}
     />
-  </div>
-{:else if pageFondVisiteGuidee === 'mesures' || pageFondVisiteGuidee === 'dossiers'}
-  <PagesService
-    etapeActive={pageFondVisiteGuidee}
-    idService="ID-SERVICE-VISITE-GUIDEE"
-    {referentiel}
-    visible={{
-      rolesResponsabilites: true,
-      risques: true,
-      descriptionService: true,
-      mesures: true,
-      indiceCyber: true,
-      dossiers: true,
-      homologation: true,
-    }}
-    estLectureSeule={{
-      rolesResponsabilites: false,
-      risques: false,
-      descriptionService: false,
-      mesures: false,
-      indiceCyber: false,
-      dossiers: false,
-      homologation: false,
-    }}
-    modeVisiteGuidee={true}
-    {featureFlags}
-    preferencesUtilisateur={{
-      afficheExplicationRisquesV2: false,
-    }}
-    suggestionsService={{
-      finalisationDescriptionServiceImporte: false,
-    }}
-    peutHomologuer={true}
-    {nonce}
-  />
-{/if}
+  {:else if pageFondVisiteGuidee === 'besoinsSecuriteV2'}
+    <CreationV2
+      modeVisiteGuidee={true}
+      entite={{ siret: '1234', nom: '', departement: '75' }}
+    />
+  {:else if pageFondVisiteGuidee === 'tableauDeBord'}
+    <div class="avec-padding">
+      <TableauDeBord
+        estSuperviseur={false}
+        estAdmin={false}
+        avecGestionOrganisations={false}
+        modeVisiteGuidee={true}
+        profilUtilisateurComplet={true}
+      />
+    </div>
+  {:else if pageFondVisiteGuidee === 'mesures' || pageFondVisiteGuidee === 'dossiers' || pageFondVisiteGuidee === 'risques'}
+    <PagesService
+      etapeActive={pageFondVisiteGuidee}
+      idService="ID-SERVICE-VISITE-GUIDEE"
+      {referentiel}
+      visible={{
+        rolesResponsabilites: true,
+        risques: true,
+        descriptionService: true,
+        mesures: true,
+        indiceCyber: true,
+        dossiers: true,
+        homologation: true,
+      }}
+      estLectureSeule={{
+        rolesResponsabilites: false,
+        risques: false,
+        descriptionService: false,
+        mesures: false,
+        indiceCyber: false,
+        dossiers: false,
+        homologation: false,
+      }}
+      modeVisiteGuidee={true}
+      {featureFlags}
+      preferencesUtilisateur={{
+        afficheExplicationRisquesV2: false,
+      }}
+      suggestionsService={{
+        finalisationDescriptionServiceImporte: false,
+      }}
+      peutHomologuer={true}
+      {nonce}
+    />
+  {/if}
+</div>
 
 <style lang="scss">
   :global(#visite-guidee-spa) {
@@ -77,5 +90,11 @@
 
   .avec-padding {
     padding: 32px 20px;
+  }
+
+  .conteneur-actions {
+    display: flex;
+    gap: 16px;
+    justify-content: end;
   }
 </style>
