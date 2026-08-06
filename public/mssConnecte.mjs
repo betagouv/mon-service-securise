cdc337fb671474d91cc306ebce4081e239575380
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-08-06 17:54:34 +0200

    Affiche la visite guidée de façon forcée sur la page /visiteGuidée
    ...en attendant de gérer l'ouveture/fermeture de la visite

--- public/mssConnecte.mjs
+++ public/mssConnecte.mjs
@@ -1,20 +1,10 @@
 import initialiseComportementModale from './modules/interactions/modale.mjs';
-import lisDonneesPartagees from './modules/donneesPartagees.mjs';
 import { lanceDecompteDeconnexion } from './modules/deconnexion.js';
 
 $(() => {
   initialiseComportementModale($('.rideau#deconnexion'));
 
   document.body.dispatchEvent(new CustomEvent('svelte-recharge-tiroir'));
-
-  const etatVisiteGuidee = lisDonneesPartagees('etat-visite-guidee');
-  if (etatVisiteGuidee.dejaTerminee === false) {
-    document.body.dispatchEvent(
-      new CustomEvent('svelte-recharge-visite-guidee', {
-        detail: etatVisiteGuidee,
-      })
-    );
-  }
 
   $('lab-anssi-centre-aide').on('lienclique', (e) => {
     if (e.detail.target.id === 'centre-aide-visite-guidee') {
