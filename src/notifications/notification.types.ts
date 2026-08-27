ae7e5f3dbdf8c93c4dd6935ce9d39433b6b0e3da
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-08-27 14:47:50 +0200

    [SOIN] Extrais "SourceNouveautes"
    Notre but est de rendre le centre de notifications "Open for extension, closed for modifications" : on veut rajouter la lecture des notifications transactionnelles sans avoir à éditer le code du centre. On veut simplement brancher "un nouvel élément". Donc, en faisant émerger les Sources, on se crée de la place pour faire apparaître la source des notifications transactionelles.

---
+++ src/notifications/notification.types.ts
@@ -0,0 +1,26 @@
+import { UUID } from '../typesBasiques.js';
+
+export enum StatutLecture {
+  lue = 'lue',
+  nonLue = 'nonLue',
+}
+
+export type TypeNotification = 'nouveaute' | 'tache';
+
+export type CanalDiffusion = 'centreNotifications' | 'page';
+
+export type NotificationDuCentre = {
+  id: string;
+  type: TypeNotification;
+  titre: string;
+  titreCta: string;
+  lien: string;
+  canalDiffusion: CanalDiffusion;
+  statutLecture: StatutLecture;
+  doitNotifierLecture: boolean;
+  horodatage: Date;
+};
+
+export interface SourceNotifications {
+  notificationsPour(idUtilisateur: UUID): Promise<NotificationDuCentre[]>;
+}
