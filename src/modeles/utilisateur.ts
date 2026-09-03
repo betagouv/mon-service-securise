3abaf415751e33917d2c55e4c506485689dd1392
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-09-03 11:35:18 +0200

    Ajoute le nouveau type de notification au rapport hebdomadaire

--- src/modeles/utilisateur.ts
+++ src/modeles/utilisateur.ts
@@ -9,18 +9,18 @@ import { UUID } from '../typesBasiques.js';
 import { AdaptateurJWT } from '../adaptateurs/adaptateurJWT.interface.js';
 import { SourceAuthentification } from './sourceAuthentification.js';
 import { AdaptateurMail } from '../adaptateurs/adaptateurMail.interface.js';
+import { IdNotificationTransactionnelle } from '../referentiel.types.js';
 
 export type EstimationNombreServices = {
   borneBasse: string;
   borneHaute: string;
 };
 
-type PreferencesRecapitulatif = {
-  mentionDansMesure: boolean;
-};
+type PreferencesRecapitulatif = Record<IdNotificationTransactionnelle, boolean>;
 
 const PREFERENCES_RECAPITULATIF_PAR_DEFAUT: PreferencesRecapitulatif = {
   mentionDansMesure: true,
+  responsableMesure: true,
 };
 
 export type DonneesUtilisateur = {
