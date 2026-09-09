e931de27a5daf26273489d6a0a1b5f268226dd68
Nephtys <stefanie.loiseleur@beta.gouv.fr>
2026-09-09 16:55:37 +0200

    Ajoute l'id de l'utilisateur acteur à l'évènement EvenementDossierHomologationImporte

--- src/bus/evenementDossierHomologationImporte.js
+++ src/bus/evenementDossierHomologationImporte.js
@@ -1,14 +1,18 @@
 class EvenementDossierHomologationImporte {
-  constructor({ idService, dossier }) {
+  constructor({ idService, dossier, idUtilisateur }) {
     if (!idService)
       throw Error("Impossible d'instancier l'événement sans ID de service");
 
     if (!dossier)
       throw Error("Impossible d'instancier l'événement sans dossier");
 
+    if (!idUtilisateur)
+      throw Error("Impossible d'instancier l'événement sans ID d'utilisateur");
+
     this.idService = idService;
     this.dossier = dossier;
     this.importe = true;
+    this.idUtilisateur = idUtilisateur;
   }
 }
 
