f7f76cc2bca4d88f83b53d2d92766237e638c26f
Nephtys <stefanie.loiseleur@beta.gouv.fr>
2026-09-16 11:37:30 +0200

    Mets à jour la façon d'utiliser pMap suite au passage à la v7

--- src/utilitaires/pMap.ts
+++ src/utilitaires/pMap.ts
@@ -1,16 +1,16 @@
-import pmap from 'p-map';
+import pMap, { type Mapper } from 'p-map';
 
 const avecPMap = <TEntree, TSortie>(
   items: Iterable<TEntree>,
-  fonctionAAppliquer: pmap.Mapper<TEntree, TSortie>
-) => pmap(items, fonctionAAppliquer, { concurrency: 2 });
+  fonctionAAppliquer: Mapper<TEntree, TSortie>
+) => pMap(items, fonctionAAppliquer, { concurrency: 2 });
 
 export const avecPMapPourChaqueElement = <TEntree, TSortie>(
   itemsDansPromesse: Promise<TEntree[]>,
-  fonctionAAppliquer: pmap.Mapper<TEntree, TSortie>
+  fonctionAAppliquer: Mapper<TEntree, TSortie>
 ) => itemsDansPromesse.then((items) => avecPMap(items, fonctionAAppliquer));
 
 export const avecPMapPourChaqueElementSansPromesse = <TEntree, TSortie>(
   items: Iterable<TEntree>,
-  fonctionAAppliquer: pmap.Mapper<TEntree, TSortie>
+  fonctionAAppliquer: Mapper<TEntree, TSortie>
 ) => avecPMap(items, fonctionAAppliquer);
